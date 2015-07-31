fb6fbcbd81b86a946b56788651cc741bf39e6d2e
Tim Coulter <tim@Tims-MacBook-Pro.local>
2015-07-31 15:02:14 -0700

    Whoops, almost forgot these.

--- packages/truffle/truffle-exec.bash
+++ packages/truffle/truffle-exec.bash
@@ -2,10 +2,10 @@
 # Farm everything out to grunt, using the package-local version.
 export TRUFFLE_WORKING_DIRECTORY=`pwd`
 
-if [ "$TRUFFLE_NPM_LOCATION" == "" ]; 
-then 
-  export TRUFFLE_NPM_LOCATION=$(npm config --global get prefix)/lib/node_modules/truffle/ 
+if [ "$TRUFFLE_NPM_LOCATION" == "" ];
+then
+  export TRUFFLE_NPM_LOCATION=$(npm config --global get prefix)/lib/node_modules/truffle/
 fi
 
 cd $TRUFFLE_NPM_LOCATION
-$TRUFFLE_NPM_LOCATION/node_modules/.bin/coffee $TRUFFLE_NPM_LOCATION/truffle-exec.coffee -- $@
+$TRUFFLE_NPM_LOCATION/node_modules/.bin/babel-node $TRUFFLE_NPM_LOCATION/truffle-exec.es6 $@
--- packages/truffle/truffle.bash
+++ packages/truffle/truffle.bash
@@ -2,10 +2,10 @@
 # Farm everything out to truffle.coffee, using the package-local version of coffee.
 export TRUFFLE_WORKING_DIRECTORY=`pwd`
 
-if [ "$TRUFFLE_NPM_LOCATION" == "" ]; 
-then 
-  export TRUFFLE_NPM_LOCATION=$(npm config --global get prefix)/lib/node_modules/truffle/ 
+if [ "$TRUFFLE_NPM_LOCATION" == "" ];
+then
+  export TRUFFLE_NPM_LOCATION=$(npm config --global get prefix)/lib/node_modules/truffle/
 fi
 
 cd $TRUFFLE_NPM_LOCATION
-$TRUFFLE_NPM_LOCATION/node_modules/.bin/coffee $TRUFFLE_NPM_LOCATION/truffle.coffee -- $@
+$TRUFFLE_NPM_LOCATION/node_modules/.bin/babel-node $TRUFFLE_NPM_LOCATION/truffle.es6 $@
