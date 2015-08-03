b52874229c56e97c62542d52ee6e17c6b8abcd91
Tim Coulter <tim@Tims-MacBook-Pro.local>
2015-08-03 09:44:40 -0700

    Don't clobber environment variables in truffle.bash and truffle-exec.bash, if they're already set. And add a hack to keep babel from clobbering -e functionality.

--- packages/truffle-core/truffle.bash
+++ packages/truffle-core/truffle.bash
@@ -1,11 +1,17 @@
 #!/bin/bash
-# Farm everything out to truffle.coffee, using the package-local version of coffee.
-export TRUFFLE_WORKING_DIRECTORY=`pwd`
+if [ -z "$TRUFFLE_WORKING_DIRECTORY" ];
+then
+  export TRUFFLE_WORKING_DIRECTORY=`pwd`
+fi
 
-if [ "$TRUFFLE_NPM_LOCATION" == "" ];
+if [ -z "$TRUFFLE_NPM_LOCATION" ];
 then
   export TRUFFLE_NPM_LOCATION=$(npm config --global get prefix)/lib/node_modules/truffle/
 fi
 
+# Hack. babel-node will clobber -e, and it doesn't look like `--` will stop it.
+args=$@
+args=${args//-e/--environment}
+
 cd $TRUFFLE_NPM_LOCATION
-$TRUFFLE_NPM_LOCATION/node_modules/.bin/babel-node $TRUFFLE_NPM_LOCATION/truffle.es6 $@
+$TRUFFLE_NPM_LOCATION/node_modules/.bin/babel-node -- $TRUFFLE_NPM_LOCATION/truffle.es6 ${args}
