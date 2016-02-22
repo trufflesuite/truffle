2a8e47ad02cf3ec1d19901bda05106b8c3daf74c
Tim Coulter <tim@Tims-MacBook-Pro.local>
2016-02-22 15:12:57 -0800

    Add token standards note to MetaCoin.sol. Fixes #73.

--- packages/truffle-core/example/contracts/MetaCoin.sol
+++ packages/truffle-core/example/contracts/MetaCoin.sol
@@ -1,5 +1,10 @@
 import "ConvertLib.sol";
 
+// This is just a simple example of a coin-like contract.
+// It is not standards compatible and cannot be expected to talk to other
+// coin/token contracts. If you want to create a standards-compliant
+// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+
 contract MetaCoin {
 	mapping (address => uint) balances;
 
