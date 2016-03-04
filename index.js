29fe7cee8ac2585086728282ca170b40904c63a9
Tim Coulter <tim@timothyjcoulter.com>
2016-03-04 12:17:13 -0800

    Completely remove dependency of babel-node within Truffle's codebase.

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  init: require("./lib/init"),
+  create: require("./lib/create"),
+  config: require("./lib/config"),
+  contracts: require("./lib/contracts"),
+  build: require("./lib/build"),
+  test: require("./lib/test"),
+  exec: require("./lib/exec"),
+  console: require("./lib/repl"),
+  serve: require("./lib/serve")
+};
