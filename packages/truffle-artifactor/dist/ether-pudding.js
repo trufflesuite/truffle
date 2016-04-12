c8f700662ce987112b20a5a046d65caeb31543cc
Tim Coulter <tim@Tims-MacBook-Pro.local>
2016-04-12 14:18:16 -0700

    2.0.7. Looks like estimateGas never made it to npm.

--- packages/truffle-artifactor/dist/ether-pudding.js
+++ packages/truffle-artifactor/dist/ether-pudding.js
@@ -374,7 +374,7 @@ var Pudding =
 
 	module.exports = {
 		"name": "ether-pudding",
-		"version": "2.0.6",
+		"version": "2.0.7",
 		"description": "Pudding - a (more) delightful Ethereum contract abstraction",
 		"author": "Tim Coulter",
 		"main": "./index.js",
