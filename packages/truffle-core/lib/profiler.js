a3538c56eeaf2199fa409010bf2aa1a52bb522ef
Tim Coulter <tim@Tims-MacBook-Pro.local>
2016-05-26 16:01:08 -0700

    Move networks functionality to profiler.

--- packages/truffle-core/lib/profiler.js
+++ packages/truffle-core/lib/profiler.js
@@ -71,6 +71,44 @@ module.exports = {
     });
   },
 
+  deployed_networks: function(options, callback) {
+    Pudding.requireAll(options.contracts_build_directory, function(err, contracts) {
+      if (err) return callback(err);
+
+      var ids_to_names = {};
+      var networks = {};
+
+      Object.keys(options.networks).forEach(function(network_name) {
+        var network = options.networks[network_name];
+
+        // Ignore the test network that's configured by default.
+        if (network_name == "test" && network.network_id == null) {
+          return;
+        }
+
+        var network_id = network.network_id || "default";
+        ids_to_names[network_id] = network_name;
+        networks[network_name] = {};
+      });
+
+      contracts.forEach(function(contract) {
+        Object.keys(contract.all_networks).forEach(function(network_id) {
+          var network_name = ids_to_names[network_id] || network_id;
+
+          if (networks[network_name] == null) {
+            networks[network_name] = {};
+          }
+
+          if (contract.address == null) return;
+
+          networks[network_name][contract.contract_name] = contract.address;
+        });
+      });
+
+      callback(null, networks);
+    });
+  },
+
   imports: function(file, callback) {
     fs.readFile(file, "utf8", function(err, body) {
       if (err) callback(err);
