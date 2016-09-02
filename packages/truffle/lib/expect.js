4d55d68205b0c8694fd1c10ea4af4af4aa1a82b2
Tim Coulter <tim@Tims-MacBook-Pro.local>
2016-09-02 09:36:25 -0700

    Add function to expect one of a specific set of options.

--- packages/truffle/lib/expect.js
+++ packages/truffle/lib/expect.js
@@ -5,6 +5,32 @@ var Expect = {
         throw new Error("Expected parameter '" + key + "' not passed to function.");
       }
     });
+  },
+
+  one: function(options, expected_keys) {
+    var found = [];
+
+    expected_keys.forEach(function(key) {
+      if (options[key] != null) {
+        found.push(1);
+      } else {
+        found.push(0);
+      }
+    });
+
+    var total = found.reduce(function(t, value) {
+      return t + value;
+    });
+
+    if (total == 1) return;
+
+    var amount = total;
+
+    if (total == 0) {
+      amount = "none"
+    }
+
+    throw new Error("Expected one of the following parameters, but found " + amount + ": " + expected_keys.join(", "));
   }
 }
 
