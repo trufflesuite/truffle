bab9f4b791c42b3052a8da4f1e80cfd7b0779e0d
Tim Coulter <tim@timothyjcoulter.com>
2016-10-12 08:41:12 -0700

    Upgrade solidity parser and solc to support solc 0.4.x.

--- packages/truffle-core/example/contracts/Migrations.sol
+++ packages/truffle-core/example/contracts/Migrations.sol
@@ -1,9 +1,11 @@
+pragma solidity ^0.4.2;
+
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
   modifier restricted() {
-    if (msg.sender == owner) _
+    if (msg.sender == owner) _;
   }
 
   function Migrations() {
