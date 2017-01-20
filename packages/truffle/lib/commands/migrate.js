040761becb7cb93c8f973cd376aa47ad99ced3d2
Tim Coulter <tim@timothyjcoulter.com>
2017-01-20 09:19:40 -0800

    Move network detection to Environment object which also detects from address if not set.

--- packages/truffle/lib/commands/migrate.js
+++ packages/truffle/lib/commands/migrate.js
@@ -1,9 +1,10 @@
+var OS = require("os");
 var Config = require("truffle-config");
 var Contracts = require("../contracts");
 var Resolver = require("truffle-resolver");
 var Artifactor = require("truffle-artifactor");
 var Migrate = require("truffle-migrate");
-var Networks = require("../networks");
+var Environment = require("../environment");
 var Web3 = require("web3");
 
 var command = {
@@ -34,18 +35,18 @@ var command = {
     Contracts.compile(config, function(err) {
       if (err) return done(err);
 
-      Networks.detectNetwork(config, function(err) {
+      Environment.detect(config, function(err) {
         if (err) return done(err);
 
-        var web3 = new Web3(config.provider);
-        web3.eth.getAccounts(function(err, accounts) {
+        Migrate.needsMigrating(config, function(err, needsMigrating) {
           if (err) return done(err);
 
-          if (!config.from) {
-            config.from = accounts[0];
+          if (needsMigrating) {
+            config.logger.log("Using network '" + config.network + "'." + OS.EOL);
+            Migrate.run(config, done);
+          } else {
+            done();
           }
-
-          Migrate.run(config, done);
         });
       });
     });
