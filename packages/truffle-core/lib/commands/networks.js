525f28708f5d5991fff2353ff5ec5f4687b79372
Tim Coulter <tim@timothyjcoulter.com>
2017-01-23 11:19:59 -0800

    Beef up display of networks and unconfigured networks that have been deployed to. Add `truffle networks —clean` to remove any unconfigured networks from contract artifacts.

--- packages/truffle-core/lib/commands/networks.js
+++ packages/truffle-core/lib/commands/networks.js
@@ -4,33 +4,21 @@ var Networks = require("../networks");
 var command = {
   command: 'networks',
   description: 'Show addresses for deployed contracts on each network',
-  builder: {},
+  builder: {
+    clean: {
+      describe: "Remove network artifacts that don't belong to any configuration",
+      type: "boolean",
+      default: false
+    }
+  },
   run: function (options, done) {
     var config = Config.detect(options);
 
-    Networks.deployed(config, function(err, networks) {
-      if (err) return callback(err);
-
-      Object.keys(networks).sort().forEach(function(network_name) {
-        options.logger.log("")
-
-        var output = Object.keys(networks[network_name]).sort().map(function(contract_name) {
-          var address = networks[network_name][contract_name];
-          return contract_name + ": " + address;
-        });
-
-        if (output.length == 0) {
-          output = ["No contracts deployed."];
-        }
-
-        options.logger.log("Network: " + network_name);
-        options.logger.log("  " + output.join("\n  "))
-      });
-
-      options.logger.log("");
-
-      done();
-    });
+    if (options.clean) {
+      Networks.clean(config, done);
+    } else {
+      Networks.display(config, done);
+    }
   }
 }
 
