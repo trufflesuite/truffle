8c19744bee16f61a75f91138f7f84235757926db
Tim Coulter <tim@timothyjcoulter.com>
2017-01-27 15:12:53 -0800

    Add `overwrite` option to `.deploy()`; will detect if there’s a deployed version regardless of whether or not its been deployed (good for package management).

--- packages/truffle-deployer/src/actions/deploy.js
+++ packages/truffle-deployer/src/actions/deploy.js
@@ -1,17 +1,39 @@
 module.exports = function(contract, args, deployer) {
   return function() {
-    var prefix = "Deploying ";
-    if (contract.isDeployed()) {
-      prefix = "Replacing ";
-    }
+    var should_deploy = true;
 
-    deployer.logger.log(prefix + contract.contract_name + "...");
+    // First detect the network to see if it's deployed.
+    return contract.detectNetwork().then(function() {
+      // Evaluate any arguments if they're promises (we need to do this in all cases to maintain consistency)
+      return Promise.all(args)
+    }).then(function(new_args) {
+      // Check the last argument. If it's an object that tells us not to overwrite, then lets not.
+      if (new_args.length > 0) {
+        var last_arg = new_args[new_args.length - 1];
+        if (typeof last_arg == "object" && last_arg.overwrite === false && contract.isDeployed()) {
+          should_deploy = false;
+        }
+        delete last_arg.overwrite;
+      }
 
-    // Evaluate any arguments if they're promises
-    return Promise.all(args).then(function(new_args) {
-      return contract.new.apply(contract, new_args);
+      if (should_deploy == true) {
+        var prefix = "Deploying ";
+        if (contract.isDeployed()) {
+          prefix = "Replacing ";
+        }
+        deployer.logger.log(prefix + contract.contract_name + "...");
+        return contract.new.apply(contract, new_args);
+      } else {
+        return contract.deployed();
+      }
     }).then(function(instance) {
-      deployer.logger.log(contract.contract_name + ": " + instance.address);
+      if (should_deploy == true) {
+        deployer.logger.log(contract.contract_name + ": " + instance.address);
+      } else {
+        deployer.logger.log("Didn't deploy " + contract.contract_name + "; using " + instance.address);
+      }
+
+      // Ensure the address is set on the contract.
       contract.address = instance.address;
     });
   };
