0192404743ebd15165668aed77b9e594b30740d2
Tim Coulter <tim@timothyjcoulter.com>
2017-02-02 15:27:46 -0800

    Start of new init feature

--- packages/truffle-core/lib/commands/init.js
+++ packages/truffle-core/lib/commands/init.js
@@ -1,13 +1,61 @@
 var Config = require("truffle-config");
-var init = require("../init");
+var Init = require("truffle-init");
+var OS = require("os");
 
 var command = {
   command: 'init',
   description: 'Initialize new Ethereum project with example contracts and tests',
   builder: {},
   run: function (options, done) {
-    var config = Config.default();
-    init(config.working_directory, done);
+    var config = Config.default().with({
+      logger: console
+    });
+
+    var example_name = "default";
+
+    if (options._ && options._.length > 0) {
+      example_name = options._[0];
+    }
+
+    Init.fromGithub(example_name, config.working_directory, function(err, project_config) {
+      if (err) return done(err);
+
+      var docs_url = project_config.docs_url || "https://github.com/trufflesuite/truffle-init-" + example_name;
+
+      config.logger.log("Project initialized." +
+        OS.EOL +
+        OS.EOL +
+        "  Documentation: " + docs_url +
+        OS.EOL
+      );
+
+      project_config.commands = project_config.commands || {};
+
+      var longest_command_length = 0;
+      var command_names = Object.keys(project_config.commands);
+
+      command_names.forEach(function(key) {
+        if (key.length > longest_command_length) {
+          longest_command_length = key.length;
+        }
+      });
+
+      if (command_names.length > 0) {
+        config.logger.log("Commands:" + OS.EOL);
+      }
+
+      command_names.forEach(function(key) {
+        var spacing = "";
+
+        while (key.length + spacing.length < longest_command_length) {
+          spacing += " ";
+        }
+
+        config.logger.log("  " + key + ": " + spacing + project_config.commands[key]);
+      });
+
+      config.logger.log("");
+    });
   }
 }
 
