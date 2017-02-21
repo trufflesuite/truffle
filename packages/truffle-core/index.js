d58e398584d35be2ff852d241dbdd0af439f5716
Tim Coulter <tim@timothyjcoulter.com>
2017-02-21 17:48:00 -0500

    Remove references to items that have been pulled out into their own modules.

--- packages/truffle-core/index.js
+++ packages/truffle-core/index.js
@@ -3,16 +3,12 @@ var pkg = require("./package.json");
 module.exports = {
   build: require("./lib/build"),
   create: require("./lib/create"),
-  compiler: require("truffle-compile"),
   config: require("./lib/config"),
   console: require("./lib/repl"),
   contracts: require("./lib/contracts"),
-  require: require("truffle-require"),
   init: require("./lib/init"),
-  migrate: require("truffle-migrate"),
   package: require("./lib/package"),
   serve: require("./lib/serve"),
-  sources: require("truffle-contract-sources"),
   test: require("./lib/test"),
   version: pkg.version
 };
