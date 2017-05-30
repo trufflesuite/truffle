aeb5ea1782d6b4d8d92ec1b606c2161de1152844
Tim Coulter <tim@timothyjcoulter.com>
2017-05-30 10:13:18 -0700

    First bundle! Woot!

--- webpack/cli.webpack.config.js
+++ webpack/cli.webpack.config.js
@@ -3,9 +3,10 @@ var fs = require("fs");
 var OS = require("os");
 var prependFile = require('prepend-file');
 var WebpackOnBuildPlugin = require('on-build-webpack');
+var webpack = require('webpack')
 
-var outputDir = path.join(__dirname, '..', 'build');
-var outputFilename = 'cli.node.js';
+var outputDir = path.join(__dirname, '..');
+var outputFilename = 'cli.bundled.js';
 
 module.exports = {
   entry: path.join(__dirname, "..", "cli.js"),
@@ -19,6 +20,26 @@ module.exports = {
       { test: /\.js$/, use: "shebang-loader" }
     ]
   },
+  externals: [
+    // If you look at webpack's docs, `externals` doesn't need to be a function.
+    // But I never got it to work otherwise, so this is a function because "it works".
+    function(context, request, callback) {
+
+      // truffle-config uses the original-require module.
+      // Here, we leave it as an external, and use the original-require
+      // module that's a dependency of Truffle instead.
+      if (/^original-require$/.test(request)) {
+        return callback(null, 'commonjs original-require');
+      }
+
+      // We want to leave solc as an eternal dependency as well (for now)
+      if (/^solc$/.test(request)) {
+        return callback(null, 'commonjs solc');
+      }
+
+      callback();
+    }
+  ],
   plugins: [
     // Put the shebang back on and make sure it's executable.
     new WebpackOnBuildPlugin(function(stats) {
@@ -31,6 +52,9 @@ module.exports = {
   ],
   resolve: {
     alias: {
+      // fsevents is used by chokidar and is an optional requirement
+      // It doesn't pack well, and is OS X specific, so let's get rid of it.
+      "fsevents": path.join(__dirname, "..", "./nil.js"),
       "ws": path.join(__dirname, "..", "./nil.js"),
       "scrypt": "js-scrypt",
       "secp256k1": path.join(__dirname, "..", "node_modules", "secp256k1", "elliptic.js")
