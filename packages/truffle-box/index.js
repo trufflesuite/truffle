67a420035678d29a3ef309258ab087c090fd8628
g. nicholas d'andrea <nick@gnidan.org>
2017-06-23 13:20:46 -0400

    Add options to unbox() to support logging

--- packages/truffle-box/index.js
+++ packages/truffle-box/index.js
@@ -1,15 +1,24 @@
 var utils = require("./lib/utils");
 
 var Box = {
-  unbox: function(url, destination) {
+  unbox: function(url, destination, options) {
+    options = options || {};
+    options.logger = options.logger || {log: function() {}}
+
     return Promise.resolve()
       .then(function() {
+        options.logger.log("Downloading Box");
         return utils.downloadBox(url, destination)
       })
       .then(function() {
+        options.logger.log("Unpacking Box");
         return utils.unpackBox(destination);
       })
       .then(function(boxConfig) {
+        if (boxConfig.epilogue) {
+          options.logger.log(project_config.epilogue.replace("\n", OS.EOL));
+        }
+
         return boxConfig;
       });
   }
