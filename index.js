eba72f748036c49ef6914b8facfcad906e9be820
Alex Miller <asmiller1989@gmail.com>
2017-08-25 12:12:21 -0700

    Adds ability to input multiple mnemonics (backwards compatible). This allows the user to create an HDWalletProvider with multiple WalletSubProviders. I needed this because I was deploying remotely to an INFURA node (using HDWalletProvider to deploy), but also needed to test a second account in a Truffle test (which could not be unlocked). The first mnemonic passed is the default account, but any number of other accounts can be passed and unlocked in the provider for testing. This is fully compatible with previous versions - a string may also be passed instead of an array.

--- index.js
+++ index.js
@@ -6,20 +6,39 @@ var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
 var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
 var Web3 = require("web3");
 
-function HDWalletProvider(mnemonic, provider_url, address_index) {
-  this.mnemonic = mnemonic;
-  this.hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
+function HDWalletProvider(mnemonics, provider_url, address_index) {
+  var self = this;
 
+  // 'mnemonics' can either be a single string or an array of strings.
+  if (typeof mnemonics == 'string') { this.mnemonics = [ mnemonics ]; }
+  else { this.mnemonics = mnemonics; }
+
+  this.hdwallets = [];
+  // Create a hdwallet instance for each mnemonic.
+  this.mnemonics.forEach(function(m) {
+    // if (this.hdwallets === undefined) { this.hdwallets = []; }
+    self.hdwallets.push(hdkey.fromMasterSeed(bip39.mnemonicToSeed(m)));
+  })
+
+  // Start a ProviderEngine and add a SubProvider for each wallet supplied.
+  this.engine = new ProviderEngine();
+  // Use the address index across all provided wallets.
   if (address_index == null) {
     address_index = 0;
   }
-
   this.wallet_hdpath = "m/44'/60'/0'/0/";
-  this.wallet = this.hdwallet.derivePath(this.wallet_hdpath + address_index).getWallet();
-  this.address = "0x" + this.wallet.getAddress().toString("hex");
 
-  this.engine = new ProviderEngine();
-  this.engine.addProvider(new WalletSubprovider(this.wallet, {}));
+  this.hdwallets.forEach(function(w, i) {
+    // add the subprovider
+    var wallet = w.derivePath(self.wallet_hdpath + address_index).getWallet();
+    self.engine.addProvider(new WalletSubprovider(wallet, {}));
+    // Save the wallet/address if this is the first one
+    if (i == 0) {
+      self.wallet = wallet;
+      self.address =  "0x" + wallet.getAddress().toString("hex");
+    }
+  })
+  // Add the other SubProviders
   this.engine.addProvider(new FiltersSubprovider());
   this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));
   this.engine.start(); // Required by the provider engine.
