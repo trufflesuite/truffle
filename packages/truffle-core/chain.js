3c0c28658691b65383cbca2f09e4b42e1a44bcf2
g. nicholas d'andrea <nick@gnidan.org>
2017-10-03 12:08:21 -0400

    Remove trailing spaces

--- packages/truffle-core/chain.js
+++ packages/truffle-core/chain.js
@@ -2,8 +2,8 @@
 
 var TestRPC = require("ethereumjs-testrpc");
 
-// This script takes one argument: A strinified JSON object meant 
-// to be parsed and then passed to TestRPC.server(). 
+// This script takes one argument: A strinified JSON object meant
+// to be parsed and then passed to TestRPC.server().
 
 if (process.argv.length <= 2) {
   throw new Error("Fatal: No arguments passed to default environment; please contact the Truffle developers for help.");
@@ -24,7 +24,7 @@ server.listen(options.port, options.hostname, function(err, state) {
     console.error(err);
     process.exit(1);
   }
-  
+
   console.log("Truffle Develop started.");
   console.log();
 });
