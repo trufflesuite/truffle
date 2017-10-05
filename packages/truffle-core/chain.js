2b1b803d4a62c3477bf9d5d7d64889ef52d4ec6d
g. nicholas d'andrea <nick@gnidan.org>
2017-10-05 00:59:49 -0400

    Separate core concerns into modular mixins

--- packages/truffle-core/chain.js
+++ packages/truffle-core/chain.js
@@ -4,32 +4,9 @@ var IPC = require("node-ipc").IPC;
 var TestRPC = require("ethereumjs-testrpc");
 var path = require("path");
 
-function Server(networkID, ipcConfig) {
-  var self = this;
-
-  this.ipc = new IPC();
-
-  this.ipc.config.id = networkID;
-
-  // set config
-  Object.keys(ipcConfig).forEach(function(key) {
-    self.ipc.config[key] = ipcConfig[key];
-  });
-
-  // socket filename
-}
-
-Server.prototype.start = function(callback) {
-  var ipc = this.ipc;
-
-  var dirname = ipc.config.socketRoot;
-  var basename = `${ipc.config.appspace}${ipc.config.id}`;
-  var servePath = path.join(dirname, basename);
-
-  ipc.serve(servePath, function() { callback(ipc); });
-
-  ipc.server.start();
-}
+/*
+ * Options
+ */
 
 // This script takes one argument: A strinified JSON object meant
 // to be parsed and then passed to TestRPC.server().
@@ -50,55 +27,160 @@ options.network_id = options.network_id || 4447;
 options.seed = options.seed || "yum chocolate";
 options.gasLimit = options.gasLimit || 0x47e7c4;
 
+
+
+/*
+ * IPC server
+ */
+function Supervisor(networkID, ipcConfig, testrpcOptions) {
+  var self = this;
+
+  // init IPC
+  this.ipc = new IPC();
+  this.ipc.config.id = networkID;
+  // set config
+  Object.keys(ipcConfig).forEach(function(key) {
+    self.ipc.config[key] = ipcConfig[key];
+  });
+
+  this.mixins = [];
+}
+
+Supervisor.prototype.mixin = function(mixin) {
+  this.mixins.push(mixin);
+};
+
+Supervisor.prototype.start = function() {
+  var self = this;
+
+  var ipc = this.ipc;
+
+  // socket filename
+  var dirname = ipc.config.socketRoot;
+  var basename = `${ipc.config.appspace}${ipc.config.id}`;
+  var servePath = path.join(dirname, basename);
+
+  ipc.serve(servePath, function() {
+    self.mixins.forEach(function(mixin) {
+      if (mixin.start) {
+        mixin.start(self);
+      }
+    });
+
+    ipc.server.on('connect', function(socket) {
+      self.mixins.forEach(function(mixin) {
+        if (mixin.connect) {
+          mixin.connect(self, socket);
+        }
+      });
+    });
+
+    ipc.server.on('socket.disconnected', function() {
+      self.mixins.forEach(function(mixin) {
+        if (mixin.disconnect) {
+          mixin.disconnect(self);
+        }
+      });
+    });
+  });
+
+  ipc.server.start();
+}
+
+Supervisor.prototype.emit = function(socket, message, data) {
+  this.ipc.server.emit(socket, message, data);
+};
+
+Supervisor.prototype.exit = function() {
+  var self = this;
+
+  this.ipc.server.stop();
+  this.mixins.forEach(function(mixin) {
+    if (mixin.exit) {
+      mixin.exit(self);
+    }
+  });
+}
+
+/*
+ * Lifecycle
+ * (quit on last connection)
+ */
+function LifecycleMixin() {
+  var self = this;
+}
+
+LifecycleMixin.prototype.start = function(supervisor) {
+  this.connections = 0;
+};
+
+LifecycleMixin.prototype.connect = function(supervisor) {
+  this.connections++;
+};
+
+LifecycleMixin.prototype.disconnect = function(supervisor) {
+  this.connections--;
+
+  if (this.connections <= 0) {
+    supervisor.exit();
+  }
+};
+
+
+/*
+ * TestRPC Server
+ */
+function TestRPCMixin(options) {
+  this.testrpc = TestRPC.server(options);
+}
+
+
+TestRPCMixin.prototype.start = function(supervisor) {
+  var self = this;
+
+  this.ready = new Promise(function(accept, reject) {
+    self.testrpc.listen(options.port, options.hostname, function(err, state) {
+      if (err) {
+        reject(err);
+      }
+
+      accept(state);
+    });
+  });
+};
+
+TestRPCMixin.prototype.connect = function(supervisor, socket) {
+  var self = this;
+  this.ready.then(function() {
+    supervisor.emit(socket, 'app.ready');
+  });
+}
+
+TestRPCMixin.prototype.exit = function(supervisor) {
+  this.testrpc.close(function(err) {
+    if (err) {
+      console.error(err.stack || err);
+      process.exit(1);
+    } else {
+      process.exit();
+    }
+  });
+};
+
+
+/*
+ * Process event handling
+ */
 process.on('uncaughtException', function(e) {
   console.error(e.stack);
   process.exit(1);
 })
 
-var server = new Server("truffleDevelop", {retry: 1500});
 
-server.start(function(ipc) {
-
-  var connected = 0;
-
-  var testrpc = TestRPC.server(options);
-
-  var ready = new Promise(function(accept, reject) {
-    testrpc.listen(options.port, options.hostname, function(err, state) {
-      if (err) {
-        reject(err);
-      }
-
-      accept();
-    });
-  });
-
-  ready.catch(function(err) {
-    console.error(err);
-    process.exit(1);
-  });
-
-  ipc.server.on('connect', function(socket) {
-    connected++;
-
-    ready.then(function() {
-      ipc.server.emit(socket, 'app.ready');
-    });
-  });
-
-  ipc.server.on('socket.disconnected', function() {
-    connected--;
-
-    if (connected <= 0) {
-      ipc.server.stop();
-      testrpc.close(function(err) {
-        if (err) {
-          console.error(err.stack || err);
-          process.exit(1);
-        } else {
-          process.exit();
-        }
-      });
-    }
-  });
-});
+/*
+ * Main
+ */
+var supervisor = new Supervisor("truffleDevelop", {retry: 1500});
+supervisor.mixin(new LifecycleMixin());
+supervisor.mixin(new TestRPCMixin(options));
+supervisor.start();
