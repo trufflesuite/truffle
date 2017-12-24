71520a23c354b2c219ec12681d33e8a67fbd1887
Alex Miller <asmiller1989@gmail.com>
2017-12-24 12:29:21 -0800

    Removing unused WalletSubprovider

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var bip39 = require("bip39");
 var hdkey = require('ethereumjs-wallet/hdkey');
 var ProviderEngine = require("web3-provider-engine");
 var FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
-var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
 var HookedSubprovider = require('web3-provider-engine/subproviders/hooked-wallet.js');
 var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
 var Web3 = require("web3");
@@ -42,7 +41,6 @@ function HDWalletProvider(mnemonic, provider_url, address_index=0, num_addresses
       cb(null, rawTx);
     }
   }));
-  // this.engine.addProvider(new WalletSubprovider(this.wallets[this.addresses[0]], {}));
   this.engine.addProvider(new FiltersSubprovider());
   this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));
   this.engine.start(); // Required by the provider engine.
