372a9a2756889679cf2c1b4b508d2c71d9748f72
g. nicholas d'andrea <nick@gnidan.org>
2018-01-09 22:15:27 -0500

    Make sagas into just a file

--- packages/truffle-debugger/lib/sagas.js
+++ packages/truffle-debugger/lib/sagas.js
@@ -1,10 +1,10 @@
 import { all, call, fork, race, take } from 'redux-saga/effects';
 
-import controllerSaga from "../controller/sagas";
-import soliditySaga from "../solidity/sagas";
-import evmSaga from "../evm/sagas";
+import controllerSaga from "./controller/sagas";
+import soliditySaga from "./solidity/sagas";
+import evmSaga from "./evm/sagas";
 
-import { END_OF_TRACE } from "../controller/actions";
+import { END_OF_TRACE } from "./controller/actions";
 
 export function* sessionSaga() {
   yield fork(controllerSaga);
