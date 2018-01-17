93b018e563521e277130f291b2c013a26af1adfb
cgewecke <christophergewecke@gmail.com>
2018-01-16 16:57:01 -0800

    Add tests

---
+++ packages/truffle-compile/test/Ordered.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.18;
+
+contract Ordered {
+  function theFirst() public pure {}
+  function second() public pure {}
+  function andThird() public pure {}
+}
+
+contract Empty {}
