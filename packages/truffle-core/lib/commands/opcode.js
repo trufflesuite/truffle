be8210bbeb77fb97dae85319182f0b260847122c
g. nicholas d'andrea <nick@gnidan.org>
2018-01-23 21:51:31 -0500

    Remove lib/contracts.js and move to truffle-workflow-compile

--- packages/truffle-core/lib/commands/opcode.js
+++ packages/truffle-core/lib/commands/opcode.js
@@ -10,7 +10,7 @@ var command = {
   run: function (options, done) {
     var Config = require("truffle-config");
     var TruffleError = require("truffle-error");
-    var Contracts = require("../contracts");
+    var Contracts = require("truffle-workflow-compile");
     var CodeUtils = require("truffle-code-utils");
 
     if (options._.length == 0) {
