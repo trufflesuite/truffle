f3269914527a99aa3ec05c2c889ce5695ab2102f
cgewecke <christophergewecke@gmail.com>
2018-01-25 15:56:35 -0800

    Clarify error messages

--- packages/truffle-contract/statuserror.js
+++ packages/truffle-contract/statuserror.js
@@ -12,14 +12,15 @@ function StatusError(args, tx, receipt) {
 
   if(receipt.gasUsed === gasLimit){
 
-    message = "Transaction: " + tx + " exited with an error (status 0 - invalid opcode).\n" +
+    message = "Transaction: " + tx + " exited with an error (status 0) after consuming all gas.\n" +
       "Please check that the transaction:\n" +
       "    - satisfies all conditions set by Solidity `assert` statements.\n" +
-      "    - has enough gas to execute all internal Solidity function calls.\n";
+      "    - has enough gas to execute the full transaction.\n" +
+      "    - does not trigger an invalid opcode by other means (ex: accessing an array out of bounds).";
 
   } else {
 
-    message = "Transaction: " + tx + " exited with an error (status 0 - revert).\n" +
+    message = "Transaction: " + tx + " exited with an error (status 0).\n" +
       "Please check that the transaction:\n" +
       "    - satisfies all conditions set by Solidity `require` statements.\n" +
       "    - does not trigger a Solidity `revert` statement.\n";
