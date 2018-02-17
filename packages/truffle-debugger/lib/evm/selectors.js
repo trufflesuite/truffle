fe636ce8973a694f5f20e9102df926b2311d3ff4
g. nicholas d'andrea <nick@gnidan.org>
2018-02-16 20:43:16 -0500

    Add new selector abstraction

--- packages/truffle-debugger/lib/evm/selectors.js
+++ packages/truffle-debugger/lib/evm/selectors.js
@@ -1,97 +1,100 @@
 import { createSelector } from "reselect";
-import { createNestedSelector } from "../selectors";
+import { createSelectorTree, createLeaf } from "../selectors";
 
 import trace from "../trace/selectors";
 
-const callstack = (state) => state.evm.callstack;
+const selector = createSelectorTree({
+  current: {
+    callstack: (state) => state.evm.callstack,
 
-const currentCall = createSelector(
-  [callstack],
+    call: createLeaf(
+      ["./callstack"],
 
-  (stack) => stack.length ? stack[stack.length - 1] : {}
-)
+      (stack) => stack.length ? stack[stack.length - 1] : {}
+    )
+  },
 
-let currentState = createNestedSelector({
-  call: currentCall,
-  callstack: callstack
-});
+  /**
+   * Data views about next step of execution for the EVM
+   */
+  nextStep: {
 
+    /**
+     * evm.nextStep.programCounter
+     */
+    programCounter: createLeaf(
+      [trace.step], (step) => step.pc
+    ),
 
-const programCounter = createSelector(
-  [trace.step],
+    /**
+     * evm.nextStep.isJump
+     */
+    isJump: createLeaf(
+      [trace.step], (step) => (
+        step.op != "JUMPDEST" && step.op.indexOf("JUMP") == 0
+      )
+    ),
 
-  (step) => step.pc
-);
+    /**
+     * evm.nextStep.isCall
+     *
+     * whether the next opcode will switch to another calling context
+     */
+    isCall: createLeaf(
+      [trace.step], (step) => step.op == "CALL" || step.op == "DELEGATECALL"
+    ),
 
-const isJump = createSelector(
-  [trace.step],
+    /**
+     * evm.nextStep.isCreate
+     */
+    isCreate: createLeaf(
+      [trace.step], (step) => step.op == "CREATE"
+    ),
 
-  (step) => step.op != "JUMPDEST" && step.op.indexOf("JUMP") == 0
-);
+    /**
+     * evm.nextStep.isHalting
+     *
+     * whether the next instruction halts or returns from a calling context
+     */
+    isHalting: createLeaf(
+      [trace.step], (step) => step.op == "STOP" || step.op == "RETURN"
+    ),
 
-const isCall = createSelector(
-  [trace.step],
+    /**
+     * evm.nextStep.callAddress
+     *
+     * address transferred to by call operation
+     */
+    callAddress: createLeaf(
+      ["./isCall", trace.step], (matches, step) => {
+        if (!matches) return null;
 
-  (step) => step.op == "CALL" || step.op == "DELEGATECALL"
-);
+        let address = step.stack[step.stack.length - 2]
+        address = "0x" + address.substring(24);
+        return address;
+      }
+    ),
 
-const isCreate = createSelector(
-  [trace.step],
+    /**
+     * evm.nextStep.createBinary
+     *
+     * binary code to execute via create operation
+     */
+    createBinary: createLeaf(
+      ["./isCreate", trace.step], (matches, step) => {
+        if (!matches) return null;
 
-  (step) => step.op == "CREATE"
-);
+        const memory = step.memory.join("");
 
-const isHalting = createSelector(
-  [trace.step],
+        // Get the code that's going to be created from memory.
+        // Note we multiply by 2 because these offsets are in bytes.
+        const inputOffset = parseInt(step.stack[step.stack.length - 2], 16) * 2;
+        const inputSize = parseInt(step.stack[step.stack.length - 3], 16) * 2;
 
-  (step) => step.op == "STOP" || step.op == "RETURN"
-);
-
-const callAddress = createSelector(
-  [isCall, trace.step],
-
-  (matches, step) => {
-    if (!matches) return null;
-
-    let address = step.stack[step.stack.length - 2]
-    address = "0x" + address.substring(24);
-    return address;
+        return "0x" + memory.substring(inputOffset, inputOffset + inputSize);
+      }
+    ),
   }
-);
 
-const createBinary = createSelector(
-  [isCreate, trace.step],
-
-  (matches, step) => {
-    if (!matches) return null;
-
-    const memory = step.memory.join("");
-
-    // Get the code that's going to be created from memory.
-    // Note we multiply by 2 because these offsets are in bytes.
-    const inputOffset = parseInt(step.stack[step.stack.length - 2], 16) * 2;
-    const inputSize = parseInt(step.stack[step.stack.length - 3], 16) * 2;
-
-    return "0x" + memory.substring(inputOffset, inputOffset + inputSize);
-  }
-);
-
-
-let nextStep = createNestedSelector({
-  programCounter,
-
-  isJump,
-  isCall,
-  isCreate,
-  isHalting,
-
-  callAddress,
-  createBinary
 });
-
-let selector = createNestedSelector({
-  current: currentState,
-  nextStep
-});
-
 export default selector;
--- packages/truffle-debugger/lib/selectors.js
+++ packages/truffle-debugger/lib/selectors.js
@@ -1,10 +1,110 @@
-import { createStructuredSelector } from "reselect";
+import debugModule from "debug";
+const debug = debugModule("debugger:selectors");
+
+import { createSelector, createStructuredSelector } from "reselect";
+import jsonpointer from "json-pointer";
+import path from "path";
 
 export function createNestedSelector (selectors) {
+  debug("creating nested selectors %o", Object.keys(selectors));
   let selector = createStructuredSelector(selectors);
   Object.keys(selectors).forEach( (prop) => {
     selector[prop] = selectors[prop];
   });
 
   return selector;
+}
+
+class Leaf {
+  constructor(deps, selector) {
+    this.deps = deps;
+    this.selector = selector;
+  }
+
+  contextualize(resolve, pointer) {
+    let resolved = this.deps
+      .map( (dep) => {
+        if (typeof dep == 'string') {
+          if (dep == "") {
+            dep = "/";
+          }
+
+          let abspath = path.resolve(pointer, "..", dep);
+
+          return (...args) => {
+            debug("args: %o", args);
+            let selector = resolve(abspath);
+            debug("resolved selector: %o", selector);
+            let result = selector.apply(selector, args);
+            debug("result: %o", result);
+            return result;
+          }
+        }
+
+        return dep;
+      });
+
+    return createSelector(resolved, this.selector);
+  }
+}
+
+export function createLeaf(deps, selector) {
+  return new Leaf(deps, selector);
+}
+
+class Tree {
+  setRoot(root) {
+    this.root = root;
+  }
+
+  resolve(abspath) {
+    debug("selecting %s", abspath);
+    var resolved;
+    try {
+      let parsed = jsonpointer.parse(abspath);
+      debug("path: %o", parsed);
+
+      let cur = this.root;
+      for (let step of parsed) {
+        debug("step: %o, obj: %o", step, cur);
+        cur = cur[step];
+      }
+
+      return cur;
+
+    } catch (e) {
+      debug("failed, root: %O", this.root);
+      throw e;
+    }
+
+    return resolved;
+  }
+}
+
+export function createSelectorTree (root) {
+  let tree = new Tree();
+
+  let selector = _createNode(root, tree.resolve.bind(tree), "");
+
+  tree.setRoot(selector);
+
+  return selector;
+}
+
+function _createNode(node, resolve, pointer = "") {
+  if (node instanceof Leaf) {
+    debug("creating leaf at %s", pointer);
+    return node.contextualize(resolve, pointer);
+  } else if (node instanceof Object && !(node instanceof Function)) {
+    debug("recursing %o", node);
+    return createNestedSelector(Object.assign({},
+      ...Object.entries(node).map(
+        ([key, child]) => ({
+          [key]: _createNode(child, resolve, `${pointer}/${key}`)
+        })
+      )
+    ));
+  } else {
+    return node;
+  }
 }
