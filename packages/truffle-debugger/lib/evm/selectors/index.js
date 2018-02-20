abc5d0177e6bb4607094a5fb5b228bf723d26eb5
g. nicholas d'andrea <nick@gnidan.org>
2018-02-20 18:45:58 -0500

    Improve EVM selectors
    - Add `evm.current.state` and `evm.current.{stack|memory|storage}`
    - Add `evm.next.step.trace`

--- packages/truffle-debugger/lib/evm/selectors/index.js
+++ packages/truffle-debugger/lib/evm/selectors/index.js
@@ -4,6 +4,7 @@ import trace from "lib/trace/selectors";
 
 
 const evm = createSelectorTree({
+
   /**
    * evm.current
    */
@@ -21,6 +22,43 @@ const evm = createSelectorTree({
       ["./callstack"],
 
       (stack) => stack.length ? stack[stack.length - 1] : {}
+    ),
+
+    /**
+     * evm.current.state
+     *
+     * evm state info: as of last operation, before op defined in step
+     */
+    state: createLeaf(
+      [trace.step], ({depth, error, gas, memory, stack, storage}) =>
+        ({depth, error, gas, memory, stack, storage})
+    ),
+
+    /**
+     * evm.current.stack
+     *
+     * stack data
+     */
+    stack: createLeaf(
+      [trace.step], (step) => step.stack
+    ),
+
+    /**
+     * evm.current.memory
+     *
+     * memory data
+     */
+    memory: createLeaf(
+      [trace.step], (step) => step.memory
+    ),
+
+    /**
+     * evm.current.storage,
+     *
+     * storage data
+     */
+    storage: createLeaf(
+      [trace.step], (step) => step.storage
     )
   },
 
@@ -30,21 +68,40 @@ const evm = createSelectorTree({
   next: {
 
     /**
+     * evm.next.state
+     *
+     * evm state as a result of next step operation
+     */
+    state: createLeaf(
+      [trace.next], ({depth, error, gas, memory, stack, storage}) =>
+        ({depth, error, gas, memory, stack, storage})
+    ),
+
+    /**
      * evm.next.step
      */
     step: {
       /**
+       * evm.next.step.trace
+       *
+       * trace step info related to next evm operation
+       */
+      trace: createLeaf(
+        [trace.step], ({gasCost, op, pc}) => ({gasCost, op, pc})
+      ),
+
+      /**
        * evm.next.step.programCounter
        */
       programCounter: createLeaf(
-        [trace.step], (step) => step.pc
+        ["./trace"], (step) => step.pc
       ),
 
       /**
        * evm.next.step.isJump
        */
       isJump: createLeaf(
-        [trace.step], (step) => (
+        ["./trace"], (step) => (
           step.op != "JUMPDEST" && step.op.indexOf("JUMP") == 0
         )
       ),
@@ -55,14 +112,14 @@ const evm = createSelectorTree({
        * whether the next opcode will switch to another calling context
        */
       isCall: createLeaf(
-        [trace.step], (step) => step.op == "CALL" || step.op == "DELEGATECALL"
+        ["./trace"], (step) => step.op == "CALL" || step.op == "DELEGATECALL"
       ),
 
       /**
        * evm.next.step.isCreate
        */
       isCreate: createLeaf(
-        [trace.step], (step) => step.op == "CREATE"
+        ["./trace"], (step) => step.op == "CREATE"
       ),
 
       /**
@@ -71,7 +128,7 @@ const evm = createSelectorTree({
        * whether the next instruction halts or returns from a calling context
        */
       isHalting: createLeaf(
-        [trace.step], (step) => step.op == "STOP" || step.op == "RETURN"
+        ["./trace"], (step) => step.op == "STOP" || step.op == "RETURN"
       ),
 
       /**
@@ -80,10 +137,12 @@ const evm = createSelectorTree({
        * address transferred to by call operation
        */
       callAddress: createLeaf(
-        ["./isCall", trace.step], (matches, step) => {
+        ["./isCall", "./trace", "/current/state"],
+
+        (matches, step, {stack}) => {
           if (!matches) return null;
 
-          let address = step.stack[step.stack.length - 2]
+          let address = stack[stack.length - 2]
           address = "0x" + address.substring(24);
           return address;
         }
@@ -95,17 +154,17 @@ const evm = createSelectorTree({
        * binary code to execute via create operation
        */
       createBinary: createLeaf(
-        ["./isCreate", trace.step], (matches, step) => {
-          if (!matches) return null;
+        ["./isCreate", "./trace", "/current/state"],
 
-          const memory = step.memory.join("");
+        (matches, step, {stack, memory}) => {
+          if (!matches) return null;
 
           // Get the code that's going to be created from memory.
           // Note we multiply by 2 because these offsets are in bytes.
-          const inputOffset = parseInt(step.stack[step.stack.length - 2], 16) * 2;
-          const inputSize = parseInt(step.stack[step.stack.length - 3], 16) * 2;
+          const offset = parseInt(stack[stack.length - 2], 16) * 2;
+          const length = parseInt(stack[stack.length - 3], 16) * 2;
 
-          return "0x" + memory.substring(inputOffset, inputOffset + inputSize);
+          return "0x" + memory.join("").substring(offset, offset + length);
         }
       ),
     }
