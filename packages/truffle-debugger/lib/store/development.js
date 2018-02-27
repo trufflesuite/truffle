9f44d149d8bd8c9fcd920cb41588f756e1e2ca64
g. nicholas d'andrea <nick@gnidan.org>
2018-02-27 06:29:27 -0500

    Make remote-redux-devtools not explode

--- packages/truffle-debugger/lib/store/development.js
+++ packages/truffle-debugger/lib/store/development.js
@@ -8,6 +8,18 @@ import commonConfigure from "./common";
 export default function configureStore (reducer, saga, initialState) {
   const composeEnhancers = composeWithDevTools({
     realtime: true,
+    actionsBlacklist: [
+      "RECEIVE_TRACE", "NODE_EXIT", "NODE_ENTER", "SCOPE", "DECLARE_VARIABLE",
+      "ASSIGN", "ADVANCE", "SAVE_STEPS", "BEGIN_STEP", "NEXT"
+    ],
+    stateSanitizer: (state) => ({
+      session: state.session,
+      context: state.context,
+      evm: state.evm,
+      solidity: state.solidity,
+      data: state.data,
+    }),
+
     port: 1117
   });
 
