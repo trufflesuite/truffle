58c9c30c88ac328ba964abdf287733ab5aabb9b5
cgewecke <christophergewecke@gmail.com>
2018-03-05 16:23:15 -0800

    Complete web3 50 blocks error override

--- packages/truffle-contract/lib/subscriptions.js
+++ packages/truffle-contract/lib/subscriptions.js
@@ -4,13 +4,13 @@ module.exports = {
    * @param  {String} e.g. "newHeads"
    * @return {Promise} subscription enabled on resolution
    */
-  subscribe: function(contract, topic){
+  subscribe: function(contract, topic, id){
     return new Promise((accept, reject) => {
-        contract.currentProvider.send({
+        contract.web3.currentProvider.send({
           jsonrpc: "2.0",
           method: "eth_subscribe",
           params: [topic],
-          id: new Date().getTime()
+          id: id
         },
         (err, result) => (err) ? reject(err) : accept(result));
     })
@@ -22,8 +22,7 @@ module.exports = {
    */
   unsubscribe: function(contract, id){
     return new Promise((accept, reject) => {
-
-        contract.currentProvider.send({
+        contract.web3.currentProvider.send({
           jsonrpc: "2.0",
           method: "eth_unsubscribe",
           params: [id],
