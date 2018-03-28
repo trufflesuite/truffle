29cf398d7996c3e7a6977e0e05fba5363c52c483
g. nicholas d'andrea <nick@gnidan.org>
2018-03-27 21:34:03 -0400

    Fiddle with redux middleware logging and remotedev

--- packages/truffle-debugger/lib/store/common.js
+++ packages/truffle-debugger/lib/store/common.js
@@ -6,6 +6,47 @@ import { compose, createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import createLogger from "redux-cli-logger";
 
+export function abbreviateValues(value, options = {}, depth = 0) {
+  options.stringLimit = options.stringLimit || 66;
+  options.arrayLimit = options.arrayLimit || 8;
+  options.recurseLimit = options.recurseLimit || 4;
+
+  if (depth > options.recurseLimit) {
+    return "...";
+  }
+
+  const recurse = (child) => abbreviateValues(child, options, depth + 1);
+
+  if (value instanceof Array) {
+    if (value.length > options.arrayLimit) {
+      value = [
+        ...value.slice(0, options.arrayLimit / 2),
+        "...",
+        ...value.slice(value.length - options.arrayLimit / 2 + 1)
+      ];
+    }
+
+    return value.map(recurse);
+
+  } else if (value instanceof Object) {
+    return Object.assign({},
+      ...Object.entries(value).map(
+        ([k, v]) => ({ [recurse(k)]: recurse(v) })
+      )
+    );
+
+  } else if (typeof value === "string" && value.length > options.stringLimit) {
+    let inner = "...";
+    let extractAmount = (options.stringLimit - inner.length) / 2;
+    let leading = value.slice(0, Math.ceil(extractAmount));
+    let trailing = value.slice(value.length - Math.floor(extractAmount));
+    return `${leading}${inner}${trailing}`;
+
+  } else {
+    return value;
+  }
+}
+
 export default function configureStore (reducer, saga, initialState, composeEnhancers) {
   const sagaMiddleware = createSagaMiddleware();
 
@@ -15,41 +56,11 @@ export default function configureStore (reducer, saga, initialState, composeEnha
 
   const loggerMiddleware = createLogger({
     log: reduxDebug,
-    stateTransformer: (state) => ({
-      ...state,
-
-      data: {
-        ...Object.assign(
-          {},
-          ...Object.entries(state.data)
-            .map( ([context, scope]) => ({
-              [context]: {
-                ...Object.assign(
-                  {},
-                  ...Object.entries(scope)
-                    .filter( ([id, {variables}]) => variables && variables.length > 0 )
-                    .map(
-                      ([id, {variables}]) => ({
-                        [id]: (variables || []).map( (v) => v.name )
-                      })
-                    )
-                )
-              }
-            }))
-        )
-      },
-
-      context: {
-        list: ["..."],
-        indexForAddress: {"...": "..."},
-        indexForBinary: {"...": "..."},
-      },
-
-      trace: {
-        ...state.trace,
-        steps: ["..."]
-      }
-    })
+    stateTransformer: (state) => abbreviateValues(state, {
+      arrayLimit: 4,
+      recurseLimit: 3
+    }),
+    actionTransformer: abbreviateValues,
   });
 
   let store = createStore(
