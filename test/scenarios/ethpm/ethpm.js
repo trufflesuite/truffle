096aaec49fe15044f5fb2c92c5fdceffcc00d060
cgewecke <christophergewecke@gmail.com>
2018-04-03 19:22:25 -0700

    Fix ethpm test: extraneous artifact

--- test/scenarios/ethpm/ethpm.js
+++ test/scenarios/ethpm/ethpm.js
@@ -43,8 +43,8 @@ describe("EthPM commands", function() {
         log(logger.contents());
         return done(err);
       }
+
       assert(fs.existsSync(path.join(config.contracts_build_directory, "PLCRVoting.json")));
-      assert(fs.existsSync(path.join(config.contracts_build_directory, "EIP20.json")));
       assert(fs.existsSync(path.join(config.contracts_build_directory, "Local.json")));
 
       CommandRunner.run("publish", config, function(err) {
@@ -57,6 +57,7 @@ describe("EthPM commands", function() {
           log(output);
           done(err);
         }
+
         assert(output.includes('Uploading sources and publishing'), 'Should have found sources');
         done();
       })
