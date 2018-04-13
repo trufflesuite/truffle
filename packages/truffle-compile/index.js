ceedffc7c13378944b45bacadcc83bd2a67b8dc9
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-04-12 21:38:09 -0400

    Fix `compile()` to callback original path mappings
    Because `solc` outputs os-independent paths like `/c/Users/gnidan/.../ConvertLib.sol`, we need to convert these back to whatever path was originally specified instead.

--- packages/truffle-compile/index.js
+++ packages/truffle-compile/index.js
@@ -139,7 +139,7 @@ var compile = function(sources, options, callback) {
   var files = [];
   Object.keys(standardOutput.sources).forEach(function(filename) {
     var source = standardOutput.sources[filename];
-    files[source.id] = filename;
+    files[source.id] = originalPathMappings[filename];
   });
 
   var returnVal = {};
