7ef2cc7154a75a324eec4ad681f86c6cc6c8fee4
cgewecke <christophergewecke@gmail.com>
2018-04-23 21:49:16 -0700

    Add compiler --list options

--- packages/truffle-core/test/compile.js
+++ packages/truffle-core/test/compile.js
@@ -3,15 +3,20 @@ var Box = require("truffle-box");
 var Contracts = require("truffle-workflow-compile");
 var Artifactor = require("truffle-artifactor");
 var Resolver = require("truffle-resolver");
+var MemoryStream = require('memorystream');
+var command = require('../lib/commands/compile');
 var path = require("path");
 var fs = require("fs-extra");
 var glob = require("glob");
 
 describe("compile", function() {
   var config;
+  var output = '';
+  var memStream;
 
   before("Create a sandbox", function(done) {
     this.timeout(10000);
+
     Box.sandbox(function(err, result) {
       if (err) return done(err);
       config = result;
@@ -26,6 +31,7 @@ describe("compile", function() {
         }
       };
       config.network = "default";
+      config.logger = {log: (val) => val && memStream.write(val)};
       done();
     });
   });
@@ -37,6 +43,8 @@ describe("compile", function() {
       done();
     })
   })
+
+  afterEach("Clear MemoryStream", () => output = '');
 
   it('compiles all initial contracts', function(done) {
     this.timeout(10000);
@@ -95,6 +103,79 @@ describe("compile", function() {
     var contract = config.resolver.require("MetaCoin.sol");
     assert.equal(Object.keys(contract.networks).length, 0, "Expected the contract to be managing zero networks");
   });
+
+  describe('solc listing options', function(){
+
+    beforeEach(() => {
+      memStream = new MemoryStream();
+      memStream.on('data', function(data){ output += data.toString()});
+    })
+
+    it("prints a truncated list of solcjs versions", function(done){
+      this.timeout(5000);
+
+      const options = {
+        list: "releases"
+      };
+
+      command.run(config.with(options), (err, result) => {
+        if(err) return done(err);
+
+        memStream.on('end', function() {
+          const arr = JSON.parse(output);
+          assert(arr.length === 11);
+          done();
+        });
+
+        memStream.end('');
+      })
+    })
+
+    it("prints a list of docker tags", function(done){
+      this.timeout(5000);
+
+      const options = {
+        list: "docker"
+      };
+
+      command.run(config.with(options), (err, result) => {
+        if(err) return done(err);
+
+        memStream.on('end', function() {
+          const arr = JSON.parse(output);
+          assert(arr.length === 11);
+          assert(typeof arr[0] === 'string');
+          done();
+        });
+
+        memStream.end('');
+      })
+    })
+
+    it("prints a full list of releases when --all is set", function(done){
+      this.timeout(5000);
+
+      const options = {
+        list: "releases",
+        all: true
+      };
+
+      command.run(config.with(options), (err, result) => {
+        if(err) return done(err);
+
+        memStream.on('end', function() {
+          const arr = JSON.parse(output);
+          assert(arr.length > 11);
+          assert(typeof arr[0] === 'string');
+          done();
+        });
+
+        memStream.end('');
+      })
+    })
+  });
+
+
 
   // TODO: Kept this as a comment because I'm confused if it applies.
   // Since the binary and abi are updated with every compile, and they're not within
