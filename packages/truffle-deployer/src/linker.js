3e62aace29ba8aa68bd6a4f62efb88d433cc4f98
cgewecke <christophergewecke@gmail.com>
2018-05-16 15:05:50 -0700

    Remove unused code

--- packages/truffle-deployer/src/linker.js
+++ packages/truffle-deployer/src/linker.js
@@ -34,45 +34,4 @@ module.exports = {
       destination.link(library);
     });
   },
-
-  // autolink: function(contract, available_contracts, logger) {
-  //   // Abstract contract passed.
-  //   if (contract.binary == null) return;
-  //
-  //   var self = this;
-  //   var regex = /__[^_]+_+/g;
-  //
-  //   logger = logger || console;
-  //
-  //   var unlinked_libraries = contract.unlinked_binary.match(regex);
-  //
-  //   // Nothing to link.
-  //   if (unlinked_libraries == null) return;
-  //
-  //   if (unlinked_libraries.length == 0) {
-  //     throw new Error("Cannot auto link " + contract.contract_name + "; " + contract.contract_name + " has no library dependencies.")
-  //   }
-  //
-  //   unlinked_libraries = unlinked_libraries.map(function(name) {
-  //     // Remove underscores
-  //     return name.replace(/_/g, "");
-  //   }).sort().filter(function(name, index, arr) {
-  //     // Remove duplicates
-  //     if (index + 1 >= arr.length) {
-  //       return true;
-  //     }
-  //
-  //     return name != arr[index + 1];
-  //   });
-  //
-  //   unlinked_libraries.forEach(function(name) {
-  //     var library = available_contracts[name];
-  //
-  //     if (library == null) {
-  //       throw new Error("Cannot auto link " + contract.contract_name + "; " + contract.contract_name + " unknown dependency " + name + ".")
-  //     }
-  //
-  //     self.link(library, contract, logger);
-  //   });
-  // }
 };
