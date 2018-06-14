927e3b25124e4a4182eec8e201cacc495174f38d
cgewecke <christophergewecke@gmail.com>
2018-06-13 20:07:27 -0700

    Colorize truffle-contract tests

--- packages/truffle-contract/scripts/test.sh
+++ packages/truffle-contract/scripts/test.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 if [ "$GETH" == true ]; then
-  mocha --timeout 50000 --grep @geth
+  mocha --timeout 50000 --grep @geth --colors
 else
-  mocha --no-warnings --timeout 7000
+  mocha --no-warnings --timeout 7000 --colors
 fi
