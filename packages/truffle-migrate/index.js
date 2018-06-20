fe5bb716f98f697a2ad11be9cf09620f7390a53f
cgewecke <christophergewecke@gmail.com>
2018-06-19 19:40:08 -0700

    Restructure Migration callbacks as class methods

--- packages/truffle-migrate/index.js
+++ packages/truffle-migrate/index.js
@@ -25,92 +25,23 @@ class Migration {
     this.isFirst = false;
     this.isLast = false;
     this.options = options || {};
-
   }
 
-  async run(options, callback) {
+  // ------------------------------------- Private -------------------------------------------------
+  /**
+   * Loads & validates migration, then runs it.
+   * @param  {Object}   options  config and command-line
+   * @param  {Object}   context  web3
+   * @param  {Object}   deployer truffle module
+   * @param  {Object}   resolver truffle module
+   * @param  {Function} callback
+   */
+  async _load(options, context, deployer, resolver, callback){
     const self = this;
-    const logger = options.logger;
-    const resolver = new ResolverIntercept(options.resolver);
-    const web3 = new Web3();
-    web3.setProvider(options.provider);
 
-    // Initial context.
-    const context = {
-      web3: web3
-    };
-
-    const deployer = new Deployer({
-      logger: {
-        log: function(msg) {
-          logger.log(msg);
-        },
-        error: function(msg){
-          logger.error(msg);
-        }
-      },
-      network: options.network,
-      network_id: options.network_id,
-      provider: options.provider,
-      basePath: path.dirname(self.file)
-    });
-
-    this.reporter.migration = self;
-    this.reporter.deployer = deployer;
-    this.reporter.listen();
-
-    const file = path.relative(options.migrations_directory, self.file)
-
-    const preMigrationsData = {
-      file: file,
-      isFirst: this.isFirst,
-      network: options.network,
-      networkId: options.network_id,
-    }
-
-    await self.emitter.emit('preMigrate', preMigrationsData);
-
-    var finish = async function(migrateFn) {
-      try {
-        await deployer.start();
-
-        if (migrateFn && migrateFn.then !== undefined){
-          await deployer.then(() => migrateFn);
-        }
-
-        if (options.save === false) return;
-
-        const Migrations = resolver.require("./Migrations.sol");
-
-        if (Migrations && Migrations.isDeployed()) {
-          await self.emitter.emit('saveMigration', self.number);
-          const migrations = await Migrations.deployed();
-          await migrations.setCompleted(self.number);
-        }
-
-        await self.emitter.emit('postMigrate', self.isLast);
-        await options.artifactor.saveAll(resolver.contracts());
-        deployer.finish();
-
-        if (self.isLast){
-          self.emitter.clearListeners();
-        }
-        // Prevent errors thrown in the callback from triggering the below catch()
-        process.nextTick(callback);
-      } catch(e) {
-        const payload = {
-          type: 'migrateErr',
-          error: e
-        };
-
-        await self.emitter.emit('error', payload);
-        deployer.finish();
-        callback(e);
-      };
-    };
-
+    // Load assets and run `execute`
     try {
-      const accounts = await web3.eth.getAccounts();
+      const accounts = await context.web3.eth.getAccounts();
       const requireOptions = {
         file: self.file,
         context: context,
@@ -128,15 +59,120 @@ class Migration {
           return callback(new Error(msg));
         }
 
+        // `migrateFn` might be sync or async. We negotiate that difference in
+        // `execute` through the deployer API.
         const migrateFn = fn(deployer, options.network, accounts);
-        await finish(migrateFn);
+        await self._deploy(options, deployer, resolver, migrateFn, callback);
       });
 
     } catch(err){
       callback(err)
     }
   }
+
+  /**
+   * Initiates deployer sequence, then manages migrations info
+   * publication to chain / artifact saving.
+   * @param  {Object}   options     config and command-line
+   * @param  {Object}   deployer    truffle module
+   * @param  {Object}   resolver    truffle module
+   * @param  {[type]}   migrateFn   module.exports of a migrations.js
+   */
+  async _deploy(options, deployer, resolver, migrateFn, callback) {
+    const self = this;
+
+    try {
+      await deployer.start();
+
+      // Allow migrations method to be async and
+      // deploy to use await
+      if (migrateFn && migrateFn.then !== undefined){
+        await deployer.then(() => migrateFn);
+      }
+
+      // Migrate without saving
+      if (options.save === false) return;
+
+      // Write migrations record to chain
+      const Migrations = resolver.require("./Migrations.sol");
+
+      if (Migrations && Migrations.isDeployed()) {
+        await self.emitter.emit('saveMigration', self.number);
+        const migrations = await Migrations.deployed();
+        await migrations.setCompleted(self.number);
+      }
+
+      // Save artifacts to local filesystem
+      await self.emitter.emit('postMigrate', self.isLast);
+      await options.artifactor.saveAll(resolver.contracts());
+      deployer.finish();
+
+      // Cleanup
+      if (self.isLast){
+        self.emitter.clearListeners();
+      }
+      // Prevent errors thrown in the callback from triggering the below catch()
+      process.nextTick(callback);
+    } catch(e) {
+      const payload = {
+        type: 'migrateErr',
+        error: e
+      };
+
+      await self.emitter.emit('error', payload);
+      deployer.finish();
+      callback(e);
+    };
+  }
+
+  // ------------------------------------- Public -------------------------------------------------
+  /**
+   * Instantiates a deployer, connects this migration and its deployer to the reporter
+   * and launches a migration file's deployment sequence
+   * @param  {Object}   options  config and command-line
+   * @param  {Function} callback
+   */
+  async run(options, callback) {
+    const self = this;
+    const logger = options.logger;
+    const resolver = new ResolverIntercept(options.resolver);
+    const web3 = new Web3();
+    web3.setProvider(options.provider);
+
+    // Initial context.
+    const context = {
+      web3: web3
+    };
+
+    // Instantiate a Deployer
+    const deployer = new Deployer({
+      logger: logger,
+      network: options.network,
+      network_id: options.network_id,
+      provider: options.provider,
+      basePath: path.dirname(self.file)
+    });
+
+    // Connect reporter to this migration
+    self.reporter.migration = self;
+    self.reporter.deployer = deployer;
+    self.reporter.listen();
+
+    // Get file path and emit pre-migration event
+    const file = path.relative(options.migrations_directory, self.file)
+
+    const preMigrationsData = {
+      file: file,
+      isFirst: self.isFirst,
+      network: options.network,
+      networkId: options.network_id,
+    }
+
+    await self.emitter.emit('preMigrate', preMigrationsData);
+    await self._load(options, context, deployer, resolver, callback);
+  }
 }
+
 
 const Migrate = {
   Migration: Migration,
