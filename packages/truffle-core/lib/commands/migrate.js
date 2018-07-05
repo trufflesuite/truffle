e1a7c02ae706b639b207a6bd06ad9208a31a479a
cgewecke <christophergewecke@gmail.com>
2018-07-04 21:09:45 -0700

    (Partially) fix production-mode migrations

--- packages/truffle-core/lib/commands/migrate.js
+++ packages/truffle-core/lib/commands/migrate.js
@@ -113,7 +113,7 @@ var command = {
       }
     };
 
-    async function executePostDryRunMigration(buildDir){
+    async function executePostDryRunMigration(config, buildDir, callback){
       let accept = true;
 
       if (options.interactive){
@@ -131,10 +131,10 @@ var command = {
 
         environment.detect(config, function(err) {
           config.dryRun = false;
-          runMigrations(config, done);
+          runMigrations(config, callback);
         })
       } else {
-        done();
+        callback();
       }
     }
 
@@ -177,11 +177,9 @@ var command = {
             return done(err)
           };
 
-          async.eachSeries(
-            migrationConfigs,
-            executePostDryRunMigration.bind(buildDir),
-            done
-          );
+          async.eachSeries(migrationConfigs, function(config, callback){
+            executePostDryRunMigration(config, currentBuild, callback);
+          }, done);
 
         // Development
         } else {
