f2058487feba8037f90d98e467a69a7955ad04bc
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-08-09 20:10:15 -0400

    Buffer external compiler output
    To send complete lines to `console.log`
    
    - Define buffer generator
    - Use buffer generator for child process output

--- packages/truffle-external-compile/index.js
+++ packages/truffle-external-compile/index.js
@@ -17,21 +17,104 @@ const DEFAULT_ABI = [{
   type: "fallback"
 }];
 
+/**
+ * buffer a line of data, yielding each full line
+ *
+ * returned generator alternates between two states:
+ * 1. reset
+ * 2. read/write
+ *
+ * usage:
+ *
+ *   let gen = bufferLines();
+ *
+ *   // first reset
+ *   gen.next(); // reset
+ *
+ *   // pass string data with zero or more new lines
+ *   // or pass `null` to signal EOF
+ *   let { value, done } = gen.next(data);
+ *
+ *   // if done, value possibly contains string value with unterminated output
+ *   // otherwise, value contains any/all complete lines
+ */
+function *bufferLines() {
+  let buffer = [];
+
+  while (true) {
+    // read input string or null as eof
+    const input = yield;
+
+    // eof returns buffer
+    if (input == null) {
+      const unterminated = buffer.join("");
+
+      return (unterminated)
+        ? [ `${unterminated}%` ]
+        : [];
+    }
+
+    // split lines
+    // last element is always partial line
+    const data = input.split("\n");
+
+    // add first element to buffer
+    let [ first ] = data.slice(0);
+    buffer.push(first);
+
+    if (data.length > 1) {
+      // split off partial line to save as new buffer
+      const [ last ] = data.slice(-1);
+      const [ ...middle ] = data.slice(1, -1);
+
+      // use buffer as first element (now complete line)
+      // and yield all complete lines
+      const lines = [ buffer.join(""),  ...middle ];
+      yield lines;
+
+      // reset buffer
+      buffer = [last];
+
+    } else {
+      // nothing to see yet
+      yield [];
+    }
+  }
+}
+
+/**
+ * run a command, forwarding data to arbitrary logger.
+ * invokes callback when process exits, error on nonzero exit code.
+ */
 const runCommand = promisify(function (command, options, callback) {
   const { cwd, logger, input } = options;
   const child = exec(command, { cwd, input });
 
-  child.stdout.on('data', function(data) {
-    data = data.toString().replace(/(\r|\n)+$/, '');
-    logger.log(data);
-  });
+  // wrap buffer generator for easy use
+  const buffer = (func) => {
+    const gen = bufferLines();
 
-  child.stderr.on('data', function(data) {
-    data = data.toString().replace(/(\r|\n)+$/, '');
-    logger.log(data);
-  });
+    return (data) => {
+      gen.next();
+
+      let { value: lines } = gen.next(data);
+      for (let line of lines) {
+        func(line);
+      }
+    }
+  };
+
+  const log = buffer(logger.log);
+  const warn = buffer(logger.warn || logger.log);
+
+  child.stdout.on('data', data => log(data.toString()));
+  child.stderr.on('data', data => warn(data.toString()));
 
   child.on('close', function(code) {
+    // close streams to flush unterminated lines
+    log(null);
+    warn(null);
+
     // If the command didn't exit properly, show the output and throw.
     if (code !== 0) {
       var err = new Error("Unknown exit code: " + code);
@@ -42,9 +125,13 @@ const runCommand = promisify(function (command, options, callback) {
   });
 });
 
+/**
+ * identify and process contents as one of:
+ * 1. JSON literal
+ * 2. Hex string
+ * 3. Raw binary data
+ */
 function decodeContents (contents) {
-  // accepts JSON, a hex string, or raw binary
-
   // JSON
   try {
     return JSON.parse(contents);
