3dcfd0b82b433f28f4f1d61e083250b0d5bc8ddd
Mike Seese <seesemichaelj@gmail.com>
2018-09-14 13:27:09 -0400

    use `set -ex` to stop the bootstrap script on failure

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# The below ` || exit 1;` is added to stop the script if a command fails
+# The below tells bash to stop the script if any of the commands fail
+set -ex
 
-npm run lerna:bootstrap || exit 1;
-npm run build:decode-utils || exit 1; # must come before decoder and debugger
-npm run build:decoder || exit 1; # must come before debugger
-npm run build:debugger || exit 1;
+npm run lerna:bootstrap
+npm run build:decode-utils # must come before decoder and debugger
+npm run build:decoder # must come before debugger
+npm run build:debugger
