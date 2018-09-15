25a5abfb5fc9dec5db6d957120e04c0d61610b90
eggplantzzz <tmfeickert@gmail.com>
2018-09-14 21:52:44 -0400

    Add test for including force option with unbox method

--- packages/truffle-box/test/unbox.js
+++ packages/truffle-box/test/unbox.js
@@ -1,13 +1,11 @@
-var path = require("path");
-var fs = require("fs-extra");
-var assert = require("assert");
-
-var Box = require("../");
-
+const path = require("path");
+const fs = require("fs-extra");
+const assert = require("assert");
+const Box = require("../");
 var TRUFFLE_BOX_DEFAULT = "git@github.com:trufflesuite/truffle-init-default.git";
 
 describe("Unbox", function() {
-  var destination = path.join(__dirname, ".truffle_test_tmp");
+  const destination = path.join(__dirname, ".truffle_test_tmp");
 
   before("mkdir", async() => fs.ensureDir(destination));
   after("remove tmp dir", async() => fs.remove(destination));
@@ -38,7 +36,7 @@ describe("Unbox", function() {
   it("won't re-init if anything exists in the destination folder", function(done) {
     this.timeout(5000);
 
-    var contracts_directory = path.join(destination, "contracts");
+    const contracts_directory = path.join(destination, "contracts");
 
     // Assert our precondition
     assert(fs.existsSync(contracts_directory), "contracts directory should exist for this test to be meaningful");
@@ -58,6 +56,23 @@ describe("Unbox", function() {
           } else {
             done(new Error("Unknown error received: " + e.stack));
           }
+        });
+    });
+  });
+
+  it("will re-init if force is true in the options and files exist", function(done) {
+    this.timeout(5000);
+
+    const truffleConfigPath = path.join(destination, "truffle.js");
+
+    // Assert our precondition
+    assert(fs.existsSync(truffleConfigPath), "truffle.js should exist for this test to be meaningful");
+
+    fs.remove(truffleConfigPath).then(() => {
+      Box.unbox(TRUFFLE_BOX_DEFAULT, destination, { force: true })
+        .then((boxConfig) => {
+          assert(fs.existsSync(truffleConfigPath) === true, "truffle.js got recreated");
+          done();
         });
     });
   });
