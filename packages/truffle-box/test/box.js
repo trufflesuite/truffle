35ebea3490b99d0279973a0aaca675d93aac63f7
freethinkingaway <cruz.adam.molina@gmail.com>
2018-09-24 17:11:45 -0700

    updated tests for force flag & prompt, also updated syntax

--- packages/truffle-box/test/box.js
+++ packages/truffle-box/test/box.js
@@ -2,18 +2,16 @@ const path = require("path");
 const fs = require("fs-extra");
 const assert = require("assert");
 const Box = require("../");
-var TRUFFLE_BOX_DEFAULT = "git@github.com:trufflesuite/truffle-init-default.git";
+const TRUFFLE_BOX_DEFAULT = "git@github.com:trufflesuite/truffle-init-default.git";
 
-describe("truffle-box Box", function() {
-  describe(".unbox()", function() {
+describe("truffle-box Box", () => {
+  describe(".unbox()", () => {
     const destination = path.join(__dirname, ".truffle_test_tmp");
 
     before("mkdir", async() => fs.ensureDir(destination));
     after("remove tmp dir", async() => fs.remove(destination));
 
-    it("unboxes truffle box from github", function() {
-      this.timeout(5000);
-
+    it("unboxes truffle box from github", () => {
       return Box.unbox(TRUFFLE_BOX_DEFAULT, destination)
         .then(function (truffleConfig) {
           assert.ok(truffleConfig);
@@ -23,9 +21,9 @@ describe("truffle-box Box", function() {
             "Unboxed project should have truffle config."
           );
         });
-    });
+    }).timeout(5000);
 
-    it("ignores files listed in the truffle-init.json file, and removes the truffle-init.json file", function() {
+    it("ignores files listed in the truffle-init.json file, and removes the truffle-init.json file", () => {
       // Assert the file is not there first.
       assert(fs.existsSync(path.join(destination, "truffle-init.json")) == false, "truffle-init.json shouldn't be available to the user!");
 
@@ -34,48 +32,28 @@ describe("truffle-box Box", function() {
       assert(fs.existsSync(path.join(destination, ".gitignore")) == false, ".gitignore didn't get removed!");
     });
 
-    it("won't re-init if anything exists in the destination folder", function(done) {
-      this.timeout(5000);
+    it("prompts if init/unbox used and redundant files/folders exist in the target directory", (done) => {
+      var EXPECTED_TIMEOUT = 5000;
+      var timeout = setTimeout(done, EXPECTED_TIMEOUT);
 
-      const contracts_directory = path.join(destination, "contracts");
+      Box.unbox(TRUFFLE_BOX_DEFAULT, destination)
+        .then(function(boxConfig) {
+          clearTimeout(timeout);
+          done(new Error('Unexpected response'));
+        });
+    }).timeout(6000);
 
-      // Assert our precondition
-      assert(fs.existsSync(contracts_directory), "contracts directory should exist for this test to be meaningful");
-
-      fs.remove(contracts_directory).then(() => {
-        Box.unbox(TRUFFLE_BOX_DEFAULT, destination)
-          .then(function(boxConfig) {
-            assert(
-              fs.existsSync(contracts_directory) == false,
-              "Contracts directory got recreated when it shouldn't have"
-            );
-            done();
-          })
-          .catch(function(e) {
-            if (e.message.indexOf("Something already exists at the destination.") >= 0) {
-              done();
-            } else {
-              done(new Error("Unknown error received: " + e.stack));
-            }
-          });
-      });
-    });
-
-    it("will re-init if force is true in the options and files exist", function(done) {
-      this.timeout(5000);
-
+    it("allows init/unbox if force flag used and redundant files/folders exist in the target directory", (done) => {
       const truffleConfigPath = path.join(destination, "truffle.js");
 
       // Assert our precondition
       assert(fs.existsSync(truffleConfigPath), "truffle.js should exist for this test to be meaningful");
 
-      fs.remove(truffleConfigPath).then(() => {
-        Box.unbox(TRUFFLE_BOX_DEFAULT, destination, { force: true })
-          .then((boxConfig) => {
-            assert(fs.existsSync(truffleConfigPath) === true, "truffle.js got recreated");
-            done();
-          });
-      });
-    });
+      Box.unbox(TRUFFLE_BOX_DEFAULT, destination, { force: true })
+        .then((boxConfig) => {
+          assert(fs.existsSync(truffleConfigPath) === true, "truffle.js got recreated");
+          done();
+        });
+    }).timeout(5000);
   });
 });
