e59db25d6e567749adac68654526185956cd3010
Faina Shalts <fainashalts@Fainas-MacBook-Pro-2.local>
2018-09-26 09:25:21 -0700

    stylistic changes and variable renaming

--- packages/truffle-core/lib/mnemonics/mnemonic.js
+++ packages/truffle-core/lib/mnemonics/mnemonic.js
@@ -12,7 +12,7 @@ const bip39 = require('bip39');
 const hdkey = require('ethereumjs-wallet/hdkey');
 const crypto = require('crypto');
 
-const mnemonic = {
+const getOrGenerateMnemonic = {
 
   /**
   * gets user-level mnemonic from user config, and if missing generates a new mnemonic
@@ -39,35 +39,36 @@ const mnemonic = {
   * @returns {Object} mnemonicObject
   */
   getAccountsInfo: function () {
-    let self = this;
 
-    let mnemonic = self.mnemonic();
+    let mnemonic = this.mnemonic();
     let accounts = [];
     let privateKeys = [];
     let mnemonicObject = {};
 
     let hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
-    let address_index=0;
-    let num_addresses=10;
-    let wallet_hdpath = "m/44'/60'/0'/0/";
+    let addressIndex = 0;
+    let numAddresses = 10;
+    let walletHdpath = "m/44'/60'/0'/0/";
 
 
-    for (let i = address_index; i < address_index + num_addresses; i++) {
-      let wallet = hdwallet.derivePath(wallet_hdpath + i).getWallet();
+    for (let i = addressIndex; i < addressIndex + numAddresses; i++) {
+      let wallet = hdwallet.derivePath(walletHdpath + i).getWallet();
       let addr = '0x' + wallet.getAddress().toString('hex');
       let privKey = wallet.getPrivateKey().toString('hex');
       accounts.push(addr);
       privateKeys.push(privKey);
     }
 
-    mnemonicObject.mnemonic = mnemonic; 
-    mnemonicObject.accounts = accounts;
-    mnemonicObject.privateKeys = privateKeys;
     
-    return mnemonicObject;
+    
+    return {
+      mnemonic,
+      accounts,
+      privateKeys,
+    };
 
   }
 
 }
 
-module.exports = mnemonic;
+module.exports = getOrGenerateMnemonic;
