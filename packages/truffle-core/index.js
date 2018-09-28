7df752d389359e2764ec74f242adde4cc33155d1
eggplantzzz <tmfeickert@gmail.com>
2018-09-28 14:14:17 -0400

    Remove some references to files that don't exist

--- packages/truffle-core/index.js
+++ packages/truffle-core/index.js
@@ -3,10 +3,8 @@ var pkg = require("./package.json");
 module.exports = {
   build: require("./lib/build"),
   create: require("./lib/create"),
-  config: require("./lib/config"),
   console: require("./lib/repl"),
   contracts: require("truffle-workflow-compile"),
-  init: require("./lib/init"),
   package: require("./lib/package"),
   test: require("./lib/test"),
   version: pkg.version
