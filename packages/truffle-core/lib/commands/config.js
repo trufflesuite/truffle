41b99d1265eb663c95014ab074f933a453506239
Faina Shalts <fainashalts@Fainas-MacBook-Pro-2.local>
2018-10-10 20:59:49 -0700

    refactor config file

--- packages/truffle-core/lib/commands/config.js
+++ packages/truffle-core/lib/commands/config.js
@@ -21,46 +21,11 @@ const command = {
   * @param {Func} callback
   */
   run: function (options, callback) {
-    if(options.enableAnalytics) {
-      command.setAnalytics(true);
-    } else if (options.disableAnalytics){
-      command.setAnalytics(false);
-    } else {
-      command.setUserConfigViaPrompt();
-    }
-  },
-  /**
-  * set user-level options for analytics
-  * @param {bool} analyticsBool
-  * @param {Object} userConfig
-  */
-  setAnalytics: function (analyticsBool) {
     const nanoid = require('nanoid');
     const Configstore = require('configstore');
     const userConfig = new Configstore('truffle', {}, { globalConfigPath: true });
-
-    if(analyticsBool === true) {
-      if(!userConfig.get('uniqueId')) {
-        let userId =  nanoid();
-        userConfig.set({ 'uniqueId': userId });
-      } 
-      userConfig.set({ 'enableAnalytics': true });
-      userConfig.set({ 'analyticsSet': true });
-      userConfig.set({ 'analyticsMessageDateTime': Date.now() });
-    } else {
-      userConfig.set({ 'enableAnalytics': false });
-      userConfig.set({ 'analyticsSet': true });
-      userConfig.set({ 'analyticsMessageDateTime': Date.now() });
-    }
-  },
-  /**
-  * prompt user to determine values for user-level analytics config options
-  * @param {Object} userConfig
-  */
-  setUserConfigViaPrompt: async function(){
-    const Configstore = require('configstore');
-    const userConfig = new Configstore('truffle', {}, { globalConfigPath: true });
     const inquirer = require('inquirer');
+
     const analyticsInquiry = [
       {
         type : "list",
@@ -87,33 +52,67 @@ const command = {
       }
     ];
 
-    if(!userConfig.get('analyticsSet') && process.stdin.isTTY === true) {
-      await inquirer.prompt(analyticsInquiry)
-      .then(async answer => {
-        if(answer.analyticsInquiry === analyticsInquiry[0].choices[0]) {
-          command.setAnalytics(true);
-        } else {
-          command.setAnalytics(false);
-        }
-      });
-    } else if (userConfig.get('analyticsSet') && userConfig.get('enableAnalytics')) {
-      await inquirer.prompt(analyticsDisable)
-      .then(async answer => {
-        if(answer.analyticsDisable) {
-         command.setAnalytics(false); 
-        } else {
-          command.setAnalytics(true);
-        }
-      });
-    } else if(userConfig.get('analyticsSet') && !userConfig.get('enableAnalytics')) {
-      await inquirer.prompt(analyticsEnable)
-      .then(async answer => {
-        if(answer.analyticsEnable) {
-          command.setAnalytics(true);
-        } else {
-          command.setAnalytics(false);
-        }
-      });
+    /**
+    * set user-level options for analytics
+    * @param {bool} analyticsBool
+    * @param {Object} userConfig
+    */
+    function setAnalytics (analyticsBool) {
+      if(analyticsBool === true) {
+        if(!userConfig.get('uniqueId')) {
+          let userId =  nanoid();
+          userConfig.set({ 'uniqueId': userId });
+        } 
+        userConfig.set({ 'enableAnalytics': true });
+        userConfig.set({ 'analyticsSet': true });
+        userConfig.set({ 'analyticsMessageDateTime': Date.now() });
+      } else {
+        userConfig.set({ 'enableAnalytics': false });
+        userConfig.set({ 'analyticsSet': true });
+        userConfig.set({ 'analyticsMessageDateTime': Date.now() });
+      }
+    };
+    /**
+    * prompt user to determine values for user-level analytics config options
+    * @param {Object} userConfig
+    */
+    async function setUserConfigViaPrompt () {
+      if(!userConfig.get('analyticsSet') && process.stdin.isTTY === true) {
+        await inquirer.prompt(analyticsInquiry)
+        .then(async answer => {
+          if(answer.analyticsInquiry === analyticsInquiry[0].choices[0]) {
+            setAnalytics(true);
+          } else {
+            setAnalytics(false);
+          }
+        });
+      } else if (userConfig.get('analyticsSet') && userConfig.get('enableAnalytics')) {
+        await inquirer.prompt(analyticsDisable)
+        .then(async answer => {
+          if(answer.analyticsDisable) {
+           setAnalytics(false); 
+          } else {
+            setAnalytics(true);
+          }
+        });
+      } else if(userConfig.get('analyticsSet') && !userConfig.get('enableAnalytics')) {
+        await inquirer.prompt(analyticsEnable)
+        .then(async answer => {
+          if(answer.analyticsEnable) {
+            setAnalytics(true);
+          } else {
+            setAnalytics(false);
+          }
+        });
+      }
+    };
+
+    if(options.enableAnalytics) {
+      setAnalytics(true);
+    } else if (options.disableAnalytics){
+      setAnalytics(false);
+    } else {
+      setUserConfigViaPrompt();
     }
   }
 }
