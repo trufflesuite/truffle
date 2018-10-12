7ccf442d5b87fd4ae24a8abfcd3f708604eba948
Mike Seese <seesemichaelj@gmail.com>
2018-10-12 01:03:34 -0400

    fix subslot array bases

--- packages/truffle-decoder/lib/decode/storage.ts
+++ packages/truffle-decoder/lib/decode/storage.ts
@@ -96,6 +96,7 @@ export default async function decodeStorageReference(definition: DecodeUtils.Ast
           length: baseSize
         };
 
+        currentReference.index -= 1;
 
         ranges.push(childRange);
       }
