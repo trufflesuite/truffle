9512e2b77db3d1f06d31ff0d609c516dcb1efa33
eggplantzzz <tmfeickert@gmail.com>
2018-10-23 16:13:36 -0400

    Pull some logic for collecting version information out of the run method and adjust the code

--- packages/truffle-core/lib/commands/version.js
+++ packages/truffle-core/lib/commands/version.js
@@ -1,6 +1,6 @@
-var command = {
-  command: 'version',
-  description: 'Show version number and exit',
+const command = {
+  command: "version",
+  description: "Show version number and exit",
   builder: {},
   help: {
     usage: "truffle version",
@@ -9,19 +9,11 @@ var command = {
   run: function (options, done) {
     var version = require("../version");
 
-    var bundle_version;
-
-    if (version.bundle) {
-      bundle_version = "v" + version.bundle;
-    } else {
-      bundle_version = "(unbundled)";
-    }
-
-    options.logger.log("Truffle " + bundle_version + " (core: " + version.core + ")");
+    options.logger.log("Truffle " + version.bundle + " (core: " + version.core + ")");
     options.logger.log("Solidity v" + version.solc + " (solc-js)");
 
     done();
   }
-};
+}
 
 module.exports = command;
--- packages/truffle-core/lib/version.js
+++ packages/truffle-core/lib/version.js
@@ -1,15 +1,22 @@
-var pkg = require("../package.json");
-var solcpkg = require("solc/package.json");
-
-var bundle_version = null;
+const pkg = require("../package.json");
+const solcpkg = require("solc/package.json");
+const bundle_version = null;
 
 // NOTE: Webpack will replace BUNDLE_VERSION with a string.
 if (typeof BUNDLE_VERSION != "undefined") {
   bundle_version = BUNDLE_VERSION;
 }
 
+let bundleVersion;
+
+if (version.bundle) {
+  bundleVersion = "v" + version.bundle;
+} else {
+  bundleVersion = "(unbundled)";
+}
+
 module.exports = {
   core: pkg.version,
-  bundle: bundle_version,
+  bundle: bundleVersion,
   solc: solcpkg.version
 };
