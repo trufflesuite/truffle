c41ee089edd881e79c27092fbe04f1a38772c822
Mick de Graaf <mick@mickdegraaf.nl>
2018-10-24 14:58:12 +0200

    Fix undefined sourcePath in debugger

--- packages/truffle-debugger/lib/session/sagas/index.js
+++ packages/truffle-debugger/lib/session/sagas/index.js
@@ -1,7 +1,7 @@
 import debugModule from "debug";
 const debug = debugModule("debugger:session:sagas");
 
-import { cancel, call, all, fork, take, put } from 'redux-saga/effects';
+import { cancel, call, all, fork, take, put } from "redux-saga/effects";
 
 import { prefixName } from "lib/helpers";
 
@@ -15,98 +15,104 @@ import * as web3 from "lib/web3/sagas";
 
 import * as actions from "../actions";
 
-export function *saga () {
+export function* saga() {
   debug("starting listeners");
-  let listeners = yield *forkListeners();
+  let listeners = yield* forkListeners();
 
   // receiving & saving contracts into state
   debug("waiting for contract information");
   let { contexts, sources } = yield take(actions.RECORD_CONTRACTS);
 
   debug("recording contract binaries");
-  yield *recordContexts(...contexts);
+  yield* recordContexts(...contexts);
 
   debug("recording contract sources");
-  yield *recordSources(...sources);
+  yield* recordSources(...sources);
 
   debug("waiting for start");
   // wait for start signal
-  let {txHash, provider} = yield take(actions.START);
+  let { txHash, provider } = yield take(actions.START);
   debug("starting");
 
   // process transaction
   debug("fetching transaction info");
-  let err = yield *fetchTx(txHash, provider);
+  let err = yield* fetchTx(txHash, provider);
   if (err) {
     debug("error %o", err);
-    yield *error(err);
-
+    yield* error(err);
   } else {
     debug("visiting ASTs");
     // visit asts
-    yield *ast.visitAll();
+    yield* ast.visitAll();
 
     debug("readying");
     // signal that stepping can begin
-    yield *ready();
+    yield* ready();
   }
 }
 
 export default prefixName("session", saga);
 
-
-function *forkListeners() {
+function* forkListeners() {
   return yield all(
-    [ast, controller, data, evm, solidity, trace, web3]
-      .map( app => fork(app.saga) )
+    [ast, controller, data, evm, solidity, trace, web3].map(app =>
+      fork(app.saga)
+    )
   );
 }
 
 function* fetchTx(txHash, provider) {
-  let result = yield *web3.inspectTransaction(txHash, provider);
+  let result = yield* web3.inspectTransaction(txHash, provider);
 
   if (result.error) {
     return result.error;
   }
 
-  yield *evm.begin(result);
+  yield* evm.begin(result);
 
-  let addresses = yield *trace.processTrace(result.trace);
+  let addresses = yield* trace.processTrace(result.trace);
   if (result.address && addresses.indexOf(result.address) == -1) {
     addresses.push(result.address);
   }
 
-  let binaries = yield *web3.obtainBinaries(addresses);
+  let binaries = yield* web3.obtainBinaries(addresses);
 
   yield all(
-    addresses.map( (address, i) => call(recordInstance, address, binaries[i]) )
+    addresses.map((address, i) => call(recordInstance, address, binaries[i]))
   );
 }
 
 function* recordContexts(...contexts) {
   for (let { contractName, binary, sourceMap } of contexts) {
-    yield *evm.addContext(contractName, { binary });
+    yield* evm.addContext(contractName, { binary });
 
     if (sourceMap) {
-      yield *solidity.addSourceMap(binary, sourceMap);
+      yield* solidity.addSourceMap(binary, sourceMap);
     }
   }
 }
 
 function* recordSources(...sources) {
-  for (let { sourcePath, source, ast } of sources) {
-    yield *solidity.addSource(source, sourcePath, ast);
+  for (let i = 0; i < sources.length; i++) {
+    const sourceData = sources[i];
+    if (sourceData !== undefined && sourceData !== null) {
+      yield* solidity.addSource(
+        sourceData.source,
+        sourceData.sourcePath,
+        sourceData.ast
+      );
+    }
   }
 }
 
-function *recordInstance(address, binary) {
-  yield *evm.addInstance(address, binary);
+function* recordInstance(address, binary) {
+  yield* evm.addInstance(address, binary);
 }
 
-function *ready() {
+function* ready() {
   yield put(actions.ready());
 }
 
-function *error(err) {
+function* error(err) {
   yield put(actions.error(err));
 }
