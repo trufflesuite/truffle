ac8304ccffcb70800a084d6f2065d46f0e9d9da0
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-10-24 18:50:31 -0400

    Remove extraneous require()
    Requiring `main.js` sets up an extra process listener inside the
    main Truffle process that we don't want.

--- packages/truffle-core/lib/services/analytics/index.js
+++ packages/truffle-core/lib/services/analytics/index.js
@@ -1,18 +1,17 @@
-require("./main.js");
 const analytics = {
-	send: function(eventObject) {
-		let analyticsPath;
-		const path = require('path');
-		if (typeof BUNDLE_ANALYTICS_FILENAME != "undefined") {
-		  analyticsPath = path.join(__dirname, BUNDLE_ANALYTICS_FILENAME);
-		} else {
-		  analyticsPath = path.join(__dirname, "main.js");
-		}
+  send: function(eventObject) {
+    let analyticsPath;
+    const path = require("path");
+    if (typeof BUNDLE_ANALYTICS_FILENAME != "undefined") {
+      analyticsPath = path.join(__dirname, BUNDLE_ANALYTICS_FILENAME);
+    } else {
+      analyticsPath = path.join(__dirname, "main.js");
+    }
 
-		const cp = require("child_process");
-		const child = cp.fork(analyticsPath);
-		child.send(eventObject);
-	},
-}
+    const cp = require("child_process");
+    const child = cp.fork(analyticsPath);
+    child.send(eventObject);
+  }
+};
 
 module.exports = analytics;
