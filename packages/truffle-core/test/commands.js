1feb89f09f8a11d6574cee024b73824ce680303c
freethinkingaway <cruz.adam.molina@gmail.com>
2018-10-25 10:48:34 -0700

    rm _

--- packages/truffle-core/test/commands.js
+++ packages/truffle-core/test/commands.js
@@ -12,29 +12,29 @@ describe("Commander", function() {
   });
 
   it("will infer commands based on initial letters entered", function() {
-    var actualCommand = commander.getCommand("m", _).command;
+    var actualCommand = commander.getCommand("m").command;
     assert.equal(actualCommand, commands.migrate);
   });
 
   it("will infer commands based on initial letters entered, even when typos exist later", function() {
-    var actualCommand = commander.getCommand("complie", _).command;
+    var actualCommand = commander.getCommand("complie").command;
     assert.equal(actualCommand, commands.compile);
   });
 
   it("will NOT infer a command if not given enough information", function() {
     // Note: "co" matches "console" and "compile"
-    var actualCommand = commander.getCommand("co", _);
+    var actualCommand = commander.getCommand("co");
     assert.equal(actualCommand, null);
   });
 
   it("will infer commands based on initial letters entered, given matches for shorter substrings", function() {
     // Note: "co" matches "console" and "compile"
-    var actualCommand = commander.getCommand("com", _).command;
+    var actualCommand = commander.getCommand("com").command;
     assert.equal(actualCommand, commands.compile);
   });
 
   it("will ignore inferring if full command is specified", function() {
-    var actualCommand = commander.getCommand("console", _).command;
+    var actualCommand = commander.getCommand("console").command;
     assert.equal(actualCommand, commands.console);
   });
 });
