235f5410afc4d5b53f34e214996f355424e90ab8
Ben Burns <benjamin.c.burns@gmail.com>
2018-11-02 10:24:52 -0700

    hdwallet provider: Update deps, webpack, fix test

--- packages/truffle-hdwallet-provider/test/provider.js
+++ packages/truffle-hdwallet-provider/test/provider.js
@@ -1,10 +1,10 @@
-const Ganache = require('ganache-core');
-const assert = require('assert');
-const WalletProvider = require('../index.js');
-const EthUtil = require('ethereumjs-util');
+const Ganache = require("ganache-core");
+const assert = require("assert");
+const WalletProvider = require("../src/index.js");
+const EthUtil = require("ethereumjs-util");
 
-describe("HD Wallet Provider", function(done) {
-  const Web3 = require('web3');
+describe("HD Wallet Provider", function() {
+  const Web3 = require("web3");
   const web3 = new Web3();
   const port = 8545;
   let server;
@@ -24,7 +24,7 @@ describe("HD Wallet Provider", function(done) {
     provider.engine.stop();
   });
 
-  it('provides for a mnemonic', function(done){
+  it("provides for a mnemonic", function(done) {
     const truffleDevAccounts = [
       "0x627306090abab3a6e1400e9345bc60c78a8bef57",
       "0xf17f52151ebef6c7334fad080c5704d77216b732",
@@ -38,7 +38,8 @@ describe("HD Wallet Provider", function(done) {
       "0x5aeda56215b167893e80b4fe645ba6d5bab767de"
     ];
 
-    const mnemonic = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat';
+    const mnemonic =
+      "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
     provider = new WalletProvider(mnemonic, `http://localhost:${port}`, 0, 10);
 
     assert.deepEqual(provider.getAddresses(), truffleDevAccounts);
@@ -50,27 +51,32 @@ describe("HD Wallet Provider", function(done) {
     });
   });
 
-  it("throws on invalid mnemonic", function (done) {
+  it("throws on invalid mnemonic", function(done) {
     try {
-      provider = new WalletProvider("takoyaki is delicious", "http://localhost:8545", 0, 1);
+      provider = new WalletProvider(
+        "takoyaki is delicious",
+        "http://localhost:8545",
+        0,
+        1
+      );
       assert.fail("Should throw on invalid mnemonic");
-    } catch(e) {
+    } catch (e) {
       assert.equal(e.message, "Mnemonic invalid or undefined");
       done();
     }
   });
 
-  it('provides for a private key', function(done){
-    const privateKey = '3f841bf589fdf83a521e55d51afddc34fa65351161eead24f064855fc29c9580'; //random valid private key generated with ethkey
+  it("provides for a private key", function(done) {
+    const privateKey =
+      "3f841bf589fdf83a521e55d51afddc34fa65351161eead24f064855fc29c9580"; //random valid private key generated with ethkey
     provider = new WalletProvider(privateKey, `http://localhost:${port}`);
     web3.setProvider(provider);
 
     const addresses = provider.getAddresses();
-    assert.equal(addresses[0], '0xc515db5834d8f110eee96c3036854dbf1d87de2b');
-    addresses.forEach((address) => {
-      assert(EthUtil.isValidAddress(address), 'invalid address');
+    assert.equal(addresses[0], "0xc515db5834d8f110eee96c3036854dbf1d87de2b");
+    addresses.forEach(address => {
+      assert(EthUtil.isValidAddress(address), "invalid address");
     });
-
 
     web3.eth.getBlockNumber((err, number) => {
       assert(number === 0);
@@ -78,30 +84,45 @@ describe("HD Wallet Provider", function(done) {
     });
   });
 
-  it('provides for an array of private keys', function(done){
+  it("provides for an array of private keys", function(done) {
     const privateKeys = [
-      '3f841bf589fdf83a521e55d51afddc34fa65351161eead24f064855fc29c9580',
-      '9549f39decea7b7504e15572b2c6a72766df0281cea22bd1a3bc87166b1ca290',
+      "3f841bf589fdf83a521e55d51afddc34fa65351161eead24f064855fc29c9580",
+      "9549f39decea7b7504e15572b2c6a72766df0281cea22bd1a3bc87166b1ca290"
     ];
 
     const privateKeysByAddress = {
-      '0xc515db5834d8f110eee96c3036854dbf1d87de2b': '3f841bf589fdf83a521e55d51afddc34fa65351161eead24f064855fc29c9580',
-      '0xbd3366a0e5d2fb52691e3e08fabe136b0d4e5929': '9549f39decea7b7504e15572b2c6a72766df0281cea22bd1a3bc87166b1ca290',
+      "0xc515db5834d8f110eee96c3036854dbf1d87de2b":
+        "3f841bf589fdf83a521e55d51afddc34fa65351161eead24f064855fc29c9580",
+      "0xbd3366a0e5d2fb52691e3e08fabe136b0d4e5929":
+        "9549f39decea7b7504e15572b2c6a72766df0281cea22bd1a3bc87166b1ca290"
     };
 
-
-    provider = new WalletProvider(privateKeys, `http://localhost:${port}`, 0, privateKeys.length); //pass in num_addresses to load full array
+    provider = new WalletProvider(
+      privateKeys,
+      `http://localhost:${port}`,
+      0,
+      privateKeys.length
+    ); //pass in num_addresses to load full array
     web3.setProvider(provider);
 
     const addresses = provider.getAddresses();
-    assert.equal(addresses.length, privateKeys.length, 'incorrect number of wallets derived');
-    addresses.forEach((address) => {
-      assert(EthUtil.isValidAddress(address), 'invalid address');
-      const privateKey = new Buffer(privateKeysByAddress[address], 'hex');
-      const expectedAddress = `0x${EthUtil.privateToAddress(privateKey).toString('hex')}`;
-      assert.equal(address, expectedAddress, 'incorrect address for private key');
+    assert.equal(
+      addresses.length,
+      privateKeys.length,
+      "incorrect number of wallets derived"
+    );
+    addresses.forEach(address => {
+      assert(EthUtil.isValidAddress(address), "invalid address");
+      const privateKey = Buffer.from(privateKeysByAddress[address], "hex");
+      const expectedAddress = `0x${EthUtil.privateToAddress(
+        privateKey
+      ).toString("hex")}`;
+      assert.equal(
+        address,
+        expectedAddress,
+        "incorrect address for private key"
+      );
     });
-
 
     web3.eth.getBlockNumber((err, number) => {
       assert(number === 0);
@@ -109,4 +130,3 @@ describe("HD Wallet Provider", function(done) {
     });
   });
 });
-
