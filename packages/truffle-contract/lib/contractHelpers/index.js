b73991788bfc5e490e74191ae2a1ca8f7c087e03
eggplantzzz <tmfeickert@gmail.com>
2018-11-13 13:56:52 -0500

    Extract properties into separate file

--- packages/truffle-contract/lib/contractHelpers/index.js
+++ packages/truffle-contract/lib/contractHelpers/index.js
@@ -1,3 +1,5 @@
 module.exports = {
-  constructorMethods: require("./constructorMethods")
+  constructorMethods: require("./constructorMethods"),
+  bootstrap: require("./bootstrap"),
+  properties: require("./properties")
 };
