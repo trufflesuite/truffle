0e8160a0f87abf2b933e1f62f2f0d7285ecfd99a
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2018-11-16 23:13:01 -0800

    truffle run scenario tests

---
+++ packages/truffle/test/scenarios/commands/run.js
@@ -0,0 +1,142 @@
+const assert = require("assert");
+const CommandRunner = require("../commandrunner");
+const MemoryLogger = require("../memorylogger");
+const sandbox = require("../sandbox");
+const path = require("path");
+
+const logger = new MemoryLogger();
+let config, project;
+
+const setupSandboxLogger = source => {
+  project = path.join(__dirname, source);
+  return sandbox.create(project).then(conf => {
+    config = conf;
+    config.logger = logger;
+  });
+};
+
+describe("truffle run", () => {
+  describe("when run without arguments", () => {
+    beforeEach(() => {
+      return setupSandboxLogger("../../sources/run/mockProjectWithPlugin");
+    });
+
+    it("displays general help", done => {
+      CommandRunner.run("run", config, () => {
+        const output = logger.contents();
+        assert(output.includes("Usage:        truffle run [<command>]"));
+        done();
+      });
+    }).timeout(10000);
+
+    it("should not error", done => {
+      CommandRunner.run("run", config, error => {
+        assert(typeof error === "undefined");
+        done();
+      });
+    }).timeout(10000);
+  });
+
+  describe("when run with an argument", () => {
+    describe("without plugins configured", () => {
+      beforeEach(() => {
+        return setupSandboxLogger("../../sources/run/mockProjectWithoutPlugin");
+      });
+
+      it("whines about having no plugins configured", done => {
+        CommandRunner.run("run mock", config, () => {
+          const output = logger.contents();
+          assert(output.includes("Error: No plugins detected"));
+          done();
+        });
+      }).timeout(10000);
+    }).timeout(10000);
+
+    describe("with plugins configured", () => {
+      describe("error handling", () => {
+        before(() => {
+          return setupSandboxLogger(
+            "../../sources/run/mockProjectWithBadPluginFormat"
+          );
+        });
+
+        it("throws error when plugins formatted incorrectly", () => {
+          CommandRunner.run("run mock", config, () => {
+            const output = logger.contents();
+            assert(output.includes("Error: Plugins configured incorrectly."));
+            done();
+          });
+        }).timeout(10000);
+
+        before(() => {
+          return setupSandboxLogger(
+            "../../sources/run/mockProjectWithMissingPluginModule"
+          );
+        });
+
+        it("throws error when plugins configured but not installed", () => {
+          CommandRunner.run("run mock", config, () => {
+            const output = logger.contents();
+            assert(output.includes("listed as a plugin, but not found"));
+            done();
+          });
+        }).timeout(10000);
+
+        before(() => {
+          return setupSandboxLogger(
+            "../../sources/run/mockProjectWithMissingPluginConfig"
+          );
+        });
+
+        it("throws error when plugins are missing truffle-plugin.json", () => {
+          CommandRunner.run("run mock", config, () => {
+            const output = logger.contents();
+            assert(output.includes("Error: truffle-plugin.json not found"));
+            done();
+          });
+        }).timeout(10000);
+
+        before(() => {
+          return setupSandboxLogger("../../sources/run/mockProjectWithPlugin");
+        });
+
+        it("throws error when configured/installed plugins don't support the given arg/command", () => {
+          CommandRunner.run("run mock", config, () => {
+            const output = logger.contents();
+            assert(output.includes("command not supported"));
+            done();
+          });
+        }).timeout(10000);
+
+        before(() => {
+          return setupSandboxLogger(
+            "../../sources/run/mockProjectWithAbsolutePath"
+          );
+        });
+
+        it("throws error if command in truffle-plugin.json uses an absolute path", () => {
+          CommandRunner.run("run mock", config, () => {
+            const output = logger.contents();
+            assert(output.includes("Error: Absolute paths not allowed!"));
+            done();
+          });
+        }).timeout(10000);
+      });
+      describe("when plugins configured and installed that support a given argument", () => {
+        before(() => {
+          return setupSandboxLogger(
+            "../../sources/run/mockProjectWithWorkingPlugin"
+          );
+        });
+
+        it("runs the command script", () => {
+          CommandRunner.run("run mock", config, () => {
+            const output = logger.contents();
+            assert(output.includes("Running truffle-mock!"));
+            done();
+          });
+        }).timeout(10000);
+      });
+    });
+  }).timeout(10000);
+});
