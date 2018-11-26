5e74483974c4dd221660f594cc9edd893cd4a941
Harry Altman <harry.j.altman@gmail.com>
2018-11-26 15:16:11 -0500

    Remove extraneous function argument.

--- packages/truffle-debugger/test/data/decode/helpers.js
+++ packages/truffle-debugger/test/data/decode/helpers.js
@@ -2,7 +2,6 @@ import debugModule from "debug";
 const debug = debugModule("test:data:decode");
 
 import Ganache from "ganache-cli";
-import Web3 from "web3";
 import { assert } from "chai";
 import changeCase from "change-case";
 
@@ -15,7 +14,7 @@ import { cleanBigNumbers } from "lib/data/decode/utils";
 import data from "lib/data/selectors";
 import solidity from "lib/solidity/selectors";
 
-export function *generateUints() {
+export function* generateUints() {
   let x = 0;
   while (true) {
     yield x;
@@ -31,12 +30,11 @@ function fileName(testName) {
   return `${contractName(testName)}.sol`;
 }
 
-
 function generateTests(fixtures) {
   for (let { name, value: expected } of fixtures) {
-    it(`correctly decodes ${name}`,
-      () => { assert.deepEqual(this.decode(name), expected); }
-    );
+    it(`correctly decodes ${name}`, () => {
+      assert.deepEqual(this.decode(name), expected);
+    });
   }
 }
 
@@ -50,13 +48,13 @@ function lastStatementLine(source) {
   }
 }
 
-
 async function prepareDebugger(testName, sources) {
-  const provider = Ganache.provider({seed: "debugger", gasLimit: 7000000});
-  const web3 = new Web3(provider);
+  const provider = Ganache.provider({ seed: "debugger", gasLimit: 7000000 });
 
-  let { abstractions, artifacts: contracts, files } =
-    await prepareContracts(provider, sources);
+  let { abstractions, artifacts: contracts, files } = await prepareContracts(
+    provider,
+    sources
+  );
 
   let instance = await abstractions[contractName(testName)].deployed();
   let receipt = await instance.run();
@@ -67,12 +65,12 @@ async function prepareDebugger(testName, sources) {
   let session = bugger.connect();
 
   let source = sources[fileName(testName)];
-  
+
   //we'll need the debugger-internal ID of this source
   let debuggerSources = session.view(solidity.info.sources);
-  let matchingSources = Object.values(debuggerSources)
-    .filter((sourceObject) =>
-      sourceObject.sourcePath.includes(contractName(testName)));
+  let matchingSources = Object.values(debuggerSources).filter(sourceObject =>
+    sourceObject.sourcePath.includes(contractName(testName))
+  );
   let sourceId = matchingSources[0].id;
 
   let breakpoint = {
@@ -80,7 +78,7 @@ async function prepareDebugger(testName, sources) {
     line: lastStatementLine(source)
   };
 
-  session.addBreakpoint(breakpoint,true);
+  session.addBreakpoint(breakpoint);
 
   session.continueUntilBreakpoint();
 
@@ -92,9 +90,7 @@ async function getDecode(session) {
   const refs = session.view(data.current.identifiers.refs);
 
   const decode = session.view(data.views.decoder);
-  return (name) => cleanBigNumbers(
-    decode(definitions[name], refs[name])
-  );
+  return name => cleanBigNumbers(decode(definitions[name], refs[name]));
 }
 
 export function describeDecoding(testName, fixtures, selector, generateSource) {
