08ce85243bafb3b408e804cd26f0008ad8336544
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2018-12-05 22:58:26 -0800

    update util.js

--- packages/truffle-contract/test/util.js
+++ packages/truffle-contract/test/util.js
@@ -45,7 +45,13 @@ var util = {
       compilers: {
         solc: {
           version: "0.5.0",
-          settings: {}
+          settings: {
+            optimizer: {
+              enabled: false,
+              runs: 200
+            },
+            evmVersion: "byzantium"
+          }
         }
       }
     };
