94304fbc9de43fe85a332ddf3c0071e3eb356bee
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2018-12-14 23:54:36 -0800

    truffle-compile-legacy tests (redundant)

---
+++ packages/truffle-compile-legacy/test/sources/v0.4.x/InheritB.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.22;
+
+// These are out of alphabetic order
+// Solc will alphabetize them, we should restore source-order.
+contract InheritB {
+  event LogD();
+  event LogC();
+  constructor() public {}
+}
