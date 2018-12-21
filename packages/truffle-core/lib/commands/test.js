53756d6d762d61096242ddc25030f112bfefded5
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2018-12-21 10:01:43 -0800

    fix network_id to match GanacheUI

--- packages/truffle-core/lib/commands/test.js
+++ packages/truffle-core/lib/commands/test.js
@@ -150,7 +150,7 @@ var command = {
           var ganacheOptions = {
             host: "127.0.0.1",
             port: 7545,
-            network_id: 4447,
+            network_id: 5777,
             mnemonic:
               "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat",
             gasLimit: config.gas,
