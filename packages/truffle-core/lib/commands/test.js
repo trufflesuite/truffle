719cc137a765a6f24204ba4764f20ab9f186fbd1
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2018-12-27 10:27:58 -0800

    legacy testing: refactor cmd to test.js

--- packages/truffle-core/lib/commands/test.js
+++ packages/truffle-core/lib/commands/test.js
@@ -51,138 +51,131 @@ var command = {
     ]
   },
   run: function(options, done) {
-    if (options.legacy) {
-      const TestLegacy = require("./test-legacy");
-      TestLegacy.run(options, done);
-    } else {
-      var OS = require("os");
-      var dir = require("node-dir");
-      var temp = require("temp");
-      var Config = require("truffle-config");
-      var Artifactor = require("truffle-artifactor");
-      var Develop = require("../develop");
-      var Test = require("../test");
-      var fs = require("fs");
-      var copy = require("../copy");
-      var Environment = require("../environment");
+    var OS = require("os");
+    var dir = require("node-dir");
+    var temp = require("temp");
+    var Config = require("truffle-config");
+    var Artifactor = require("truffle-artifactor");
+    var Develop = require("../develop");
+    var Test = require("../test");
+    var fs = require("fs");
+    var copy = require("../copy");
+    var Environment = require("../environment");
 
-      var config = Config.detect(options);
+    var config = Config.detect(options);
 
-      // if "development" exists, default to using that for testing
-      if (!config.network && config.networks.development) {
-        config.network = "development";
+    // if "development" exists, default to using that for testing
+    if (!config.network && config.networks.development) {
+      config.network = "development";
+    }
+
+    if (!config.network) {
+      config.network = "test";
+    }
+
+    var ipcDisconnect;
+
+    var files = [];
+
+    if (options.file) {
+      files = [options.file];
+    } else if (options._.length > 0) {
+      Array.prototype.push.apply(files, options._);
+    }
+
+    function getFiles(callback) {
+      if (files.length != 0) {
+        return callback(null, files);
       }
 
-      if (!config.network) {
-        config.network = "test";
-      }
+      dir.files(config.test_directory, callback);
+    }
 
-      var ipcDisconnect;
+    getFiles(function(err, files) {
+      if (err) return done(err);
 
-      var files = [];
+      files = files.filter(function(file) {
+        return file.match(config.test_file_extension_regexp) != null;
+      });
 
-      if (options.file) {
-        files = [options.file];
-      } else if (options._.length > 0) {
-        Array.prototype.push.apply(files, options._);
-      }
-
-      function getFiles(callback) {
-        if (files.length != 0) {
-          return callback(null, files);
-        }
-
-        dir.files(config.test_directory, callback);
-      }
-
-      getFiles(function(err, files) {
+      temp.mkdir("test-", function(err, temporaryDirectory) {
         if (err) return done(err);
 
-        files = files.filter(function(file) {
-          return file.match(config.test_file_extension_regexp) != null;
-        });
+        function cleanup() {
+          var args = arguments;
+          // Ensure directory cleanup.
+          temp.cleanup(() => {
+            // Ignore cleanup errors.
+            done.apply(null, args);
+            if (ipcDisconnect) {
+              ipcDisconnect();
+            }
+          });
+        }
 
-        temp.mkdir("test-", function(err, temporaryDirectory) {
+        function run() {
+          // Set a new artifactor; don't rely on the one created by Environments.
+          // TODO: Make the test artifactor configurable.
+          config.artifactor = new Artifactor(temporaryDirectory);
+
+          Test.run(
+            config.with({
+              test_files: files,
+              contracts_build_directory: temporaryDirectory
+            }),
+            cleanup
+          );
+        }
+
+        var environmentCallback = function(err) {
           if (err) return done(err);
+          // Copy all the built files over to a temporary directory, because we
+          // don't want to save any tests artifacts. Only do this if the build directory
+          // exists.
+          fs.stat(config.contracts_build_directory, function(err) {
+            if (err) return run();
 
-          function cleanup() {
-            var args = arguments;
-            // Ensure directory cleanup.
-            temp.cleanup(() => {
-              // Ignore cleanup errors.
-              done.apply(null, args);
-              if (ipcDisconnect) {
-                ipcDisconnect();
-              }
-            });
-          }
+            copy(config.contracts_build_directory, temporaryDirectory, function(
+              err
+            ) {
+              if (err) return done(err);
 
-          function run() {
-            // Set a new artifactor; don't rely on the one created by Environments.
-            // TODO: Make the test artifactor configurable.
-            config.artifactor = new Artifactor(temporaryDirectory);
-
-            Test.run(
-              config.with({
-                test_files: files,
-                contracts_build_directory: temporaryDirectory
-              }),
-              cleanup
-            );
-          }
-
-          var environmentCallback = function(err) {
-            if (err) return done(err);
-            // Copy all the built files over to a temporary directory, because we
-            // don't want to save any tests artifacts. Only do this if the build directory
-            // exists.
-            fs.stat(config.contracts_build_directory, function(err) {
-              if (err) return run();
-
-              copy(
-                config.contracts_build_directory,
-                temporaryDirectory,
-                function(err) {
-                  if (err) return done(err);
-
-                  config.logger.log(
-                    "Using network '" + config.network + "'." + OS.EOL
-                  );
-
-                  run();
-                }
+              config.logger.log(
+                "Using network '" + config.network + "'." + OS.EOL
               );
+
+              run();
             });
+          });
+        };
+
+        if (config.networks[config.network]) {
+          Environment.detect(config, environmentCallback);
+        } else {
+          var ipcOptions = {
+            network: "test"
           };
 
-          if (config.networks[config.network]) {
-            Environment.detect(config, environmentCallback);
-          } else {
-            var ipcOptions = {
-              network: "test"
-            };
+          var ganacheOptions = {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: 4447,
+            mnemonic:
+              "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat",
+            gasLimit: config.gas,
+            noVMErrorsOnRPCResponse: true
+          };
 
-            var ganacheOptions = {
-              host: "127.0.0.1",
-              port: 7545,
-              network_id: 4447,
-              mnemonic:
-                "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat",
-              gasLimit: config.gas,
-              noVMErrorsOnRPCResponse: true
-            };
-
-            Develop.connectOrStart(ipcOptions, ganacheOptions, function(
-              started,
-              disconnect
-            ) {
-              ipcDisconnect = disconnect;
-              Environment.develop(config, ganacheOptions, environmentCallback);
-            });
-          }
-        });
+          Develop.connectOrStart(ipcOptions, ganacheOptions, function(
+            started,
+            disconnect
+          ) {
+            ipcDisconnect = disconnect;
+            Environment.develop(config, ganacheOptions, environmentCallback);
+          });
+        }
       });
-    }
+    });
   }
 };
 
