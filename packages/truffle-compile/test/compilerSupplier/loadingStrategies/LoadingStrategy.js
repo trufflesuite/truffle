0349efd62c73919f2e9137301d7be4c06825210f
eggplantzzz <tmfeickert@gmail.com>
2019-01-15 16:58:37 -0500

    Add basic test for default config values

--- packages/truffle-compile/test/compilerSupplier/loadingStrategies/LoadingStrategy.js
+++ packages/truffle-compile/test/compilerSupplier/loadingStrategies/LoadingStrategy.js
@@ -1,13 +1,31 @@
 const assert = require("assert");
-const sinon = require("sinon");
-const request = require("request-promise");
 const {
   LoadingStrategy
 } = require("../../../compilerSupplier/loadingStrategies");
-let result;
+let result, expectedDefaultConfig;
 
 describe("LoadingStrategy base class", () => {
   beforeEach(() => {
     instance = new LoadingStrategy();
+    expectedDefaultConfig = {
+      versionsUrl: "https://solc-bin.ethereum.org/bin/list.json",
+      compilerUrlRoot: "https://solc-bin.ethereum.org/bin/",
+      dockerTagsUrl:
+        "https://registry.hub.docker.com/v2/repositories/ethereum/solc/tags/",
+      cache: true
+    };
+  });
+
+  it("has a config with some default values", () => {
+    const {
+      versionsUrl,
+      compilerUrlRoot,
+      dockerTagsUrl,
+      cache
+    } = instance.config;
+    assert(versionsUrl === expectedDefaultConfig.versionsUrl);
+    assert(compilerUrlRoot === expectedDefaultConfig.compilerUrlRoot);
+    assert(dockerTagsUrl === expectedDefaultConfig.dockerTagsUrl);
+    assert(cache === expectedDefaultConfig.cache);
   });
 });
