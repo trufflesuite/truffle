e2b9aa806d7093471f122e38d9a34ec884e0508e
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2019-01-24 20:08:38 -0500

    Extend main schema's ABI type

--- packages/truffle-db/src/data/schema.ts
+++ packages/truffle-db/src/data/schema.ts
@@ -1,11 +1,17 @@
 import { scopeSchemas } from "./utils";
 
-import { schema as artifactsSchema } from "truffle-db/artifacts";
+import { abiItem, schema as artifactsSchema } from "truffle-db/artifacts";
 import { schema as workspaceSchema } from "truffle-db/pouch";
 
 export const schema = scopeSchemas({
   subschemas: {
     artifacts: artifactsSchema,
     workspace: workspaceSchema,
-  }
+  },
+  typeDefs: [
+    [abiItem],
+    `extend type ABI {
+      items: [AbiItem]!
+    }`
+  ]
 });
