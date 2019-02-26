e1297462cb7e6b33f209f342aca0cd62dc79d5f1
Mike Seese <seesemichaelj@gmail.com>
2019-02-25 19:54:48 -0500

    remove 0.20 web3 from legacy system

--- packages/truffle-legacy-system/index.js
+++ packages/truffle-legacy-system/index.js
@@ -2,15 +2,6 @@ const deploy = require("./lib/deployer/deploy");
 const deployMany = require("./lib/deployer/deploymany");
 
 module.exports = {
-  shimWeb3: web3 => {
-    if (web3.sha3) {
-      web3.utils = {};
-      web3.utils.sha3 = web3.sha3;
-      return web3;
-    } else {
-      return web3;
-    }
-  },
   deploy,
   deployMany
 };
