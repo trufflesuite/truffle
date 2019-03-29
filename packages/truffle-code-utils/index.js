60620a5ab71f588418f3ce236e1b23a98703b6ed
Harry Altman <harry.j.altman@gmail.com>
2019-03-28 21:51:18 -0400

    Remove unused parts of truffle-decode-utils

--- packages/truffle-code-utils/index.js
+++ packages/truffle-code-utils/index.js
@@ -9,23 +9,29 @@ module.exports = {
    * @param  {String} hex_string Hex string representing the code
    * @return Array               Array of instructions
    */
-  parseCode: function (hex_string) {
+  parseCode: function(hex_string) {
     // Convert to an array of bytes (denoted by substrings)
     var code = hex_string.match(/(..?)/g);
 
-    // Remove the last 43 bytes, which is the contract metadata
+    // Remove the contract metadata; last two bytes encode its length (not
+    // including those two bytes)
     // TODO: Make this conditional, as it's assumed the metadata exists.
-    code.splice(-43);
+    let metadataLength = parseInt(
+      code[code.length - 2] + code[code.length - 1],
+      16
+    );
+    code.splice(-(metadataLength + 2));
 
     // Convert to Buffer
     code = Buffer.from(code.join("").replace("0x", ""), "hex");
 
     var instructions = [];
     for (var pc = 0; pc < code.length; pc++) {
-      var opcode = opcodes(code[pc], true);
+      let opcode = {};
       opcode.pc = pc;
-      if (opcode.name.slice(0, 4) === 'PUSH') {
-        var length = code[pc] - 0x5f;
+      opcode.name = opcodes(code[pc]);
+      if (opcode.name.slice(0, 4) === "PUSH") {
+        var length = code[pc] - 0x60 + 1; //0x60 is code for PUSH1
         opcode.pushData = code.slice(pc + 1, pc + length + 1);
 
         // convert pushData to hex
