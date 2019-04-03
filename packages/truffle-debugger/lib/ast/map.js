dc8819d4c0d9ed6f01f49922f90904666f4692ce
Harry Altman <harry.j.altman@gmail.com>
2019-04-02 22:51:54 -0500

    Add anyInRange function

--- packages/truffle-debugger/lib/ast/map.js
+++ packages/truffle-debugger/lib/ast/map.js
@@ -3,7 +3,6 @@ const debug = debugModule("debugger:ast:map");
 
 import IntervalTree from "node-interval-tree";
 
-
 /**
  * @private
  */
@@ -13,7 +12,7 @@ export function getRange(node) {
   let [start, length] = node.src
     .split(":")
     .slice(0, 2)
-    .map( (i) => parseInt(i) );
+    .map(i => parseInt(i));
 
   return [start, start + length];
 }
@@ -24,18 +23,18 @@ export function getRange(node) {
 export function rangeNodes(node, pointer = "") {
   if (node instanceof Array) {
     return [].concat(
-      ...node.map( (sub, i) => rangeNodes(sub, `${pointer}/${i}`) )
+      ...node.map((sub, i) => rangeNodes(sub, `${pointer}/${i}`))
     );
   } else if (node instanceof Object) {
     let results = [];
 
     if (node.src) {
-      results.push({pointer, range: getRange(node)});
+      results.push({ pointer, range: getRange(node) });
     }
 
     return results.concat(
-      ...Object.keys(node).map(
-        (key) => rangeNodes(node[key], `${pointer}/${key}`)
+      ...Object.keys(node).map(key =>
+        rangeNodes(node[key], `${pointer}/${key}`)
       )
     );
   } else {
@@ -46,24 +45,38 @@ export function rangeNodes(node, pointer = "") {
 /**
  * @private
  */
-export function findRange(node, sourceStart, sourceLength) {
+export function findOverlappingRange(node, sourceStart, sourceLength) {
   let ranges = rangeNodes(node);
   let tree = new IntervalTree();
 
-  ranges.forEach( ({range, pointer}) => {
+  ranges.forEach(({ range, pointer }) => {
     let [start, end] = range;
 
-    tree.insert(start, end, {range, pointer});
+    tree.insert(start, end, { range, pointer });
   });
 
   let sourceEnd = sourceStart + sourceLength;
 
-  let overlapping = tree.search(sourceStart, sourceEnd);
+  return tree.search(sourceStart, sourceEnd);
+  //returns everything overlapping the given range
+}
 
+/**
+ * @private
+ */
+export function findRange(node, sourceStart, sourceLength) {
   // find nodes that fully contain requested range,
   // return longest pointer
-  return overlapping
-    .filter( ({range}) => sourceStart >= range[0] && sourceEnd <= range[1] )
-    .map( ({pointer}) => pointer )
-    .reduce( (a, b) => a.length > b.length ? a : b, "" );
+  let sourceEnd = sourceStart + sourceLength;
+  return findOverlappingRange(node, sourceStart, sourceLength)
+    .filter(({ range }) => sourceStart >= range[0] && sourceEnd <= range[1])
+    .map(({ pointer }) => pointer)
+    .reduce((a, b) => (a.length > b.length ? a : b), "");
+}
+
+/**
+ * @private
+ */
+export function anyInRange(node, sourceStart, sourceLength) {
+  return findOverlappingRange(node, sourceStart, sourceLength).length > 0;
 }
