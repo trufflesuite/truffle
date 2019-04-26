145060b930ee9ccfed4dbfc195b9cb17f453e14a
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-04-25 22:46:26 -0700

    only test currently untested code

--- packages/truffle-workflow-compile/test/index.js
+++ packages/truffle-workflow-compile/test/index.js
@@ -1,99 +1,54 @@
 const Contracts = require("../index");
 const assert = require("assert");
+const { existsSync } = require("fs");
 
-let logger, config, options;
+let config;
 
 beforeEach(() => {
-  logger = {
-    log(stringToLog) {
-      this.loggedStuff = this.loggedStuff + stringToLog;
-    },
-    loggedStuff: ""
-  };
-  options = {
-    logger,
-    quiet: false
-  };
   config = {
-    contracts_directory: "./sources",
-    contracts_build_directory: "./build"
+    contracts_directory: "./test/sources",
+    contracts_build_directory: "./test/build"
   };
 });
 
-describe("Contracts.reportCompilationStarted", () => {
-  it("reports start of compilation when options.quiet false", () => {
-    Contracts.reportCompilationStarted(options);
-    assert(logger.loggedStuff.includes("Compiling your contracts"));
-  });
+describe("Contracts.compileSources", () => {
+  it("throws when attempting to compile with an unsupported compiler", async () => {
+    compilers = ["unsupportedCompiler"];
 
-  it("omits start of compilation when options.quiet set true", () => {
-    options.quiet = true;
-    Contracts.reportCompilationStarted(options);
-    assert(logger.loggedStuff === "");
-  });
-});
-
-describe("Contracts.reportNothingToCompile", () => {
-  it("reports nothing to compile when options.quiet set false", () => {
-    Contracts.reportNothingToCompile(options);
-    assert(logger.loggedStuff.includes("nothing to compile"));
-  });
-
-  it("omits reporting when options.quiet set true", () => {
-    options.quiet = true;
-    Contracts.reportNothingToCompile(options);
-    assert(logger.loggedStuff === "");
+    try {
+      await Contracts.compileSources(config, compilers);
+      assert.fail("Expected an error!");
+    } catch (err) {
+      assert(err.message.includes("Unsupported compiler"));
+    }
   });
 });
 
-describe("Contracts.reportCompilationFinished", () => {
-  beforeEach(() => {
-    config.compilersInfo = {};
-  });
+describe("Contracts.compile", () => {
+  it("when config.all is true, all contracts in contracts_directory are recompiled", () => {
+    let contractName;
 
-  it("reports artifacts written after successful compilation when options.quiet set false", () => {
-    config.compilersInfo.solc = {}; // a compiler key is added after being used to compile
-    Contracts.reportCompilationFinished(options, config);
-    assert(logger.loggedStuff.includes("Artifacts written"));
-  });
+    // initial compile
+    Contracts.compile(config, (err, { contracts }) => {
+      if (err) assert.fail(err);
+      else {
+        contractName = Object.keys(contracts)[0];
+        assert(
+          existsSync(`${config.contracts_build_directory}/${contractName}.json`)
+        );
+      }
+    });
 
-  it("reports nothing when nothing new compiled and options.quiet set false", () => {
-    Contracts.reportCompilationFinished(options, config);
-    assert(logger.loggedStuff === "undefined");
-  });
-
-  it("reports where contract build artifacts have been written after successful compilation (options.quiet = false)", () => {
-    config.compilersInfo.solc = {}; // a compiler key is added after being used to compile
-    options.contracts_build_directory = config.contracts_build_directory;
-    Contracts.reportCompilationFinished(options, config);
-    assert(logger.loggedStuff.includes(`${options.contracts_build_directory}`));
-  });
-
-  it("reports compiler used after successful compilation (options.quiet = false)", () => {
-    config.compilersInfo.solc = { version: "0.5.0" };
-    Contracts.reportCompilationFinished(options, config);
-    assert(
-      logger.loggedStuff.includes(`solc: ${config.compilersInfo.solc.version}`)
-    );
-  });
-
-  it("reports multiple compilers used after successful compilation (options.quiet = false)", () => {
-    config.compilersInfo.solc = { version: "0.5.0" };
-    config.compilersInfo.vyper = { version: "0.1.0b9" };
-    Contracts.reportCompilationFinished(options, config);
-    assert(
-      logger.loggedStuff.includes(`solc:  ${config.compilersInfo.solc.version}`)
-    );
-    assert(
-      logger.loggedStuff.includes(
-        `vyper: ${config.compilersInfo.vyper.version}`
-      )
-    );
-  });
-
-  it("omits reporting when options.quiet set true", () => {
-    options.quiet = true;
-    Contracts.reportCompilationFinished(options, config);
-    assert(logger.loggedStuff === "");
+    // compile again
+    config.all = true;
+    Contracts.compile(config, (err, output) => {
+      if (err) assert.fail(err);
+      else {
+        assert(
+          output.solc ===
+            `${process.cwd()}/${config.contracts_directory}/${contractName}.sol`
+        );
+      }
+    });
   });
 });
