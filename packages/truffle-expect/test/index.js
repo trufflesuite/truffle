a59603c3426983051436ba516307d99e1fd95a7e
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-04-25 22:04:00 -0700

    only test currently untested code

--- packages/truffle-expect/test/index.js
+++ packages/truffle-expect/test/index.js
@@ -7,44 +7,10 @@ const options = {
   another: 5
 };
 
-describe("expect.options", () => {
-  it("does nothing when expected key values exist", () => {
-    expect.options(options, ["example", "another"]);
-    expect.options(options, ["another", "example"]);
-  });
-
-  it("throws when passed an undefined key value", () => {
-    assert.throws(
-      () =>
-        expect.options(options, ["example", "another", "missing_key_value"]),
-      "Should have thrown!"
-    );
-    assert.throws(
-      () =>
-        expect.options(options, ["another", "missing_key_value", "example"]),
-      "Should have thrown!"
-    );
-  });
-});
-
 describe("expect.one", () => {
-  it("does nothing when at least one key value exists", () => {
-    expect.one(options, ["example", "optional_key"]),
-      expect.one(options, ["optional_key", "example", "other_optional_key"]);
-  });
-
-  it("throws when all key values are undefined", () => {
+  it("throws when given key values are undefined", () => {
     assert.throws(
       () => expect.options(options, ["optional_key", "other_optional_key"]),
-      "Should have thrown!"
-    );
-    assert.throws(
-      () =>
-        expect.options(options, [
-          "other_optional_key",
-          "optional_key",
-          "another_optional_key"
-        ]),
       "Should have thrown!"
     );
   });
