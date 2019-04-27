be59474bbe7de636868bb0fc225e8fe05707d12a
g. nicholas d'andrea <nick@gnidan.org>
2019-04-26 21:35:11 -0400

    Add sketch of ArtifactsLoader class
    Accepting a `db` instance as an argument and providing a `load` method
    to invoke with no arguments.

--- packages/truffle-db/src/loaders/artifacts/index.ts
+++ packages/truffle-db/src/loaders/artifacts/index.ts
@@ -1,5 +1,80 @@
+import gql from "graphql-tag";
+
 import { TruffleDB } from "truffle-db/db";
 
-const db = new TruffleDB({contracts_build_directory: process.argv[2] || process.cwd()});
+const GetContractNames = gql`
+query GetContractNames {
+  artifacts {
+    contractNames
+  }
+}
+`;
 
-export const loaderDB = TruffleDB;
+const GetBytecode = gql`
+query GetBytecode($name: String!) {
+  artifacts {
+    contract(name: $name) {
+      constructor {
+        createBytecode {
+          bytes
+        }
+      }
+    }
+  }
+}
+`;
+
+const AddBytecodes = gql`
+input BytecodeInput {
+  bytes: Bytes!
+}
+
+mutation AddBytecodes($bytecodes: [BytecodeInput!]!) {
+  workspace {
+    bytecodesAdd(input: {
+      bytecodes: $bytecodes
+    }) {
+      bytecodes {
+        id
+      }
+    }
+  }
+}`;
+
+
+
+export class ArtifactsLoader {
+  private db: TruffleDB;
+
+  constructor (db: TruffleDB) {
+    this.db = db;
+  }
+
+  async load (): Promise<void> {
+    const {
+      data: {
+        artifacts: {
+          contractNames
+        }
+      }
+    } = await this.db.query(GetContractNames);
+
+    await this.loadBytecodes(contractNames);
+  }
+
+  async loadBytecodes(contractNames: string[]) {
+    const createBytecodes = await Promise.all(contractNames.map(
+      async (name) =>
+        (await this.db.query(GetBytecode, { name }))
+          .data
+          .artifacts
+          .contract
+          .constructor
+          .createBytecode
+    ));
+
+    const bytecodes = [...createBytecodes];
+
+    await this.db.query(AddBytecodes, { bytecodes });
+  }
+}
---
+++ packages/truffle-db/src/loaders/artifacts/test/index.ts
@@ -0,0 +1,52 @@
+import fs from "fs";
+import path from "path";
+
+import gql from "graphql-tag";
+import { TruffleDB } from "truffle-db";
+import { ArtifactsLoader } from "truffle-db/loaders/artifacts";
+const pointer = require("json-pointer");
+
+
+import { generateId } from "test/helpers";
+
+const fixturesDirectory = path.join(__dirname, "sources");
+
+// minimal config
+const config = {
+  contracts_build_directory: fixturesDirectory
+};
+
+const db = new TruffleDB(config);
+const Migrations = require(path.join(fixturesDirectory, "Migrations.json"));
+
+const GetWorkspaceBytecode: boolean = gql`
+query GetWorkspaceBytecode($id: ID!) {
+  workspace {
+    bytecode(id: $id) {
+      id
+      bytes
+    }
+  }
+}`;
+
+it("loads create bytecodes", async () => {
+  // arrange
+  const expectedId = generateId({ bytes: Migrations.bytecode })
+  const loader = new ArtifactsLoader(db);
+
+  // act
+  await loader.load();
+
+  // assert
+  const {
+    data: {
+      workspace: {
+        bytecode: {
+          bytes
+        }
+      }
+    }
+  } = await db.query(GetWorkspaceBytecode, { id: expectedId });
+
+  expect(bytes).toEqual(Migrations.bytecode);
+});
