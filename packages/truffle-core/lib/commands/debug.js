d59c6825d94e6b162a095795a669777b459f79de
Harry Altman <harry.j.altman@gmail.com>
2019-05-01 15:57:06 -0400

    Clone config the old-fashioned way

--- packages/truffle-core/lib/commands/debug.js
+++ packages/truffle-core/lib/commands/debug.js
@@ -60,10 +60,15 @@ var command = {
       let compilePromise = new Promise(function(accept, reject) {
         config.logger.log("Compiling your contracts...");
 
-        let compileConfig = {
-          ...config,
-          quiet: true
-        };
+        //we need to set up a config object for the compiler.
+        //it's the same as the existing config, but we turn on quiet.
+        //unfortunately, we don't have Babel here, so cloning is annoying.
+        let compileConfig = Object.assign(
+          {},
+          ...Object.entries(config).map(([key, value]) => ({ [key]: value }))
+        ); //clone
+        compileConfig.quiet = true;
+
         compile.all(compileConfig, function(err, contracts, files) {
           if (err) {
             return reject(err);
