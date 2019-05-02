e6597ad0e883c9b6bbc76363bf91d784bb032192
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-02 11:13:59 -0700

    make yarn bootstrap also prepare truffle-contract

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,3 +9,4 @@ yarn build:truffle-contract-schema # must come before decode-utils
 yarn build:decode-utils # must come before decoder
 yarn build:decoder # must come before debugger
 yarn build:debugger
+yarn build:truffle-contract
