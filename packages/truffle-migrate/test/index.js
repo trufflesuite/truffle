afea0f739da42e828bd2f0a0099ce4b1d9e48a39
eggplantzzz <tmfeickert@gmail.com>
2019-05-03 15:01:30 +0200

    Continue developing tests

--- packages/truffle-migrate/test/index.js
+++ packages/truffle-migrate/test/index.js
@@ -2,7 +2,7 @@ const Migrate = require("../index");
 const Config = require("truffle-config");
 const assert = require("assert");
 const sinon = require("sinon");
-let options;
+let options, migrations;
 
 describe("Migrate", () => {
   before(() => {
@@ -45,7 +45,7 @@ describe("Migrate", () => {
         options.reset = undefined;
         sinon
           .stub(Migrate, "lastCompletedMigration")
-          .returns(Promise.resolve(667));
+          .returns(Promise.resolve(666));
         sinon.stub(Migrate, "runFrom");
       });
       afterEach(() => {
@@ -60,6 +60,71 @@ describe("Migrate", () => {
             done();
           })
           .catch(done);
+      });
+    });
+  });
+
+  describe("runMigrations(migrations, options)", () => {
+    beforeEach(() => {
+      sinon.stub(Migrate, "wrapProvider").returns("fake wrapped providerzzz");
+      sinon.stub(Migrate, "wrapResolver");
+      migrations = [
+        {
+          run: () => {
+            return Promise.resolve();
+          }
+        }
+      ];
+    });
+    afterEach(() => {
+      Migrate.wrapProvider.restore();
+      Migrate.wrapResolver.restore();
+    });
+
+    it("calls wrapProvider with the provider and logger", done => {
+      Migrate.runMigrations(migrations, options)
+        .then(() => {
+          assert(
+            Migrate.wrapProvider.calledWith(options.provider, options.logger)
+          );
+          done();
+        })
+        .catch(done);
+    });
+    it("calls wrapResolver with the resolver and the wrapped provider", done => {
+      Migrate.runMigrations(migrations, options)
+        .then(() => {
+          assert(
+            Migrate.wrapResolver.calledWith(
+              options.resolver,
+              "fake wrapped providerzzz"
+            )
+          );
+          done();
+        })
+        .catch(done);
+    });
+    describe("when an error occurs in a migration", () => {
+      beforeEach(() => {
+        migrations = [
+          {
+            run: () => {
+              return Promise.reject(new Error("Somethin bad!"));
+            }
+          }
+        ];
+      });
+
+      it("returns a resolved Promise after running migrations", done => {
+        Migrate.runMigrations(migrations, options)
+          .then(() => {
+            assert(false, "This code should not run");
+            done();
+          })
+          .catch(error => {
+            assert(error.message === "Somethin bad!");
+            done();
+          });
       });
     });
   });
