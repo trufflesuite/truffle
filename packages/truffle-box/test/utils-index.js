fbebeec257b8c00aa9c2be9315ed0b5a60f0418a
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-06 13:50:26 -0700

    add some code coverage

---
+++ packages/truffle-box/test/utils-index.js
@@ -0,0 +1,13 @@
+const assert = require("assert");
+const utils = require("../lib/utils");
+
+describe("utils", () => {
+  it("setUpBox throws when passed an invalid boxConfig", () => {
+    let boxConfig = {};
+
+    utils
+      .setUpBox(boxConfig)
+      .then(() => assert(false, "didn't throw!"))
+      .catch(e => assert(e.stack.includes("Error")));
+  });
+});
