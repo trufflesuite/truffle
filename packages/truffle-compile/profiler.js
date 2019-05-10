88e1a11092d9098819311d2c29b55633a8faba30
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-10 15:38:15 -0700

    compiler errors caught at top-level (index.js)

--- packages/truffle-compile/profiler.js
+++ packages/truffle-compile/profiler.js
@@ -7,7 +7,6 @@ const fs = require("fs");
 const Parser = require("./parser");
 const expect = require("truffle-expect");
 const find_contracts = require("truffle-contract-sources");
-const semver = require("semver");
 const debug = require("debug")("compile:profiler"); // eslint-disable-line no-unused-vars
 
 module.exports = {
@@ -327,19 +326,6 @@ module.exports = {
             try {
               imports = self.getImports(result.file, result);
             } catch (err) {
-              if (err.message.includes("requires different compiler version")) {
-                const contractSolcPragma = err.message.match(
-                  /pragma solidity[^;]*/gm
-                );
-                // if there's a match provide the helpful error, otherwise return solc's error output
-                if (contractSolcPragma) {
-                  const contractSolcVer = contractSolcPragma[0];
-                  const configSolcVer = semver.valid(solc.version());
-                  err.message = err.message.concat(
-                    `\n\nError: Truffle is currently using solc ${configSolcVer}, but one or more of your contracts specify "${contractSolcVer}".\nPlease update your truffle config or pragma statement(s).\n(See https://truffleframework.com/docs/truffle/reference/configuration#compiler-configuration for information on\nconfiguring Truffle to use a specific solc compiler version.)\n`
-                  );
-                }
-              }
               return finished(err);
             }
 
