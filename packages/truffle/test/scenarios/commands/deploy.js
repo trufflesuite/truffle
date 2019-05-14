0bb9f2baf27b825e457f1b10fe9a7292c62a40e3
eggplantzzz <tmfeickert@gmail.com>
2019-05-14 21:08:47 +0200

    Edit the two tests added for migrate and deploy

--- packages/truffle/test/scenarios/commands/deploy.js
+++ packages/truffle/test/scenarios/commands/deploy.js
@@ -1,27 +1,19 @@
 const { assert } = require("chai");
 const CommandRunner = require("../commandrunner");
+const sandbox = require("../sandbox");
 const Server = require("../server");
-const tmp = require("tmp");
-const fse = require("fs-extra");
 const path = require("path");
 
 describe("truffle deploy (alias for migrate)", () => {
-  let config, sourcePath;
+  let config, projectPath;
 
-  before(done => Server.start(done));
-  after(done => Server.stop(done));
-
-  beforeEach("set up config for logger", () => {
-    tempDir = tmp.dirSync({ unsafeCleanup: true });
-    sourcePath = path.join(__dirname, "../../sources/migrations/init");
-    fse.copySync(sourcePath, tempDir.name);
-    config = { working_directory: tempDir.name };
-    config.logger = logger;
+  before("before all setup", async () => {
+    await Server.start();
+    projectPath = path.join(__dirname, "../../sources/migrations/init");
+    config = await sandbox.create(projectPath);
+    config.logger = { log: () => {} };
   });
-
-  afterEach("clear working_directory", () => {
-    tempDir.removeCallback();
-  });
+  after(async () => await Server.stop());
 
   describe("when run on the most basic truffle project", () => {
     it("doesn't throw", done => {
