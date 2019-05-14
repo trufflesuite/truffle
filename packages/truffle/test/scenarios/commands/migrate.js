97f8727512a5fb9b97914fd70614f6ce61fe9fcf
eggplantzzz <tmfeickert@gmail.com>
2019-05-14 20:06:54 +0200

    Remove logging and start ganache server before tests

--- packages/truffle/test/scenarios/commands/migrate.js
+++ packages/truffle/test/scenarios/commands/migrate.js
@@ -1,20 +1,22 @@
 const { assert } = require("chai");
 const CommandRunner = require("../commandrunner");
-const MemoryLogger = require("../memorylogger");
+const Server = require("../server");
 const tmp = require("tmp");
 const fse = require("fs-extra");
 const path = require("path");
 
 describe("truffle migrate", () => {
   let config, sourcePath;
-  const logger = new MemoryLogger();
+
+  before(done => Server.start(done));
+  after(done => Server.stop(done));
 
   beforeEach("set up config for logger", () => {
     tempDir = tmp.dirSync({ unsafeCleanup: true });
     sourcePath = path.join(__dirname, "../../sources/migrations/init");
     fse.copySync(sourcePath, tempDir.name);
     config = { working_directory: tempDir.name };
-    config.logger = logger;
+    config.logger = { log: () => {} };
   });
 
   afterEach("clear working_directory", () => {
@@ -24,7 +26,6 @@ describe("truffle migrate", () => {
   describe("when run on the most basic truffle project", () => {
     it("doesn't throw", done => {
       CommandRunner.run("migrate", config, error => {
-        console.log(error);
         assert(error === undefined, "error should be undefined here");
         done();
       });
