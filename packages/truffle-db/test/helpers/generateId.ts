dd4df8bb14945ecb69c015b2ea119abf58773a63
Faina Shalts <fainashalts@fainas-mbp.home>
2019-05-13 20:08:46 -0700

    Remove helpers directory from test directory outside of src

--- packages/truffle-db/test/helpers/generateId.ts
+++
@@ -1,4 +0,0 @@
-import { soliditySha3 } from "web3-utils";
-const jsonStableStringify = require('json-stable-stringify');
-
-export const generateId = (obj) => soliditySha3(jsonStableStringify(obj));
