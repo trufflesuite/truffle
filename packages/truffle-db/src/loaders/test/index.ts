9fedacdef43854a45733842c1c3d9511835f2bf3
Faina Shalts <fainashalts@fainas-mbp.home>
2019-05-16 11:52:24 -0700

    Add mocking of manual compilation to loader test file

--- packages/truffle-db/src/loaders/test/index.ts
+++ packages/truffle-db/src/loaders/test/index.ts
@@ -1,22 +1,42 @@
 import fs from "fs";
 import path from "path";
 import gql from "graphql-tag";
-
 import { TruffleDB } from "truffle-db";
+import * as Contracts from "truffle-workflow-compile";
 
+jest.mock("truffle-workflow-compile", () => ({
+ compile: function(config, callback) {
+   const magicSquare= require(path.join(__dirname,"..", "artifacts", "test", "sources", "MagicSquare.json"));
+   const migrations = require(path.join(__dirname, "..", "artifacts", "test", "sources", "Migrations.json"));
+   const squareLib = require(path.join(__dirname, "..", "artifacts", "test", "sources", "SquareLib.json"));
+   callback(null, {
+     "contracts": [{
+       "contract_name": "MagicSquare",
+       ...magicSquare
+     },
+     {
+       "contract_name": "Migrations",
+       ...migrations
+     },
+     {
+       "contract_name": "SquareLib",
+       ...squareLib
+     }
+     ]
+   });
+ }
+}));
 
 const fixturesDirectory = path.join(__dirname, "..", "artifacts", "test");
 
 // minimal config
 const config = {
-  contracts_build_directory: path.join(fixturesDirectory, "build"),
+  contracts_build_directory: path.join(fixturesDirectory, "sources"),
   contracts_directory: path.join(fixturesDirectory, "compilationSources"),
   all: true
 };
 
 const db = new TruffleDB(config);
-
-const  Build = require(path.join(fixturesDirectory, "build", "SimpleStorage.json"));
 
 const Load = gql `
   mutation LoadArtifacts {
@@ -26,7 +46,7 @@ const Load = gql `
       }
     }
   }
-` 
+`
 
 it("loads artifacts and returns true ", async () => {
   const {
@@ -38,5 +58,5 @@ it("loads artifacts and returns true ", async () => {
       }
     }
   } = await db.query(Load);
-  expect(success).toEqual(true); 
+  expect(success).toEqual(true);
 });
