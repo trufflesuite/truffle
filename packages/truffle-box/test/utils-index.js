3920b46ce53943d95d8a2d91cfdd2bf760872218
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-16 16:56:51 -0700

    bit more coverage

--- packages/truffle-box/test/utils-index.js
+++ packages/truffle-box/test/utils-index.js
@@ -1,5 +1,7 @@
 const assert = require("assert");
 const utils = require("../lib/utils");
+const sinon = require("sinon");
+const tmp = require("tmp");
 
 describe("utils", () => {
   it("setUpBox throws when passed an invalid boxConfig", () => {
@@ -11,5 +13,13 @@ describe("utils", () => {
       .catch(e =>
         assert(e.stack.match(/(Error:).*(post-unpack).*(undefined)/g))
       );
+  });
+
+  it("setUpTempDirectory throws when tmpDir creation fails", () => {
+    sinon.stub(tmp, "dirSync").throws();
+    assert.throws(() => {
+      utils.setUpTempDirectory();
+    }, "should have thrown!");
+    tmp.dirSync.restore();
   });
 });
