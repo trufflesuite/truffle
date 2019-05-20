71166da2aac8e7e43243a63ef2571f383f28d5c4
g. nicholas d'andrea <nick@gnidan.org>
2019-05-20 13:44:02 -0400

    Reduce weight of ugly Promise constructor

--- packages/truffle-core/lib/debug/compiler.js
+++ packages/truffle-core/lib/debug/compiler.js
@@ -6,16 +6,18 @@ class DebugCompiler {
   }
 
   async compile() {
-    const { contracts, files } = await new Promise((accept, reject) => {
-      //we need to set up a config object for the compiler.
-      //it's the same as the existing config, but we turn on quiet.
-      //unfortunately, we don't have Babel here, so cloning is annoying.
-      let compileConfig = Object.assign(
-        {},
-        ...Object.entries(this.config).map(([key, value]) => ({ [key]: value }))
-      ); //clone
-      compileConfig.quiet = true;
+    //we need to set up a config object for the compiler.
+    //it's the same as the existing config, but we turn on quiet.
+    //unfortunately, we don't have Babel here, so cloning is annoying.
+    const compileConfig = Object.assign(
+      {},
+      ...Object.entries(this.config).map(([key, value]) => ({ [key]: value }))
+    ); //clone
+    compileConfig.quiet = true;
 
+    // since `compile.all()` returns two results, we can't use promisify
+    // and are instead stuck with using an explicit Promise constructor
+    const { contracts, files } = await new Promise((accept, reject) => {
       compile.all(compileConfig, (err, contracts, files) => {
         if (err) {
           return reject(err);
