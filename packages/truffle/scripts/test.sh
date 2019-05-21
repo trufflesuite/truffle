9e596b924bcdac41adda3d20bf56a3c519f80aff
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-20 22:22:54 -0700

    specify skipping extra build on integration job, keep build for local dev testing

--- packages/truffle/scripts/test.sh
+++ packages/truffle/scripts/test.sh
@@ -6,6 +6,8 @@ if [ "$GETH" == true ]; then
   mocha --timeout 50000 --grep @ganache --invert --colors $@
 elif [ "$COVERAGE" == true ]; then
   NO_BUILD=true mocha --no-warnings --timeout 7000 --grep @geth --invert --colors $@
-else
+elif [ "$INTEGRATION" == true ]; then
   mocha --no-warnings --timeout 7000 --grep @geth --invert --colors $@
+else
+  yarn build-cli && mocha --no-warnings --timeout 7000 --grep @geth --invert --colors $@
 fi
