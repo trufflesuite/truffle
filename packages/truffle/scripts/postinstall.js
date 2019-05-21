87c689f39bee5caa53094e9289d00509f895a05f
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-21 09:53:32 -0700

    rework postinstall.js

--- packages/truffle/scripts/postinstall.js
+++ packages/truffle/scripts/postinstall.js
@@ -2,15 +2,13 @@ const { statSync } = require("fs");
 const { execSync } = require("child_process");
 
 const bundledCLI = "./build/cli.bundled.js";
-const unbundledCLI = "../truffle-core/cli.js";
 
 const postinstallObtain = () => {
   try {
     statSync(bundledCLI);
     execSync(`node ${bundledCLI} obtain --solc=0.5.0`);
   } catch ({ message }) {
-    if (message.includes("no such file"))
-      return execSync(`node ${unbundledCLI} obtain --solc=0.5.0`);
+    if (message.includes("no such file")) return;
     throw new Error(message);
   }
 };
