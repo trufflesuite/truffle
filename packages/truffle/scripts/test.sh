927584229897b1d898da6bdfa4e3ed3edae8c1de
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-20 22:12:33 -0700

    rm extra build

--- packages/truffle/scripts/test.sh
+++ packages/truffle/scripts/test.sh
@@ -3,9 +3,9 @@
 set -o errexit
 
 if [ "$GETH" == true ]; then
-  yarn build-cli && mocha --timeout 50000 --grep @ganache --invert --colors $@
+  mocha --timeout 50000 --grep @ganache --invert --colors $@
 elif [ "$COVERAGE" == true ]; then
   NO_BUILD=true mocha --no-warnings --timeout 7000 --grep @geth --invert --colors $@
 else
-  yarn build-cli && mocha --no-warnings --timeout 7000 --grep @geth --invert --colors $@
+  mocha --no-warnings --timeout 7000 --grep @geth --invert --colors $@
 fi
