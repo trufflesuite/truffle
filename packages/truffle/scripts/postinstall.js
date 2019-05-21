fc93a26c2ee14bfc9fdf8c1d06f31706ed4de0fd
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-21 15:51:40 -0700

    place default solc version into var & improve error msg

--- packages/truffle/scripts/postinstall.js
+++ packages/truffle/scripts/postinstall.js
@@ -2,14 +2,17 @@ const { statSync } = require("fs");
 const { execSync } = require("child_process");
 
 const bundledCLI = "./build/cli.bundled.js";
+const defaultSolc = "0.5.0";
 
 const postinstallObtain = () => {
   try {
     statSync(bundledCLI);
-    execSync(`node ${bundledCLI} obtain --solc=0.5.0`);
+    execSync(`node ${bundledCLI} obtain --solc=${defaultSolc}`);
   } catch ({ message }) {
     if (message.includes("no such file")) return;
-    throw new Error(message);
+    throw new Error(
+      `Error while attempting to download and cache solc ${defaultSolc}: ${message}`
+    );
   }
 };
 
