df753afb9c34d8a20a9ae9b6e17934112309a40f
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-22 09:48:07 -0700

    misc

--- packages/truffle-contract/index.js
+++ packages/truffle-contract/index.js
@@ -2,7 +2,7 @@ const Schema = require("truffle-contract-schema");
 const Contract = require("./lib/contract");
 const truffleContractVersion = require("./package.json").version;
 
-const contract = function(options = {}) {
+const contract = (options = {}) => {
   const binary = Schema.normalize(options);
 
   // Note we don't use `new` here at all. This will cause the class to
