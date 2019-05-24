6ed7e0b1aef3aa9a477bd3fa3318c8af2912a0d5
g. nicholas d'andrea <nick@gnidan.org>
2019-05-24 18:11:47 -0400

    Wrap setJSTestGlobals args in {}

--- packages/truffle-core/lib/test.js
+++ packages/truffle-core/lib/test.js
@@ -105,13 +105,13 @@ const Test = {
       runner
     );
 
-    await this.setJSTestGlobals(
+    await this.setJSTestGlobals({
       config,
       web3,
       accounts,
       testResolver,
       runner
-    );
+    });
 
     // Finally, run mocha.
     process.on("unhandledRejection", reason => {
@@ -196,7 +196,13 @@ const Test = {
     });
   },
 
-  setJSTestGlobals: function(config, web3, accounts, testResolver, runner) {
+  setJSTestGlobals: function({
+    config,
+    web3,
+    accounts,
+    testResolver,
+    runner
+  }) {
     return new Promise(function(accept) {
       global.web3 = web3;
       global.assert = chai.assert;
