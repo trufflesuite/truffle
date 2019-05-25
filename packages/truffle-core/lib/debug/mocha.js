84764d9ca15165d20fd450045fd023e052bbf5a3
g. nicholas d'andrea <nick@gnidan.org>
2019-05-24 21:35:10 -0400

    Support debugging existing transaction promises
    Adds new syntax (inside a `truffle test` JS test):
    
        // before
        const { tx } = await magicSquare.generateMagicSquare(3);
    
        // with debugging
        const { tx } = await __debug(magicSquare.generateMagicSquare(3));
                             ^^^^^^^^                                  ^
                                  \__ just a "regular ol' function" __/
    
    Update CLIDebugHook to:
    
    - Ensure __debug global also returns normal result
    - Define a `catchPromiEvent` method to wait for `methodTransaction` and
      then capture the method info, the result, and the txHash
    - Detect promiEvent in main `invoke` method and defer to catch
    
    NOTE: this doesn't support calls! calls do not return promiEvents.

--- packages/truffle-core/lib/debug/mocha.js
+++ packages/truffle-core/lib/debug/mocha.js
@@ -12,12 +12,14 @@ class CLIDebugHook {
     // tests that take a long time _after_ debug break just won't timeout
     this.disableTimeout();
 
-    const txHash = await this.invoke(method, ...args);
+    const { txHash, result } = await this.invoke(method, ...args);
 
     this.config.logger.log("");
     const interpreter = await new CLIDebugger(this.config).run(txHash);
     await interpreter.start();
     this.config.logger.log("");
+
+    return result;
   }
 
   disableTimeout() {
@@ -25,12 +27,28 @@ class CLIDebugHook {
   }
 
   async invoke(method, ...args) {
-    if (method) {
-      if (method) {
-        const { tx } = await method.sendTransaction(...args);
-        return tx;
-      }
+    if (method && typeof method.on === "function") {
+      return await this.catchPromiEvent(method);
+    } else if (method) {
+      const result = await method.sendTransaction(...args);
+      const { tx: txHash } = result;
+
+      return { txHash, result };
     }
+  }
+
+  catchPromiEvent(promiEvent) {
+    return new Promise((accept, reject) => {
+      promiEvent.on("methodTransaction", info => {
+        promiEvent
+          .then(result => {
+            const { tx: txHash } = result;
+
+            accept({ txHash, info, result });
+          })
+          .catch(reject);
+      });
+    });
   }
 }
 
