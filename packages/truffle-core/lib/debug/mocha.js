400e6fd417868b28f6962d52809475f2c2198021
g. nicholas d'andrea <nick@gnidan.org>
2019-05-24 21:10:03 -0400

    Add mocha debugging hook module
    - Define class with `async debug()` method
    - Encapsulate timeout logic
    - Encapsulate method invocation via new syntax:
    
        // with debugging
        const { tx } = await hook.debug(magicSquare.generateMagicSquare(3));
                             ^^^^^^^^^^^                                  ^
                                     \__ just a "regular ol' function" __/
    
    - Return regular result of method invocation
    - Define a `detectMethod` method to wait for `execute:send:method` to
      capture method info

---
+++ packages/truffle-core/lib/debug/mocha.js
@@ -0,0 +1,71 @@
+const { CLIDebugger } = require("./cli");
+
+class CLIDebugHook {
+  constructor(config, runner) {
+    this.config = config;
+    this.runner = runner; // mocha runner (**not** lib/test/testrunner)
+  }
+
+  async debug(operation) {
+    // turn off timeouts for the current runnable
+    // HACK we don't turn it back on because it doesn't work...
+    // tests that take a long time _after_ debug break just won't timeout
+    this.disableTimeout();
+
+    const {
+      txHash,
+      result,
+      method: {
+        abi: { name: methodName },
+        args,
+        contract: { contractName }
+      }
+    } = await this.invoke(operation);
+
+
+    this.config.logger.log("");
+    const interpreter = await new CLIDebugger(this.config).run(txHash);
+    await interpreter.start();
+    this.config.logger.log("");
+
+    return result;
+  }
+
+  disableTimeout() {
+    this.runner.currentRunnable.timeout(0);
+  }
+
+  async invoke(operation) {
+    const method = await this.detectMethod(operation);
+    const { action } = method;
+
+    switch (action) {
+      case "send": {
+        const result = await operation;
+        const { tx: txHash } = result;
+
+        return { txHash, method, result };
+      }
+      default: {
+        throw new Error(`Unsupported action for debugging: ${action}`);
+      }
+    }
+  }
+
+  detectMethod(promiEvent) {
+    return new Promise(accept => {
+      promiEvent.on("execute:send:method", ({ abi, args, contract }) => {
+        accept({
+          abi,
+          args,
+          contract,
+          action: "send"
+        });
+      });
+    });
+  }
+}
+
+module.exports = {
+  CLIDebugHook
+};
