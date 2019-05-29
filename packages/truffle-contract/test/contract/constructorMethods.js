2c5e7d281fef0c9090819e873c432dc32ec4658a
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-29 14:48:17 -0700

    moar coverage

--- packages/truffle-contract/test/contract/constructorMethods.js
+++ packages/truffle-contract/test/contract/constructorMethods.js
@@ -99,3 +99,141 @@ describe("TruffleContract.deployed()", () => {
     );
   });
 });
+
+describe("TruffleContract.defaults()", () => {
+  it("sets instance class_defaults when not already set", () => {
+    const freshTruffleContract = TruffleContract();
+    freshTruffleContract.class_defaults = undefined;
+    assert.deepStrictEqual(freshTruffleContract.defaults(), {});
+  });
+});
+
+describe("TruffleContract.isDeployed()", () => {
+  it("returns false when instance network_id not set", () => {
+    const freshTruffleContract = TruffleContract();
+    assert.strictEqual(freshTruffleContract.isDeployed(), false);
+  });
+});
+
+describe("TruffleContract.detectNetwork()", () => {
+  it("throws when provider not present", () => {
+    const freshTruffleContract = TruffleContract();
+    assert.rejects(
+      async () => await freshTruffleContract.detectNetwork(),
+      {
+        name: "Error",
+        message: /(Provider not set or invalid)/
+      },
+      "should have thrown!"
+    );
+  });
+});
+
+describe("TruffleContract.detectNetwork()", () => {
+  it("throws when network not set and provider not present", () => {
+    const freshTruffleContract = TruffleContract();
+    assert.rejects(
+      async () => await freshTruffleContract.detectNetwork(),
+      {
+        name: "Error",
+        message: /(Provider not set or invalid)/
+      },
+      "should have thrown!"
+    );
+  });
+
+  it("throws when network set and provider not present", async () => {
+    const freshTruffleContract = TruffleContract();
+    freshTruffleContract.network_id = 1234;
+    freshTruffleContract.networks[freshTruffleContract.network_id] =
+      "dummyNetwork";
+    assert.rejects(
+      async () => await freshTruffleContract.detectNetwork(),
+      {
+        name: "Error",
+        message: /(Provider not set or invalid)/
+      },
+      "should have thrown!"
+    );
+  });
+});
+
+describe("TruffleContract.setNetwork()", () => {
+  it("returns w/o setting network_id when passed falsy network_id", () => {
+    const freshTruffleContract = TruffleContract();
+    assert.strictEqual(freshTruffleContract.network_id, undefined);
+    freshTruffleContract.setNetwork(null);
+    assert.strictEqual(freshTruffleContract.network_id, undefined);
+  });
+});
+
+describe("TruffleContract.setNetworkType()", () => {
+  it("sets network_type on Web3Shim", () => {
+    const freshTruffleContract = TruffleContract();
+    // default Web3Shim networkType
+    assert.strictEqual(freshTruffleContract.web3.networkType, "ethereum");
+    freshTruffleContract.setNetworkType("quorum");
+    assert.strictEqual(freshTruffleContract.web3.networkType, "quorum");
+  });
+});
+
+describe("TruffleContract.setWallet()", () => {
+  it("sets wallet on Web3Shim", () => {
+    const freshTruffleContract = TruffleContract();
+    const mockWalletObj = {};
+    assert(freshTruffleContract.web3.eth.accounts.wallet);
+    freshTruffleContract.setWallet(mockWalletObj);
+    assert.deepStrictEqual(
+      freshTruffleContract.web3.eth.accounts.wallet,
+      mockWalletObj
+    );
+  });
+});
+
+describe("TruffleContract.resetAddress()", () => {
+  it("resets deployed contract instance address on current network to undefined", () => {
+    const freshTruffleContract = TruffleContract();
+    freshTruffleContract.network_id = 1234;
+    freshTruffleContract.networks[freshTruffleContract.network_id] = {
+      address: "0x1234"
+    };
+    assert.strictEqual(freshTruffleContract.network.address, "0x1234");
+    freshTruffleContract.resetAddress();
+    assert.strictEqual(freshTruffleContract.network.address, undefined);
+  });
+});
+
+describe("TruffleContract.link()", () => {
+  it("throws if passed an undeployed instance", () => {
+    const freshTruffleContract = TruffleContract();
+    const mockTruffleContract = TruffleContract();
+    assert.throws(() => {
+      freshTruffleContract.link(mockTruffleContract),
+        {
+          name: "Error",
+          message: /Cannot link contract without an address/
+        },
+        "should have thrown!";
+    });
+  });
+
+  it("links an object collection of library contracts", () => {
+    const freshTruffleContract = TruffleContract();
+    freshTruffleContract.network_id = 1234;
+    freshTruffleContract.networks[freshTruffleContract.network_id] = {
+      address: "0x1234"
+    };
+    const mockLibraryObj = { Library1: "0x4321", Library2: "0x4567" };
+    freshTruffleContract.link(mockLibraryObj);
+    assert.deepStrictEqual(freshTruffleContract.links, mockLibraryObj);
+  });
+});
+
+describe("TruffleContract.clone()", () => {
+  it("when passed a non-object, clones an instance and uses passed value as the network_id", () => {
+    const freshTruffleContract = TruffleContract();
+    const newNetworkID = "1234";
+    const clonedTruffleContract = freshTruffleContract.clone(newNetworkID);
+    assert.strictEqual(clonedTruffleContract.network_id, newNetworkID);
+  });
+});
