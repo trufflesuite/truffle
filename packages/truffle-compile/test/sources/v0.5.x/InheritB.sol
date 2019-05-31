bd0b2db2c2a282276dec920245cb80f99d392146
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-31 16:20:14 -0700

    add test sources for native solcjs parsing test

---
+++ packages/truffle-compile/test/sources/v0.5.x/InheritB.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+// These are out of alphabetic order
+// Solc will alphabetize them, we should restore source-order.
+contract InheritB {
+  event LogD();
+  event LogC();
+  constructor() public {}
+}
