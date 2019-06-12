18b21ed7e322f48095daecec51bf48879a0ca37f
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-06-12 16:06:58 -0700

    pull out merging/assigning into utils.js

--- packages/truffle-artifactor/utils.js
+++ packages/truffle-artifactor/utils.js
@@ -1,4 +1,5 @@
 const fse = require("fs-extra");
+const _ = require("lodash");
 
 const writeArtifact = (_completeArtifact, _outputPath) => {
   _completeArtifact.updatedAt = new Date().toISOString();
@@ -9,4 +10,22 @@ const writeArtifact = (_completeArtifact, _outputPath) => {
   );
 };
 
-module.exports = { writeArtifact };
+const finalizeArtifact = (
+  _normalizedExistingArtifact,
+  _normalizedNewArtifact
+) => {
+  const _knownNetworks = _.merge(
+    {},
+    _normalizedExistingArtifact.networks,
+    _normalizedNewArtifact.networks
+  );
+  const _completeArtifact = _.assign(
+    {},
+    _normalizedExistingArtifact,
+    _normalizedNewArtifact,
+    { networks: _knownNetworks }
+  );
+  return _completeArtifact;
+};
+
+module.exports = { writeArtifact, finalizeArtifact };
