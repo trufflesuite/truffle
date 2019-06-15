26b9cea72f4255c2419742d7f5af39a88d57e016
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-06-14 22:19:29 -0700

    rm underscores

--- packages/truffle-artifactor/utils.js
+++ packages/truffle-artifactor/utils.js
@@ -1,31 +1,31 @@
 const fse = require("fs-extra");
 const _ = require("lodash");
 
-const writeArtifact = (_completeArtifact, _outputPath) => {
-  _completeArtifact.updatedAt = new Date().toISOString();
+const writeArtifact = (completeArtifact, outputPath) => {
+  completeArtifact.updatedAt = new Date().toISOString();
   fse.writeFileSync(
-    _outputPath,
-    JSON.stringify(_completeArtifact, null, 2),
+    outputPath,
+    JSON.stringify(completeArtifact, null, 2),
     "utf8"
   );
 };
 
 const finalizeArtifact = (
-  _normalizedExistingArtifact,
-  _normalizedNewArtifact
+  normalizedExistingArtifact,
+  normalizedNewArtifact
 ) => {
-  const _knownNetworks = _.merge(
+  const knownNetworks = _.merge(
     {},
-    _normalizedExistingArtifact.networks,
-    _normalizedNewArtifact.networks
+    normalizedExistingArtifact.networks,
+    normalizedNewArtifact.networks
   );
-  const _completeArtifact = _.assign(
+  const completeArtifact = _.assign(
     {},
-    _normalizedExistingArtifact,
-    _normalizedNewArtifact,
-    { networks: _knownNetworks }
+    normalizedExistingArtifact,
+    normalizedNewArtifact,
+    { networks: knownNetworks }
   );
-  return _completeArtifact;
+  return completeArtifact;
 };
 
 module.exports = { writeArtifact, finalizeArtifact };
