b1d6c61ea77931738a1b38e20ae2b6e6c490eadb
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-06-18 11:56:49 -0700

    es6 utils/unbox.js

--- packages/truffle-box/lib/utils/unbox.js
+++ packages/truffle-box/lib/utils/unbox.js
@@ -10,37 +10,33 @@ const inquirer = require("inquirer");
 function verifyURL(url) {
   // Next let's see if the expected repository exists. If it doesn't, ghdownload
   // will fail spectacularly in a way we can't catch, so we have to do it ourselves.
-  return new Promise(function(accept, reject) {
-    var configURL = parseURL(
-      vcsurl(url)
+  return new Promise((accept, reject) => {
+    const configURL = parseURL(
+      `${vcsurl(url)
         .replace("github.com", "raw.githubusercontent.com")
-        .replace(/#.*/, "") + "/master/truffle-box.json"
+        .replace(/#.*/, "")}/master/truffle-box.json`
     );
 
-    var options = {
+    const options = {
       method: "HEAD",
-      uri: "https://" + configURL.host + configURL.path
+      uri: `https://${configURL.host}${configURL.path}`
     };
-    request(options, function(error, r) {
+    request(options, (error, { statusCode }) => {
       if (error) {
         return reject(
           new Error(
-            "Error making request to " +
-              options.uri +
-              ". Got error: " +
-              error.message +
-              ". Please check the format of the requested resource."
+            `Error making request to ${options.uri}. Got error: ${
+              error.message
+            }. Please check the format of the requested resource.`
           )
         );
-      } else if (r.statusCode === 404) {
+      } else if (statusCode === 404) {
         return reject(
           new Error(
-            "Truffle Box at URL " +
-              url +
-              " doesn't exist. If you believe this is an error, please contact Truffle support."
+            `Truffle Box at URL ${url} doesn't exist. If you believe this is an error, please contact Truffle support.`
           )
         );
-      } else if (r.statusCode !== 200) {
+      } else if (statusCode !== 200) {
         return reject(
           new Error(
             "Error connecting to github.com. Please check your internet connection and try again."
@@ -53,20 +49,20 @@ function verifyURL(url) {
 }
 
 function fetchRepository(url, dir) {
-  return new Promise(function(accept, reject) {
+  return new Promise((accept, reject) => {
     // Download the package from github.
     ghdownload(url, dir)
-      .on("err", function(err) {
+      .on("err", err => {
         reject(err);
       })
-      .on("end", function() {
+      .on("end", () => {
         accept();
       });
   });
 }
 
-function prepareToCopyFiles(tempDir, boxConfig) {
-  const needingRemoval = boxConfig.ignore || [];
+function prepareToCopyFiles(tempDir, { ignore }) {
+  const needingRemoval = ignore || [];
 
   // remove box config file
   needingRemoval.push("truffle-box.json");
@@ -101,8 +97,8 @@ async function promptOverwrites(contentCollisions, logger = console) {
       }
     ];
 
-    await inquirer.prompt(overwriting).then(answer => {
-      if (answer.overwrite) {
+    await inquirer.prompt(overwriting).then(({ overwrite }) => {
+      if (overwrite) {
         fs.removeSync(file);
         overwriteContents.push(file);
       }
@@ -139,15 +135,15 @@ async function copyTempIntoDestination(tmpDir, destination, options) {
   }
 }
 
-function installBoxDependencies(boxConfig, destination) {
-  var postUnpack = boxConfig.hooks["post-unpack"];
+function installBoxDependencies({ hooks }, destination) {
+  const postUnpack = hooks["post-unpack"];
 
-  return new Promise(function(accept, reject) {
+  return new Promise((accept, reject) => {
     if (postUnpack.length === 0) {
       return accept();
     }
 
-    exec(postUnpack, { cwd: destination }, function(err, stdout, stderr) {
+    exec(postUnpack, { cwd: destination }, (err, stdout, stderr) => {
       if (err) return reject(err);
       accept(stdout, stderr);
     });
