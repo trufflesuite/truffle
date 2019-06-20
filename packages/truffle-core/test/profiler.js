6d239a7e359386f194620ff8e3f98e21d77431a9
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-06-20 14:24:25 -0700

    update truffle-core profiler test for updated sandbox method (test/profiler.js)

--- packages/truffle-core/test/profiler.js
+++ packages/truffle-core/test/profiler.js
@@ -8,39 +8,38 @@ var Artifactor = require("truffle-artifactor");
 
 // TOOD: Move this to truffle-compile!
 
-describe('profiler', function() {
+describe("profiler", function() {
   var config;
 
-  before("Create a sandbox", function(done) {
+  before("Create a sandbox", async () => {
     this.timeout(10000);
-    Box.sandbox(function(err, result) {
-      if (err) return done(err);
-      config = result;
-      config.resolver = new Resolver(config);
-      config.artifactor = new Artifactor(config.contracts_build_directory);
-      config.network = "development";
-      done();
-    });
+    config = await Box.sandbox("default");
+    config.resolver = new Resolver(config);
+    config.artifactor = new Artifactor(config.contracts_build_directory);
+    config.network = "development";
   });
 
-  after("Cleanup tmp files", function(done){
-    glob('tmp-*', (err, files) => {
-      if(err) done(err);
+  after("Cleanup tmp files", function(done) {
+    glob("tmp-*", (err, files) => {
+      if (err) done(err);
       files.forEach(file => fs.removeSync(file));
       done();
     });
   });
 
-  it('profiles example project successfully', function(done) {
-    Profiler.required_sources(config.with({
-      paths: ["./ConvertLib.sol"],
-      base_path: config.contracts_directory
-    }), function(err, allSources, compilationTargets) {
-      if (err) return done(err);
+  it("profiles example project successfully", function(done) {
+    Profiler.required_sources(
+      config.with({
+        paths: ["./ConvertLib.sol"],
+        base_path: config.contracts_directory
+      }),
+      function(err, allSources, compilationTargets) {
+        if (err) return done(err);
 
-      assert.equal(Object.keys(allSources).length, 3);
-      assert.equal(compilationTargets.length, 2);
-      done();
-    });
+        assert.equal(Object.keys(allSources).length, 3);
+        assert.equal(compilationTargets.length, 2);
+        done();
+      }
+    );
   });
 });
