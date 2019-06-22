dffa5ac55903ab975c70ba2bcdcbb70457233a85
skywing <skywing@users.noreply.github.com>
2019-06-22 17:26:50 -0400

    back to original

--- packages/truffle-core/lib/command.js
+++ packages/truffle-core/lib/command.js
@@ -100,45 +100,14 @@ class Command {
       }
     });
 
-    try {
-      const inputOptions = [];
-      inputStrings.map(i => {
-        if (i.startsWith("--")) {
-          inputOptions.push(i);
-        }
-      });
+    const newOptions = Object.assign({}, clone, argv);
 
-      const validOptions = [];
-      result.command.help.options.map(item => {
-        let opt = item.option.split(" ")[0];
-        if (opt.startsWith("--")) {
-          validOptions.push(opt);
-        }
-      });
-
-      let notValidOptions = inputOptions.filter(
-        opt => !validOptions.includes(opt)
-      );
-
-      if (notValidOptions.length > 0) {
-        return callback(
-          new TaskError(
-            "Unsupported (Undocumented) command line option: " + notValidOptions
-          )
-        );
-      }
-
-      const newOptions = Object.assign({}, clone, argv);
-
-      result.command.run(newOptions, callback);
-      analytics.send({
-        command: result.name ? result.name : "other",
-        args: result.argv._,
-        version: bundled || "(unbundled) " + core
-      });
-    } catch (err) {
-      callback(err);
-    }
+    result.command.run(newOptions, callback);
+    analytics.send({
+      command: result.name ? result.name : "other",
+      args: result.argv._,
+      version: bundled || "(unbundled) " + core
+    });
   }
 
   displayGeneralHelp() {
