4ec96b61c1b5ac38257d9a71d98221b8bc6fd9aa
eggplantzzz <tmfeickert@gmail.com>
2019-06-28 12:58:25 -0400

    Remove .js from profiler require statements

--- packages/truffle-core/test/profiler.js
+++ packages/truffle-core/test/profiler.js
@@ -2,13 +2,13 @@ var assert = require("chai").assert;
 var fs = require("fs-extra");
 var glob = require("glob");
 var Box = require("truffle-box");
-var Profiler = require("truffle-compile/profiler.js");
+var Profiler = require("truffle-compile/profiler");
 var Resolver = require("truffle-resolver");
 var Artifactor = require("truffle-artifactor");
 
 // TOOD: Move this to truffle-compile!
 
-describe('profiler', function() {
+describe("profiler", function() {
   var config;
 
   before("Create a sandbox", function(done) {
@@ -23,24 +23,27 @@ describe('profiler', function() {
     });
   });
 
-  after("Cleanup tmp files", function(done){
-    glob('tmp-*', (err, files) => {
-      if(err) done(err);
+  after("Cleanup tmp files", function(done) {
+    glob("tmp-*", (err, files) => {
+      if (err) done(err);
       files.forEach(file => fs.removeSync(file));
       done();
     });
   });
 
-  it('profiles example project successfully', function(done) {
-    Profiler.required_sources(config.with({
-      paths: ["./ConvertLib.sol"],
-      base_path: config.contracts_directory
-    }), function(err, allSources, compilationTargets) {
-      if (err) return done(err);
+  it("profiles example project successfully", function(done) {
+    Profiler.required_sources(
+      config.with({
+        paths: ["./ConvertLib.sol"],
+        base_path: config.contracts_directory
+      }),
+      function(err, allSources, compilationTargets) {
+        if (err) return done(err);
 
-      assert.equal(Object.keys(allSources).length, 3);
-      assert.equal(compilationTargets.length, 2);
-      done();
-    });
+        assert.equal(Object.keys(allSources).length, 3);
+        assert.equal(compilationTargets.length, 2);
+        done();
+      }
+    );
   });
 });
