cec3b7912a6e4137e0be9018f7ef93019137c090
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-07-05 11:20:37 -0700

    keep getImports sync (profiler/getImports)

--- packages/truffle-compile/profiler/getImports.js
+++ packages/truffle-compile/profiler/getImports.js
@@ -2,14 +2,14 @@ const { isExplicitlyRelative } = require("./isExplicitlyRelative");
 const Parser = require("../parser");
 const path = require("path");
 
-const getImports = async (file, { body, source }, solc, parserSolc) => {
+const getImports = (file, { body, source }, solc, parserSolc) => {
   let imports;
 
   // No imports in vyper!
   if (path.extname(file) === ".vy") return [];
 
-  if (parserSolc) imports = await Parser.parseImports(body, parserSolc);
-  else imports = await Parser.parseImports(body, solc);
+  if (parserSolc) imports = Parser.parseImports(body, parserSolc);
+  else imports = Parser.parseImports(body, solc);
 
   // Convert explicitly relative dependencies of modules back into module paths.
   return imports.map(
