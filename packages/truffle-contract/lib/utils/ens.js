ca82bc79e54d01b08c52fc99b5daae6358fb680f
eggplantzzz <tmfeickert@gmail.com>
2019-07-05 10:09:05 -0400

    Add extra check in ens module

--- packages/truffle-contract/lib/utils/ens.js
+++ packages/truffle-contract/lib/utils/ens.js
@@ -15,7 +15,9 @@ module.exports = {
     const ensjs = this.getNewENSJS(web3.currentProvider);
 
     const convertedNames = inputArgs.map((argument, index) => {
-      if (methodABI.inputs[index].type === "address") {
+      if (index + 1 >= methodABI.inputs.length) {
+        return argument;
+      } else if (methodABI.inputs[index].type === "address") {
         // Check all address arguments for ENS names
         const argIsAddress = isAddress(argument);
         if (argIsAddress) return argument;
