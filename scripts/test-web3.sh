3ff5a6f6e491b0843fe0d4a8c4fd01330b0bc360
g. nicholas d'andrea <nick@gnidan.org>
2019-07-12 11:48:57 -0400

    Add link script for testing web3

---
+++ scripts/test-web3.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Exit script as soon as a command fails.
+set -o errexit
+
+link_web3() {
+  git clone git@github.com:ethereum/web3.js.git ../web3.js
+  pushd ../web3.js
+  git checkout release/1.0
+  npm install
+  npm run build
+  pushd packages/web3
+  yarn link
+
+  popd
+  popd
+
+  for $pkg in packages/*
+  do
+    cd $pkg
+    yarn link web3
+    cd ..
+  done
+}
+
+link_web3
