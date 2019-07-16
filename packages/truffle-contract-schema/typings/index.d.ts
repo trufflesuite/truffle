f1d12761c224d32e580da8913cd4b70b15f5772f
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-07-16 11:14:41 -0700

    use ContractObject from spec & export (truffle-contract-schema/typings)

--- packages/truffle-contract-schema/typings/index.d.ts
+++ packages/truffle-contract-schema/typings/index.d.ts
@@ -1,7 +1,10 @@
 declare module "truffle-contract-schema" {
-  import Contract from "truffle-contract";
+  import { ContractObject } from "truffle-contract-schema/spec";
+
   namespace Schema {
-    export function normalize(dirtyObj: object, opts?: object): Contract.TruffleContract;
+    export function normalize(dirtyObj: object, opts?: object): ContractObject;
   }
+
+  export type ContractObject = ContractObject;
   export default Schema;
 }
