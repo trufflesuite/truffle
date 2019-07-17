e208428ee799fb51b02c6a84b0a06a04062a704f
Jaehoon Shim <jshim@lgcns.com>
2019-07-17 13:44:06 +0900

    changed to adding extra option '--create-dir'

--- packages/truffle-core/lib/commands/init.js
+++ packages/truffle-core/lib/commands/init.js
@@ -3,7 +3,7 @@ var command = {
   description: "Initialize new and empty Ethereum project",
   builder: {},
   help: {
-    usage: "truffle init [--force]",
+    usage: "truffle init [--force] [--create-dir <name>]",
     options: [
       {
         option: "--force",
@@ -11,6 +11,11 @@ var command = {
           "Initialize project in the current directory regardless of its " +
           "state. Be careful, this\n                    will potentially overwrite files " +
           "that exist in the directory."
+      },
+      {
+        option: "--create-dir <anem>",
+        description:
+          "Specify the directory name to create a directory and initialize within."
       }
     ]
   },
@@ -18,6 +23,7 @@ var command = {
     var Config = require("truffle-config");
     var OS = require("os");
     var UnboxCommand = require("./unbox");
+    var fs = require("fs");
 
     var config = Config.default().with({
       logger: console
@@ -34,6 +40,22 @@ var command = {
           " - Or, browse the Truffle Boxes at <http://truffleframework.com/boxes>!"
       );
       process.exit(1);
+    }
+
+    if (options["create-dir"] && options["create-dir"].length > 0) {
+      try {
+        const path = options["create-dir"];
+        fs.mkdirSync(path, { recursive: true });
+        process.chdir(path);
+        config.logger.log();
+        config.logger.log(
+          "Creating a new empty Truffle box in \x1b[32m%s\x1b[0m.",
+          path
+        );
+      } catch (err) {
+        config.logger.log(err);
+        process.exit(1);
+      }
     }
 
     // defer to `truffle unbox` command with "bare" box as arg
