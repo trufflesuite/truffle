6f6123566b6f65b09d08b36cdfb17a05e5b03665
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2019-07-27 22:10:21 -0400

    Add support for debugging deploys
    - Reformat printing a bit to handle lack of abi name

--- packages/truffle-core/lib/debug/mocha.js
+++ packages/truffle-core/lib/debug/mocha.js
@@ -3,7 +3,6 @@ const util = require("util");
 
 const { CLIDebugger } = require("./cli");
 const execute = require("truffle-contract/lib/execute");
-const { DebugPrinter } = require("./printer");
 
 class CLIDebugHook {
   constructor(config, compilation, runner) {
@@ -18,17 +17,9 @@ class CLIDebugHook {
     // tests that take a long time _after_ debug break just won't timeout
     this.disableTimeout();
 
-    const {
-      txHash,
-      result,
-      method: {
-        abi: { name: methodName },
-        args,
-        contract: { contractName }
-      }
-    } = await this.invoke(operation);
+    const { txHash, result, method } = await this.invoke(operation);
 
-    this.printStartTestHook({ contractName, methodName, args });
+    this.printStartTestHook(method);
 
     const interpreter = await new CLIDebugger(this.config, {
       compilation: this.compilation
@@ -51,18 +42,29 @@ class CLIDebugHook {
     const { action } = method;
 
     switch (action) {
+      case "deploy": {
+        const result = await operation;
+
+        return {
+          txHash: result.transactionHash, // different name; who knew
+          method,
+          result
+        };
+      }
       case "send": {
         const result = await operation;
 
         return { txHash: result.tx, method, result };
       }
       case "call": {
+        // replays as send
         const { contract, fn, abi, args, address } = method;
 
         // get the result of the call
         const result = await operation;
 
         // and replay it as a transaction so we can debug
+        // bit of a HACK: properly making a call act like a tx requires forking
         const { tx: txHash } = await execute.send.call(
           contract,
           fn,
@@ -80,7 +82,7 @@ class CLIDebugHook {
 
   detectMethod(promiEvent) {
     return new Promise(accept => {
-      for (let action of ["send", "call"]) {
+      for (let action of ["send", "call", "deploy"]) {
         promiEvent.on(
           `execute:${action}:method`,
           ({ fn, abi, args, contract, address }) => {
@@ -98,11 +100,27 @@ class CLIDebugHook {
     });
   }
 
-  printStartTestHook({ contractName, methodName, args }) {
-    const formatOperation = (contractName, methodName, args) =>
-      colors.bold(
-        `${contractName}.${methodName}(${args.map(util.inspect).join(", ")})`
-      );
+  printStartTestHook(method) {
+    const formatOperation = ({
+      action,
+      contract: { contractName },
+      abi,
+      args
+    }) => {
+      switch (action) {
+        case "deploy": {
+          return colors.bold(
+            `${contractName}.new(${args.map(util.inspect).join(", ")})`
+          );
+        }
+        case "call":
+        case "send": {
+          return colors.bold(
+            `${contractName}.${abi.name}(${args.map(util.inspect).join(", ")})`
+          );
+        }
+      }
+    };
 
     this.config.logger.log("");
     this.config.logger.log("  ...");
@@ -112,9 +130,7 @@ class CLIDebugHook {
       )
     );
     this.config.logger.log("  Test run interrupted.");
-    this.config.logger.log(
-      `  Debugging ${formatOperation(contractName, methodName, args)}`
-    );
+    this.config.logger.log(`  Debugging ${formatOperation(method)}`);
     this.config.logger.log(
       colors.cyan(
         ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
