74e0d032075c2b541f804c9ad9dd4f98ac5fb1aa
Tim Coulter <tim@timothyjcoulter.com>
2019-08-12 13:13:58 -0700

    Add browser compiler supplier (browser: true)
    Add browser loading strategy that uses VersionRange to download truffle -- but instead of caching it on disk, it caches it in a script tag. More work can be done here to support all downloaded versions. Currently this supplier will only support the first ersion downloaded, and then no others for the rest of the session.

--- packages/truffle-compile/compilerSupplier/loadingStrategies/Browser.js
+++ packages/truffle-compile/compilerSupplier/loadingStrategies/Browser.js
@@ -1,15 +1,60 @@
-const LoadingStrategy = require("./LoadingStrategy");
+const VersionRange = require("./VersionRange");
 const solcWrap = require("solc/wrapper");
 
-class Browser extends LoadingStrategy {
-  load() {
+const COMPILER_SLUG = "Module";
+
+// Note: This code should only be run in browser context.
+// This will suffer from only loading the first compiler downloaded
+// because we have to add a script tag. Why? Ask Solidity.
+// TODO: Attempt to store newly loaded Module variable in memory
+// and allow loading of other versions.
+class Browser extends VersionRange {
+  //// Overridden functions
+
+  addFileToCache(code) {
+    this.addScriptTagWithCompilerCode(code);
+  }
+
+  fileIsCached() {
+    return this.hasCompilerBeenLoaded();
+  }
+
+  compilerFromString(code) {
+    // If a script tag exists, use that as the source of truth.
+    if (!this.hasCompilerBeenLoaded()) {
+      this.addScriptTagWithCompilerCode(code);
+    }
+
     return this.getBrowserSolc();
   }
 
+  getCachedSolcByFileName() {
+    return this.getBrowserSolc();
+  }
+
+  //// New functions
+
+  hasCompilerBeenLoaded() {
+    return window[COMPILER_SLUG] != null;
+  }
+
+  addScriptTagWithCompilerCode(code) {
+    if (typeof document == "undefined") {
+      throw new Error(
+        "No document object. Are you sure you're running in the browser?"
+      );
+    }
+
+    var s = document.createElement("script");
+    s.innerHTML = code;
+    document.body.appendChild(s);
+  }
+
   getBrowserSolc() {
+    const soljson = window[COMPILER_SLUG];
+    const wrapped = solcWrap(soljson);
     this.removeListener();
-    // Gross I know. See here: https://github.com/ethereum/solc-js/issues/31
-    return solcWrap(window.Module);
+    return wrapped;
   }
 }
 
