789c3d7d635ffa2f1f78498a1971e4ef28ff44c8
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-08-13 22:02:45 -0700

    grep only @quorum tests for quorum job (truffle/scripts/test.sh)

--- packages/truffle/scripts/test.sh
+++ packages/truffle/scripts/test.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 if [ "$GETH" == true ]; then
   mocha --timeout 50000 --grep '@ganache|@standalone' --invert --colors $@
+elif [ "$QUORUM" == true ]; then
+  mocha --timeout 50000 --grep @quorum --colors $@
 elif [ "$COVERAGE" == true ]; then
   NO_BUILD=true mocha --no-warnings --timeout 7000 --grep @geth --invert --colors $@
 elif [ "$INTEGRATION" == true ]; then
