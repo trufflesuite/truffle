7f1623b8f5a20c5fe1374a003eb7671455a06a05
eggplantzzz <tmfeickert@gmail.com>
2019-08-23 15:43:52 -0400

    Rename truffle-reporters to @truffle/reporters

--- packages/truffle-deployer/test/errors.js
+++ packages/truffle-deployer/test/errors.js
@@ -1,7 +1,7 @@
 const ganache = require("ganache-core");
 const Web3 = require("web3");
 const assert = require("assert");
-const Reporter = require("truffle-reporters").migrationsV5;
+const Reporter = require("@truffle/reporters").migrationsV5;
 const EventEmitter = require("events");
 
 const Deployer = require("../index");
