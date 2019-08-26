895f1d6723ede80f03874cba3dcc62f54fb59eaf
eggplantzzz <tmfeickert@gmail.com>
2019-08-26 16:24:23 -0400

    Rename truffle-workflow-compile to @truffle/workflow-compile

--- packages/truffle-core/lib/commands/exec.js
+++ packages/truffle-core/lib/commands/exec.js
@@ -37,7 +37,7 @@ const command = {
   },
   run: function(options, done) {
     const Config = require("truffle-config");
-    const Contracts = require("truffle-workflow-compile");
+    const Contracts = require("@truffle/workflow-compile");
     const ConfigurationError = require("../errors/configurationerror");
     const Require = require("truffle-require");
     const { Environment } = require("truffle-environment");
