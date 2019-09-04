d378c0283f995e69fac22199ad079997a500dfa1
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-09-04 15:42:03 -0700

    readjust tezos getID overload again (lib/tezos-overloads)

--- packages/truffle-interface-adapter/lib/tezos-overloads.ts
+++ packages/truffle-interface-adapter/lib/tezos-overloads.ts
@@ -11,22 +11,28 @@ export const TezosDefinition = {
 }
 
 const overrides = {
-// The ts-ignores are ignoring the checks that are
-// saying that web3.eth.net.getId is a function and doesn't
-// have a `method` property, which it does
-  "getId": (web3: Web3Shim) => {
-  // @ts-ignore
-  const _oldgetId = web3.eth.net.getId;
-  // @ts-ignore
-  web3.eth.net.getId = async() => {
-    // chaincode-fabric-evm currently returns a "fabric-evm" string
-    // instead of a hex networkID. Instead of trying to decode the hexToNumber,
-    // let's just accept `fabric-evm` as a valid networkID for now.
+  // The ts-ignores are ignoring the checks that are
+  // saying that web3.eth.net.getId is a function and doesn't
+  // have a `method` property, which it does
+  getId: (web3: Web3Shim) => {
     // @ts-ignore
-    const currentHost = web3.currentProvider.host
-    const parsedHost = currentHost.match(/(^https?:\/\/)(.*?)\:\d.*/)[2];
+    const _oldGetId = web3.eth.net.getId;
     // @ts-ignore
-    await eztz.node.setProvider(parsedHost)
+    web3.eth.net.getId = async () => {
+      // chaincode-fabric-evm currently returns a "fabric-evm" string
+      // instead of a hex networkID. Instead of trying to decode the hexToNumber,
+      // let's just accept `fabric-evm` as a valid networkID for now.
+      // @ts-ignore
+      const currentHost = web3.currentProvider.host;
+      const parsedHost = currentHost.match(/(^https?:\/\/)(.*?)\:\d.*/)[2];
+      // @ts-ignore
+      await eztz.node.setProvider(parsedHost);
+      // @ts-ignore
+      const { chain_id } = await eztz.rpc.getHead();
+      // @ts-ignore
+      return chain_id;
+    };
+  },
     // @ts-ignore
     const { chain_id } = await eztz.rpc.getHead()
     // @ts-ignore
