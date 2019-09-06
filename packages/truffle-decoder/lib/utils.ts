cb31e949224b0a65ac1b16b429421414eb97d752
Harry Altman <harry.j.altman@gmail.com>
2019-09-06 17:03:57 -0400

    Better attempt to detect libraries

--- packages/truffle-decoder/lib/utils.ts
+++ packages/truffle-decoder/lib/utils.ts
@@ -1,4 +1,4 @@
-import { AstDefinition, AstReferences, AbiUtils, Contexts as ContextsUtils } from "truffle-codec-utils";
+import { AstDefinition, AstReferences, ContractKind, AbiUtils, Contexts as ContextsUtils, EVM as EVMUtils } from "truffle-codec-utils";
 import { ContractObject } from "truffle-contract-schema/spec";
 import * as CodecUtils from "truffle-codec-utils";
 
@@ -24,11 +24,37 @@ export function makeContext(contract: ContractObject, node: AstDefinition, isCon
     contractName: contract.contractName,
     binary,
     contractId: node ? node.id : undefined,
-    contractKind: node ? node.contractKind : "contract", //assume contracts are ordinary contracts unless told otherwise
+    contractKind: contractKind(contract, node),
     isConstructor,
     abi: AbiUtils.computeSelectors(abi),
     payable: AbiUtils.abiHasPayableFallback(abi),
     hasFallback: AbiUtils.abiHasFallback(abi),
     compiler: contract.compiler
   };
+}
+
+//attempts to determine if the given contract is a library or not
+function contractKind(contract: ContractObject, node?: AstDefinition): ContractKind {
+  //first: if we have a node, use its listed contract kind
+  if(node) {
+    return node.contractKind;
+  }
+  //next: check the contract kind field on the contract object itself, if it exists.
+  //however this isn't implemented yet so we'll skip it.
+  //next: if we have no direct info on the contract kind, but we do
+  //have the deployed bytecode, we'll use a HACK:
+  //we'll assume it's an ordinary contract, UNLESS its deployed bytecode begins with
+  //PUSH20 followed by 20 0s, in which case we'll assume it's a library
+  //(note: this will fail to detect libraries from before Solidity 0.4.20)
+  if(contract.deployedBytecode) {
+    const pushAddressInstruction = (
+      0x60 +
+      EVMUtils.ADDRESS_SIZE -
+      1
+    ).toString(16); //"73"
+    const libraryString = "0x" + pushAddressInstruction + "00".repeat(EVMUtils.ADDRESS_SIZE);
+    return contract.deployedBytecode.startsWith(libraryString) ? "library" : "contract";
+  }
+  //finally, in the absence of anything to go on, we'll assume it's an ordinary contract
+  return "contract";
 }
