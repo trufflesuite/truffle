c4be45e972dc656e5e5013493fd297b34b6ad849
Mike Seese <seesemichaelj@gmail.com>
2019-09-12 12:56:17 -0700

    add optional lerna version bump command

--- scripts/publish-dist-tag.sh
+++ scripts/publish-dist-tag.sh
@@ -4,6 +4,13 @@
 set -ex
 
 distTag=$1 # first arg after yarn publish-dist-tag
+
+# Optional `lerna version` 'bump' command
+# must be an explicit version string _or_ one of:
+#   'major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', or 'prerelease'.
+# Omitting will prompt the user
+lernaVersionCommand=$2
+
 currentGitBranch=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "${distTag}" == "" ];
@@ -23,7 +30,7 @@ fi
 node ./scripts/npm-access.js
 
 ## Bump package versions and commit
-lerna version --no-git-tag-version --preid $distTag
+lerna version --no-git-tag-version --preid $distTag $lernaVersionCommand
 git add packages/*/package.json
 git commit -m "Publish truffle@${distTag}"
 
