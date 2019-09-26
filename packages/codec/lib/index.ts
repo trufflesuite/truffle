2be5ba8221a0a209a98e20b5cb2569deeb40750a
Harry Altman <harry.j.altman@gmail.com>
2019-09-26 17:49:12 -0400

    Avoid indiscriminately exporting all types :P

--- packages/codec/lib/index.ts
+++ packages/codec/lib/index.ts
@@ -1,12 +1,13 @@
 //So, what shall codec export...?
 
-//First: Export all the types! (Maybe we should be more selective here? oh well)
-export * from "./types";
-export { Types, Values, Errors } from "./format";
+//First: export the data format
+export * from "./format";
+
 //next: export all the utils!
 //you can't do "export * as Name" for whatever reason so...
 import * as Utils from "./utils";
-export { Utils } ;
+export { Utils };
+
 //next: export the interface, duh
 export * from "./interface";
 
@@ -26,3 +27,19 @@ export { readStack } from "./read/stack";
 export { encodeAbi, encodeTupleAbi } from "./encode/abi";
 export { encodeMappingKey } from "./encode/key";
 //(actually we use at least one of these in tests atm so we'd better export!)
+
+//now: what types should we export? (other than the ones from ./format)
+//public-facing types for the interface
+export { ContractState, DecodedVariable, DecodedTransaction, DecodedLog, EventOptions } from "./types/interface";
+export * from "./types/decoding"; //all the decoding result types
+export * from "./types/errors"; //the various errors we might throw
+
+//for those who want more low-level stuff...
+export { AstDefinition, AstReferences } from "./types/ast";
+export { DataPointer } from "./types/pointer";
+export { EvmInfo } from "./types/evm";
+export { StorageAllocations, AbiAllocations, MemoryAllocations } from "./types/allocation";
+export { StorageLength } from "./types/storage";
+//and for those who want *all* the types...
+import * as InternalTypes from "./types";
+export { InternalTypes }; //warning: not organized!
