284f05e87d7fbc8d284ab3b3b13c8bf63c3dae92
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-09-27 11:56:02 -0700

    return a TezosContract when specifying tezos network (contract/index)

--- packages/contract/index.js
+++ packages/contract/index.js
@@ -1,6 +1,7 @@
 const Schema = require("@truffle/contract-schema");
 const Contract = require("./lib/contract");
 const truffleContractVersion = require("./package.json").version;
+const TezosContract = require("@truffle/tezos-contract");
 
 const contract = (json = {}, { networks, network: networkName }) => {
   const networkType = networks[networkName].type
@@ -8,7 +9,9 @@ const contract = (json = {}, { networks, network: networkName }) => {
     : "ethereum";
 
   // Note we don't use `new` here at all. This will cause the class to
-  // "mutate" instead of instantiate an instance.
+  // "mutate" instead of instantiate an instance
+  if (networkType === "tezos")
+    return TezosContract.clone(normalizedArtifactObject);
   return Contract.clone(normalizedArtifactObject);
 };
 
