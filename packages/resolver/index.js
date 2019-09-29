84b1dc6707e016a41f940dc5053831305c85d0a7
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-09-29 10:54:47 -0700

    refactor resolver to use this instead of self & pass options to TruffleContract (resolver/index)

--- packages/resolver/index.js
+++ packages/resolver/index.js
@@ -22,17 +22,15 @@ function Resolver(options) {
 
 // This function might be doing too much. If so, too bad (for now).
 Resolver.prototype.require = function(import_path, search_path) {
-  var self = this;
-
-  for (let i = 0; i < self.sources.length; i++) {
-    var source = self.sources[i];
-    var result = source.require(import_path, search_path);
+  let abstraction;
+  this.sources.forEach(source => {
+    const result = source.require(import_path, search_path);
     if (result) {
-      var abstraction = contract(result);
-      provision(abstraction, self.options);
-      return abstraction;
+      abstraction = contract(result, this.options);
+      provision(abstraction, this.options);
     }
-  }
+  });
+  if (abstraction) return abstraction;
   throw new Error(
     "Could not find artifacts for " + import_path + " from any sources"
   );
