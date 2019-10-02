1f1a8812acb1ba321fcbf9a7b00545a7dc0e7a3d
g. nicholas d'andrea <nick@gnidan.org>
2019-10-02 18:56:05 -0400

    Export only explicit interfaces from types

--- packages/codec/lib/index-typedoc.ts
+++ packages/codec/lib/index-typedoc.ts
@@ -12,6 +12,17 @@ import * as Format from "./format";
 export { Format };
 
 export * from "./interface";
-export * from "./types/interface";
-export * from "./types/decoding";
-export * from "./types/errors";
+export {
+  ContractState, DecodedVariable, DecodedTransaction, DecodedLog, EventOptions
+} from "./types/interface";
+
+export {
+  AbiArgument,
+  AnonymousDecoding,
+  ConstructorDecoding,
+  EventDecoding,
+  FunctionDecoding,
+  MessageDecoding,
+  UnknownDecoding
+} from "./types/decoding";
+// export * from "./types/errors";
