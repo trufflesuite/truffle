a22c830fdacbd0cf26c4d8c480511a0533d995e8
g. nicholas d'andrea <nick@gnidan.org>
2019-10-02 14:13:38 -0400

    Setup Typedoc
    - Use @gnd/typedoc for 3.6 compatibility and outstanding PR #1079
    - Use typedoc module naming plugin
    - Use regular typedoc for that plugin

---
+++ packages/codec/lib/index-typedoc.ts
@@ -0,0 +1,9 @@
+/**
+ * @module @truffle/codec
+ *//** */
+
+export * from "./format";
+export * from "./interface";
+export * from "./types/interface";
+export * from "./types/decoding";
+export * from "./types/errors";
