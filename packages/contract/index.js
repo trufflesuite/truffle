aba6e4d6b9692d8f2f02a2d9e35c62a2cd70ab83
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-03 14:17:14 -0700

    simplify contract() param input (contract/index)

--- packages/contract/index.js
+++ packages/contract/index.js
@@ -2,7 +2,7 @@ const Schema = require("@truffle/contract-schema");
 const Contract = require("./lib/contract");
 const truffleContractVersion = require("./package.json").version;
 
-const contract = (json = {}) => {
+const contract = (json = {}, networkType = "ethereum") => {
   const normalizedArtifactObject = Schema.normalize(json);
 
   // Note we don't use `new` here at all. This will cause the class to
