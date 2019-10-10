5c3694e00250ecaaa4e4f3809d3079f326e88272
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-10 09:14:14 -0700

    update test for current state of compile-ligo (compile-ligo/test/test_compiler)

--- packages/compile-ligo/test/test_compiler.js
+++ packages/compile-ligo/test/test_compiler.js
@@ -3,9 +3,7 @@ const assert = require("assert");
 const Config = require("@truffle/config");
 const compile = require("../index");
 
-describe("vyper compiler", function() {
-  this.timeout(10000);
-
+describe("ligo compiler", () => {
   const defaultSettings = {
     contracts_directory: path.join(__dirname, "./sources/"),
     quiet: true,
@@ -13,56 +11,53 @@ describe("vyper compiler", function() {
   };
   const config = new Config().merge(defaultSettings);
 
-  it("compiles vyper contracts", function(done) {
-    compile.all(config, function(err, contracts, paths) {
+  it("compiles ligo contracts", done => {
+    compile.all(config, (err, contracts, paths) => {
       assert.equal(err, null, "Compiles without error");
 
-      paths.forEach(function(path) {
+      paths.forEach(path => {
         assert(
-          [".vy", ".v.py", ".vyper.py"].some(
-            extension => path.indexOf(extension) !== -1
-          ),
-          "Paths have only vyper files"
+          [".ligo"].some(extension => path.indexOf(extension) !== -1),
+          "Paths should only be ligo files"
         );
       });
 
-      const hex_regex = /^[x0-9a-fA-F]+$/;
-
       [
-        contracts.VyperContract1,
-        contracts.VyperContract2,
-        contracts.VyperContract3
+        contracts.LigoContract1 //,
+        //        contracts.LigoContract2
       ].forEach((contract, index) => {
         assert.notEqual(
           contract,
           undefined,
-          `Compiled contracts have VyperContract${index + 1}`
+          `Compiled contract should be LigoContract${index + 1}`
         );
+
         assert.equal(
-          contract.contract_name,
-          `VyperContract${index + 1}`,
-          "Contract name is set correctly"
+          contract.contractName,
+          `LigoContract${index + 1}`,
+          "Contract name is set incorrectly"
         );
 
-        assert.notEqual(
-          contract.abi.indexOf("vyper_action"),
-          -1,
-          "ABI has function from contract present"
+        assert.equal(
+          contract.abi.length,
+          0,
+          "LIGO compiler doesn't currently output contract ABI, something is amiss!"
         );
 
         assert(
-          hex_regex.test(contract.bytecode),
-          "Bytecode has only hex characters"
+          /([parameter][storage][code]){1}/.test(contract.code),
+          "Contract code stored improperly, something is wrong!"
         );
+
         assert(
-          hex_regex.test(contract.deployedBytecode),
-          "Deployed bytecode has only hex characters"
+          contract.source.includes("function main"),
+          "Contract source stored improperly, something is wrong!"
         );
 
         assert.equal(
           contract.compiler.name,
-          "vyper",
-          "Compiler name set correctly"
+          "ligo",
+          "Compiler name set incorrectly!"
         );
       });
 
@@ -70,49 +65,25 @@ describe("vyper compiler", function() {
     });
   });
 
-  it("skips solidity contracts", function(done) {
-    compile.all(config, function(err, contracts, paths) {
-      assert.equal(err, null, "Compiles without error");
+  it("skips solidity contracts", done => {
+    compile.all(config, (err, contracts, paths) => {
+      assert.equal(err, null, "Compiled with an error");
 
-      paths.forEach(function(path) {
-        assert.equal(path.indexOf(".sol"), -1, "Paths have no .sol files");
+      paths.forEach(path => {
+        assert.equal(
+          path.indexOf(".sol"),
+          -1,
+          "Paths should not return .sol files"
+        );
       });
 
       assert.equal(
         contracts.SolidityContract,
         undefined,
-        "Compiled contracts have no SolidityContract"
+        "SolidityContract was compiled!!"
       );
 
       done();
-    });
-  });
-
-  describe("with external options set", function() {
-    const configWithSourceMap = new Config().merge(defaultSettings).merge({
-      compilers: {
-        vyper: {
-          settings: {
-            sourceMap: true
-          }
-        }
-      }
-    });
-
-    it("compiles when sourceMap option set true", function(done) {
-      compile.all(configWithSourceMap, function(err, contracts) {
-        [
-          contracts.VyperContract1,
-          contracts.VyperContract2,
-          contracts.VyperContract3
-        ].forEach((contract, index) => {
-          assert(
-            contract.sourceMap,
-            `source map have to not be empty. ${index + 1}`
-          );
-        });
-        done();
-      });
     });
   });
 });
