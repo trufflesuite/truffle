7d7b8c8aac620985da17364cfe745686b6f78c02
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-11 11:47:09 -0700

    make Common.necessary method (compile-common/index)

--- packages/compile-common/index.js
+++ packages/compile-common/index.js
@@ -1,5 +1,8 @@
 const { callbackify, promisify } = require("util");
 const findContracts = promisify(require("@truffle/contract-sources"));
+const { ProfilerUpdated } = promisify(
+  require("@truffle/compile-solidity/profiler").updated
+);
 
 const Common = {};
 
@@ -11,6 +14,23 @@ Common.all = async (compile, options, callback) => {
     return callback(error);
   }
   options.paths = files;
+  compile.with_dependencies(options, callback);
+};
+
+Common.necessary = async (compile, options, callback) => {
+  let updated;
+  options.logger = options.logger || console;
+
+  try {
+    updated = await ProfilerUpdated(options);
+  } catch (error) {
+    return callback(error);
+  }
+
+  if (updated.length === 0 && options.quiet !== true)
+    return callback(null, [], {});
+
+  options.paths = updated;
   compile.with_dependencies(options, callback);
 };
 
