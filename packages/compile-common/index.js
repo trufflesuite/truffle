a6b42c8b72fcd8450af02f1ef1f4a26b7e26f069
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-11 11:26:37 -0700

    make Common.all method (compile-common/index)

--- packages/compile-common/index.js
+++ packages/compile-common/index.js
@@ -1,7 +1,17 @@
-"use strict";
+const { callbackify, promisify } = require("util");
+const findContracts = promisify(require("@truffle/contract-sources"));
 
-module.exports = compileCommon;
+const Common = {};
 
-function compileCommon() {
-  // TODO
-}
+Common.all = async (compile, options, callback) => {
+  let files;
+  try {
+    files = await findContracts(options.contracts_directory);
+  } catch (error) {
+    return callback(error);
+  }
+  options.paths = files;
+  compile.with_dependencies(options, callback);
+};
+
+module.exports = Common;
