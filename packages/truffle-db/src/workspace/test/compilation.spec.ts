3cbc134413aeeeb5dc282c672f4e4c1b472c2d57
Amal Sudama <cds.sudama@gmail.com>
2019-10-14 18:17:08 -0400

    Refactor compilation tests

--- packages/truffle-db/src/workspace/test/compilation.spec.ts
+++ packages/truffle-db/src/workspace/test/compilation.spec.ts
@@ -3,17 +3,14 @@ import { Query, Mutation } from './queries';
 
 const { AddSource } = Mutation;
 
-/*
- * Compilation
- */
-
 const { GetCompilation } = Query;
 const { AddCompilation } = Mutation;
 
 describe("Compilation", () => {
-  const client = new WorkspaceClient();
+  const wsClient = new WorkspaceClient();
 
-  let sourceId;
+  let sourceId
+  let variables, addCompilationResult
 
   beforeEach(async () => {
     //add source and get id
@@ -21,76 +18,73 @@ describe("Compilation", () => {
       contents: Migrations.source,
       sourcePath: Migrations.sourcePath
     };
-    const sourceResult = await client.execute(AddSource, sourceVariables);
+    const sourceResult = await wsClient.execute(AddSource, sourceVariables);
     sourceId = sourceResult.sourcesAdd.sources[0].id;
-  });
 
-  it("adds compilation", async () => {
-    const expectedId = generateId({
-      compiler: Migrations.compiler,
-      sourceIds: [{ id: sourceId }]
-    });
-
-    const variables = {
+    variables = {
       compilerName: Migrations.compiler.name,
       compilerVersion: Migrations.compiler.version,
       sourceId: sourceId,
       abi: JSON.stringify(Migrations.abi)
     };
+    addCompilationResult = await wsClient.execute(AddCompilation, variables);
 
-  // add compilation
-    {
-      const data = await client.execute(AddCompilation, variables);
-      expect(data).toHaveProperty("compilationsAdd");
+  });
 
-      const { compilationsAdd } = data;
-      expect(compilationsAdd).toHaveProperty("compilations");
+  test("can be added", async() => {
+    expect(addCompilationResult).toHaveProperty("compilationsAdd");
 
-      const { compilations } = compilationsAdd;
-      expect(compilations).toHaveLength(1);
+    const { compilationsAdd } = addCompilationResult;
+    expect(compilationsAdd).toHaveProperty("compilations");
 
-      for (let compilation of compilations) {
-        expect(compilation).toHaveProperty("compiler");
-        expect(compilation).toHaveProperty("sources");
-        const { compiler, sources, contracts } = compilation;
+    const { compilations } = compilationsAdd;
+    expect(compilations).toHaveLength(1);
 
-        expect(compiler).toHaveProperty("name");
-
-        expect(sources).toHaveLength(1);
-        for (let source of sources) {
-          expect(source).toHaveProperty("contents");
-        }
-
-        expect(contracts).toHaveLength(1);
-
-        for(let contract of contracts) {
-          expect(contract).toHaveProperty("source");
-          expect(contract).toHaveProperty("name");
-          expect(contract).toHaveProperty("ast");
-        }
-      }
-    }
-      //ensure retrieved as matching
-    {
-      const data = await client.execute(GetCompilation, { id: expectedId });
-      expect(data).toHaveProperty("compilation");
-
-      const { compilation } = data;
-      expect(compilation).toHaveProperty("id");
+    for (let compilation of compilations) {
       expect(compilation).toHaveProperty("compiler");
       expect(compilation).toHaveProperty("sources");
+      const { compiler, sources, contracts } = compilation;
 
-      const { sources } = compilation;
+      expect(compiler).toHaveProperty("name");
 
+      expect(sources).toHaveLength(1);
       for (let source of sources) {
-        expect(source).toHaveProperty("id");
-        const { id } = source;
-        expect(id).not.toBeNull();
-
         expect(source).toHaveProperty("contents");
-        const { contents } = source;
-        expect(contents).not.toBeNull();
       }
+
+      expect(contracts).toHaveLength(1);
+
+      for(let contract of contracts) {
+        expect(contract).toHaveProperty("source");
+        expect(contract).toHaveProperty("name");
+        expect(contract).toHaveProperty("ast");
+      }
+    }
+  });
+
+  test("can be queried", async() => {
+    const expectedId = generateId({
+      compiler: Migrations.compiler,
+      sourceIds: [{ id: sourceId }]
+    });
+
+    const getCompilationResult = await wsClient.execute(GetCompilation, { id: expectedId });
+    expect(getCompilationResult).toHaveProperty("compilation");
+
+    const { compilation } = getCompilationResult;
+    expect(compilation).toHaveProperty("id");
+    expect(compilation).toHaveProperty("compiler");
+    expect(compilation).toHaveProperty("sources");
+
+    const { sources } = compilation;
+
+    for (let source of sources) {
+      expect(source).toHaveProperty("id");
+      const { id } = source;
+      expect(id).not.toBeNull();
+      expect(source).toHaveProperty("contents");
+      const { contents } = source;
+      expect(contents).not.toBeNull();
     }
   });
 });
