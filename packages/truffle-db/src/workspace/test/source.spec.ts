f531cd3d181ba29a1595925f2aa5a149f6d8e730
Amal Sudama <cds.sudama@gmail.com>
2019-10-14 19:56:34 -0400

    Refactor source tests

--- packages/truffle-db/src/workspace/test/source.spec.ts
+++ packages/truffle-db/src/workspace/test/source.spec.ts
@@ -6,51 +6,55 @@ const { AddSource } = Mutation;
 
 
 describe("Source", () => {
-  it("adds source", async () => {
-    const client = new WorkspaceClient();
+  let wsClient, addSourceResult
 
-    const expectedId = generateId({
-      contents: Migrations.source,
-      sourcePath: Migrations.sourcePath
-    });
-    const variables = {
-      contents: Migrations.source,
-      sourcePath: Migrations.sourcePath,
-    };
-
-    // add source
-    {
-      const data = await client.execute(AddSource, variables);
-      expect(data).toHaveProperty("sourcesAdd");
-
-      const { sourcesAdd } = data;
-      expect(sourcesAdd).toHaveProperty("sources");
-
-      const { sources } = sourcesAdd;
-      expect(sources).toHaveLength(1);
-
-      const source = sources[0];
-      expect(source).toHaveProperty("id");
-
-      const { id } = source;
-      expect(id).toEqual(expectedId);
-    }
-
-    // ensure retrieved as matching
-    {
-      const data = await client.execute(GetSource, { id: expectedId });
-      expect(data).toHaveProperty("source");
-
-      const { source } = data;
-      expect(source).toHaveProperty("id");
-      expect(source).toHaveProperty("contents");
-      expect(source).toHaveProperty("sourcePath");
-
-      const { id, contents, sourcePath } = source;
-      expect(id).toEqual(expectedId);
-      expect(contents).toEqual(variables.contents);
-      expect(sourcePath).toEqual(variables.sourcePath);
-    }
+  const expectedId = generateId({
+    contents: Migrations.source,
+    sourcePath: Migrations.sourcePath
   });
+
+  const variables = {
+    contents: Migrations.source,
+    sourcePath: Migrations.sourcePath,
+  };
+
+  beforeEach(async() => {
+    wsClient = new WorkspaceClient();
+    addSourceResult = await wsClient.execute(AddSource, variables);
+  })
+
+  test("can be added", async () => {
+    expect(addSourceResult).toHaveProperty("sourcesAdd");
+
+    const { sourcesAdd } = addSourceResult;
+    expect(sourcesAdd).toHaveProperty("sources");
+
+    const { sources } = sourcesAdd;
+    expect(sources).toHaveLength(1);
+
+    const source = sources[0];
+    expect(source).toHaveProperty("id");
+
+    const { id } = source;
+    expect(id).toEqual(expectedId);
+
+  });
+
+  test("can be queried", async() => {
+    const getSourceResult = await wsClient.execute(GetSource, { id: expectedId });
+    expect(getSourceResult).toHaveProperty("source");
+
+    const { source } = getSourceResult;
+    expect(source).toHaveProperty("id");
+    expect(source).toHaveProperty("contents");
+    expect(source).toHaveProperty("sourcePath");
+
+    const { id, contents, sourcePath } = source;
+    expect(id).toEqual(expectedId);
+    expect(contents).toEqual(variables.contents);
+    expect(sourcePath).toEqual(variables.sourcePath);
+
+  });
+
 });
 
