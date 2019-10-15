2b6ac979fb68cd60ecb8a426a9872e3185c57b2a
Harry Altman <harry.j.altman@gmail.com>
2019-10-15 15:45:27 -0400

    Backport use of Generator type to develop

--- packages/decoder/lib/read/index.ts
+++ packages/decoder/lib/read/index.ts
@@ -6,15 +6,26 @@ import * as Pointer from "../types/pointer";
 import { EvmState } from "../types/evm";
 import { DecoderRequest } from "../types/request";
 
-export default function* read(pointer: Pointer.DataPointer, state: EvmState): IterableIterator<Uint8Array | DecoderRequest> {
+export default function* read(
+  pointer: Pointer.DataPointer,
+  state: EvmState
+): Generator<DecoderRequest, Uint8Array, Uint8Array> {
   if (Pointer.isStackPointer(pointer) && state.stack) {
     return stack.readStack(state.stack, pointer.stack.from, pointer.stack.to);
   } else if (Pointer.isStoragePointer(pointer) && state.storage) {
     return yield* storage.readRange(state.storage, pointer.storage);
   } else if (Pointer.isMemoryPointer(pointer) && state.memory) {
-    return memory.readBytes(state.memory, pointer.memory.start, pointer.memory.length);
+    return memory.readBytes(
+      state.memory,
+      pointer.memory.start,
+      pointer.memory.length
+    );
   } else if (Pointer.isCalldataPointer(pointer) && state.calldata) {
-    return memory.readBytes(state.calldata, pointer.calldata.start, pointer.calldata.length);
+    return memory.readBytes(
+      state.calldata,
+      pointer.calldata.start,
+      pointer.calldata.length
+    );
     //there is no need for a separate calldata read function; the existing memory read function
     //will do fine
   } else if (Pointer.isStackLiteralPointer(pointer)) {
