76e35e7517c8c8dc73563b87446082bf44a678ee
eggplantzzz <tmfeickert@gmail.com>
2019-10-16 11:51:35 -0300

    Namespace a bunch of methods under the 'helpers' property

--- packages/core/lib/commands/test.js
+++ packages/core/lib/commands/test.js
@@ -43,9 +43,13 @@ const command = {
   },
   run: function(options, done) {
     const Config = require("@truffle/config");
-    const Artifactor = require("@truffle/artifactor");
-    const Test = require("../test");
     const { Environment, Develop } = require("@truffle/environment");
+    const {
+      copyArtifactsToTempDir,
+      determineTestFilesToRun,
+      performCleanup,
+      prepareConfigAndRunTests
+    } = this.helpers;
 
     const config = Config.detect(options);
 
@@ -64,7 +68,7 @@ const command = {
     let files = [];
 
     try {
-      files = this.determineTestFilesToRun(options, config);
+      files = determineTestFilesToRun(options, config);
       files = files.filter(file => {
         return file.match(config.test_file_extension_regexp) !== null;
       });
@@ -72,27 +76,17 @@ const command = {
       return done(error);
     }
 
-    function performCleanup() {
-      if (ipcDisconnect) ipcDisconnect();
-    }
-
-    async function prepareConfigAndRunTests({ config, temporaryDirectory }) {
-      // Set a new artifactor; don't rely on the one created by Environments.
-      // TODO: Make the test artifactor configurable.
-      config.artifactor = new Artifactor(temporaryDirectory);
-
-      const testConfig = config.with({
-        test_files: files,
-        contracts_build_directory: temporaryDirectory
-      });
-      await Test.run(testConfig);
-    }
-
     if (config.networks[config.network]) {
       Environment.detect(config)
-        .then(() => this.copyArtifactsToTempDir(config))
-        .then(prepareConfigAndRunTests)
-        .then(() => performCleanup())
+        .then(() => copyArtifactsToTempDir(config))
+        .then(({ config, temporaryDirectory }) => {
+          return prepareConfigAndRunTests({
+            config,
+            files,
+            temporaryDirectory
+          });
+        })
+        .then(() => performCleanup(ipcDisconnect))
         .then(() => done())
         .catch(done);
     } else {
@@ -114,9 +108,15 @@ const command = {
         (started, disconnect) => {
           ipcDisconnect = disconnect;
           Environment.develop(config, ganacheOptions)
-            .then(() => this.copyArtifactsToTempDir(config))
-            .then(prepareConfigAndRunTests)
-            .then(() => performCleanup())
+            .then(() => copyArtifactsToTempDir(config))
+            .then(({ config, temporaryDirectory }) => {
+              return prepareConfigAndRunTests({
+                config,
+                files,
+                temporaryDirectory
+              });
+            })
+            .then(() => performCleanup(ipcDisconnect))
             .then(() => done())
             .catch(done);
         }
@@ -124,46 +124,66 @@ const command = {
     }
   },
 
-  copyArtifactsToTempDir: async config => {
-    const temp = require("temp").track();
-    const { promisify } = require("util");
-    const copy = require("../copy");
-    const fs = require("fs");
-    const OS = require("os");
-    // Copy all the built files over to a temporary directory, because we
-    // don't want to save any tests artifacts. Only do this if the build directory
-    // exists.
-    const temporaryDirectory = temp.mkdirSync("test-");
-    try {
-      fs.statSync(config.contracts_build_directory);
-    } catch (_error) {
-      return { config, temporaryDirectory };
-    }
+  helpers: {
+    copyArtifactsToTempDir: async config => {
+      const temp = require("temp").track();
+      const { promisify } = require("util");
+      const copy = require("../copy");
+      const fs = require("fs");
+      const OS = require("os");
+      // Copy all the built files over to a temporary directory, because we
+      // don't want to save any tests artifacts. Only do this if the build directory
+      // exists.
+      const temporaryDirectory = temp.mkdirSync("test-");
+      try {
+        fs.statSync(config.contracts_build_directory);
+      } catch (_error) {
+        return { config, temporaryDirectory };
+      }
 
-    await promisify(copy)(config.contracts_build_directory, temporaryDirectory);
-    config.logger.log("Using network '" + config.network + "'." + OS.EOL);
-    return { config, temporaryDirectory };
-  },
-
-  determineTestFilesToRun: (options, config) => {
-    const path = require("path");
-    const fs = require("fs");
-    const glob = require("glob");
-    let files = [];
-    if (options.file) {
-      files = [options.file];
-    } else if (options._.length > 0) {
-      Array.prototype.push.apply(files, options._);
-    }
-
-    if (files.length === 0) {
-      const directoryContents = glob.sync(
-        `${config.test_directory}${path.sep}*`
+      await promisify(copy)(
+        config.contracts_build_directory,
+        temporaryDirectory
       );
-      files =
-        directoryContents.filter(item => fs.statSync(item).isFile()) || [];
+      config.logger.log("Using network '" + config.network + "'." + OS.EOL);
+      return { config, temporaryDirectory };
+    },
+    determineTestFilesToRun: (options, config) => {
+      const path = require("path");
+      const fs = require("fs");
+      const glob = require("glob");
+      let files = [];
+      if (options.file) {
+        files = [options.file];
+      } else if (options._.length > 0) {
+        Array.prototype.push.apply(files, options._);
+      }
+
+      if (files.length === 0) {
+        const directoryContents = glob.sync(
+          `${config.test_directory}${path.sep}*`
+        );
+        files =
+          directoryContents.filter(item => fs.statSync(item).isFile()) || [];
+      }
+      return files;
+    },
+    performCleanup: ipcDisconnect => {
+      if (ipcDisconnect) ipcDisconnect();
+    },
+    prepareConfigAndRunTests: async ({ config, temporaryDirectory, files }) => {
+      const Artifactor = require("@truffle/artifactor");
+      const Test = require("../test");
+      // Set a new artifactor; don't rely on the one created by Environments.
+      // TODO: Make the test artifactor configurable.
+      config.artifactor = new Artifactor(temporaryDirectory);
+
+      const testConfig = config.with({
+        test_files: files,
+        contracts_build_directory: temporaryDirectory
+      });
+      await Test.run(testConfig);
     }
-    return files;
   }
 };
 
