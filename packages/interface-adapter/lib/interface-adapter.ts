8437a61ab825a9be2f76a0f1ec9b383a01821ce6
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-21 13:02:47 -0700

    initial boilerplate (interface-adapter/lib/interface-adapter)

--- packages/interface-adapter/lib/interface-adapter.ts
+++ packages/interface-adapter/lib/interface-adapter.ts
@@ -1,1 +1,15 @@
-export class InterfaceAdapter {}
+export class InterfaceAdapter {
+  /* ... */
+  constructor (networkType) {
+    switch(getNetworkTypeClass(networkType)) {
+      case "evm-like":
+        // use web3 shim
+      case "tezos":
+        // use taquito
+    }
+  }
+
+  getNetworkId() {
+    return ...
+  }
+}
