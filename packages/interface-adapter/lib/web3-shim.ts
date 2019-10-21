4691086d1c0cb84b151bb996994a3a2361b2f2d4
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-21 15:14:32 -0700

    make setNetworkType public (interface-adapter/lib/web3-shim)

--- packages/interface-adapter/lib/web3-shim.ts
+++ packages/interface-adapter/lib/web3-shim.ts
@@ -71,7 +71,7 @@ export class Web3Shim extends Web3 {
     initInterface(this);
   }
 
-  setNetworkType(networkType: NetworkType) {
+  public setNetworkType(networkType: NetworkType) {
     this.networkType = networkType;
     initInterface(this);
   }
