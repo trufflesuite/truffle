9523580cf7e258b28948cfba8357e0da25b9169c
g. nicholas d'andrea <nick@gnidan.org>
2019-10-22 18:54:31 -0400

    Add some missing module indexes

---
+++ packages/codec/lib/allocate/index.ts
@@ -0,0 +1,11 @@
+import * as Abi from "./abi";
+export { Abi };
+
+import * as Memory from "./memory";
+export { Memory };
+
+import * as Storage from "./storage";
+export { Storage };
+
+import * as Types from "./types";
+export { Types };
