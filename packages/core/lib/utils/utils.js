7f18a5076bec74ff7cfbdf7bede4bfc75c5cc9f4
divyanshu1610 <divyanshu1610@gmail.com>
2019-10-23 22:13:33 +0530

    fixed changes

--- packages/core/lib/utils/utils.js
+++ packages/core/lib/utils/utils.js
@@ -1,27 +1,3 @@
-// Regex for checking --option=value
-const argTester = /--[a-zA-Z0-9]+[-]*[a-z0-9A-Z]*=[a-zA-Z0-9.-:]+/;
-
-// Reformats the arguments --option=value to  --option value
-const reformatArguments = inputArguments => {
-  inputArguments.map((arg, i) => {
-    // If arg is in form --option=value
-    if (argTester.test(arg)) {
-      if (arg.replace(argTester, "").length === 0) {
-        let splittedArgs = arg.split("=");
-        // Check if value=true then don't include the value  in inputArguments
-        if (splittedArgs[1] === "true") {
-          inputArguments.splice(i, 1, splittedArgs[0]);
-        } else if (splittedArgs[1] === "false") {
-          // if value is  false then remove that inputArguments
-          inputArguments.splice(i, 1);
-        } else {
-          inputArguments.splice(i, 1, ...splittedArgs); // push down the splittedArgs
-        }
-      }
-    }
-  });
-};
-
 // Extracts the input flags --option from the arguments  of type `--option=value` or `--option value` or `--flag`
 const extractFlags = inputArguments => {
   // Get all the args that begins with `--`. This also includes `--option=value`
@@ -31,18 +7,13 @@ const extractFlags = inputArguments => {
 
   // Extract only the flags i.e `--option` from `--option=value`
   inputFlags.map((flag, i) => {
-    if (!flag) {
+    let indexOfEqualsSign = flag.indexOf("=");
+    if (indexOfEqualsSign > 0) {
+      flag = flag.slice(0, indexOfEqualsSign);
       inputFlags.splice(i, 1, flag);
-    } else {
-      let iOfEqual = flag.indexOf("=");
-      if (iOfEqual > 0) {
-        flag = flag.slice(0, iOfEqual);
-        inputFlags.splice(i, 1, flag);
-      }
     }
   });
   return inputFlags;
 };
 
-module.exports.reformatArguments = reformatArguments;
-module.exports.extractFlags = extractFlags;
+module.exports = { extractFlags };
