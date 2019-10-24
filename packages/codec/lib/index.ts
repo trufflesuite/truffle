2c3dd0dc05ea8cb7b64b336ed81a1cc868b13b05
g. nicholas d'andrea <nick@gnidan.org>
2019-10-24 18:44:49 -0400

    Move Format module documentation to place where from it renders

--- packages/codec/lib/index.ts
+++ packages/codec/lib/index.ts
@@ -15,7 +15,19 @@ require("source-map-support/register");
 //First: export the data format
 import * as Format from "@truffle/codec/format";
 export {
-  /** @category Data */
+  /**
+   * Here's the decoder output format.
+   * Most of this doesn't have explanatory documentation
+   * because it's largely self-explanatory, but particularly
+   * non-obvious parts have been documented for clarity.
+   *
+   * A note on optional fields: A number of types or values
+   * have optional fields.  These contain helpful
+   * but non-essential information, or information which
+   * for technical reasons we can't guarantee we can determine.
+   *
+   * @category Data
+   */
   Format
 };
 
