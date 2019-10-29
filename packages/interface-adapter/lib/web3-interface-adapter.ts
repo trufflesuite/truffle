c9e25eceb66a99cacbbfbcf80762ad8e9ceee4ba
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-29 10:43:53 -0700

    initial Web3InterfaceAdapter boilerpate (interface-adapter/lib/web3-interface-adapter)

---
+++ packages/interface-adapter/lib/web3-interface-adapter.ts
@@ -0,0 +1,36 @@
+import { Web3Shim, Web3ShimOptions } from "./web3-shim";
+
+export interface InterfaceAdapterOptions extends Web3ShimOptions {}
+export type NetworkId = Number | String;
+
+const supportedEvmNetworks = ["ethereum", "fabric-evm", "quorum"];
+
+const getNetworkTypeClass = ({
+  networkType = "ethereum"
+}: InterfaceAdapterOptions) => {
+  if (supportedEvmNetworks.includes(networkType)) return "evm-like";
+  return networkType;
+};
+
+export class InterfaceAdapter {
+  public adapter?: Web3Shim | InterfaceAdapter;
+  constructor(options?: InterfaceAdapterOptions) {
+    switch (getNetworkTypeClass(options)) {
+      case "evm-like":
+        this.adapter = new Web3Shim({
+          provider: options.provider,
+          networkType: options.networkType
+        });
+        break;
+      default:
+        throw Error(
+          `Sorry, "${options.networkType}" is not supported at this time.`
+        );
+    }
+    return this.adapter;
+  }
+
+  public getNetworkId(): Promise<NetworkId> {
+    return this.adapter.getNetworkId();
+  }
+}
