68892dfabef88d4887798a25587555c30e053a14
eggplantzzz <tmfeickert@gmail.com>
2019-10-31 11:41:16 -0400

    Change to equivalent syntax that is hopefully cleaner

--- packages/core/lib/networks.js
+++ packages/core/lib/networks.js
@@ -155,7 +155,7 @@ const Networks = {
       const fileContents = fs.readFileSync(filePath, "utf8");
       const body = JSON.parse(fileContents);
 
-      for (let installedNetworkId in body.networks) {
+      for (let installedNetworkId of Object.keys(body.networks)) {
         let found = false;
         for (let i = 0; i < configuredNetworks.length; i++) {
           const configuredNetwork = configuredNetworks[i];
