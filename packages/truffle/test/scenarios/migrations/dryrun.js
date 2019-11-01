f8a3601ce49700f0a2a431412d8ee51187b9a5ff
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-11-01 15:24:50 -0700

    update test to use InterfaceAdapter (truffle/test/scenarios/migrations/dryrun)

--- packages/truffle/test/scenarios/migrations/dryrun.js
+++ packages/truffle/test/scenarios/migrations/dryrun.js
@@ -6,6 +6,7 @@ const Server = require("../server");
 const Reporter = require("../reporter");
 const sandbox = require("../sandbox");
 const Web3 = require("web3");
+const { InterfaceAdapter } = require("@truffle/interface-adapter");
 
 const log = console.log;
 
@@ -18,7 +19,6 @@ function processErr(err, output) {
 
 describe("migrate (dry-run)", function() {
   let config;
-  let web3;
   const project = path.join(__dirname, "../../sources/migrations/success");
   const logger = new MemoryLogger();
 
@@ -37,8 +37,8 @@ describe("migrate (dry-run)", function() {
     const provider = new Web3.providers.HttpProvider("http://localhost:8545", {
       keepAlive: false
     });
-    web3 = new Web3(provider);
-    networkId = await web3.eth.net.getId();
+    const interfaceAdapter = new InterfaceAdapter({ provider });
+    networkId = await interfaceAdapter.getNetworkId();
   });
 
   it("uses the dry-run option", function(done) {
