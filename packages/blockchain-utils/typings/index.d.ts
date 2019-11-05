fe28f608ec578289fc8db3ea5f7fb19e1b84903c
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-11-05 11:00:59 -0800

    import & apply class provider typings (blockchain-utils/typings/index)

--- packages/blockchain-utils/typings/index.d.ts
+++ packages/blockchain-utils/typings/index.d.ts
@@ -1,13 +1,8 @@
-import {
-  HttpProviderBase,
-  IpcProviderBase,
-  WebsocketProviderBase
-} from "web3-core-helpers";
+import { HttpProvider } from "web3-providers-http";
+import { IpcProvider } from "web3-providers-ipc";
+import { WebsocketProvider } from "web3-providers-ws";
 
-export type Provider =
-  | HttpProviderBase
-  | IpcProviderBase
-  | WebsocketProviderBase;
+export type Provider = HttpProvider | IpcProvider | WebsocketProvider;
 
 export interface JsonRpcResponse {
   jsonrpc: string;
