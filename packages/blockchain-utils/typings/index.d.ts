41e740d0cd0f0f4be3e3527d0de39e5407d67565
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-11-07 11:06:10 -0800

    pull out Web3 provider typings & mv from @truffle/blockchain-utils to @truffle/provider

--- packages/blockchain-utils/typings/index.d.ts
+++ packages/blockchain-utils/typings/index.d.ts
@@ -1,21 +1,3 @@
-import { HttpProvider } from "web3-providers-http";
-import { IpcProvider } from "web3-providers-ipc";
-import { WebsocketProvider } from "web3-providers-ws";
-
-export type Provider = HttpProvider | IpcProvider | WebsocketProvider;
-
-export interface JsonRpcResponse {
-  jsonrpc: string;
-  id: number;
-  result?: any;
-  error?: string;
-}
-
-export interface Callback<ResultType> {
-  (error: Error): void;
-  (error: null, val: ResultType): void;
-}
-
 export type parsedUriObject = {
   genesis_hash?: string;
   block_hash?: string;
