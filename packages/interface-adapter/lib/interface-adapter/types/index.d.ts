4392bb30dc173ce6f90d328db566353fef955655
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-11-07 14:45:07 -0800

    update web3 typings (interface-adapter/lib/interface-adapter/types/index)

--- packages/interface-adapter/lib/interface-adapter/types/index.d.ts
+++ packages/interface-adapter/lib/interface-adapter/types/index.d.ts
@@ -1,7 +1,7 @@
-import { Block as EvmBlock } from "web3/eth/types";
-import { BlockType as EvmBlockType } from "web3/eth/types";
-import { Transaction as EvmTransaction } from "web3/eth/types";
+import { Block as EvmBlock } from "web3-eth";
+import { Transaction as EvmTransaction } from "web3-core";
 
+export type EvmBlockType = number | string;
 export type NetworkId = Number | String;
 export type Block = EvmBlock | any;
 export type BlockType = EvmBlockType | any;
