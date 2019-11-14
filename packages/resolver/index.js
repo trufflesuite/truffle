9c62263aba86e5f1f1c42cb46f71991c94a3af35
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-11-14 08:47:49 -0800

    add isBundled prototype method to Resolver (resolver/index)

--- packages/resolver/index.js
+++ packages/resolver/index.js
@@ -87,4 +87,9 @@ Resolver.prototype.resolve = function(import_path, imported_from, callback) {
   );
 };
 
+Resolver.prototype.isBundled = () => {
+  if (typeof BUNDLE_VERSION === "undefined") return false;
+  return true;
+};
+
 module.exports = Resolver;
