64179b2b5b45a95d2eacadfb042cccd92698a676
eggplantzzz <tmfeickert@gmail.com>
2019-11-14 11:45:32 +0100

    Get rid of the dreaded async flow for migrations and use Promises instead

--- packages/migrate/index.js
+++ packages/migrate/index.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const dir = require("node-dir");
 const path = require("path");
-const async = require("async");
 const expect = require("@truffle/expect");
 const Config = require("@truffle/config");
 const Reporter = require("@truffle/reporters").migrationsV5;
@@ -81,7 +80,6 @@ const Migrate = {
       }
 
       const lastMigration = await this.lastCompletedMigration(options);
-
       // Don't rerun the last completed migration.
       await this.runFrom(lastMigration + 1, options);
 
@@ -144,30 +142,20 @@ const Migrate = {
       migrations
     });
 
-    return new Promise((resolve, reject) => {
-      return async.eachSeries(
-        migrations,
-        (migration, finished) => {
-          migration
-            .run(clone)
-            .then(() => {
-              finished();
-            })
-            .catch(error => {
-              finished(error);
-            });
-        },
-        async error => {
-          await this.emitter.emit("postAllMigrations", {
-            dryRun: options.dryRun,
-            error: error ? error.toString() : null
-          });
-
-          if (error) return reject(error);
-          return resolve();
-        }
-      );
-    });
+    try {
+      await Promise.all(migrations.map(migration => migration.run(clone)));
+      await this.emitter.emit("postAllMigrations", {
+        dryRun: options.dryRun,
+        error: null
+      });
+      return;
+    } catch (error) {
+      await this.emitter.emit("postAllMigrations", {
+        dryRun: options.dryRun,
+        error: error.toString()
+      });
+      throw error;
+    }
   },
 
   wrapResolver: function(resolver, provider) {
