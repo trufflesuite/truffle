bb05c167281127b3545b8783a3ceb401176eeb7d
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-11-15 08:23:18 -0800

    fix typings to import & use ContractObject generated from spec as SpecContractObject (contract-schema/typings/index)

--- packages/contract-schema/typings/index.d.ts
+++ packages/contract-schema/typings/index.d.ts
@@ -1,10 +1,13 @@
 declare module "@truffle/contract-schema" {
-  import { ContractObject } from "@truffle/contract-schema/spec";
+  import { ContractObject as SpecContractObject } from "@truffle/contract-schema/spec";
 
   namespace Schema {
-    export function normalize(dirtyObj: object, opts?: object): ContractObject;
+    export function normalize(
+      dirtyObj: object,
+      opts?: object
+    ): SpecContractObject;
   }
 
-  export type ContractObject = ContractObject;
+  export type ContractObject = SpecContractObject;
   export default Schema;
 }
