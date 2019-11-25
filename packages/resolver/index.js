ce377a8abb2dc247d3e9a097cd57beb6a99bca0a
eggplantzzz <tmfeickert@gmail.com>
2019-11-25 13:53:57 +0100

    Remove old resolve method and importing of the 'whilst' method from async

--- packages/resolver/index.js
+++ packages/resolver/index.js
@@ -1,4 +1,3 @@
-const whilst = require("async/whilst");
 const contract = require("@truffle/contract");
 const expect = require("@truffle/expect");
 const provision = require("@truffle/provisioner");
@@ -23,57 +22,6 @@ Resolver.prototype.require = function(import_path, search_path) {
   if (abstraction) return abstraction;
   throw new Error(
     "Could not find artifacts for " + import_path + " from any sources"
-  );
-};
-
-Resolver.prototype.resolve = function(import_path, imported_from, callback) {
-  var self = this;
-
-  if (typeof imported_from === "function") {
-    callback = imported_from;
-    imported_from = null;
-  }
-
-  var resolved_body = null;
-  var resolved_path = null;
-  var current_index = -1;
-  var current_source;
-
-  whilst(
-    function() {
-      return !resolved_body && current_index < self.sources.length - 1;
-    },
-    function(next) {
-      current_index += 1;
-      current_source = self.sources[current_index];
-
-      current_source.resolve(import_path, imported_from, function(
-        err,
-        body,
-        file_path
-      ) {
-        if (!err && body) {
-          resolved_body = body;
-          resolved_path = file_path;
-        }
-        next(err);
-      });
-    },
-    function(err) {
-      if (err) return callback(err);
-
-      if (!resolved_body) {
-        var message = "Could not find " + import_path + " from any sources";
-
-        if (imported_from) {
-          message += "; imported from " + imported_from;
-        }
-
-        return callback(new Error(message));
-      }
-
-      callback(null, resolved_body, resolved_path, current_source);
-    }
   );
 };
 
