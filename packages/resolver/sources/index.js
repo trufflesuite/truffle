8cbe23a5133dfd0f3fa1e9841621a57a903015d1
eggplantzzz <tmfeickert@gmail.com>
2019-12-17 11:54:30 -0500

    Remove some dead ends

--- packages/resolver/sources/index.js
+++ packages/resolver/sources/index.js
@@ -2,14 +2,12 @@ const EPMSource = require("./epm");
 const NPMSource = require("./npm");
 const GlobalNPMSource = require("./globalnpm");
 const FSSource = require("./fs");
-const TestSource = require("./TestSource");
 
-module.exports = (options, findContracts) => {
+module.exports = options => {
   return [
     new EPMSource(options.working_directory, options.contracts_build_directory),
     new NPMSource(options.working_directory),
     new GlobalNPMSource(),
-    new FSSource(options.working_directory, options.contracts_build_directory),
-    // new TestSource(options, findContracts)
-  ]
-}
+    new FSSource(options.working_directory, options.contracts_build_directory)
+  ];
+};
