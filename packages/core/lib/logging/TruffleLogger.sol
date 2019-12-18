d4ae28961d2a624fcb9a4654826126d1f38ae50d
Adrian Li <li.adrianmc@gmail.com>
2019-12-18 06:38:50 -0500

    add ability to call log function directly

--- packages/core/lib/logging/TruffleLogger.sol
+++ packages/core/lib/logging/TruffleLogger.sol
@@ -7,4 +7,28 @@ contract TruffleLogger {
   event __Log(string str);
   event __Log(bytes32 b32);
   event __Log(address addr);
+
+  function Log(bool x) public {
+    emit __Log(x);
+  }
+
+  function Log(int x) public {
+    emit __Log(x);
+  }
+
+  function Log(uint x) public {
+    emit __Log(x);
+  }
+
+  function Log(string memory x) public {
+    emit __Log(x);
+  }
+
+  function Log(bytes32 x) public {
+    emit __Log(x);
+  }
+
+  function Log(address x) public {
+    emit __Log(x);
+  }
 }
