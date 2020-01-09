7cc8161d11c82bd3715e97749a59f1dda4758dcc
g. nicholas d'andrea <nick@gnidan.org>
2020-01-09 18:26:51 -0500

    Implement schema-generic interface to PouchDB
    - Introduce abstract class Databases<R, C, M, CR, CM> as home to
      generic all/get/add methods, separate from actual workspace schema.
    
    - Generate ID based on ID field in definitions (for `add()`)

--- packages/truffle-db/src/workspace/pouch/types.ts
+++ packages/truffle-db/src/workspace/pouch/types.ts
@@ -1,4 +1,13 @@
 import PouchDB from "pouchdb";
+import PouchDBDebug from "pouchdb-debug";
+import PouchDBFind from "pouchdb-find";
+import { soliditySha3 } from "web3-utils";
+import jsonStableStringify from "json-stable-stringify";
+
+export interface WorkspaceDatabasesOptions<C> {
+  directory: string;
+  definitions: CollectionDefinitions<C>;
+}
 
 export type ResourceMapping<R, C> = {
   [K in CollectionName<C>]: ResourceName<R>;
@@ -10,6 +19,149 @@ export type MutationMapping<C, M> = {
     input: { [N in K]: any[] };
   };
 };
+
+export abstract class Databases<
+  R,
+  C,
+  M,
+  CR extends ResourceMapping<R, C>,
+  CM extends MutationMapping<C, M>
+> {
+  private collections: CollectionDatabases<C>;
+  private definitions: CollectionDefinitions<C>;
+
+  private ready: Promise<void>;
+
+  constructor(options: WorkspaceDatabasesOptions<C>) {
+    this.setup(options);
+
+    this.ready = this.initialize();
+  }
+
+  protected setup({ definitions }: WorkspaceDatabasesOptions<C>) {
+    this.definitions = definitions;
+
+    PouchDB.plugin(PouchDBDebug);
+    PouchDB.plugin(PouchDBFind);
+
+    this.collections = Object.keys(definitions)
+      .map((resource: CollectionName<C>) => ({
+        [resource]: this.createDatabase(resource)
+      }))
+      .reduce((a, b) => ({ ...a, ...b })) as CollectionDatabases<C>;
+  }
+
+  protected abstract createDatabase(
+    resource: CollectionName<C>
+  ): PouchDB.Database;
+
+  private async initialize() {
+    for (const [resource, definition] of Object.entries(this.definitions)) {
+      const collection = this.collections[resource as CollectionName<C>];
+
+      const { createIndexes } = definition as CollectionDefinition<C>;
+
+      for (let index of createIndexes || []) {
+        await collection.createIndex({ index });
+      }
+    }
+  }
+
+  public async all<N extends CollectionName<C>>(
+    collectionName: N
+  ): Promise<CollectionResult<C, N>> {
+    return await this.find<N>(collectionName, { selector: [] });
+  }
+
+  public async find<N extends CollectionName<C>>(
+    collectionName: N,
+    options: PouchDB.Find.FindRequest<{}>
+  ): Promise<CollectionResult<C, N>> {
+    await this.ready;
+
+    try {
+      const { docs }: any = await this.collections[collectionName].find(
+        options
+      );
+
+      return docs.map(doc => ({ ...doc, id: doc["_id"] }));
+    } catch (error) {
+      console.log(`Error fetching all ${collectionName}\n`);
+      console.log(error);
+      return ([] as unknown) as CollectionResult<C, N>;
+    }
+  }
+
+  public async get<N extends CollectionName<C>>(
+    collectionName: N,
+    id: string
+  ): Promise<ResourceResult<R, C, CR, N> | null> {
+    await this.ready;
+
+    try {
+      const result = await this.collections[collectionName].get(id);
+      return {
+        ...result,
+        id
+      } as ResourceResult<R, C, CR, N>;
+    } catch (_) {
+      return null;
+    }
+  }
+
+  public async add<N extends CollectionName<C>>(
+    collectionName: N,
+    input: MutationInput<C, M, CM, N>
+  ): Promise<MutationResult<R, C, CR, N>> {
+    await this.ready;
+
+    const { idFields } = this.definitions[collectionName];
+
+    const resources = await Promise.all(
+      input[collectionName].map(
+        async (resourceInput: MutationInput<C, M, CM, N>) => {
+          const id = soliditySha3(
+            jsonStableStringify(
+              idFields.reduce(
+                (obj, field) => ({
+                  ...obj,
+                  [field]: resourceInput[field]
+                }),
+                {}
+              )
+            )
+          );
+
+          // check for existing
+          const resource = await this.get(collectionName, id);
+          if (resource) {
+            return resource;
+          }
+
+          const resourceAdded = await this.collections[collectionName].put({
+            ...resourceInput,
+            _id: id
+          });
+
+          return {
+            ...resourceInput,
+            id
+          } as ResourceResult<R, C, CR, N>;
+        }
+      )
+    );
+
+    return {
+      [collectionName]: resources
+    } as MutationResult<R, C, CR, N>;
+  }
+
+  async destroy<N extends CollectionName<C>>(collectionName: N) {
+    await this.ready;
+
+    await this.collections[collectionName].destroy();
+  }
+}
 
 type CollectionDatabases<C> = {
   [name in CollectionName<C>]: PouchDB.Database;
