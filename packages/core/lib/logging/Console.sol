6734ff62e4840a4707e147fffdb62a1b4943248b
Adrian Li <li.adrianmc@gmail.com>
2020-01-16 15:00:16 +0900

    add named log events to Console.sol

--- packages/core/lib/logging/Console.sol
+++ packages/core/lib/logging/Console.sol
@@ -8,6 +8,13 @@ library Console {
   event _TruffleLog(bytes32 b32);
   event _TruffleLog(address addr);
 
+  event _TruffleLogNamed(bytes32 label, bool boolean);
+  event _TruffleLogNamed(bytes32 label, int num);
+  event _TruffleLogNamed(bytes32 label, uint num);
+  event _TruffleLogNamed(bytes32 label, string str);
+  event _TruffleLogNamed(bytes32 label, bytes32 b32);
+  event _TruffleLogNamed(bytes32 label, address addr);
+
   function log(bool x) public {
     emit _TruffleLog(x);
   }
@@ -30,5 +37,29 @@ library Console {
 
   function log(address x) public {
     emit _TruffleLog(x);
+  }
+
+  function log(bytes32 x, bool y) public {
+    emit _TruffleLogNamed(x, y);
+  }
+
+  function log(bytes32 x, int y) public {
+    emit _TruffleLogNamed(x, y);
+  }
+
+  function log(bytes32 x, uint y) public {
+    emit _TruffleLogNamed(x, y);
+  }
+
+  function log(bytes32 x, string memory y) public {
+    emit _TruffleLogNamed(x, y);
+  }
+
+  function log(bytes32 x, bytes32 y) public {
+    emit _TruffleLogNamed(x, y);
+  }
+
+  function log(bytes32 x, address y) public {
+    emit _TruffleLogNamed(x, y);
   }
 }
