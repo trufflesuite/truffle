d23baf2709d9189519fdec8e0da84ddbac966aa3
Adrian Li <li.adrianmc@gmail.com>
2020-01-16 15:10:21 +0900

    add handler for named log events

--- packages/contract/lib/utils/index.js
+++ packages/contract/lib/utils/index.js
@@ -81,8 +81,8 @@ const Utils = {
   },
 
   consoleLog(receiptLogs) {
-    const TruffleLogEvents = receiptLogs.filter(x => x.event === "_TruffleLog");
-    TruffleLogEvents.forEach(event => {
+    const logEvents = receiptLogs.filter(x => x.event === "_TruffleLog");
+    logEvents.forEach(event => {
       const result = event.args;
 
       // A mapping to show how to output into our console
@@ -101,6 +101,34 @@ const Utils = {
       types.forEach(type => {
         if (result.hasOwnProperty(type)) {
           console.log(outputMap[type]);
+        }
+      });
+    });
+
+    const namedLogEvents = receiptLogs.filter(
+      x => x.event === "_TruffleLogNamed"
+    );
+    namedLogEvents.forEach(event => {
+      const result = event.args;
+
+      // A mapping to show how to output into our console
+      const outputMap = {
+        num: result[1].toString(),
+        boolean: result[1],
+        str: result[1],
+        b32: result[1],
+        addr: result[1]
+      };
+
+      // A list of all "supported" types: ["num", "boolean", "str", ...]
+      const types = Object.keys(outputMap);
+
+      // Find out what type our result is, and log it out if it matches
+      types.forEach(type => {
+        if (result.hasOwnProperty(type)) {
+          const label = web3Utils.toAscii(result[0]);
+          const value = outputMap[type];
+          console.log(label, value);
         }
       });
     });
