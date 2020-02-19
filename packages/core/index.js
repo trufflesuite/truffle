2073e9dbfbf2ebd2bb39a058f984234d855ffd99
eggplantzzz <tmfeickert@gmail.com>
2020-02-19 10:25:09 -0500

    Correct path to library

--- packages/core/index.js
+++ packages/core/index.js
@@ -2,7 +2,7 @@ var pkg = require("./package.json");
 
 module.exports = {
   build: require("./lib/build"),
-  create: require("./lib/commands/create"),
+  create: require("./lib/commands/create/helpers"),
   console: require("./lib/repl"),
   contracts: require("@truffle/workflow-compile"),
   package: require("./lib/package"),
