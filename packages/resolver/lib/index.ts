d1b2746da8eeab86f27d5d918c770dbc87f7170b
g. nicholas d'andrea <nick@gnidan.org>
2020-04-13 22:33:34 -0400

    Convert Resolver to class
    and remove two `any`s

--- packages/resolver/lib/index.ts
+++ packages/resolver/lib/index.ts
@@ -4,86 +4,90 @@ const expect = require("@truffle/expect");
 const provision = require("@truffle/provisioner");
 const sources = require("./sources");
 
-type Source = any;
-type Callback = any;
+import { ResolverSource } from "./source";
 
-function Resolver(options: any) {
-  expect.options(options, ["working_directory", "contracts_build_directory"]);
+type Callback = (
+  err: Error | null,
+  body?: string,
+  filePath?: string,
+  source?: ResolverSource
+) => void;
 
-  this.options = options;
-  this.sources = sources(options);
-}
+class Resolver {
+  options: any;
+  sources: ResolverSource[];
 
-// This function might be doing too much. If so, too bad (for now).
-Resolver.prototype.require = function(
-  import_path: string,
-  search_path: string
-) {
-  let abstraction;
-  this.sources.forEach((source: Source) => {
-    const result = source.require(import_path, search_path);
-    if (result) {
-      abstraction = contract(result);
-      provision(abstraction, this.options);
-    }
-  });
-  if (abstraction) return abstraction;
-  throw new Error(
-    "Could not find artifacts for " + import_path + " from any sources"
-  );
-};
+  constructor(options: any) {
+    expect.options(options, ["working_directory", "contracts_build_directory"]);
 
-Resolver.prototype.resolve = function(
-  import_path: string,
-  imported_from: string,
-  callback: Callback
-) {
-  var self = this;
-
-  if (typeof imported_from === "function") {
-    callback = imported_from;
-    imported_from = null;
+    this.options = options;
+    this.sources = sources(options);
   }
 
-  var resolved_body: string | null = null;
-  var resolved_path: string | null = null;
-  var source: Source;
-  var current_index = 0;
+  // This function might be doing too much. If so, too bad (for now).
+  require(import_path: string, search_path: string) {
+    let abstraction;
+    this.sources.forEach((source: ResolverSource) => {
+      const result = source.require(import_path, search_path);
+      if (result) {
+        abstraction = contract(result);
+        provision(abstraction, this.options);
+      }
+    });
+    if (abstraction) return abstraction;
+    throw new Error(
+      "Could not find artifacts for " + import_path + " from any sources"
+    );
+  }
 
-  whilst(
-    function() {
-      return !resolved_body && current_index <= self.sources.length - 1;
-    },
-    function(next: any) {
-      source = self.sources[current_index];
-      source
-        .resolve(import_path, imported_from)
-        .then((result: { body: string; filePath: string }) => {
-          if (result.body) {
-            resolved_body = result.body;
-            resolved_path = result.filePath;
+  resolve(import_path: string, imported_from: string, callback: Callback) {
+    var self = this;
+
+    if (typeof imported_from === "function") {
+      callback = imported_from;
+      imported_from = null;
+    }
+
+    var resolved_body: string | null = null;
+    var resolved_path: string | null = null;
+    var source: ResolverSource;
+    var current_index = 0;
+
+    whilst(
+      function() {
+        return !resolved_body && current_index <= self.sources.length - 1;
+      },
+      function(next: any) {
+        source = self.sources[current_index];
+        source
+          .resolve(import_path, imported_from)
+          .then((result: { body: string; filePath: string }) => {
+            if (result.body) {
+              resolved_body = result.body;
+              resolved_path = result.filePath;
+            }
+            current_index++;
+            next();
+          })
+          .catch(next);
+      },
+      function(err: any) {
+        if (err) return callback(err);
+
+        if (!resolved_body) {
+          var message = "Could not find " + import_path + " from any sources";
+
+          if (imported_from) {
+            message += "; imported from " + imported_from;
           }
-          current_index++;
-          next();
-        })
-        .catch(next);
-    },
-    function(err: any) {
-      if (err) return callback(err);
 
-      if (!resolved_body) {
-        var message = "Could not find " + import_path + " from any sources";
-
-        if (imported_from) {
-          message += "; imported from " + imported_from;
+          return callback(new Error(message));
         }
 
-        return callback(new Error(message));
+        callback(null, resolved_body, resolved_path, source);
       }
-
-      callback(null, resolved_body, resolved_path, source);
-    }
-  );
-};
+    );
+  }
+}
 
 module.exports = Resolver;
