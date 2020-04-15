4f409892647e77e061004dc6f8cf56631cbe3d6f
wbt <wbt@users.noreply.github.com>
2020-04-15 18:53:59 -0400

    Fix compilation issue of 'this' inside function()

--- packages/contract/lib/execute.js
+++ packages/contract/lib/execute.js
@@ -114,7 +114,7 @@ const execute = {
     const constructor = this;
 
     return function() {
-      let defaultBlock = this.web3.eth.defaultBlock;
+      let defaultBlock = constructor.web3.eth.defaultBlock;
       const args = Array.prototype.slice.call(arguments);
       const lastArg = args[args.length - 1];
       const promiEvent = PromiEvent();
