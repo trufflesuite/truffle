dc94fe604c06e874ab27b7cf81b17ce4371276cb
Nicholas J. Paterno <cashlion@protonmail.com>
2020-04-20 17:59:17 -0400

    Combine loops to aggregate consoleCommands

--- packages/core/lib/commands/develop.js
+++ packages/core/lib/commands/develop.js
@@ -29,14 +29,11 @@ const command = {
     const commands = require("./index");
     const excluded = new Set(["console", "develop", "unbox", "init"]);
 
-    const availableCommands = Object.keys(commands).filter(
-      name => !excluded.has(name)
-    );
-
-    const consoleCommands = availableCommands.reduce(
-      (acc, name) => Object.assign(acc, { [name]: commands[name] }),
-      {}
-    );
+    const consoleCommands = Object.keys(commands).reduce((acc, name) => {
+      return !excluded.has(name)
+        ? Object.assign(acc, { [name]: commands[name] })
+        : acc;
+    }, {});
 
     Environment.develop(config, ganacheOptions)
       .then(() => {
