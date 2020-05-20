053b07a4b6e432e1d99fdb65bfe3b0f5a409ce01
Harry Altman <harry.j.altman@gmail.com>
2020-05-20 17:35:56 -0400

    Add types to source-fetcher

--- packages/source-fetcher/lib/index.ts
+++ packages/source-fetcher/lib/index.ts
@@ -1,5 +1,8 @@
 import "source-map-support/register";
 
-const Fetchers: any[] = [];
+import { Fetcher, FetcherConstructor } from "./types";
+export { Fetcher, FetcherConstructor };
+
+const Fetchers: FetcherConstructor[] = [];
 
 export default Fetchers;
---
+++ packages/source-fetcher/lib/types.ts
@@ -0,0 +1,72 @@
+export interface FetcherConstructor {
+  forNetworkId(networkId: number): Fetcher;
+}
+
+export interface Fetcher {
+  readonly name: string;
+  isNetworkValid(): boolean;
+  hasAddress(address: string): boolean;
+  fetchSourcesForAddress(
+    address: string
+  ): {
+    sources: SourcesByName;
+    options: SolcOptions;
+  };
+}
+
+interface SourcesByName {
+  [sourcePath: string]: string;
+}
+
+//apologies if reinventing the wheel here
+interface SolcOptions {
+  version: string;
+  settings: SolcSettings;
+}
+
+//only including settings that would alter compiled result
+interface SolcSettings {
+  remappings?: string[];
+  optimizer?: OptimizerSettings;
+  evmVersion: string; //not gonna enumerate these
+  debug?: DebugSettings;
+  metadata?: MetadataSettings;
+  libraries: LibrarySettings;
+}
+
+interface LibrarySettings {
+  [contractPath: string]: {
+    [libraryName: string]: string;
+  };
+}
+
+interface MetadataSettings {
+  useLiteralContent?: boolean;
+  bytecodeHash?: "none" | "ipfs" | "bzzr1";
+}
+
+interface DebugSettings {
+  revertStrings?: "default" | "strip" | "debug" | "verboseDebug";
+}
+
+interface OptimizerSettings {
+  enabled?: boolean;
+  runs?: number;
+  details?: OptimizerDetails;
+}
+
+interface OptimizerDetails {
+  peephole?: boolean;
+  jumpdestRemover?: boolean;
+  orderLiterals?: boolean;
+  deduplicate?: boolean;
+  cse?: boolean;
+  constantOptimizer?: boolean;
+  yul?: boolean;
+  yulDetails?: YulDetails;
+}
+
+interface YulDetails {
+  stackAllocation?: boolean;
+  optimizerSteps?: string;
+}
