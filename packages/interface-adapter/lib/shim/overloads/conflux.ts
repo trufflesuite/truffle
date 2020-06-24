26681de17ed038afc7326893e566b45d75372fd6
Dayong Wang <dayong@conflux-chain.org>
2020-06-24 18:16:09 +0800

    handle error of send transaction

--- packages/interface-adapter/lib/shim/overloads/conflux.ts
+++ packages/interface-adapter/lib/shim/overloads/conflux.ts
@@ -1,17 +1,20 @@
 import { Web3Shim } from "..";
 import PromiEvent from "web3/promiEvent";
+// @ts-ignore
+import PromiEventImpl from "web3-core-promievent"
 import { Callback, TransactionReceipt as web3TxReceipt } from "web3/types";
 import { TxHash, Transaction, TransactionReceipt } from "lib/adapter/types";
 import { Tx as web3Tx } from "web3/eth/types";
 import { EventEmitter } from "events";
 import Eth from "web3/eth";
 import {
-  // Conflux,
+  Conflux,
   TransactionReceipt as cfxTxReceipt,
   Transaction as cfxTx,
   TransactionConfig
+  // @ts-ignore
 } from "js-conflux-sdk";
-import { Conflux } from "/Users/pana/Projects/conflux/js-conflux-sdk";
+import Web3 from "web3";
 
 // We simply return plain ol' Web3.js
 export const ConfluxDefinition = {
@@ -44,7 +47,7 @@ const overrides = {
   },
 
   getBlockNumber: (web3: Web3Shim) => {
-    const newMethod = function(cbk?: Callback<number>): Promise<number> {
+    const newMethod = function (cbk?: Callback<number>): Promise<number> {
       let _promise = web3.cfx.getEpochNumber("latest_state"); // TODO hardcode
       return _promise;
     };
@@ -52,14 +55,14 @@ const overrides = {
   },
 
   getNetworkId: (web3: Web3Shim) => {
-    const newMethod = function(cb?: Callback<number>): Promise<number> {
+    const newMethod = function (cb?: Callback<number>): Promise<number> {
       return Promise.resolve(1592304361448); // TODO hardcode
     };
     web3.eth.net.getId = newMethod;
   },
 
   getAccounts: (web3: Web3Shim) => {
-    const newMethod = function(cb?: Callback<string[]>): Promise<string[]> {
+    const newMethod = function (cb?: Callback<string[]>): Promise<string[]> {
       return web3.cfx.provider.call("accounts");
     };
     web3.eth.getAccounts = newMethod;
@@ -82,7 +85,7 @@ const overrides = {
    * 3. 字段的缺失?
    */
   getBlock: (web3: Web3Shim) => {
-    const newMethod = async function(epochNum: any): Promise<any> {
+    const newMethod = async function (epochNum: any): Promise<any> {
       if (epochNum == "latest") {
         epochNum = "latest_state";
       }
@@ -98,7 +101,7 @@ const overrides = {
    * main difference is 'blockNumber'
    */
   getTransaction: (web3: Web3Shim) => {
-    const newMethod = async function(tx: TxHash): Promise<Transaction> {
+    const newMethod = async function (tx: TxHash): Promise<Transaction> {
       let trans = await web3.cfx.getTransactionByHash(tx);
       // @ts-ignore
       trans.blockNumber = trans.epochHeight;
@@ -115,7 +118,7 @@ const overrides = {
    * outcomeStatus -> status
    */
   getTransactionReceipt: (web3: Web3Shim) => {
-    const newMethod = async function(tx: TxHash): Promise<TransactionReceipt> {
+    const newMethod = async function (tx: TxHash): Promise<TransactionReceipt> {
       let txReceipt = await web3.cfx.getTransactionReceipt(tx);
       if (txReceipt) {
         // @ts-ignore
@@ -134,7 +137,7 @@ const overrides = {
   },
 
   getBalance: (web3: Web3Shim) => {
-    const newMethod = async function(address: string): Promise<string> {
+    const newMethod = async function (address: string): Promise<string> {
       let balance = await web3.cfx.getBalance(address, "latest_state");
       return balance.toString();
     };
@@ -142,7 +145,7 @@ const overrides = {
   },
 
   getCode: (web3: Web3Shim) => {
-    const newMethod = async function(address: string): Promise<string> {
+    const newMethod = async function (address: string): Promise<string> {
       try {
         let code = await web3.cfx.getCode(address);
         return code;
@@ -154,7 +157,7 @@ const overrides = {
   },
 
   estimateGas: (web3: Web3Shim) => {
-    const newMethod = async function(txConfig: Transaction): Promise<number> {
+    const newMethod = async function (txConfig: Transaction): Promise<number> {
       let result = await web3.cfx.estimateGasAndCollateral(txConfig);
       // @ts-ignore
       return Promise.resolve(result.gasUsed);
@@ -164,12 +167,12 @@ const overrides = {
 
   sendTransaction: (web3: Web3Shim) => {
     // @ts-ignore
-    let newMethod = function(
+    let newMethod = function (
       tx: web3Tx,
       cb?: Callback<string>
       // @ts-ignore
     ): cfxPromiEvent<web3TxReceipt> {
-      console.trace("\n-----------sendTransaction");
+      // console.trace("\n-----------sendTransaction");
       let cfxTx: any = {};
       cfxTx.from = tx.from;
       cfxTx.to = tx.to;
@@ -180,197 +183,218 @@ const overrides = {
       // @ts-ignore
       cfxTx.gasPrice = tx.gasPrice;
       // @ts-ignore
-      let password = "123456";
-
+      let password = "12345";
       // cfxTx = {
       //   from: "0x12be576f8eb81046c6f29f1801f39b75390fd760",
       //   to: "0x1A655f04d03f522e34f22e976A30294F4C83dEAd",
       //   value: "0x100",
       // }
-      console.log("send transaction:", cfxTx);
-      let txhashP = web3.cfx.sendTransaction(cfxTx, password);
+      // console.log("send transaction:", cfxTx);
 
-      const promiEvent = new cfxPromiEvent(txhashP, web3);
-      // promiEvent.start(txhashP)
+      // old
+      //   let promiEvent = new cfxPromiEvent(cfxTx, password, web3);
+      //   return promiEvent
+      // }
 
-      return promiEvent;
+      let promiEvent = createCfxPromiEvent(web3, cfxTx, password)
+      return promiEvent.eventEmitter
     };
+
     web3.eth.sendTransaction = newMethod;
   }
 };
 
-export class cfxPromiEvent implements PromiEvent<web3TxReceipt> {
-  emiter: EventEmitter;
-  eth: Eth;
 
-  receipt: web3TxReceipt;
-  err: Error;
-  public constructor(txhashPromise: Promise<string>, web3: Web3Shim) {
-    this.emiter = new EventEmitter();
-    this.eth = web3.eth;
+function createCfxPromiEvent(web3: Web3Shim, tx: any, password: string) {
+  let promiEvent = PromiEventImpl(false)
 
-    this.start(txhashPromise);
-    // .then(console.log)
+  let fireError = function (err:string, _promiEvent:any) {
+    _promiEvent.eventEmitter.emit("error", err)
+    if (_promiEvent.reject) {
+      _promiEvent.reject(err)
+    }
   }
 
-  async start(txhashP: Promise<string>) {
+  let start = async function (_promiEvent:any) {
     try {
-      console.log("start cfx promise event", txhashP);
+      console.log("start cfx promise event");
+      let txhash = await web3.cfx.sendTransaction(tx, password);
 
-      const txhash = await txhashP;
       console.log("get txhash done.", txhash);
 
       //use async due to emit is synchronize action. and truffle will get stuck for unkown reasons.
-      (async function() {
-        this.emiter.emit("transactionHash", txhash);
+      (async function () {
+        _promiEvent.eventEmitter.emit("transactionHash", txhash);
       })();
 
       // wait packed
       console.log("wait be packed");
       const loopForTxRpt = setInterval(async () => {
         try {
-          console.log("get transaction receipt");
-          const rpt: any = await this.eth.getTransactionReceipt(txhash);
+          // console.log("\nget transaction receipt");
+          const receipt: any = await web3.eth.getTransactionReceipt(txhash);
 
           //@ts-ignore
-          if (rpt) {
-            this.receipt = rpt;
+          if (receipt) {
+            console.log(`tx is packed, get receipt ${receipt}`)
             clearInterval(loopForTxRpt);
-            this.emiter.emit("receipt", this.receipt);
-            this.emiter.emit(
-              "confirmation",
-              this.receipt.blockNumber,
-              this.receipt
-            );
+            _promiEvent.eventEmitter.emit("receipt", receipt);
+            _promiEvent.eventEmitter.emit("confirmation", receipt.blockNumber, receipt);
+            _promiEvent.resolve(receipt)
           }
         } catch (e) {
-          this.err = e;
-          this.emiter.emit("error", e);
+          fireError(e, _promiEvent)
         }
       }, 1000);
     } catch (err) {
-      throw err;
+      console.log(`start cfx promise event err:${err}`)
+      fireError(err, _promiEvent)
     }
-    // setInterval(() => console.log("."), 1000)
   }
-
-  once(
-    type: "transactionHash",
-    handler: (receipt: string) => void
-  ): PromiEvent<web3TxReceipt>;
-  once(
-    type: "receipt",
-    handler: (receipt: web3TxReceipt) => void
-  ): PromiEvent<web3TxReceipt>;
-  once(
-    type: "confirmation",
-    handler: (confNumber: number, receipt: web3TxReceipt) => void
-  ): PromiEvent<web3TxReceipt>;
-  once(
-    type: "error",
-    handler: (error: Error) => void
-  ): PromiEvent<web3TxReceipt>;
-  once(
-    type: import("web3/promiEvent").PromiEventType,
-    handler: (error: string | web3TxReceipt | Error) => void
-  ): PromiEvent<web3TxReceipt>;
-
-  public once(type: any, handler: any): PromiEvent<web3TxReceipt> {
-    this.emiter.once(type, handler);
-    return this;
-  }
-
-  on(
-    type: "transactionHash",
-    handler: (receipt: string) => void
-  ): PromiEvent<web3TxReceipt>;
-  on(
-    type: "receipt",
-    handler: (receipt: web3TxReceipt) => void
-  ): PromiEvent<web3TxReceipt>;
-  on(
-    type: "confirmation",
-    handler: (confNumber: number, receipt: web3TxReceipt) => void
-  ): PromiEvent<web3TxReceipt>;
-  on(type: "error", handler: (error: Error) => void): PromiEvent<web3TxReceipt>;
-  on(
-    type: import("web3/promiEvent").PromiEventType,
-    handler: (error: string | web3TxReceipt | Error) => void
-  ): PromiEvent<web3TxReceipt>;
-  public on(type: any, handler: any): PromiEvent<web3TxReceipt> {
-    this.emiter.on(type, handler);
-    return this;
-  }
-
-  public then<TResult1 = web3TxReceipt, TResult2 = never>(
-    onfulfilled?: (value: web3TxReceipt) => TResult1 | PromiseLike<TResult1>,
-    onrejected?: (reason: any) => TResult2 | PromiseLike<TResult2>
-  ): Promise<TResult1 | TResult2> {
-    return new Promise((resolve, reject) => {
-      console.log("execute then of cfxPromiEvent promise");
-      if (this.receipt) {
-        onfulfilled(this.receipt);
-        // @ts-ignore
-        return resolve(this.receipt);
-      }
-
-      if (this.err) {
-        if (onrejected) onrejected(this.err);
-        return reject(this.err);
-      }
-      this.emiter.on("receipt", rpt => {
-        onfulfilled(rpt);
-        // @ts-ignore
-        resolve(this.receipt);
-      });
-      this.emiter.on("error", err => {
-        if (onrejected) onrejected(err);
-        reject(err);
-      });
-    });
-  }
-
-  public catch<TResult = never>(
-    onrejected?: (reason: any) => TResult | PromiseLike<TResult>
-  ): Promise<web3TxReceipt | TResult> {
-    return new Promise((resolve, reject) => {
-      console.log("execute catch of cfxPromiEvent promise");
-      if (this.err) {
-        if (onrejected) onrejected(this.err);
-        // @ts-ignore
-        return reject(this.err);
-      }
-
-      this.emiter.on("error", err => {
-        if (onrejected) onrejected(err);
-        reject(err);
-      });
-    });
-  }
-
-  [Symbol.toStringTag]: string;
-
-  public finally(onfinally?: () => void): Promise<web3TxReceipt> {
-    return new Promise((resolve, reject) => {
-      console.log("execute then of cfxPromiEvent promise");
-      if (this.receipt) {
-        if (onfinally) onfinally();
-        // @ts-ignore
-        return resolve(this.receipt);
-      }
-      if (this.err) {
-        if (onfinally) onfinally();
-        return reject(this.err);
-      }
-      this.emiter.on("receipt", rpt => {
-        if (onfinally) onfinally();
-        // @ts-ignore
-        resolve(this.receipt);
-      });
-      this.emiter.on("error", err => {
-        if (onfinally) onfinally();
-        reject(err);
-      });
-    });
-  }
+  start(promiEvent)
+  return promiEvent;
 }
+
+
+
+// export class cfxPromiEvent implements PromiEvent<web3TxReceipt> {
+//   emiter: EventEmitter;
+//   // eth: Eth;
+//   web3: Web3Shim;
+
+//   receipt: web3TxReceipt;
+//   err: Error;
+//   public constructor(cfxTx: any, password: string, web3: Web3Shim) {
+//     this.emiter = new EventEmitter();
+//     // this.eth = web3.eth;
+//     this.web3 = web3;
+//     this.start(cfxTx, password);
+//     // .then(console.log)
+//   }
+
+//   async start(cfxTx, password) {
+//     try {
+//       // console.log("start cfx promise event", txhashP);
+
+//       const txhash = await this.web3.cfx.sendTransaction(cfxTx, password);
+//       console.log("get txhash done.", txhash);
+
+//       //use async due to emit is synchronize action. and truffle will get stuck for unkown reasons.
+//       (async function () {
+//         this.emiter.emit("transactionHash", txhash);
+//       })();
+
+//       // wait packed
+//       console.log("wait be packed");
+//       const loopForTxRpt = setInterval(async () => {
+//         try {
+//           console.log("\nget transaction receipt");
+//           const rpt: any = await this.web3.eth.getTransactionReceipt(txhash);
+
+//           //@ts-ignore
+//           if (rpt) {
+//             this.receipt = rpt;
+//             clearInterval(loopForTxRpt);
+//             this.emiter.emit("receipt", this.receipt);
+//             this.emiter.emit("confirmation", this.receipt.blockNumber, this.receipt);
+//           }
+//         } catch (e) {
+//           this.err = e;
+//           this.emiter.emit("error", e);
+//         }
+//       }, 1000);
+//     } catch (err) {
+//       console.log(`start cfx promise event err:${err}`)
+//       this.emiter.emit("error", this.receipt);
+//       this.err = err
+//       // throw err;
+//     }
+//     // setInterval(() => console.log("."), 1000)
+//   }
+
+//   public once(type: any, handler: any): PromiEvent<web3TxReceipt> {
+//     this.emiter.once(type, handler);
+//     return this;
+//   }
+
+//   public on(type: any, handler: any): PromiEvent<web3TxReceipt> {
+//     this.emiter.on(type, handler);
+//     return this;
+//   }
+
+//   public then<TResult1 = web3TxReceipt, TResult2 = never>(
+//     onfulfilled?: (value: web3TxReceipt) => TResult1 | PromiseLike<TResult1>,
+//     onrejected?: (reason: any) => TResult2 | PromiseLike<TResult2>
+//   ): Promise<TResult1 | TResult2> {
+//     return new Promise((resolve, reject) => {
+//       console.log("execute then of cfxPromiEvent promise");
+//       if (this.receipt) {
+//         onfulfilled(this.receipt);
+//         // @ts-ignore
+//         return resolve(this.receipt);
+//       }
+
+//       if (this.err) {
+//         if (onrejected) onrejected(this.err);
+//         return reject(this.err);
+//       }
+//       this.emiter.on("receipt", rpt => {
+//         onfulfilled(rpt);
+//         // @ts-ignore
+//         resolve(this.receipt);
+//       });
+//       this.emiter.on("error", err => {
+//         if (onrejected) onrejected(err);
+//         reject(err);
+//       });
+//     });
+//   }
+
+//   public catch<TResult = never>(onrejected?: (reason: any) => TResult | PromiseLike<TResult>): Promise<web3TxReceipt | TResult> {
+//     return new Promise((resolve, reject) => {
+//       console.log("execute catch of cfxPromiEvent promise");
+//       if (this.err) {
+//         if (onrejected) onrejected(this.err);
+//         // @ts-ignore
+//         return reject(this.err);
+//       }
+
+//       this.emiter.on("error", err => {
+//         if (onrejected) onrejected(err);
+//         reject(err);
+//       });
+//     });
+//   }
+
+//   [Symbol.toStringTag]: string;
+
+//   public finally(onfinally?: () => void): Promise<web3TxReceipt> {return new Promise((resolve, reject) => {
+//       console.log("execute finally of cfxPromiEvent promise");
+//       if (this.receipt) {
+//         if (onfinally) onfinally();
+//         // @ts-ignore
+//         return resolve(this.receipt);
+//       }
+//       if (this.err) {
+//         if (onfinally) onfinally();
+//         return reject(this.err);
+//       }
+//       this.emiter.on("receipt", rpt => {
+//         if (onfinally) onfinally();
+//         // @ts-ignore
+//         resolve(this.receipt);
+//       });
+//       this.emiter.on("error", err => {
+//         if (onfinally) onfinally();
+//         reject(err);
+//       });
+//     });
+//   }
+
+//   // fireError(err) {
+//   //   this.emiter.emit("error", err)
+//   //   if (promiEvent.reject) {
+//   //     promiEvent.reject(err)
+//   //   }
+//   // }
+// }
