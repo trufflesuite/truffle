068a4d8b37f4a52986fb85ff196aa075ee72b7e6
eggplantzzz <tmfeickert@gmail.com>
2020-07-20 16:50:35 -0400

    Add files for init project into core

---
+++ packages/core/lib/commands/init/source/contracts/Migrations.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Migrations {
+  address public owner;
+  uint public last_completed_migration;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier restricted() {
+    if (msg.sender == owner) _;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+}
