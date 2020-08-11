23c7b1ced5e443cc84dda4dc21affedda2da5a4e
Harry Altman <harry.j.altman@gmail.com>
2020-08-11 17:35:30 -0400

    Bump compilation target; add comment

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,3 +1,9 @@
+//Note: This class only exists due to weird Javascript stuff.  It should be
+//ExtendableError extends Error, like errors normally are, but for some reason
+//this was apparently causing problems with this.message not getting set
+//correctly.  Not clear why.  Leaving it alone for now.  But we should revisit
+//this later and change it to the proper way, and then eventually just get rid
+//of this class altogether.
 class ExtendableError {
   message: string;
   stack: string | undefined;
