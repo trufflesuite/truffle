104e1a380abadec8c77907b65afb8d38ed4ab631
Harry Altman <harry.j.altman@gmail.com>
2020-08-11 18:12:30 -0400

    Directly extend error (I hope this works!)

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,22 +1,11 @@
-//Note: This class only exists due to weird Javascript stuff.  It should be
-//ExtendableError extends Error, like errors normally are, but for some reason
-//this was apparently causing problems with this.message not getting set
-//correctly.  Not clear why.  Leaving it alone for now.  But we should revisit
-//this later and change it to the proper way, and then eventually just get rid
-//of this class altogether.
-class ExtendableError {
-  message: string;
-  stack: string | undefined;
-  name: string;
-
+//Note: This class only exists for compatibility with some old Javascript
+//stuff that avoided using Error directly for whatever reason.  Eventually
+//it should be eliminated.
+class ExtendableError extends Error {
   constructor(message: string) {
-    this.message = message;
-    this.stack = new Error().stack;
+    super(message);
     this.name = this.constructor.name;
   }
 }
-
-ExtendableError.prototype = Object.create(Error.prototype);
-ExtendableError.prototype.constructor = ExtendableError;
 
 export = ExtendableError;
