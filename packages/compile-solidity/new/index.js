5bf72269b9363c8776844e6f133bdbd16b46f518
eggplantzzz <tmfeickert@gmail.com>
2020-08-12 10:42:47 -0400

    Change return format for compile/new

--- packages/compile-solidity/new/index.js
+++ packages/compile-solidity/new/index.js
@@ -7,6 +7,7 @@ const Profiler = require("../profiler");
 const CompilerSupplier = require("../compilerSupplier");
 const { run } = require("../run");
 const { normalizeOptions } = require("../legacy/options");
+const { promisify } = require("util");
 
 // Most basic of the compile commands. Takes a hash of sources, where
 // the keys are file or module paths and the values are the bodies of
@@ -18,7 +19,7 @@ const { normalizeOptions } = require("../legacy/options");
 //   quiet: false,
 //   logger: console
 // }
-const compile = async function(sources, options) {
+const compile = async function (sources, options) {
   return await run(sources, normalizeOptions(options));
 };
 
@@ -26,7 +27,7 @@ const compile = async function(sources, options) {
 // quiet: Boolean. Suppress output. Defaults to false.
 // strict: Boolean. Return compiler warnings as errors. Defaults to false.
 // files: Array<String>. Explicit files to compile besides detected sources
-compile.all = async function(options) {
+compile.all = async function (options) {
   const paths = [
     ...new Set([
       ...(await findContracts(options.contracts_directory)),
@@ -35,9 +36,7 @@ compile.all = async function(options) {
   ];
 
   return await compile.with_dependencies(
-    Config.default()
-      .merge(options)
-      .merge({ paths })
+    Config.default().merge(options).merge({ paths })
   );
 };
 
@@ -48,19 +47,17 @@ compile.all = async function(options) {
 // quiet: Boolean. Suppress output. Defaults to false.
 // strict: Boolean. Return compiler warnings as errors. Defaults to false.
 // files: Array<String>. Explicit files to compile besides detected sources
-compile.necessary = async function(options) {
+compile.necessary = async function (options) {
   options.logger = options.logger || console;
 
   const paths = await Profiler.updated(options);
 
   return await compile.with_dependencies(
-    Config.default()
-      .merge(options)
-      .merge({ paths })
+    Config.default().merge(options).merge({ paths })
   );
 };
 
-compile.with_dependencies = async function(options) {
+compile.with_dependencies = async function (options) {
   options.logger = options.logger || console;
   options.contracts_directory = options.contracts_directory || process.cwd();
 
@@ -71,36 +68,38 @@ compile.with_dependencies = async function(options) {
     "resolver"
   ]);
 
-  var config = Config.default().merge(options);
+  const config = Config.default().merge(options);
 
-  const { allSources, required } = await new Promise((accept, reject) => {
-    Profiler.required_sources(
-      config.with({
-        paths: options.paths,
-        base_path: options.contracts_directory,
-        resolver: options.resolver
-      }),
-      (err, allSources, required) => {
-        if (err) {
-          return reject(err);
-        }
+  const { allSources, required } = await promisify(Profiler.required_sources)(
+    config.with({
+      paths: options.paths,
+      base_path: options.contracts_directory,
+      resolver: options.resolver
+    })
+  );
 
-        return accept({ allSources, required });
-      }
-    );
-  });
-
-  var hasTargets = required.length;
+  const hasTargets = required.length;
 
   hasTargets
     ? this.display(required, options)
     : this.display(allSources, options);
 
   options.compilationTargets = required;
-  return await compile(allSources, options);
+  const { sourceIndexes, contracts, compilerInfo } = await run(
+    allSources,
+    normalizeOptions(options)
+  );
+  const { name, version } = compilerInfo;
+  return [
+    {
+      sourceIndexes,
+      contracts,
+      compiler: { name, version }
+    }
+  ];
 };
 
-compile.display = function(paths, options) {
+compile.display = function (paths, options) {
   if (options.quiet !== true) {
     if (!Array.isArray(paths)) {
       paths = Object.keys(paths);
