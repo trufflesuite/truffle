e4a71461cc87a3121d1ba576d3d07b2b69a9545f
eggplantzzz <tmfeickert@gmail.com>
2020-08-12 17:14:58 -0400

    Add some typings to @truffle/compile-common to be used later

--- packages/compile-common/src/index.ts
+++ packages/compile-common/src/index.ts
@@ -1,2 +1,61 @@
 import { Profiler } from "./profiler";
 export { Profiler };
+
+type Compilation = {
+  sourceIndexes: string[];
+  contracts: CompiledContract[];
+  compiler: {
+    name: string;
+    version: string;
+  };
+};
+
+type CompilerResult = Compilation[];
+
+type CompiledContract = {
+  contractName: string;
+  contract_name: string;
+  sourcePath: string;
+  source: string;
+  sourceMap: string;
+  deployedSourceMap: string;
+  legacyAST: object;
+  ast: object;
+  abi: object[];
+  metadata: string;
+  bytecode: string;
+  deployedBytecode: string;
+  compiler: {
+    name: string;
+    version: string;
+  };
+  devdoc: object;
+  userdoc: object;
+};
+
+type ContractObject = {
+  contract_name: string;
+  sourcePath: string;
+  source: string;
+  sourceMap: string;
+  deployedSourceMap: string;
+  legacyAST: object;
+  ast: object;
+  abi: object[];
+  metadata: string;
+  bytecode: string;
+  deployedBytecode: string;
+  unlinked_binary: string;
+  compiler: {
+    name: string;
+    version: string;
+  };
+  devdoc: object;
+  userdoc: object;
+  immutableReferences;
+};
+
+interface WorkflowCompileResult {
+  compilations: Compilation[];
+  contracts: ContractObject[];
+}
