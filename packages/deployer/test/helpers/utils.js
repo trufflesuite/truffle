89d6897346cadb8512cae7f2d98898899134a441
eggplantzzz <tmfeickert@gmail.com>
2020-08-13 16:30:17 -0400

    Update some tests

--- packages/deployer/test/helpers/utils.js
+++ packages/deployer/test/helpers/utils.js
@@ -1,5 +1,5 @@
 const TruffleContract = require("@truffle/contract");
-const workflow = require("@truffle/workflow-compile");
+const Contracts = require("@truffle/workflow-compile/new");
 const path = require("path");
 const fs = require("fs-extra");
 
@@ -13,33 +13,32 @@ const utils = {
   buildDir: path.join(__dirname, "../build"),
   sourcesDir: path.join(__dirname, "../sources"),
 
-  compile: async function() {
+  compile: async function () {
     const config = {
       contracts_build_directory: utils.buildDir,
       contracts_directory: utils.sourcesDir
     };
 
-    return new Promise((accept, reject) => {
-      workflow.compile(config, err => (err ? reject(err) : accept()));
-    });
+    const compilation = await Contracts.compile(config);
+    await Contracts.save(config, compilation.contracts);
   },
 
-  evm_mine: function(web3) {
-    return new Promise(function(accept, reject) {
+  evm_mine: function (web3) {
+    return new Promise(function (accept, reject) {
       web3.currentProvider.send(
         {
           jsonrpc: "2.0",
           method: "evm_mine",
           id: new Date().getTime()
         },
-        function(err, result) {
+        function (err, result) {
           err ? reject(err) : accept(result);
         }
       );
     });
   },
 
-  startAutoMine: function(web3, interval) {
+  startAutoMine: function (web3, interval) {
     utils.miningId = setInterval(async () => {
       await utils.evm_mine(web3);
     }, interval);
@@ -51,7 +50,7 @@ const utils = {
 
   cleanUp: () => fs.removeSync(utils.buildDir),
 
-  getContract: function(name, provider, networkId, account) {
+  getContract: function (name, provider, networkId, account) {
     const json = require(`../build/${name}`);
     const contract = TruffleContract(json);
     contract.setProvider(provider);
