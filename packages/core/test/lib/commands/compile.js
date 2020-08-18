d4603730d7ceb6c07121d4d49b14e27db582b9ca
eggplantzzz <tmfeickert@gmail.com>
2020-08-18 17:28:12 -0400

    Remove some garbage accidentally left in

--- packages/core/test/lib/commands/compile.js
+++ packages/core/test/lib/commands/compile.js
@@ -1,6 +1,6 @@
 const assert = require("chai").assert;
 const Box = require("@truffle/box");
-const Contracts = requiAndSavere("@truffle/workflow-compile/new");
+const Contracts = require("@truffle/workflow-compile/new");
 const Artifactor = require("@truffle/artifactor");
 const Resolver = require("@truffle/resolver");
 const MemoryStream = require("memorystream");
