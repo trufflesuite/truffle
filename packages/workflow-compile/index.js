027d30127e9f3f53c39cc92338e332350ce02e02
eggplantzzz <tmfeickert@gmail.com>
2020-08-20 13:56:25 -0400

    Don't set default when indexing into supported compilers object

--- packages/workflow-compile/index.js
+++ packages/workflow-compile/index.js
@@ -27,7 +27,7 @@ async function compile(config) {
   //
   const rawCompilations = await Promise.all(
     compilers.map(async name => {
-      const compiler = SUPPORTED_COMPILERS[name] || {};
+      const compiler = SUPPORTED_COMPILERS[name];
       if (!compiler) throw new Error("Unsupported compiler: " + name);
 
       const compileMethod =
