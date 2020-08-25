e45b4422b7ac2ad09d2dc0f8757f260ce4df4738
Harry Altman <harry.j.altman@gmail.com>
2020-08-24 20:26:38 -0400

    Improve version checking during testing

--- packages/core/lib/test.js
+++ packages/core/lib/test.js
@@ -1,6 +1,7 @@
 const colors = require("colors");
 const chai = require("chai");
 const path = require("path");
+const fs = require("fs-extra");
 const {
   Web3Shim,
   createInterfaceAdapter
@@ -20,13 +21,15 @@ const Codec = require("@truffle/codec");
 const debug = require("debug")("lib:test");
 const Debugger = require("@truffle/debugger");
 const semver = require("semver");
+const Native = require("@truffle/compile-solidity/compilerSupplier/loadingStrategies/Native");
+const Local = require("@truffle/compile-solidity/compilerSupplier/loadingStrategies/Local");
 
 let Mocha; // Late init with "mocha" or "mocha-parallel-tests"
 
 chai.use(require("./assertions"));
 
 const Test = {
-  run: async function(options) {
+  run: async function (options) {
     expect.options(options, [
       "contracts_directory",
       "contracts_build_directory",
@@ -61,7 +64,7 @@ const Test = {
     // e.g., https://github.com/ethereum/web3.js/blob/master/lib/web3/allevents.js#L61
     // Output looks like this during tests: https://gist.github.com/tcoulter/1988349d1ec65ce6b958
     const warn = config.logger.warn;
-    config.logger.warn = function(message) {
+    config.logger.warn = function (message) {
       if (message === "cannot find event for log") {
         return;
       } else {
@@ -163,7 +166,7 @@ const Test = {
     });
   },
 
-  createMocha: function(config) {
+  createMocha: function (config) {
     // Allow people to specify config.mocha in their config.
     const mochaConfig = config.mocha || {};
 
@@ -190,11 +193,11 @@ const Test = {
     return mocha;
   },
 
-  getAccounts: function(interfaceAdapter) {
+  getAccounts: function (interfaceAdapter) {
     return interfaceAdapter.getAccounts();
   },
 
-  compileContractsWithTestFilesIfNeeded: async function(
+  compileContractsWithTestFilesIfNeeded: async function (
     solidityTestFiles,
     config,
     testResolver
@@ -211,6 +214,13 @@ const Test = {
     });
     if (config.compileAllDebug) {
       let versionString = ((compileConfig.compilers || {}).solc || {}).version;
+      //if version was native or local, must determine what version that
+      //actually corresponds to
+      if (versionString === "native") {
+        versionString = new Native().load().version();
+      } else if (fs.existsSync(version) && path.isAbsolute(version)) {
+        versionString = new Local({ version: versionString }).load().version();
+      }
       //note: I'm relying here on the fact that the current
       //default version, 0.5.16, is <0.6.3
       //the following line works with prereleases
@@ -256,7 +266,7 @@ const Test = {
     };
   },
 
-  performInitialDeploy: function(config, resolver) {
+  performInitialDeploy: function (config, resolver) {
     const migrateConfig = config.with({
       reset: true,
       resolver: resolver,
@@ -272,7 +282,7 @@ const Test = {
     }
   },
 
-  setJSTestGlobals: async function({
+  setJSTestGlobals: async function ({
     config,
     web3,
     interfaceAdapter,
@@ -292,7 +302,7 @@ const Test = {
         //HACK: both of the following should go by means
         //of the provisioner, but I'm not sure how to make
         //that work at the moment
-        contract.reloadJson = function() {
+        contract.reloadJson = function () {
           const reloaded = testResolver.require(importPath);
           this._json = reloaded._json;
         };
@@ -325,39 +335,39 @@ const Test = {
       return await hook.debug(operation);
     };
 
-    const template = function(tests) {
+    const template = function (tests) {
       this.timeout(runner.TEST_TIMEOUT);
 
-      before("prepare suite", async function() {
+      before("prepare suite", async function () {
         this.timeout(runner.BEFORE_TIMEOUT);
         await runner.initialize();
       });
 
-      beforeEach("before test", async function() {
+      beforeEach("before test", async function () {
         await runner.startTest();
       });
 
-      afterEach("after test", async function() {
+      afterEach("after test", async function () {
         await runner.endTest(this);
       });
 
       tests(accounts);
     };
 
-    global.contract = function(name, tests) {
-      Mocha.describe("Contract: " + name, function() {
+    global.contract = function (name, tests) {
+      Mocha.describe("Contract: " + name, function () {
         template.bind(this, tests)();
       });
     };
 
-    global.contract.only = function(name, tests) {
-      Mocha.describe.only("Contract: " + name, function() {
+    global.contract.only = function (name, tests) {
+      Mocha.describe.only("Contract: " + name, function () {
         template.bind(this, tests)();
       });
     };
 
-    global.contract.skip = function(name, tests) {
-      Mocha.describe.skip("Contract: " + name, function() {
+    global.contract.skip = function (name, tests) {
+      Mocha.describe.skip("Contract: " + name, function () {
         template.bind(this, tests)();
       });
     };
--- packages/core/lib/testing/Deployed.js
+++ packages/core/lib/testing/Deployed.js
@@ -1,9 +1,12 @@
 const web3Utils = require("web3-utils");
 const semver = require("semver");
 const Native = require("@truffle/compile-solidity/compilerSupplier/loadingStrategies/Native");
+const Local = require("@truffle/compile-solidity/compilerSupplier/loadingStrategies/Local");
+const path = require("path");
+const fs = require("fs-extra");
 
 var Deployed = {
-  makeSolidityDeployedAddressesLibrary: function(
+  makeSolidityDeployedAddressesLibrary: function (
     mapping,
     { solc: { version } }
   ) {
@@ -15,7 +18,7 @@ var Deployed = {
       "pragma solidity >= 0.5.0 < 0.7.0; \n\n library DeployedAddresses {" +
       "\n";
 
-    Object.keys(mapping).forEach(function(name) {
+    Object.keys(mapping).forEach(function (name) {
       var address = mapping[name];
 
       var body = "revert();";
@@ -37,18 +40,42 @@ var Deployed = {
 
     source += "}";
 
+    //note: default version is 0.5.16 which does not require any modifications
+    //so we can skip this block for it
     if (version) {
-      if (version === "native") version = new Native().load().version();
-      const v = semver.coerce(version);
-      if (semver.lt(v, "0.5.0")) source = source.replace(/ payable/gm, "");
-      source = source.replace(/0\.5\.0/gm, v);
+      //if version was native or local, must determine what version that
+      //actually corresponds to
+      if (version === "native") {
+        version = new Native().load().version();
+      } else if (fs.existsSync(version) && path.isAbsolute(version)) {
+        version = new Local({ version }).load().version();
+      }
+      //otherwise, version is a version string or version range string
+      const versionBefore = semver.satisfies(version, "0.4.x || <0.4.0", {
+        includePrerelease: true,
+        loose: true
+      });
+      const rangeBefore =
+        semver.validRange(version) &&
+        semver.gtr("0.5.0", versionString, {
+          includePrerelease: true,
+          loose: true
+        }); //gtr will throw if given undefined so must ward against
+      if (versionBefore || rangeBefore) {
+        //remove "payable"s if we're before 0.5.0
+        source = source.replace(/ payable/gm, "");
+      }
+      //regardless of version, replace all pragmas with the new version
+      const coercedVersion =
+        semver.validRange(version) || semver.coerce(version).toString();
+      source = source.replace(/0\.5\.0/gm, coercedVersion);
     }
 
     return source;
   },
 
   // Pulled from ethereumjs-util, but I don't want all its dependencies at the moment.
-  toChecksumAddress: function(address) {
+  toChecksumAddress: function (address) {
     address = address.toLowerCase().replace("0x", "");
     const hash = web3Utils.sha3(address).replace("0x", "");
     var ret = "0x";
