3461ee22fd1524276a6fe2efe91137356a920f0f
eggplantzzz <tmfeickert@gmail.com>
2020-09-08 10:57:28 -0400

    Correct mistake in calling compileAndSave with a callback

--- packages/core/lib/commands/opcode.js
+++ packages/core/lib/commands/opcode.js
@@ -1,4 +1,4 @@
-var command = {
+const command = {
   command: "opcode",
   description: "Print the compiled opcodes for a given contract",
   builder: {
@@ -18,55 +18,54 @@ var command = {
     ]
   },
   run: function (options, done) {
-    var Config = require("@truffle/config");
-    var TruffleError = require("@truffle/error");
-    var Contracts = require("@truffle/workflow-compile");
-    var CodeUtils = require("@truffle/code-utils");
+    const Config = require("@truffle/config");
+    const TruffleError = require("@truffle/error");
+    const Contracts = require("@truffle/workflow-compile");
+    const CodeUtils = require("@truffle/code-utils");
 
     if (options._.length === 0) {
       return done(new TruffleError("Please specify a contract name."));
     }
 
     var config = Config.detect(options);
-    Contracts.compileAndSave(config, function (err) {
-      if (err) return done(err);
-
-      var contractName = options._[0];
-      var Contract;
-      try {
-        Contract = config.resolver.require(contractName);
-      } catch (e) {
-        return done(
-          new TruffleError(
-            'Cannot find compiled contract with name "' + contractName + '"'
-          )
-        );
-      }
-
-      var bytecode = Contract.deployedBytecode;
-      var numInstructions = Contract.deployedSourceMap.split(";").length;
-
-      if (options.creation) {
-        bytecode = Contract.bytecode;
-        numInstructions = Contract.sourceMap.split(";").length;
-      }
-
-      var opcodes = CodeUtils.parseCode(bytecode, numInstructions);
-
-      var indexLength = (opcodes.length + "").length;
-
-      opcodes.forEach(function (opcode, index) {
-        var strIndex = index + ":";
-
-        while (strIndex.length < indexLength + 1) {
-          strIndex += " ";
+    Contracts.compileAndSave(config)
+      .then(() => {
+        const contractName = options._[0];
+        let Contract;
+        try {
+          Contract = config.resolver.require(contractName);
+        } catch (e) {
+          return done(
+            new TruffleError(
+              'Cannot find compiled contract with name "' + contractName + '"'
+            )
+          );
         }
 
-        console.log(
-          strIndex + " " + opcode.name + " " + (opcode.pushData || "")
-        );
-      });
-    });
+        let bytecode = Contract.deployedBytecode;
+        let numInstructions = Contract.deployedSourceMap.split(";").length;
+
+        if (options.creation) {
+          bytecode = Contract.bytecode;
+          numInstructions = Contract.sourceMap.split(";").length;
+        }
+        const opcodes = CodeUtils.parseCode(bytecode, numInstructions);
+
+        const indexLength = (opcodes.length + "").length;
+
+        opcodes.forEach((opcode, index) => {
+          let strIndex = index + ":";
+
+          while (strIndex.length < indexLength + 1) {
+            strIndex += " ";
+          }
+
+          console.log(
+            strIndex + " " + opcode.name + " " + (opcode.pushData || "")
+          );
+        });
+      })
+      .catch(done);
   }
 };
 
