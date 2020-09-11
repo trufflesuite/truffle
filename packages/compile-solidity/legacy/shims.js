e69aa4f04e2c3546257b2ac679c1fe051baeea70
eggplantzzz <tmfeickert@gmail.com>
2020-09-11 10:57:31 -0400

    Remove other unused shim

--- packages/compile-solidity/legacy/shims.js
+++ packages/compile-solidity/legacy/shims.js
@@ -12,48 +12,6 @@ function shimOutput({ contracts: list, sourceIndexes, compiler }) {
   return [contracts, sourceIndexes, compiler];
 }
 
-function shimBytecode(bytecode) {
-  if (!bytecode) {
-    return bytecode;
-  }
-  if (typeof bytecode === "string") {
-    return bytecode;
-  }
-
-  let { bytes, linkReferences } = bytecode;
-
-  linkReferences = linkReferences || [];
-
-  // inline link references - start by flattening the offsets
-  const flattenedLinkReferences = linkReferences
-    // map each link ref to array of link refs with only one offset
-    .map(({ offsets, length, name }) =>
-      offsets.map(offset => ({ offset, length, name }))
-    )
-    // flatten
-    .reduce((a, b) => [...a, ...b], []);
-
-  // then overwite bytes with link reference
-  bytes = flattenedLinkReferences.reduce((bytes, { offset, name, length }) => {
-    // length is a byte offset
-    const characterLength = length * 2;
-
-    let linkId = `__${name.slice(0, characterLength - 2)}`;
-    while (linkId.length < characterLength) {
-      linkId += "_";
-    }
-
-    const start = offset * 2;
-
-    return `${bytes.substring(0, start)}${linkId}${bytes.substring(
-      start + characterLength
-    )}`;
-  }, bytes);
-
-  return `0x${bytes}`;
-}
-
 module.exports = {
-  shimOutput,
-  shimBytecode
+  shimOutput
 };
