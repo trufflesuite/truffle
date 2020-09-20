7577384610643c1451f442726348de78c488b166
Amal Sudama <cds.sudama@gmail.com>
2020-09-20 14:33:49 -0400

    use callback that has this context

--- packages/core/lib/commands/create/templates/example.js
+++ packages/core/lib/commands/create/templates/example.js
@@ -1,7 +1,12 @@
 const Example = artifacts.require("Example");
 
-contract("Example", () => {
-  it("should assert true", async () => {
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("Example", function (/* accounts */) {
+  it("should assert true", async function () {
     await Example.deployed();
     return assert.isTrue(true);
   });
