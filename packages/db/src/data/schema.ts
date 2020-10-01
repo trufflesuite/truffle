3bc3b479bdf517f8102886540fc820e1ba26df59
fainashalts <faina@trufflesuite.com>
2020-10-01 09:03:09 -0700

    Remove loader subschema

--- packages/db/src/data/schema.ts
+++ packages/db/src/data/schema.ts
@@ -1,13 +1,11 @@
 import { scopeSchemas } from "./utils";
 
 import { schema as workspaceSchema } from "@truffle/db/workspace";
-import { loaderSchema } from "@truffle/db/loaders";
 
 // current subschemas come from workspace and loaders; eventually there will be one root schema
 // which will be the schema found in workspace
 export const schema = scopeSchemas({
   subschemas: {
-    workspace: workspaceSchema,
-    loaders: loaderSchema
+    workspace: workspaceSchema
   }
 });
