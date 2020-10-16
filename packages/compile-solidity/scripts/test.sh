bddada24a021e359e9c2f9ca3f76f0bf5fc49588
eggplantzzz <tmfeickert@gmail.com>
2020-10-16 11:00:29 -0400

    Increase the timeout for tests once more

--- packages/compile-solidity/scripts/test.sh
+++ packages/compile-solidity/scripts/test.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 if [ "$CI" = true ]; then
-  mocha ./test/** ./test/**/* --timeout 30000 $@
+  mocha ./test/** ./test/**/* --timeout 40000 $@
 else
   rm -rf ./node_modules/.cache/truffle
-  mocha ./test/** ./test/**/* --invert --grep native --timeout 30000 $@
+  mocha ./test/** ./test/**/* --invert --grep native --timeout 40000 $@
 fi
