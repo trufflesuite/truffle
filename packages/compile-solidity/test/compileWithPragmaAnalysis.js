32cf3dfd8633dde3736cb089d5b024009a7d2dff
eggplantzzz <tmfeickert@gmail.com>
2020-10-16 10:23:24 -0400

    Mock request to get solc versions

--- packages/compile-solidity/test/compileWithPragmaAnalysis.js
+++ packages/compile-solidity/test/compileWithPragmaAnalysis.js
@@ -1,6 +1,8 @@
 const assert = require("assert");
 const Config = require("@truffle/config");
+const { CompilerSupplier } = require("../index");
 const Resolver = require("@truffle/resolver");
+const sinon = require("sinon");
 const { compileWithPragmaAnalysis } = require("../compileWithPragmaAnalysis");
 const path = require("path");
 let paths = [];
@@ -20,6 +22,53 @@ const config = new Config().with({
   }
 });
 
+const releases = {
+  prereleases: [],
+  releases: [
+    "0.7.3",
+    "0.7.2",
+    "0.7.1",
+    "0.7.0",
+    "0.6.12",
+    "0.6.11",
+    "0.6.10",
+    "0.6.9",
+    "0.6.8",
+    "0.6.7",
+    "0.6.6",
+    "0.6.5",
+    "0.6.4",
+    "0.6.3",
+    "0.6.2",
+    "0.6.1",
+    "0.6.0",
+    "0.5.17",
+    "0.5.16",
+    "0.5.15",
+    "0.5.14",
+    "0.5.13",
+    "0.5.12",
+    "0.5.11",
+    "0.5.10",
+    "0.5.9",
+    "0.5.8",
+    "0.5.7",
+    "0.5.6",
+    "0.5.5",
+    "0.5.4",
+    "0.5.4",
+    "0.5.3",
+    "0.5.2",
+    "0.5.1",
+    "0.5.0",
+    "0.4.25",
+    "0.4.24",
+    "0.4.23",
+    "0.4.22"
+  ],
+  latestRelease: "0.7.3"
+};
+
 // she needs a resolver!
 config.resolver = new Resolver(config);
 
@@ -31,6 +80,11 @@ describe("compileWithPragmaAnalysis", () => {
         path.join(sourceDirectory, "noImports", "SourceWith0.6.0.sol"),
         path.join(sourceDirectory, "noImports", "SourceWith0.7.0.sol")
       ];
+      sinon.stub(CompilerSupplier.prototype, "getReleases").returns(releases);
+    });
+
+    after(() => {
+      CompilerSupplier.prototype.getReleases.restore();
     });
 
     // note that it will find the newest version of Solidity that satisifes
