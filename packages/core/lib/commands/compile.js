89c935f30a7efe657dfc8f61725bcaa9cea3cfc5
fainashalts <faina@trufflesuite.com>
2020-10-19 10:36:38 -0700

    Add timeout to last test in build scenario

--- packages/core/lib/commands/compile.js
+++ packages/core/lib/commands/compile.js
@@ -93,6 +93,9 @@ const command = {
         JSON.stringify(compilationOutput),
         {encoding: "utf8"}
       );
+      //add flag for saving compilation results in Truffle DB
+      config.loadDb = true;
+      return await WorkflowCompile.save(config, compilationOutput);
     }
 
     return await WorkflowCompile.save(config, compilationOutput);
