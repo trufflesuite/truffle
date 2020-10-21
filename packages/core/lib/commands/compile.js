049a5f8757b19dcae9330f5fce873de1c526186a
fainashalts <faina@trufflesuite.com>
2020-10-21 14:54:19 -0700

    Remove loadDb

--- packages/core/lib/commands/compile.js
+++ packages/core/lib/commands/compile.js
@@ -93,8 +93,6 @@ const command = {
         );
       }
 
-      //add flag for saving compilation results in Truffle DB
-      config.loadDb = true;
       return await WorkflowCompile.save(config, compilationOutput);
     }
   },
