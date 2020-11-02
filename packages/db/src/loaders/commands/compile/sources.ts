d4e424a19f69166c81cb0f797eb1741dcd07b544
g. nicholas d'andrea <gnidan@trufflesuite.com>
2020-11-02 00:25:22 -0500

    [wip] get artifacts working

--- packages/db/src/loaders/commands/compile/sources.ts
+++ packages/db/src/loaders/commands/compile/sources.ts
@@ -30,16 +30,15 @@ interface Compilation {
 export function* generateCompilationsSourcesLoad(
   compilations: Compilation[]
 ): Process<
-  Replace<
-    Compilation, {
-      contracts: (Contract & {
-        db: {
-          source: IdObject<DataModel.Source>;
-        }
-      })[];
-    }
-  >[]
+  (Compilation & {
+    contracts: (Contract & {
+      db: {
+        source: IdObject<DataModel.Source>;
+      };
+    })[];
+  })[]
 > {
+  debug("preparing to add sources");
   const { batch, unbatch } = prepareSourcesBatch(compilations);
 
   const sources = yield* generate.load("sources", batch);
