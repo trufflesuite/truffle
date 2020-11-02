72785e1fc5b50658f8eccb4ce3536015f63578b4
fainashalts <faina@trufflesuite.com>
2020-11-02 10:50:12 -0800

    Pull out .db test, use different project path, update config for that path

--- packages/truffle/test/scenarios/compile/compile.js
+++ packages/truffle/test/scenarios/compile/compile.js
@@ -61,12 +61,6 @@ describe("Repeated compilation of contracts with inheritance [ @standalone ]", f
     fse.writeFileSync(mapping[key].sourcePath, source);
   }
 
-  function checkForDb(config) {
-    const dbPath = path.join(config.working_directory, ".db/contracts");
-    const dbExists = fse.pathExistsSync(dbPath);
-    return dbExists;
-  }
-
   // ----------------------- Setup -----------------------------
 
   before("set up the server", function (done) {
@@ -84,7 +78,6 @@ describe("Repeated compilation of contracts with inheritance [ @standalone ]", f
     config = conf;
     config.network = "development";
     config.logger = logger;
-    config.db = { enabled: true };
     config.mocha = {
       reporter: new Reporter(logger)
     };
@@ -391,10 +384,39 @@ describe("Repeated compilation of contracts with inheritance [ @standalone ]", f
       throw new Error(err);
     }
   });
+});
 
-  it("creates a .db directory when db is enabled", async () => {
-    await CommandRunner.run("compile", config);
-    const dbExists = await checkForDb(config);
+describe("Compilation with db enabled", async () => {
+  const logger = new MemoryLogger();
+
+  function checkForDb(config) {
+    const dbPath = path.join(config.working_directory, ".db");
+    const dbExists = fse.pathExistsSync(dbPath);
+    return dbExists;
+  }
+
+  before("set up the server", function (done) {
+    Server.start(done);
+  });
+
+  after("stop server", function (done) {
+    Server.stop(done);
+  });
+
+  it("creates a .db directory when db is enabled", async function () {
+    this.timeout(12000);
+    const project = path.join(__dirname, "../../sources/contract_names");
+    const config = await sandbox.create(project);
+
+    try {
+      await CommandRunner.run("compile", config);
+    } catch (error) {
+      output = logger.contents();
+      log(output);
+      throw new Error(error);
+    }
+
+    const dbExists = checkForDb(config);
     assert(dbExists === true);
   });
 });
