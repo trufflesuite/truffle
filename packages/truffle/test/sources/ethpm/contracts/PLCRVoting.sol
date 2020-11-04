27167ff0b043f8efb64aab9d1b359687c19f1399
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2020-11-04 13:30:44 -0500

    Revert "Revert "ethpmV3 support""

--- packages/truffle/test/sources/ethpm/contracts/PLCRVoting.sol
+++ packages/truffle/test/sources/ethpm/contracts/PLCRVoting.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "tokens/eip20/EIP20.sol";
+import "tokens/contracts/eip20/EIP20.sol";
 import "./Local.sol";
 
 contract PLCRVoting is EIP20, Local {
@@ -10,6 +10,6 @@ contract PLCRVoting is EIP20, Local {
     }
 
     function attrUUID(address _user, uint _pollID) public pure returns (bytes32 UUID) {
-        return keccak256(_user, _pollID);
+        return keccak256(abi.encodePacked(_user, _pollID));
     }
 }
