a9c92cb40c8d9b6104130529e5475655dd73ec93
g. nicholas d'andrea <gnidan@trufflesuite.com>
2020-11-09 20:42:11 -0500

    Move initialize to new architecture

--- packages/db/src/loaders/commands/index.ts
+++ packages/db/src/loaders/commands/index.ts
@@ -3,5 +3,4 @@ const debug = logger("db:loaders:commands");
 
 export { generateCompileLoad } from "./compile";
 export { generateNamesLoad } from "./names";
-export { generateInitializeLoad } from "./initialize";
 export { generateMigrateLoad } from "./migrate";
---
+++ packages/db/src/project/initialize/index.ts
@@ -0,0 +1,11 @@
+import { logger } from "@truffle/db/logger";
+const debug = logger("db:loaders:commands:initialize");
+
+import { IdObject, resources, Process } from "@truffle/db/project/process";
+
+export function* generateInitializeLoad({
+  directory
+}): Process<IdObject<DataModel.Project>> {
+  const [project] = yield* resources.load("projects", [{ directory }]);
+  return project;
+}
