51d30ce844882e7931744f5e5367a1e591385110
eggplantzzz <tmfeickert@gmail.com>
2020-11-10 17:50:06 -0500

    Update the create command to comply with new api

--- packages/core/lib/commands/create/helpers.js
+++ packages/core/lib/commands/create/helpers.js
@@ -1,6 +1,7 @@
 const copy = require("../../copy");
 const path = require("path");
 const fs = require("fs");
+const {promisify} = require("util");
 
 const templates = {
   test: {
@@ -17,33 +18,17 @@ const templates = {
   }
 };
 
-var processFile = function(file_path, processfn, callback) {
-  fs.readFile(file_path, { encoding: "utf8" }, function(err, data) {
-    if (err != null) {
-      callback(err);
-      return;
-    }
-
-    var result = processfn(data);
-    fs.writeFile(file_path, result, { encoding: "utf8" }, callback);
-  });
+const replaceContents = function (file_path, find, replacement) {
+  const data = fs.readFileSync(file_path, {encoding: "utf8"});
+  if (typeof find === "string") {
+    find = new RegExp(find, "g");
+  }
+  const result = data.replace(find, replacement);
+  fs.writeFileSync(file_path, result, {encoding: "utf8"});
 };
 
-var replaceContents = function(file_path, find, replacement, callback) {
-  processFile(
-    file_path,
-    function(data) {
-      if (typeof find === "string") {
-        find = new RegExp(find, "g");
-      }
-      return data.replace(find, replacement);
-    },
-    callback
-  );
-};
-
-var toUnderscoreFromCamel = function(string) {
-  string = string.replace(/([A-Z])/g, function($1) {
+const toUnderscoreFromCamel = function (string) {
+  string = string.replace(/([A-Z])/g, function ($1) {
     return "_" + $1.toLowerCase();
   });
 
@@ -54,78 +39,52 @@ var toUnderscoreFromCamel = function(string) {
   return string;
 };
 
-var Create = {
-  contract: function(directory, name, options, callback) {
-    if (typeof options === "function") {
-      callback = options;
-    }
-
-    var from = templates.contract.filename;
-    var to = path.join(directory, name + ".sol");
+const Create = {
+  contract: async function (directory, name, options) {
+    const from = templates.contract.filename;
+    const to = path.join(directory, name + ".sol");
 
     if (!options.force && fs.existsSync(to)) {
-      return callback(
-        new Error("Can not create " + name + ".sol: file exists")
-      );
+      throw new Error("Can not create " + name + ".sol: file exists");
     }
 
-    copy.file(from, to, function(err) {
-      if (err) return callback(err);
+    await promisify(copy.file)(from, to);
 
-      replaceContents(to, templates.contract.name, name, callback);
-    });
+    replaceContents(to, templates.contract.name, name);
   },
 
-  test: function(directory, name, options, callback) {
-    if (typeof options === "function") {
-      callback = options;
-    }
-
-    var underscored = toUnderscoreFromCamel(name);
+  test: async function (directory, name, options) {
+    let underscored = toUnderscoreFromCamel(name);
     underscored = underscored.replace(/\./g, "_");
-    var from = templates.test.filename;
-    var to = path.join(directory, underscored + ".js");
+    const from = templates.test.filename;
+    const to = path.join(directory, underscored + ".js");
 
     if (!options.force && fs.existsSync(to)) {
-      return callback(
-        new Error("Can not create " + underscored + ".js: file exists")
-      );
+      throw new Error("Can not create " + underscored + ".js: file exists");
     }
 
-    copy.file(from, to, function(err) {
-      if (err) return callback(err);
-
-      replaceContents(to, templates.contract.name, name, function(err) {
-        if (err) return callback(err);
-        replaceContents(to, templates.contract.variable, underscored, callback);
-      });
-    });
+    await promisify(copy.file)(from, to);
+    replaceContents(to, templates.contract.name, name);
+    replaceContents(to, templates.contract.variable, underscored);
   },
 
-  migration: function(directory, name, options, callback) {
-    if (typeof options === "function") {
-      callback = options;
-    }
-
-    var underscored = toUnderscoreFromCamel(name || "");
+  migration: async function (directory, name, options) {
+    let underscored = toUnderscoreFromCamel(name || "");
     underscored = underscored.replace(/\./g, "_");
-    var from = templates.migration.filename;
-    var filename = (new Date().getTime() / 1000) | 0; // Only do seconds.
+    const from = templates.migration.filename;
+    let filename = (new Date().getTime() / 1000) | 0; // Only do seconds.
 
     if (name != null && name !== "") {
       filename += "_" + underscored;
     }
 
     filename += ".js";
-    var to = path.join(directory, filename);
+    const to = path.join(directory, filename);
 
     if (!options.force && fs.existsSync(to)) {
-      return callback(
-        new Error("Can not create " + filename + ": file exists")
-      );
+      throw new Error("Can not create " + filename + ": file exists");
     }
-
-    copy.file(from, to, callback);
+    return promisify(copy.file)(from, to);
   }
 };
 
