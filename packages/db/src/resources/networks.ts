c482f36b8f6feb2bc7f6df86056643af1562a822
fainashalts <faina@trufflesuite.com>
2020-11-11 09:25:42 -0800

    Move network-specific types and resolvers to networks resource, change default syntax for limit

--- packages/db/src/resources/networks.ts
+++ packages/db/src/resources/networks.ts
@@ -9,12 +9,25 @@ export const networks: Definition<"networks"> = {
   createIndexes: [{fields: ["historicBlock.height"]}],
   idFields: ["networkId", "historicBlock"],
   typeDefs: gql`
+    type CandidateSearchResult {
+      network: Network!
+      alreadyTried: [ID]! #will include all networks returned
+    }
+
     type Network implements Resource & Named {
       id: ID!
       name: String!
       networkId: NetworkId!
       historicBlock: Block!
       fork: Network
+      possibleAncestors(
+        alreadyTried: [ID]!
+        limit: Int # will default to 5
+      ): [CandidateSearchResult]!
+      possibleDescendants(
+        alreadyTried: [ID]!
+        limit: Int # will default to 5
+      ): [CandidateSearchResult]!
     }
 
     scalar NetworkId
@@ -34,5 +47,79 @@ export const networks: Definition<"networks"> = {
       height: Int!
       hash: String!
     }
-  `
+  `,
+  resolvers: {
+    Network: {
+      possibleAncestors: {
+        resolve: async ({id}, {limit = 5, alreadyTried}, {workspace}) => {
+          const network = await workspace.get("networks", id);
+          const result = await workspace.find("networks", {
+            selector: {
+              "historicBlock.height": {
+                $gte: null,
+                $lt: network.historicBlock.height,
+                $ne: network.historicBlock.height
+              },
+              "networkId": network.networkId,
+              "id": {
+                $nin: alreadyTried
+              }
+            },
+            sort: [{"historicBlock.height": "desc"}],
+            limit
+          });
+
+          let untriedNetworks = result.map(network => {
+            return {
+              network,
+              alreadyTried: alreadyTried
+            };
+          });
+
+          return untriedNetworks;
+        }
+      },
+      possibleDescendants: {
+        resolve: async ({id}, {limit = 5, alreadyTried}, {workspace}) => {
+          const network = await workspace.get("networks", id);
+          const result = await workspace.find("networks", {
+            selector: {
+              "historicBlock.height": {
+                $gte: null,
+                $gt: network.historicBlock.height,
+                $ne: network.historicBlock.height
+              },
+              "networkId": network.networkId,
+              "id": {
+                $nin: alreadyTried
+              }
+            },
+            sort: [{"historicBlock.height": "asc"}],
+            limit
+          });
+
+          let untriedNetworks = result.map(network => {
+            return {
+              network,
+              alreadyTried: alreadyTried
+            };
+          });
+
+          return untriedNetworks;
+        }
+      }
+    },
+    CandidateSearchResult: {
+      network: {
+        resolve: async (parent, __, {}) => {
+          return parent.network;
+        }
+      },
+      alreadyTried: {
+        resolve: (parent, __, {}) => {
+          return parent.alreadyTried;
+        }
+      }
+    }
+  }
 };
