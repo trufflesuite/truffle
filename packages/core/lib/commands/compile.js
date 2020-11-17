ee0a7365cbcb9c8f3abcc503eeff0cb0d00a81fb
g. nicholas d'andrea <gnidan@trufflesuite.com>
2020-11-17 17:49:56 -0500

    Hook up name assignments in compile and migrate

--- packages/core/lib/commands/compile.js
+++ packages/core/lib/commands/compile.js
@@ -93,7 +93,9 @@ const command = {
         );
       }
 
-      return await WorkflowCompile.save(config, compilationOutput);
+      const result = await WorkflowCompile.save(config, compilationOutput);
+      await WorkflowCompile.assignNames(config, result);
+      return result;
     }
   },
 
