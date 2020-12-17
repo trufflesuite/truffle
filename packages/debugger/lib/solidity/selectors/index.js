fa582e0250f2fbf88bba5cae2f018942ad94e5ed
Harry Altman <harry.j.altman@gmail.com>
2020-12-17 02:46:07 -0500

    Don't flag source range final when jumping to an internal source

--- packages/debugger/lib/solidity/selectors/index.js
+++ packages/debugger/lib/solidity/selectors/index.js
@@ -260,9 +260,10 @@ let solidity = createSelectorTree({
     ...createMultistepSelectors(evm.current.step),
 
     /**
-     * solidity.current.isSourceRangeFinal
+     * solidity.current.isSourceRangeFinalRaw
+     * the old version; doesn't account for internal-source problems
      */
-    isSourceRangeFinal: createLeaf(
+    isSourceRangeFinalRaw: createLeaf(
       [
         "./instructionAtProgramCounter",
         evm.current.step.programCounter,
@@ -283,6 +284,24 @@ let solidity = createSelectorTree({
           current.length != next.length ||
           current.file != next.file
         );
+      }
+    ),
+
+    /**
+     * solidity.current.isSourceRangeFinal
+     * if there's no context change, then don't return final
+     * on jumping from a user source to an internal source
+     */
+    isSourceRangeFinal: createLeaf(
+      [
+        "./isSourceRangeFinalRaw",
+        "./source",
+        "/next/source",
+        evm.current.step.isContextChange
+      ],
+
+      (isFinal, currentSource, nextSource, changesContext) => {
+        return changesContext || (isFinal && (currentSource.internal || !nextSource.internal))
       }
     ),
 
