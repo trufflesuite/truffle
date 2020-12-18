ad423fd3018224b1726c460186f7c394b569d688
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-18 14:58:57 +0200

    [package.json]: Upgrade `ethereumjs-wallet` from `0.6.3` to `^1.0.1`
    Since version `1.0.0` ethereumjs-wallet uses `scrypt-js@^3.0.1` [1]
    instead of `scrypt.js@^0.3.0` or `@web3-js/scrypt-shim@^0.1.0`. These
    older packages (with `@web3-js/scrypt-shim` currently deprecated), used
    the native dependency `scrypt` which which fails to compile with
    Node.js 14.
    
    Even though `scrypt` is now marked as optional, this looses unnecessary
    amount of time for everyone involved who's using Node.js 14.
    
    `scrypt-js` is a pure JS implementation, and as such is fully compatible
    with newer Node.js versions.
    
    Also delete the whole `typings/ethereumjs-wallet/` folder as
    `ethereumjs-wallet` is written in TypeScript and there's no need for us
    to duplicate them (wrongly).
    
    [1]: https://github.com/ethereumjs/ethereumjs-wallet/commit/71d1c44d08948e4226b9ea75324e9e5059f008df

--- packages/hdwallet-provider/typings/ethereumjs-wallet/provider-engine.d.ts
+++
@@ -1,6 +0,0 @@
-import Wallet from "./index";
-declare const HookedWalletEthTxSubprovider: any;
-export default class WalletSubprovider extends HookedWalletEthTxSubprovider {
-  constructor(wallet: Wallet, opts?: any);
-}
-export {};
