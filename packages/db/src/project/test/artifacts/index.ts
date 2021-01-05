24d3b24c3ee7fc92bb39216966917e3ec943dddd
fainashalts <faina@trufflesuite.com>
2021-01-04 21:53:55 -0800

    Add missing import

--- packages/db/src/project/test/artifacts/index.ts
+++ packages/db/src/project/test/artifacts/index.ts
@@ -12,6 +12,7 @@ import { ContractObject } from "@truffle/contract-schema/spec";
 import { Project } from "@truffle/db/project";
 import { resources } from "@truffle/db/project/process";
 import { WorkflowCompileResult } from "@truffle/compile-common/src/types";
+import WorkflowCompile from "@truffle/workflow-compile";
 
 export class ArtifactsLoader {
   private db: Db;
