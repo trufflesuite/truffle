31e4965d2568edc86c3238a3acf667ade9cca60f
fainashalts <faina@trufflesuite.com>
2021-01-12 22:14:48 -0800

    Remove unneeded test network from config

--- packages/db/src/project/test/compilationSources/truffle-config.js
+++ packages/db/src/project/test/compilationSources/truffle-config.js
@@ -4,11 +4,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
-    },
-    test: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
     }
   }
 };
