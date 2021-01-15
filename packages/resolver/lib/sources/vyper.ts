8a2fd57eea5676194a63e584135c4f9eb50dd4ee
Harry Altman <harry.j.altman@gmail.com>
2021-01-15 04:01:33 -0500

    Fix remaining problems with Vyper module imports

--- packages/resolver/lib/sources/vyper.ts
+++ packages/resolver/lib/sources/vyper.ts
@@ -17,6 +17,7 @@ export class Vyper implements ResolverSource {
   constructor(wrappedSource: ResolverSource, contractsDirectory: string) {
     this.wrappedSource = wrappedSource;
     this.cache = {};
+    debug("contractsDirectory: %s", contractsDirectory);
     this.contractsDirectory = contractsDirectory;
   }
 
@@ -28,17 +29,29 @@ export class Vyper implements ResolverSource {
   async resolve(importModule: string, importedFrom: string) {
     importedFrom = importedFrom || "";
 
+    debug("importModule: %s", importModule);
+    debug("importedFrom: %s", importedFrom);
+
     //attempt to just resolve as if it's a file path rather than Vyper module
     const directlyResolvedSource =
         await this.wrappedSource.resolve(importModule, importedFrom);
     if (directlyResolvedSource.body) {
+      debug("found directly");
       return directlyResolvedSource;
     }
     //otherwise, it's time for some Vyper module processing...
 
+    //only attempt this if what we have looks like a Vyper module
+    if (!importModule.match(/^[\w.]+$/)) {
+      debug("clearly not a Vyper module");
+      return { body: undefined, filePath: undefined };
+    }
+
     const importPath = moduleToPath(importModule); //note: no file extension yet
+    debug("importPath: %s", importPath);
     const explicitlyRelative = importPath.startsWith("./") ||
       importPath.startsWith("../");
+    debug("explicitlyRelative: %o", explicitlyRelative);
 
     const possiblePaths = [];
     //first: check for JSON in local directory
@@ -61,17 +74,23 @@ export class Vyper implements ResolverSource {
         path.join(this.contractsDirectory, importPath + ".vy")
       );
     }
+    debug("possiblePaths: %O", possiblePaths);
 
     for (const possiblePath of possiblePaths) {
+      debug("possiblePath: %s", possiblePath);
       const resolvedSource = possiblePath in this.cache
         ? this.cache[possiblePath]
         : await this.wrappedSource.resolve(possiblePath, importedFrom);
 
-      this.cache[possiblePath] = resolvedSource; //yes, even failures are cached!
+      if (!(possiblePath in this.cache)) {
+        this.cache[possiblePath] = resolvedSource; //yes, even failures are cached!
+      }
 
       if (resolvedSource.body) {
+        debug("found");
         return resolvedSource;
       }
+      debug("not found");
     }
 
     //if not found, return nothing
@@ -91,10 +110,16 @@ export class Vyper implements ResolverSource {
 }
 
 function moduleToPath(moduleName: string): string {
-  const initialDotCount = (moduleName.match(/^\.*/) || []).length;
-  const withoutInitialDots = moduleName.replace(/^\.*/, "");
+  //first: get initial dot count by matching against regular expression for
+  //initial dots, then taking captured group (note: regular expression
+  //will always match so don't have to worry about null here) and taking
+  //length
+  const initialDotCount = moduleName.match(/^(\.*)/)[1].length;
+  //then: change rest of dots to slashes
+  const withoutInitialDots = moduleName.slice(initialDotCount);
   const pathWithoutDots = withoutInitialDots.replace(/\./g, path.sep);
   let initialDotPath;
+  //then: interpret initial dots
   switch (initialDotCount) {
     case 0:
       initialDotPath = "";
@@ -106,5 +131,6 @@ function moduleToPath(moduleName: string): string {
       initialDotPath = "../".repeat(initialDotCount - 1);
       break;
   }
+  //finally: combine
   return initialDotPath + pathWithoutDots;
 }
