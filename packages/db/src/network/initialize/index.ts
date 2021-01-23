6eef74a13c21e3459350e67fcedcd2f5ea6f2b68
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-01-22 22:43:38 -0500

    Define Network abstraction
    - Emulate Project abstraction for overall structure
    - Copy network/network genealogy loading from Project.LoadMigrate

---
+++ packages/db/src/network/initialize/index.ts
@@ -0,0 +1,33 @@
+/**
+ * @category Internal processor
+ * @packageDocumentation
+ */
+import { logger } from "@truffle/db/logger";
+const debug = logger("db:network:initialize");
+
+import type { Resource, Input } from "@truffle/db/resources";
+import { resources, Process } from "@truffle/db/process";
+import * as FetchNetworkId from "./networkId";
+import * as FetchGenesisBlock from "./genesisBlock";
+export { FetchNetworkId, FetchGenesisBlock };
+
+export function* process<
+  Network extends Omit<Input<"networks">, "networkId" | "historicBock">
+>(options: {
+  network: Network;
+}): Process<
+  Network & Pick<Resource<"networks">, "id" | keyof Input<"networks">>
+> {
+  const input = {
+    ...options.network,
+    networkId: yield* FetchNetworkId.process(),
+    historicBlock: yield* FetchGenesisBlock.process()
+  };
+
+  const [{ id }] = yield* resources.load("networks", [input]);
+
+  return {
+    id,
+    ...input
+  };
+}
---
+++ packages/db/src/network/transactionBlocks.ts
@@ -0,0 +1,36 @@
+/**
+ * @category Internal processor
+ * @packageDocumentation
+ */
+import { logger } from "@truffle/db/logger";
+const debug = logger("db:network:transactionBlocks");
+
+import type { Process } from "@truffle/db/process";
+import type { DataModel } from "@truffle/db/resources";
+
+export function* process(
+  transactionHashes: string[]
+): Process<DataModel.Block[], { web3: "eth_getTransactionByHash" }> {
+  const blocks: DataModel.Block[] = [];
+
+  for (const transactionHash of transactionHashes) {
+    const response = yield {
+      type: "web3",
+      method: "eth_getTransactionByHash",
+      params: [transactionHash]
+    };
+
+    if (!response || !response.result || !response.result.blockNumber) {
+      blocks.push(null);
+      continue;
+    }
+
+    const { blockHash: hash, blockNumber } = response.result;
+
+    const height = parseInt(blockNumber);
+
+    blocks.push({ height, hash });
+  }
+
+  return blocks;
+}
