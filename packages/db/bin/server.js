e58bc87bbfb3dbbbf0c59c788162d922a631be1c
eggplantzzz <tmfeickert@gmail.com>
2021-02-01 12:18:39 -0500

    Re-add Config.detect to serve function signature in server.js

--- packages/db/bin/server.js
+++ packages/db/bin/server.js
@@ -1,8 +1,9 @@
+const Config = require("@truffle/config");
 const { serve } = require("@truffle/db");
 
 const port = 4444;
 
-const server = serve();
+const server = serve(Config.detect());
 
 server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
