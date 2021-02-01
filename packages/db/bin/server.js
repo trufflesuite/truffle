a44b6f3b0763988c46cfc777fd7efcafdc9c0e9e
eggplantzzz <tmfeickert@gmail.com>
2021-02-01 13:41:44 -0500

    Keep argument to Config.detect in server.js

--- packages/db/bin/server.js
+++ packages/db/bin/server.js
@@ -3,7 +3,9 @@ const { serve } = require("@truffle/db");
 
 const port = 4444;
 
-const server = serve(Config.detect());
+const server = serve(Config.detect({
+  workingDirectory: process.argv[2] || process.cwd()
+}));
 
 server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
