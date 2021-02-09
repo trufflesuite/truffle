4e2e6fec0519f930d56b2d6d0575a65c215e373d
Harry Altman <harry.j.altman@gmail.com>
2021-02-08 20:58:56 -0500

    Add tests of compatibleNativize

---
+++ packages/decoder/test/current/contracts/CompatibleTest.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+contract CompatibleNativizeTest {
+
+  struct Pair {
+    string x;
+    string y;
+  }
+
+  event String(string z);
+  event TwoStrings(string w, string z);
+  event StringPair(Pair z);
+
+  function returnString() public pure returns (string memory z) {
+    return "hello";
+  }
+
+  function emitString() public {
+    emit String("hello");
+  }
+
+  function returnTwoStrings() public pure returns (string memory w, string memory z) {
+    return ("hello", "goodbye");
+  }
+
+  function emitTwoStrings() public {
+    emit TwoStrings("hello", "goodbye");
+  }
+
+  function returnStringPair() public pure returns (Pair memory z) {
+    return Pair("hello", "goodbye");
+  }
+
+  function emitStringPair() public {
+    emit StringPair(Pair("hello", "goodbye"));
+  }
+}
