58b380ecc323e4c08a66d34a8fb3f9c17a4d2090
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-02-24 18:58:38 -0500

    Simplify ugly Event component conditional

--- packages/db-kit/src/cli/components/codec/Event.tsx
+++ packages/db-kit/src/cli/components/codec/Event.tsx
@@ -11,37 +11,42 @@ export interface EventProps {
   decoding: LogDecoding | undefined;
 }
 
-export const Event = ({ log, decoding }: EventProps) => {
-  const element = !decoding ? (
-    <Box flexDirection="column">
-      <Text bold>Unknown event</Text>
-      <Text italic>Address:</Text>
+const UnknownEvent = ({ log }: Omit<EventProps, "decoding">) =>
+  <Box flexDirection="column">
+    <Text bold>Unknown event</Text>
+    <Text italic>Address:</Text>
+    <Box marginLeft={2}>
+      <Text color="gray">{log.address}</Text>
+    </Box>
+    <Text italic>Data:</Text>
+    <Box marginLeft={2}>
+      <Text color="gray">{log.data}</Text>
+    </Box>
+    <Text italic>Topics:</Text>
+    {log.topics.length === 0 ? (
       <Box marginLeft={2}>
-        <Text color="gray">{log.address}</Text>
+        <Text color="gray" italic>
+          (none)
+        </Text>
       </Box>
-      <Text italic>Data:</Text>
-      <Box marginLeft={2}>
-        <Text color="gray">{log.data}</Text>
-      </Box>
-      <Text italic>Topics:</Text>
-      {log.topics.length === 0 ? (
-        <Box marginLeft={2}>
-          <Text color="gray" italic>
-            (none)
+    ) : (
+      log.topics.map((topic, index) => (
+        <Box marginLeft={2} key={index}>
+          <Text color="gray">
+            {topic}
+            {index !== log.topics.length - 1 ? "," : ""}
           </Text>
         </Box>
-      ) : (
-        log.topics.map((topic, index) => (
-          <Box marginLeft={2} key={index}>
-            <Text color="gray">
-              {topic}
-              {index !== log.topics.length - 1 ? "," : ""}
-            </Text>
-          </Box>
-        ))
-      )}
-    </Box>
-  ) : (
+      ))
+    )}
+  </Box>;
+
+export const Event = ({ log, decoding }: EventProps) => {
+  if (!decoding) {
+    return <UnknownEvent log={log} />;
+  }
+
+  return (
     <Box flexDirection="column">
       <Text>
         {decoding.kind === "anonymous" ? (
@@ -66,6 +71,4 @@ export const Event = ({ log, decoding }: EventProps) => {
       <Text>)</Text>
     </Box>
   );
-
-  return <Box>{element}</Box>;
 };
