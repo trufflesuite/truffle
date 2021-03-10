283ea828b672d9e0664104a754a4f711efbb89fc
Rosco Kalis <roscokalis@gmail.com>
2021-03-10 17:34:14 +0100

    Use ganache in ipfs tests

--- packages/preserve-to-ipfs/test/ipfs.test.ts
+++ packages/preserve-to-ipfs/test/ipfs.test.ts
@@ -7,53 +7,36 @@ import { fetch } from "./utils/fetch";
 import { IpfsClient } from "../lib/ipfs-adapter";
 import { describeForNode12 } from "./utils/conditional";
 import { preserveToIpfs, preserveToIpfsWithEvents } from "./utils/preserve";
+import Ganache from "ganache";
 
-const IPFS_BIN = `${__dirname}/../node_modules/.bin/jsipfs`;
-
-interface IpfsNode {
-  apiAddr: {
-    toString(): string;
-  };
-  stop(): Promise<void>;
-}
+jest.setTimeout(20000);
 
 describeForNode12("preserve", () => {
-  const IpfsdCtl = require("ipfsd-ctl");
+  // Default IPFS nodes exposed by Ganache
+  const address = "http://localhost:5001/api/v0";
 
-  let node: IpfsNode;
-  let address: string;
+  let ganacheServer: any;
+  let ipfsClient: IpfsClient;
 
   beforeAll(async () => {
-    jest.setTimeout(20000);
+    ganacheServer = Ganache.server({ flavor: "filecoin" });
+    await ganacheServer.provider.blockchain.waitForReady();
+    await ganacheServer.listen(7777);
 
-    node = await IpfsdCtl.createController({
-      type: "js",
-      ipfsBin: IPFS_BIN,
-      test: true,
-      disposable: true,
-      ipfsHttpModule: createIpfsClient
-    });
-
-    address = node.apiAddr.toString();
+    ipfsClient = createIpfsClient({ url: address });
   });
 
   afterAll(async () => {
-    await node.stop();
+    await ganacheServer?.close();
   });
 
   for (const { name, target, events } of tests) {
     // separate describe block for each test case
     describe(`test: ${name}`, () => {
-      let ipfs: IpfsClient;
-
-      beforeAll(async () => {
-        ipfs = createIpfsClient({ url: address });
-      });
-
       it("saves correctly to IPFS", async () => {
         const cid = await preserveToIpfs(target, address);
 
-        const retrieved = await fetch({ cid, ipfs });
+        const retrieved = await fetch({ cid, ipfs: ipfsClient });
 
         expect(await Preserve.Targets.stringify(retrieved)).toEqual(
           await Preserve.Targets.stringify(target)
