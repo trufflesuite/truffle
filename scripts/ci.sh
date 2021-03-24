6b016e6179a63c93a2b3ebbe4a766f5f296de282
eggplantzzz <tmfeickert@gmail.com>
2021-03-24 11:04:53 -0400

    Update Geth start options

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -11,18 +11,17 @@ run_geth() {
     -p 8546:8546 \
     -p 30303:30303 \
     ethereum/client-go:stable \
-    --rpc \
-    --rpcaddr '0.0.0.0' \
-    --rpcport 8545 \
-    --rpccorsdomain '*' \
+    --http \
+    --http.addr '0.0.0.0' \
+    --http.port 8545 \
+    --http.corsdomain '*' \
     --ws \
-    --wsaddr '0.0.0.0' \
-    --wsorigins '*' \
+    --ws.addr '0.0.0.0' \
+    --ws.origins '*' \
     --nodiscover \
     --dev \
     --dev.period 0 \
     --allow-insecure-unlock \
-    --targetgaslimit '7000000' \
     js ./scripts/geth-accounts.js \
     > /dev/null &
 }
