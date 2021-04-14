f8e6b8f7ec921468cb85825d635076560670eba5
eggplantzzz <tmfeickert@gmail.com>
2021-04-14 14:59:31 -0400

    Add simple test for resolving artifacts in build directory

--- packages/resolver/test/npm.ts
+++ packages/resolver/test/npm.ts
@@ -3,14 +3,17 @@ import assert from "assert";
 import { NPM } from "../lib/sources/npm";
 const npm = new NPM("");
 
-describe("npm", function() {
-  describe("#require()", function() {
-    it("reads package name", function() {
+describe("npm", function () {
+  describe("#require()", function () {
+    it("returns file contents from artifacts in build/contracts", function () {
       let result = npm.require("package/Test", "./test/fixtures");
       assert.deepEqual(result, {});
     });
-
-    it("reads package name with /", function() {
+    it("returns file contents from artifacts in build dir", function () {
+      let result = npm.require("package/SomeArtifact", "./test/fixtures");
+      assert.deepEqual(result.hello, "mr. anderson");
+    });
+    it("reads package name with /", function () {
       let result = npm.require("@org/package/Test", "./test/fixtures");
       assert.deepEqual(result, {});
     });
