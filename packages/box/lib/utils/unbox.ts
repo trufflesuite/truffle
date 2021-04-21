b0e0e525962a020f572ed9c3327eaf32b594400a
eggplantzzz <tmfeickert@gmail.com>
2021-04-21 12:38:22 -0400

    Replace request-promise-native with axios

--- packages/box/lib/utils/unbox.ts
+++ packages/box/lib/utils/unbox.ts
@@ -1,7 +1,7 @@
 import fse from "fs-extra";
 import path from "path";
 import download from "download-git-repo";
-import rp from "request-promise-native";
+import axios from "axios";
 import vcsurl from "vcsurl";
 import { parse as parseURL } from "url";
 import { execSync } from "child_process";
@@ -12,7 +12,7 @@ import ignore from "ignore";
 
 function verifyLocalPath(localPath: string) {
   const configPath = path.join(localPath, "truffle-box.json");
-  fse.access(configPath).catch(e => {
+  fse.access(configPath).catch(_e => {
     throw new Error(`Truffle Box at path ${localPath} doesn't exist.`);
   });
 }
@@ -26,19 +26,14 @@ async function verifyVCSURL(url: string) {
       .replace(/#.*/, "")}/master/truffle-box.json`
   );
 
-  const options = {
-    method: "HEAD",
-    uri: `https://${configURL.host}${configURL.path}`,
-    resolveWithFullResponse: true,
-    simple: false
-  };
-
-  const { statusCode } = await rp(options);
-  if (statusCode === 404) {
+  const { status } = await axios.head(
+    `https://${configURL.host}${configURL.path}`
+  );
+  if (status === 404) {
     throw new Error(
       `Truffle Box at URL ${url} doesn't exist. If you believe this is an error, please contact Truffle support.`
     );
-  } else if (statusCode !== 200) {
+  } else if (status !== 200) {
     throw new Error(
       "Error connecting to github.com. Please check your internet connection and try again."
     );
