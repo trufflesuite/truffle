0a996c0318c890263efc2e30863ac4286fbc947d
fainashalts <faina@trufflesuite.com>
2021-05-07 10:38:56 -0700

    Change command-options file to return array of potential options

--- packages/core/lib/commands/command-options.js
+++ packages/core/lib/commands/command-options.js
@@ -1,35 +1,13 @@
-const addOptions = function(command) {
-  let options;
-  const networkCommands = ["compile", "console", "debug", "deploy", "migrate", "exec"];
-
-  // need to distinguish the develop command here because it doesn't make sense for it to have a network option
-  // but maybe there could be a relevant config value
-  if(command === "develop") {
-    options = [
-        {
-          option: "--config <file>",
-          description:
-            "Specify configuration file to be used. The default is truffle-config.js"
-        }
-    ]
-  } else if(networkCommands.includes(command)) {
-    options = [
-        {
-          option: "--network <name>",
-          description:
-            "Specify the network to use. Network name must exist in the configuration."
-        },
-        {
-          option: "--config",
-          description:
-            "Specify configuration file to be used. The default is truffle-config.js"
-        }
-    ]
-  } else {
-    options = [];
-  }
-
-  return options;
-};
-
-module.exports = addOptions;
+const options = [
+    {
+      option: "--network <name>",
+      description:
+        "Specify the network to use. Network name must exist in the configuration."
+    },
+    {
+      option: "--config <file>",
+      description:
+        "Specify configuration file to be used. The default is truffle-config.js"
+    }
+];
+ module.exports = options;
