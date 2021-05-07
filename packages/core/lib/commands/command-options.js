c4110bedc4f92a628dbdc3cfd24ed6ba92b4e34c
fainashalts <faina@trufflesuite.com>
2021-05-07 02:14:36 -0700

    Pull config/network options out; add logic for when to include

---
+++ packages/core/lib/commands/command-options.js
@@ -0,0 +1,35 @@
+const addOptions = function(command) {
+  let options;
+  const networkCommands = ["compile", "console", "debug", "deploy", "migrate"];
+
+  // need to distinguish the develop command here because it doesn't make sense for it to have a network option
+  // but maybe there could be a relevant config value
+  if(command === "develop") {
+    options = [
+        {
+          option: "--config <file>",
+          description:
+            "Specify configuration file to be used. The default is truffle-config.js"
+        }
+    ]
+  } else if(networkCommands.includes(command)) {
+    options = [
+        {
+          option: "--network <name>",
+          description:
+            "Specify the network to use. Network name must exist in the configuration."
+        },
+        {
+          option: "--config",
+          description:
+            "Specify configuration file to be used. The default is truffle-config.js"
+        }
+    ]
+  } else {
+    options = [];
+  }
+
+  return options;
+};
+
+module.exports = addOptions;
