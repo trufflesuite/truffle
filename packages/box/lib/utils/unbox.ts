a56184abe5de3f4ac7b7ccae46b5eec85bd6f88f
eggplantzzz <tmfeickert@gmail.com>
2021-05-10 16:51:22 -0400

    Add maxRedirects field to axios requests

--- packages/box/lib/utils/unbox.ts
+++ packages/box/lib/utils/unbox.ts
@@ -26,7 +26,10 @@ async function verifyVCSURL(url: string) {
       .replace(/#.*/, "")}/master/truffle-box.json`
   );
   try {
-    await axios.head(`https://${configURL.host}${configURL.path}`);
+    await axios.head(
+      `https://${configURL.host}${configURL.path}`,
+      { maxRedirects: 50 }
+    );
   } catch (error) {
     if (error.response.status === 404) {
       throw new Error(
--- packages/source-fetcher/lib/sourcify.ts
+++ packages/source-fetcher/lib/sourcify.ts
@@ -97,7 +97,8 @@ const SourcifyFetcher: FetcherConstructor = class SourcifyFetcher
       return await this.requestWithRetries<Types.SolcMetadata>({
         url: `https://${this.domain}/contracts/full_match/${this.networkId}/${address}/metadata.json`,
         method: "get",
-        responseType: "json"
+        responseType: "json",
+        maxRedirects: 50
       });
     } catch (error) {
       //is this a 404 error? if so just return null
@@ -117,7 +118,8 @@ const SourcifyFetcher: FetcherConstructor = class SourcifyFetcher
     return await this.requestWithRetries<string>({
       url: `https://${this.domain}/contracts/full_match/${this.networkId}/${address}/sources/${sourcePath}`,
       responseType: "text",
-      method: "get"
+      method: "get",
+      maxRedirects: 50
     });
   }
 
