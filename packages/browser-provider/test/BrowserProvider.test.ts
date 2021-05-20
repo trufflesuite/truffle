439bc801baba96b42c40bdf3a48af65c0231e29b
Rosco Kalis <roscokalis@gmail.com>
2021-05-20 19:44:11 +0200

    Add proper frontend integration
    - Remove placeholder frontend
    - Add start script for frontend (hardcoded paths)
    - Add ready() promise to web-server that waits for the frontend
    - Make sure frontend is terminated when server is terminated
    - Add error re-throwing on the provider side
    - NOTE: Actual frontend code is missing because it doesn't work in the
      monorepo

--- packages/browser-provider/test/BrowserProvider.test.ts
+++ packages/browser-provider/test/BrowserProvider.test.ts
@@ -1,12 +1,42 @@
 import { promisify } from "util";
 import { BrowserProvider } from "../lib";
 
-jest.setTimeout(20000);
+jest.setTimeout(200000);
 
 describe("BrowserProvider", () => {
-  it("should start", async () => {
-    const provider = new BrowserProvider();
-    const result = await promisify(provider.send.bind(provider))({ jsonrpc: '2.0', params: [], method: 'hello', id: 1 });
+  let provider: BrowserProvider;
+
+  beforeAll(() => {
+    provider = new BrowserProvider();
+  });
+
+  afterAll(() => {
+    provider.terminate();
+  });
+
+  it("should propagate a message to MetaMask", async () => {
+    const send = promisify(provider.send.bind(provider));
+    // const result = await send({ jsonrpc: '2.0', params: [], method: 'web3_clientVersion', id: 1 });
+
+    const msgParams = [
+      {
+        type: 'string',      // Any valid solidity type
+        name: 'Message',     // Any string label you want
+        value: 'Hi, Alice!'  // The value to sign
+     },
+     {
+       type: 'uint32',
+          name: 'A number',
+          value: '1337'
+      }
+    ];
+
+    const result = await send({
+      jsonrpc: "2.0",
+      method: "eth_signTypedData",
+      params: [msgParams, "0xe126b3E5d052f1F575828f61fEBA4f4f2603652a"],
+      id: 1
+    });
 
     console.log(result);
   });
