dd83e72066c34fac6fe1d49925933c8c3556c138
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-05-24 22:45:56 -0400

    Narrow types via type assertions
    - Add helper `expect.has` as a single-key version of `expect.options`
    - Define new types `Has<O, K>` and `HasOne<O, K>` and use for assertions
    - Rewrite `expect.one` more plainly
    - Add tests for types using tsd
    - Document exported identifiers
    - Stop using default export

--- packages/expect/src/index.ts
+++ packages/expect/src/index.ts
@@ -1,35 +1,72 @@
-const Expect = {
-  options(options, expected_keys) {
-    expected_keys.forEach(key => {
-      if (options[key] == null) {
-        throw new Error(`Expected parameter '${key}' not passed to function.`);
-      }
-    });
-  },
+/**
+ * Object type O that includes non-nullable keys K
+ */
+export type Has<O extends {}, K extends string> = O &
+  Required<NonNullable<Pick<O, K & keyof O>>>;
 
-  one(options, expected_keys) {
-    const found: number[] = [];
+/**
+ * Object type O that includes exactly one non-nullable value among keys K
+ */
+export type HasOne<O extends {}, K extends string> = O &
+  {
+    [N in K]: Has<O, N>;
+  }[K];
 
-    expected_keys.forEach(key => {
-      if (options[key] != null) {
-        found.push(1);
-      } else {
-        found.push(0);
-      }
-    });
+/**
+ * Asserts at runtime that `options` contains `key`
+ */
+export function has<O extends {}, K extends string>(
+  options: O,
+  key: K
+): asserts options is Has<O, K> {
+  // @ts-ignore to get around the fact that we know nothing about O
+  if (options[key] == null) {
+    throw new Error(`Expected parameter '${key}' not passed to function.`);
+  }
+}
 
-    const total = found.reduce((t, value) => t + value);
+/**
+ * Asserts at runtime that `options` contains all `expectedKeys`
+ */
+export function options<O extends {}, K extends string>(
+  options: O,
+  expectedKeys: K[]
+): asserts options is Has<O, K> {
+  for (const key of expectedKeys) {
+    has(options, key);
+  }
+}
 
-    // If this doesn't work in all cases, perhaps we should
-    // create an expect.onlyOne() function.
-    if (total < 1) {
-      throw new Error(
-        `Expected one of the following parameters, but found none: ${expected_keys.join(
-          ", "
-        )}`
-      );
+/**
+ * Asserts at runtime that `options` contains at least one of `expectedKeys`
+ *
+ * Post-condition: this narrows type of `options` to include _exactly one_ of
+ * `expectedKeys`, even though at runtime this accepts more than one key.
+ */
+export function one<O extends {}, K extends string>(
+  options: O,
+  expectedKeys: K[]
+): asserts options is HasOne<O, K> {
+  let found = false;
+
+  for (const key of expectedKeys) {
+    try {
+      has(options, key);
+
+      found = true;
+      break;
+    } catch {
+      continue;
     }
   }
-};
 
-export default Expect;
+  // If this doesn't work in all cases, perhaps we should
+  // create an expect.onlyOne() function.
+  if (!found) {
+    throw new Error(
+      `Expected one of the following parameters, but found none: ${expectedKeys.join(
+        ", "
+      )}`
+    );
+  }
+}
