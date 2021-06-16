f33f9fe5ed97cc85c6f9e85dbfd3a8bb3a62572e
cds-amal <cds.sudama@gmail.com>
2021-06-16 15:18:57 -0400

    Prettier

--- packages/core/test/commands.js
+++ packages/core/test/commands.js
@@ -3,48 +3,48 @@ const commands = require("../lib/commands");
 const commander = new Command(commands);
 const assert = require("assert");
 
-describe("Commander", function() {
-  before("assert preconditions", function() {
+describe("Commander", function () {
+  before("assert preconditions", function () {
     // These commands are expected to exist in tests.
     assert.notEqual(commands.migrate, null);
     assert.notEqual(commands.compile, null);
     assert.notEqual(commands.console, null);
   });
 
-  it("will infer commands based on initial letters entered", function() {
+  it("will infer commands based on initial letters entered", function () {
     var actualCommand = commander.getCommand("m").command;
     assert.equal(actualCommand, commands.migrate);
   });
 
-  it("will infer commands based on initial letters entered, even when typos exist later", function() {
+  it("will infer commands based on initial letters entered, even when typos exist later", function () {
     var actualCommand = commander.getCommand("complie").command;
     assert.equal(actualCommand, commands.compile);
   });
 
-  it("will NOT infer a command if not given enough information", function() {
+  it("will NOT infer a command if not given enough information", function () {
     // Note: "co" matches "console" and "compile"
     var actualCommand = commander.getCommand("co");
     assert.equal(actualCommand, null);
   });
 
-  it("will infer commands based on initial letters entered, given matches for shorter substrings", function() {
+  it("will infer commands based on initial letters entered, given matches for shorter substrings", function () {
     // Note: "co" matches "console" and "compile"
     var actualCommand = commander.getCommand("com").command;
     assert.equal(actualCommand, commands.compile);
   });
 
-  it("will ignore inferring if full command is specified", function() {
+  it("will ignore inferring if full command is specified", function () {
     var actualCommand = commander.getCommand("console").command;
     assert.equal(actualCommand, commands.console);
   });
 
-  it("will warn and display error for unsupported flags in commands", function() {
+  it("will warn and display error for unsupported flags in commands", async function () {
     var actualCommand = commander.getCommand("mig").command;
     assert.equal(actualCommand, commands.migrate);
 
     const originalLog = console.log || console.debug;
     let warning = "";
-    console.log = function(msg) {
+    console.log = function (msg) {
       console.log = originalLog;
       warning = msg;
     };
