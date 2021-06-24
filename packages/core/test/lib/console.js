0bc4a93d2aec72d202f9d67ce6af38ba520aba2b
eggplantzzz <tmfeickert@gmail.com>
2021-06-24 09:04:15 -0400

    Add some fixtures and a couple of tests

--- packages/core/test/lib/console.js
+++ packages/core/test/lib/console.js
@@ -1,4 +1,5 @@
 const assert = require("chai").assert;
+const path = require("path");
 const Console = require("../../lib/console");
 const commands = require("../../lib/commands");
 const sinon = require("sinon");
@@ -25,27 +26,41 @@ const consoleCommands = Object.keys(commands).reduce((acc, name) => {
     ? Object.assign(acc, {[name]: commands[name]})
     : acc;
 }, {});
+
 let truffleConsole;
 
 describe("Console", function () {
-  beforeEach(function () {
-    truffleConsole = new Console(consoleCommands, consoleOptions);
-    // this is just a stub for the repl.context to check for variables added
-    truffleConsole.repl = { context: {} };
-    sinon.stub(truffleConsole.interfaceAdapter, "getAccounts").returns(["0x0"]);
-    sinon.stub(truffleConsole, "hydrateUserDefinedVariables");
-  });
-  afterEach(function () {
-    truffleConsole.interfaceAdapter.getAccounts.restore();
-    truffleConsole.hydrateUserDefinedVariables.restore();
-  });
 
   describe("Console.setUpEnvironment", function () {
+    beforeEach(function () {
+      const pathToUserJs = path.join(config.working_directory, "test/sources/userVariables.js");
+      const pathToMoreUserJs = path.join(config.working_directory, "test/sources/moreUserVariables.js");
+      consoleOptions.console.require = [
+        pathToUserJs,
+        { path: pathToMoreUserJs },
+        { path: pathToUserJs, as: "namespace" }
+      ];
+      truffleConsole = new Console(consoleCommands, consoleOptions);
+      // this is just a stub for the repl.context to check for variables added
+      truffleConsole.repl = { context: {} };
+      sinon.stub(truffleConsole.interfaceAdapter, "getAccounts").returns(["0x0"]);
+    });
+    afterEach(function () {
+      truffleConsole.interfaceAdapter.getAccounts.restore();
+    });
+
     it("sets web3, the interface adapter, and accounts variables", async function () {
       await truffleConsole.setUpEnvironment();
       assert(truffleConsole.repl.context.web3);
       assert(truffleConsole.repl.context.interfaceAdapter);
       assert.equal(truffleConsole.repl.context.accounts[0], "0x0");
+    });
+
+    it("adds the JS to the environment (repl.context)", async function () {
+      await truffleConsole.setUpEnvironment();
+      assert.equal(truffleConsole.repl.context.bingBam, "boom");
+      assert.equal(truffleConsole.repl.context.abraham, "lincoln");
+      assert.equal(truffleConsole.repl.context.namespace.bingBam, "boom");
     });
   });
 });
