2ce77a75f8b7dded05f0e848dea5619e4895dd3c
eggplantzzz <tmfeickert@gmail.com>
2021-06-25 16:24:14 -0400

    Add test to ensure user-supplied variables won't clobber ours

--- packages/core/test/lib/console.js
+++ packages/core/test/lib/console.js
@@ -7,17 +7,6 @@ const Config = require("@truffle/config");
 const Web3 = require("web3");
 const Resolver = require("@truffle/resolver");
 const config = new Config();
-const consoleOptions = new Config().with({
-  network: "funTimeNetwork",
-  networks: {
-    funTimeNetwork: {
-      type: "ethereum"
-    }
-  },
-  network_id: 666,
-  provider: new Web3.providers.HttpProvider("http://localhost:666"),
-  resolver: new Resolver(config)
-});
 
 // Console uses the following for instantiation in packages/core/lib/console.js
 const excluded = new Set(["console", "init", "watch", "develop"]);
@@ -26,13 +15,22 @@ const consoleCommands = Object.keys(commands).reduce((acc, name) => {
     ? Object.assign(acc, {[name]: commands[name]})
     : acc;
 }, {});
-
-let truffleConsole;
+let truffleConsole, otherTruffleConsole, consoleOptions, otherConsoleOptions;
 
 describe("Console", function () {
-
   describe("Console.setUpEnvironment", function () {
     beforeEach(function () {
+      consoleOptions = new Config().with({
+        network: "funTimeNetwork",
+        networks: {
+          funTimeNetwork: {
+            type: "ethereum"
+          }
+        },
+        network_id: 666,
+        provider: new Web3.providers.HttpProvider("http://localhost:666"),
+        resolver: new Resolver(config)
+      });
       const pathToUserJs = path.join(config.working_directory, "test/sources/userVariables.js");
       const pathToMoreUserJs = path.join(config.working_directory, "test/sources/moreUserVariables.js");
       consoleOptions.console.require = [
@@ -62,5 +60,32 @@ describe("Console", function () {
       assert.equal(truffleConsole.repl.context.abraham, "lincoln");
       assert.equal(truffleConsole.repl.context.namespace.bingBam, "boom");
     });
+
+    describe("when there are name conflicts with user-supplied variables", function () {
+      beforeEach(function () {
+        otherConsoleOptions = new Config().with({
+          network: "funTimeNetwork",
+          networks: {
+            funTimeNetwork: {
+              type: "ethereum"
+            }
+          },
+          network_id: 666,
+          provider: new Web3.providers.HttpProvider("http://localhost:666"),
+          resolver: new Resolver(config)
+        });
+        otherConsoleOptions.console.require = path.join(config.working_directory, "test/sources/nameConflicts.js");
+        otherTruffleConsole = new Console(consoleCommands, otherConsoleOptions);
+        // this is just a stub for the repl.context to check for variables added
+        otherTruffleConsole.repl = { context: {} };
+      });
+
+      it("won't let users clobber Truffle variables", async function () {
+        await otherTruffleConsole.setUpEnvironment();
+        assert.notEqual(otherTruffleConsole.repl.context.accounts, "0x666");
+        assert.notEqual(otherTruffleConsole.repl.context.web3, "fakeWeb3");
+        assert.notEqual(otherTruffleConsole.repl.context.interfaceAdapter, "fakeInterfaceAdapter");
+      });
+    })
   });
 });
