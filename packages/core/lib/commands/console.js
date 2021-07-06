977af9d58f6897c95f7ee99d4b87e06025622eeb
eggplantzzz <tmfeickert@gmail.com>
2021-07-06 16:48:26 -0400

    Add --require-none option to the help for console and develop

--- packages/core/lib/commands/console.js
+++ packages/core/lib/commands/console.js
@@ -16,6 +16,13 @@ const command = {
         description: "Preload console environment from required JavaScript " +
           "file. The default export must be an object with named keys that " +
           "will be used\n                    to populate the console environment."
+      },
+      {
+        option: "--require-none",
+        description: "Do not load any user-defined JavaScript into the " +
+          "console environment. This option takes precedence over --require, " +
+          "-r, and\n                    values provided in console.require in" +
+          " your project's truffle-config.js."
       }
     ],
     allowedGlobalOptions: ["network", "config"]
