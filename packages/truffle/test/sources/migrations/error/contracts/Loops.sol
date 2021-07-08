ee572af6dba8bbc824ebb3a16d4ac6e31b90cf8f
David Murdoch <david@davidmurdoch.com>
2021-07-07 14:01:11 -1000

    Increase this tests loops to ensure OOG error
    ganache v7 deafult hardfork is berlin, which is cheaper for some
    operations now, so we need more loops.

--- packages/truffle/test/sources/migrations/error/contracts/Loops.sol
+++ packages/truffle/test/sources/migrations/error/contracts/Loops.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.5.0;
 
-
 contract Loops {
-  uint public id;
-  constructor() public {
-    for(uint i = 0; i < 10000; i++){
-      id = i;
+    uint256 public id;
+
+    constructor() public {
+        for (uint256 i = 0; i < 100000; i++) {
+            id = i;
+        }
     }
-  }
 }
