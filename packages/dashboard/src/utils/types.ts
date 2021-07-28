6c5e3ad456243ad257b93f6038099ef89457afda
Rosco Kalis <roscokalis@gmail.com>
2021-07-28 14:10:49 +0200

    Add message envelopes

--- packages/dashboard/src/utils/types.ts
+++ packages/dashboard/src/utils/types.ts
@@ -1,12 +1,15 @@
-export interface Request {
+export type Request = BrowserProviderRequest;
+
+export interface BrowserProviderRequest {
   id: number;
+  type: "browser-provider";
   payload: {
     jsonrpc: "2.0";
     method: string;
     params: any[];
     id: number;
   };
-};
+}
 
 export interface PortsConfig {
   dashboardPort: number;
