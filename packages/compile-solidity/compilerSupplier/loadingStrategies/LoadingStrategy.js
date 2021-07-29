4070281f401e5254303fc6aa881da8a87283061f
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-07-29 18:37:40 -0400

    Get rid of LoadingStrategy.prototype.errors()
    As a potential stopgap:
    
    1. Blindly replace all `noVersion` / `noWhatever` strings with analogous
       `NoVersionError` classes, putting them in new errors bucket module
    
    2. For each such new error class, if the class is used by only one
       loading strategy, move the class to that module.

--- packages/compile-solidity/compilerSupplier/loadingStrategies/LoadingStrategy.js
+++ packages/compile-solidity/compilerSupplier/loadingStrategies/LoadingStrategy.js
@@ -12,42 +12,6 @@ class LoadingStrategy {
     this.config = Object.assign({}, defaultConfig, options);
   }
 
-  errors(kind, input, error) {
-    const info = "Run `truffle compile --list` to see available versions.";
-
-    const kinds = {
-      noPath: "Could not find compiler at: " + input,
-      noVersion:
-        `Could not find a compiler version matching ${input}. ` +
-        `Please ensure you are specifying a valid version, constraint or ` +
-        `build in the truffle config. ${info}`,
-      noRequest:
-        "Failed to complete request to: " +
-        input +
-        ". Are you connected to the internet?\n\n" +
-        error,
-      noUrl: "compiler root URL missing",
-      noDocker:
-        "You are trying to run dockerized solc, but docker is not installed.",
-      noImage:
-        "Please pull " +
-        input +
-        " from docker before trying to compile with it.",
-      noNative: "Could not execute local solc binary: " + error,
-      noString:
-        "`compilers.solc.version` option must be a string specifying:\n" +
-        "   - a path to a locally installed solcjs\n" +
-        "   - a solc version or range (ex: '0.4.22' or '^0.5.0')\n" +
-        "   - a docker image name (ex: 'stable')\n" +
-        "   - 'native' to use natively installed solc\n" +
-        "Received: " +
-        input +
-        " instead."
-    };
-
-    return new Error(kinds[kind]);
-  }
-
   load(_userSpecification) {
     throw new Error(
       "Abstract method LoadingStrategy.load is not implemented for this strategy."
