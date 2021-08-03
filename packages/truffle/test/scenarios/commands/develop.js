d8ecddd21f3393001eba0b96e829da25d54e4be5
eggplantzzz <tmfeickert@gmail.com>
2021-08-02 20:10:22 -0400

    Edit test to find variable value in console output

--- packages/truffle/test/scenarios/commands/develop.js
+++ packages/truffle/test/scenarios/commands/develop.js
@@ -8,12 +8,8 @@ const sandbox = require("../sandbox");
 const logger = new MemoryLogger();
 let config, project;
 
-
 describe("truffle develop", function () {
-  project = path.join(
-    __dirname,
-    "../../sources/develop",
-  );
+  project = path.join(__dirname, "../../sources/develop");
 
   before(async function () {
     this.timeout(10000);
@@ -21,7 +17,7 @@ describe("truffle develop", function () {
     config.network = "development";
     config.logger = logger;
     config.mocha = {
-      reporter: new Reporter(logger),
+      reporter: new Reporter(logger)
     };
   });
 
@@ -32,13 +28,15 @@ describe("truffle develop", function () {
     const output = logger.contents();
 
     //prepare a helpful message to standout in CI log noise
-    const formatLines = (lines) =>
-      lines.split("\n").map((line) => `\t---truffle develop log---\t${line}`)
+    const formatLines = lines =>
+      lines
+        .split("\n")
+        .map(line => `\t---truffle develop log---\t${line}`)
         .join("\n");
     const expectedValue = "eggs and sausage, and a side of toast";
     assert(
       output.includes(expectedValue),
-      `Expected "${expectedValue}" in output:\n${formatLines(output)}`,
+      `Expected "${expectedValue}" in output:\n${formatLines(output)}`
     );
   });
 
@@ -49,14 +47,17 @@ describe("truffle develop", function () {
     const output = logger.contents();
 
     //prepare a helpful message to standout in CI log noise
-    const formatLines = (lines) =>
-      lines.split("\n").map((line) => `\t---truffle develop log---\t${line}`)
+    const formatLines = lines =>
+      lines
+        .split("\n")
+        .map(line => `\t---truffle develop log---\t${line}`)
         .join("\n");
     // `twoAccounts` is the concatenation of the first 2 accounts in `accounts`
-    const expectedValue = "0x8d9606F90B6CA5D856A9f0867a82a645e2DfFf370x370566028c7EAbDA8E408dA77A99EAf1e73DdC60";
+    // this matches two concatenated accounts
+    const addressesRegex = new RegExp(/(0x[0-9a-f]{40}){2}/, "gi");
     assert(
-      output.includes(expectedValue),
-      `Expected "${expectedValue}" in output:\n${formatLines(output)}`,
+      addressesRegex.test(output),
+      `Expected value in output:\n${formatLines(output)}`
     );
   });
 });
