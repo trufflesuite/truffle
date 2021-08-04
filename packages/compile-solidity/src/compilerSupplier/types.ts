deaa48f2ab62bd52880c81620a731ca6c6da3116
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-08-04 19:54:52 -0400

    [squashme] improve solc type

--- packages/compile-solidity/src/compilerSupplier/types.ts
+++ packages/compile-solidity/src/compilerSupplier/types.ts
@@ -1,12 +1,15 @@
 export namespace Results {
   export type Specification = {
     load: Promise<{
-      solc: any; // maybe could use better solc type
+      solc: {
+        compile(...args: any[]): any;
+        version(): string;
+      };
     }>;
     list: Promise<{
       latestRelease: string | undefined;
       releases: AsyncIterableIterator<string> | string[];
       prereleases: AsyncIterableIterator<string> | string[];
-    }>
+    }>;
   };
-};
+}
