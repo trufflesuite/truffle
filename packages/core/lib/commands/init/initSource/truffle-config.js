7a0ed334e6b46df3462118a6b7010d0ea27efdae
cds-amal <cds.sudama@gmail.com>
2021-08-05 14:37:59 -0400

    remove ambiguity from backup and db usage

--- packages/core/lib/commands/init/initSource/truffle-config.js
+++ packages/core/lib/commands/init/initSource/truffle-config.js
@@ -97,9 +97,10 @@ module.exports = {
   // false to enabled: true. The default storage location can also be
   // overridden by specifying the adapter settings, as shown in the commented code below.
   //
-  // Note: your previously migrated contracts will not be available in
-  // truffle-db. We recommnd you backup your artifacts before utilizing Truffle
-  // DB for the first time with:
+  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+  // make a backup of your artifacts to a safe location before enabling this feature.
+  //
+  // After you backed up your artifacts you can utilize db by running migrate as follows: 
   // $ truffle migrate --reset --compile-all
 
   db: {
