a1b7c75a44b38c469dc841e81f3fa173ce00e76a
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-08-04 21:44:27 -0400

    [squashme] cleanup/document types

---
+++ packages/compile-common/src/compilerSupplier/strategy.ts
@@ -0,0 +1,106 @@
+import type { Constructor as FreeConstructor } from "./constructor";
+import type { Results as FreeResults } from "./results";
+
+export namespace Strategy {
+  /**
+   * Type-level description of a compiler supplier strategy
+   */
+  export type Specification = {
+    constructor: Omit<FreeConstructor.Specification, "result">;
+    results: FreeResults.Specification;
+    allowsLoadingSpecificVersion: boolean;
+    allowsListingVersions: boolean;
+  };
+
+  /**
+   * Scoped re-exports of Constructor, using Strategy.Specification as
+   * generic param
+   */
+  export namespace Constructor {
+    /**
+     * Getter for the specified constructor, intersected with fixed result
+     * type of Strategy<S>
+     */
+    export type Specification<S extends Strategy.Specification> =
+      S["constructor"] & { result: Strategy<S>; };
+
+    /**
+     * Constructor options argument for a specified strategy
+     */
+    export type Options<S extends Strategy.Specification> = FreeConstructor.Options<
+      Strategy.Constructor.Specification<S>
+    >;
+  }
+
+  /**
+   * Type representing a constructor for specified strategy
+   */
+  export type Constructor<S extends Strategy.Specification> = FreeConstructor<
+    Strategy.Constructor.Specification<S>
+  >;
+
+  /**
+   * Results types for a particular strategy
+   */
+  export namespace Results {
+    export type Specification<S extends Strategy.Specification> =
+      S["results"];
+
+    export type Load<S extends Strategy.Specification> = FreeResults.Load<
+      Strategy.Results.Specification<S>
+    >;
+
+    export type List<S extends Strategy.Specification> = FreeResults.List<
+      Strategy.Results.Specification<S>
+    >;
+  }
+
+  /**
+   * Getter type for whether specified strategy allows argument to load()
+   */
+  export type AllowsLoadingSpecificVersion<
+    S extends Strategy.Specification
+  > = S["allowsLoadingSpecificVersion"];
+
+  /**
+   * Getter type for whether specified strategy provides version listing
+   */
+  export type AllowsListingVersions<
+    S extends Strategy.Specification
+  > = S["allowsListingVersions"];
+
+}
+
+/**
+ * An object of type Strategy<S> provides version loading and possibly version
+ * listing functionality
+ */
+export type Strategy<S extends Strategy.Specification> = BaseStrategy<S> &
+  (true extends Strategy.AllowsLoadingSpecificVersion<S>
+    ? {
+        load(version?: string): Strategy.Results.Load<S>;
+      }
+    : {
+        load(): Strategy.Results.Load<S>;
+      }) &
+  (true extends Strategy.AllowsListingVersions<S>
+    ? {
+        list(): Strategy.Results.List<S>;
+      }
+    : {});
+
+/**
+ * An object of BaseStrategy<S> allows loading an unspecified version, plus
+ * type guards to determine additional functionality
+ */
+export interface BaseStrategy<S extends Strategy.Specification> {
+  load(): Strategy.Results.Load<S>;
+
+  allowsLoadingSpecificVersion(): this is this & {
+    load(version?: string): Strategy.Results.Load<S>;
+  };
+
+  allowsListingVersions(): this is {
+    list(): Strategy.Results.List<S>;
+  };
+}
--- packages/compile-common/src/compilerSupplier/supplier.ts
+++ packages/compile-common/src/compilerSupplier/supplier.ts
@@ -1,70 +1,129 @@
-import type { Strategy, UnknownStrategy } from "./strategies";
-import type { Results } from "./results";
-
-type StrategySpecification = Strategy.Specification;
-type StrategyConstructor<
-  S extends StrategySpecification
-> = Strategy.Constructor<S>;
+import type { Strategy as FreeStrategy, BaseStrategy } from "./strategy";
+import type { Results as FreeResults } from "./results";
 
 export namespace Supplier {
+  /**
+   * Type-level description of a supplier
+   */
   export type Specification = {
     options: any;
-    results: Results.Specification;
+    results: FreeResults.Specification;
     strategies: {
-      [strategyName: string]: Omit<StrategySpecification, "results">;
+      [strategyName: string]: Omit<FreeStrategy.Specification, "results">;
     };
   };
 
-  export type StrategyName<S extends Specification> = string &
-    keyof S["strategies"];
-
+  /**
+   * Getter type for the options argument passed to the constructor for one of
+   * the named strategies for a specified supplier
+   */
   export type Options<
-    S extends Specification,
-    N extends StrategyName<S>
-  > = S["options"] & Strategy.Constructor.Options<S, N>;
+    S extends Supplier.Specification,
+    N extends Supplier.StrategyName<S>
+  > = S["options"] & Supplier.Strategy.Constructor.Options<S, N>;
+
+  export namespace Results {
+    /**
+     * Type-level specification of method results for specified supplier;
+     * used in conjunction with each strategy individually
+     */
+    export type Specification<S extends Supplier.Specification> = S["results"];
+  }
+
+  export namespace Strategies {
+    export type Specification<S extends Supplier.Specification> = S["strategies"];
+  }
+
+  /**
+   * A type representing one of the strategy names for specified supplier
+   */
+  export type StrategyName<S extends Supplier.Specification> = string &
+    keyof Supplier.Strategies.Specification<S>;
 
   export namespace Strategy {
+    /**
+     * Type-level specification of one of the named strategies for specified
+     * supplier; joins specified supplier results type
+     */
     export type Specification<
       S extends Supplier.Specification,
-      N extends StrategyName<S>
-    > = S["strategies"][N] & Pick<S, "results">;
+      N extends Supplier.StrategyName<S>
+    > = Supplier.Strategies.Specification<S>[N] & {
+      results: Supplier.Results.Specification<S>
+    };
 
     export namespace Constructor {
+      /**
+       * Type-level specification of the constructor for one of the named
+       * strategies for a specified supplier
+       */
       export type Specification<
         S extends Supplier.Specification,
-        N extends StrategyName<S>
-      > = Supplier.Strategy.Specification<S, N>["constructor"];
+        N extends Supplier.StrategyName<S>
+      > = FreeStrategy.Constructor<Supplier.Strategy.Specification<S, N>>;
 
+      /**
+       * Getter type for options argument passed to the constructor for one of
+       * the named strategies for a specified supplier
+       */
       export type Options<
         S extends Supplier.Specification,
-        N extends StrategyName<S>
+        N extends Supplier.StrategyName<S>
       > = {
-        [K in N]: Specification<S, K>["options"];
+        [K in N]: FreeStrategy.Constructor.Options<
+          Supplier.Strategy.Specification<S, K>
+        >
       }[N];
     }
 
+    /**
+     * An object of Constructor<S, N> is a JS constructor (or "class") for one
+     * of the named strategies for specified supplier
+     */
     export type Constructor<
       S extends Supplier.Specification,
-      N extends StrategyName<S>
-    > = StrategyConstructor<Strategy.Specification<S, N>>;
+      N extends Supplier.StrategyName<S>
+    > = FreeStrategy.Constructor<Supplier.Strategy.Specification<S, N>>;
   }
 
-  export type Definition<S extends Specification> = {
-    determineStrategy(options: Options<S, StrategyName<S>>): StrategyName<S>;
+  /**
+   * An object of type Definition<S> comprises constructors for each of the
+   * specified strategies, as well as a method to determine the name of which
+   * strategy to use for a given set of input options
+   */
+  export type Definition<S extends Supplier.Specification> = {
+    determineStrategy(
+      options: Options<S, Supplier.StrategyName<S>>
+    ): Supplier.StrategyName<S>;
 
     strategyConstructors: {
-      [N in StrategyName<S>]: Strategy.Constructor<S, N>;
+      [N in Supplier.StrategyName<S>]: Supplier.Strategy.Constructor<S, N>;
     };
   };
 }
 
+/**
+ * An object of type Supplier<S, N> provides an interface for loading and
+ * possibly listing versions of the compiler
+ *
+ * @dev for known strategy names, this computes the method signature of
+ *      `load()` according to the corresponding strategy specification;
+ *
+ *      for the default of "any strategy name", this type will resolve to an
+ *      interface that may require the use of defined type guards
+ *      (e.g. `if(supplier.allowsListingVersions()) { supplier.list(); }`)
+ */
 export type Supplier<
   S extends Supplier.Specification,
-  N extends Supplier.StrategyName<S>
+  N extends Supplier.StrategyName<S> = Supplier.StrategyName<S>
 > = Supplier.StrategyName<S> extends N
-  ? UnknownStrategy<Supplier.Strategy.Specification<S, N>>
-  : Strategy<Supplier.Strategy.Specification<S, N>>;
+  ? BaseStrategy<Supplier.Strategy.Specification<S, N>>
+  : FreeStrategy<Supplier.Strategy.Specification<S, N>>;
 
+/**
+ * Given the definition of specified supplier, create a function that
+ * determines+constructs a supplier strategy for a given set of options.
+ */
 export const forDefinition = <S extends Supplier.Specification>({
   determineStrategy,
   strategyConstructors
