615e5a7bcee542140145c85cf063fcffc7816b0b
g. nicholas d'andrea <gnidan@trufflesuite.com>
2021-08-12 19:23:58 -0400

    Add suggested comments

--- packages/compile-solidity/compilerSupplier/normalizeSolcVersion.js
+++ packages/compile-solidity/compilerSupplier/normalizeSolcVersion.js
@@ -1,3 +1,7 @@
+/**
+ * Convert a full version string (possibly with commit information, etc.) into
+ * a canonical short-form semver version (x.y.z)
+ */
 const normalizeSolcVersion = (input) => {
   const version = String(input);
   return version.split(":")[1].trim();
