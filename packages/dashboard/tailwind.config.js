9084601bd89b81502b549dbb868349732312cb99
Rosco Kalis <roscokalis@gmail.com>
2021-08-12 12:15:07 +0200

    Basic styling

--- packages/dashboard/tailwind.config.js
+++ packages/dashboard/tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oswald: ['Oswald'],
+      },
+      colors: {
+        'truffle-blue': '#3fe0c5',
+        'truffle-brown': '#5e464d',
+        'truffle-light': '#efe5dc',
+        'truffle-lighter': '#f8f5f0',
+      }
+    },
   },
   variants: {
     extend: {},
