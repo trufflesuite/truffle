e617c4477333fc0dcd2276c1b33e9f5434343dd2
Rosco Kalis <roscokalis@gmail.com>
2021-08-26 19:00:25 +0200

    Update metacoin-truffle config

--- packages/browser-provider/manual-test/metacoin-truffle/truffle-config.js
+++ packages/browser-provider/manual-test/metacoin-truffle/truffle-config.js
@@ -3,9 +3,10 @@ const { BrowserProvider } = require('../../dist/lib');
 module.exports = {
   networks: {
     browser: {
-      provider: () => new BrowserProvider(5000),
+      provider: () => new BrowserProvider({ dashboardPort: 5000 }),
       network_id: '*',
       skipDryRun: true,
+      networkCheckTimeout: 120000,
     },
   }
 };
