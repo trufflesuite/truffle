68594f1ab48e4f8555313f732ddbc34756177d3a
Rosco Kalis <roscokalis@gmail.com>
2021-08-26 13:12:30 +0200

    Add tests using Ethers/Web3 and fix a concurrency bug

--- packages/browser-provider/test/BrowserProvider.test.ts
+++ packages/browser-provider/test/BrowserProvider.test.ts
@@ -1,13 +1,14 @@
 import { promisify } from "util";
 import delay from "delay";
 import Ganache from "ganache-core";
+import { providers } from "ethers";
+import Web3 from "web3";
 import { BrowserProvider } from "../lib";
 import MockDashboard from "./MockDashboard";
 import { getMessageBusPorts } from "../lib/utils";
 
 jest.setTimeout(200000);
 
-// TODO: Perhaps also add some tests where we're using this provider inside Ethers.js / web3.js
 describe("BrowserProvider", () => {
   const ganachePort = 8545;
   const dashboardPort = 5000;
@@ -36,45 +37,121 @@ describe("BrowserProvider", () => {
     mockDashboard.disconnect();
   });
 
-  it("should retrieve unlocked accounts", async () => {
-    const send = promisify(browserProvider.send.bind(browserProvider));
+  describe("Direct usage", () => {
+    it("should retrieve unlocked accounts", async () => {
+      const send = promisify(browserProvider.send.bind(browserProvider));
 
-    // First connect the dashboard
-    await mockDashboard.connect(messageBusPorts.messageBusListenPort);
+      // First connect the dashboard
+      await mockDashboard.connect(messageBusPorts.messageBusListenPort);
 
-    // Then send the request
-    const response = await send({
-      jsonrpc: "2.0",
-      method: "eth_accounts",
-      params: [],
-      id: 1
+      // Then send the request
+      const response = await send({
+        jsonrpc: "2.0",
+        method: "eth_accounts",
+        params: [],
+        id: 1
+      });
+
+      expect(response).toHaveProperty('result');
+      expect(response?.result[0]).toBeDefined();
     });
 
-    expect(response).toHaveProperty('result');
-    expect(response?.result[0]).toBeDefined();
+    it("should retrieve unlocked accounts if request gets sent before dashboard connects", async () => {
+      const send = promisify(browserProvider.send.bind(browserProvider));
+
+      // First send the request
+      const request = send({
+        jsonrpc: "2.0",
+        method: "eth_accounts",
+        params: [],
+        id: 1
+      });
+
+      // Wait for a second
+      await delay(1000);
+
+      // Then connect the dashboard
+      await mockDashboard.connect(messageBusPorts.messageBusListenPort);
+
+      // Then await the response
+      const response = await request;
+
+      expect(response).toHaveProperty('result');
+      expect(response?.result[0]).toBeDefined();
+    });
   });
 
-  it("should retrieve unlocked accounts if request gets sent before dashboard connects", async () => {
-    const send = promisify(browserProvider.send.bind(browserProvider));
+  describe("Usage with Ethers.js", () => {
+    let ethersProvider: providers.Web3Provider;
 
-    // First send the request
-    const request = send({
-      jsonrpc: "2.0",
-      method: "eth_accounts",
-      params: [],
-      id: 1
+    beforeEach(() => {
+      ethersProvider = new providers.Web3Provider(browserProvider);
     });
 
-    // Wait for a second
-    await delay(1000);
+    afterEach(async () => {
+      // Ethers sends a request to get the chainId on connection, so we await it
+      // to make sure that the provider is finished with that request as well
+      await ethersProvider.ready;
+    });
 
-    // Then connect the dashboard
-    await mockDashboard.connect(messageBusPorts.messageBusListenPort);
+    it("should retrieve unlocked accounts", async () => {
+      // First connect the dashboard
+      await mockDashboard.connect(messageBusPorts.messageBusListenPort);
 
-    // Then await the response
-    const response = await request;
+      // Then send the request
+      const accounts = await ethersProvider.listAccounts();
 
-    expect(response).toHaveProperty('result');
-    expect(response?.result[0]).toBeDefined();
+      expect(accounts[0]).toBeDefined();
+    });
+
+    it("should retrieve unlocked accounts if request gets sent before dashboard connects", async () => {
+      // First send the request
+      const request = ethersProvider.listAccounts();
+
+      // Wait for a second
+      await delay(1000);
+
+      // Then connect the dashboard
+      await mockDashboard.connect(messageBusPorts.messageBusListenPort);
+
+      // Then await the response
+      const accounts = await request;
+
+      expect(accounts[0]).toBeDefined();
+    });
+  });
+
+  describe("Usage with Web3.js", () => {
+    let web3: Web3;
+
+    beforeAll(() => {
+      web3 = new Web3(browserProvider);
+    });
+
+    it("should retrieve unlocked accounts", async () => {
+      // First connect the dashboard
+      await mockDashboard.connect(messageBusPorts.messageBusListenPort);
+
+      // Then send the request
+      const accounts = await web3.eth.getAccounts();
+
+      expect(accounts[0]).toBeDefined();
+    });
+
+    it("should retrieve unlocked accounts if request gets sent before dashboard connects", async () => {
+      // First send the request
+      const request = web3.eth.getAccounts();
+
+      // Wait for a second
+      await delay(1000);
+
+      // Then connect the dashboard
+      await mockDashboard.connect(messageBusPorts.messageBusListenPort);
+
+      // Then await the response
+      const accounts = await request;
+
+      expect(accounts[0]).toBeDefined();
+    });
   });
 });
