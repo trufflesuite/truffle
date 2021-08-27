11516a98da227e2f76daaf6150f3f048bfa7d7c1
cds-amal <cds.sudama@gmail.com>
2021-08-27 10:45:49 -0400

    compile-solidity: Convert test script from bash to node

---
+++ packages/compile-solidity/scripts/test.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const execOpt = { stdio: "inherit" }
+
+//Simulate "$@" 
+//arg0 is node, arg1 is __filename
+const argv = process.argv.slice(2);
+
+let mocha = ["mocha", "./test/**", "./test/**/*", "--timeout", "70000" ];
+const grepOpts = ["--invert --grep native"];
+
+const makeCommand = (cmd, opts=[]) => [...cmd, ...opts, ...argv].join(' ');
+
+if (Boolean(process.env.CI)) {
+  execSync(makeCommand(mocha), execOpt); 
+} else {
+  const cachedTruffle = path.join(process.cwd(), "node_modules", ".cache", "truffle");
+  fs.rmdirSync(cachedTruffle, { recursive: true, force: true });
+  execSync(makeCommand(mocha, grepOpts), execOpt); 
+}
