52c3173b56c94eaf77f97e8ad29eb95ec2e0d03a
eggplantzzz <tmfeickert@gmail.com>
2021-08-30 16:20:12 -0400

    Use stable version of Geth in CI

--- scripts/geth.sh
+++ scripts/geth.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker pull ethereum/client-go:v1.10.3
+docker pull ethereum/client-go:stable
 
 docker run \
 	-v /$PWD/scripts:/scripts \
@@ -8,7 +8,7 @@ docker run \
 	-p 8545:8545 \
 	-p 8546:8546 \
 	-p 30303:30303 \
-	ethereum/client-go:v1.10.3 \
+	ethereum/client-go:stable \
 	--http \
 	--http.addr '0.0.0.0' \
 	--http.port 8545 \
