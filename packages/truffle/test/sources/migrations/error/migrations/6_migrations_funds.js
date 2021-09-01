9dfe7110b7ede1c7df4701ae906cc3b1d11d7cf6
eggplantzzz <tmfeickert@gmail.com>
2021-09-01 17:09:10 -0400

    Tweak test

--- packages/truffle/test/sources/migrations/error/migrations/6_migrations_funds.js
+++ packages/truffle/test/sources/migrations/error/migrations/6_migrations_funds.js
@@ -5,14 +5,18 @@ module.exports = async function(deployer, network, accounts) {
   console.log("the empty account -- %o", emptyAccount);
   let balance = await web3.eth.getBalance(emptyAccount);
   console.log("the balance -- %o", balance);
+  const { baseFeePerGas } = await web3.eth.getBlock("latest");
+  console.log("the base fee -- %o", baseFeePerGas);
+  // 21,000 gas to send ether
+  const priceOfGas = 21000 * baseFeePerGas;
 
   await web3.eth.sendTransaction({
     to: accounts[0],
     from: emptyAccount,
-    value: balance,
-    gasPrice: 0
+    value: balance - priceOfGas,
+    gasPrice: baseFeePerGas
   });
   console.log("about to try and deploy");
 
-  await deployer.deploy(Example, {from: emptyAccount});
+  await deployer.deploy(Example, { from: emptyAccount, });
 };
