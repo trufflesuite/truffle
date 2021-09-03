b7a2d82625369b6b14bd6c25b1a001efdd2cb8f3
eggplantzzz <tmfeickert@gmail.com>
2021-09-03 10:51:43 -0400

    Flesh out the rest of the migrations for global config stuff

--- packages/core/lib/config-migrations.js
+++ packages/core/lib/config-migrations.js
@@ -2,6 +2,8 @@ const OS = require("os");
 const path = require("path");
 const fse = require("fs-extra");
 const Conf = require("conf");
+const { promisify } = require("util");
+const copy = require("./copy");
 
 module.exports = {
   oldTruffleFolder: path.join(OS.homedir(), ".config", "truffle"),
@@ -12,12 +14,16 @@ module.exports = {
     return fse.existsSync(oldConfig) && oldConfig !== conf.path;
   },
 
-  migrate: function () {
+  migrateTruffleDataIfNecessary: async function () {
     if (!this.needsMigrated) return;
-    this.migrateDataDir();
+    this.migrateGlobalConfig();
+    const folders = ["compilers", ".db"];
+    for (const folder of folders) {
+      await this.migrateFolder(folder);
+    }
   },
 
-  migrateDataDir: function () {
+  migrateGlobalConfig: function () {
     const conf = new Conf({ projectName: "truffle" });
     const oldSettings = require(path.join(
       this.oldTruffleFolder,
@@ -25,6 +31,16 @@ module.exports = {
     ));
     for (const key in oldSettings) {
       conf.set(key, oldSettings[key]);
+    }
+  },
+
+  migrateFolder: async function (folderName) {
+    const targetPath = path.join(this.oldTruffleFolder, folderName);
+    // use conf to determine the new Truffle folder as it uses OS-appropriate locations
+    const conf = new Conf({ projectName: "truffle" });
+    const destinationPath = path.join(path.dirname(conf.path), folderName);
+    if (fse.existsSync(targetPath)) {
+      await promisify(copy)(targetPath, destinationPath, {});
     }
   }
 };
