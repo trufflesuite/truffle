391745f510b3187e1cf1514ab6db106e5fb2fccb
Rosco Kalis <roscokalis@gmail.com>
2021-09-20 12:17:36 +0200

    Add indicators for connected network and account

--- packages/dashboard/src/components/Header/Header.tsx
+++ packages/dashboard/src/components/Header/Header.tsx
@@ -1,13 +1,36 @@
 import { useWeb3React } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { providers } from "ethers";
+import { useEffect } from "react";
+import { useState } from "react";
+import { getDisplayName, getNetworkName } from "../../utils/utils";
 import Button from "../common/Button";
 
 interface Props {}
 
 function Header({}: Props) {
-  const { account, activate } = useWeb3React<providers.Web3Provider>();
+  const [networkName, setNetworkName] = useState<string>();
+  const [displayName, setDisplayName] = useState<string>();
+
+  const { account, activate, library } = useWeb3React<providers.Web3Provider>();
   const injectedConnector = new InjectedConnector({});
+
+  useEffect(() => {
+    const updateNetwork = async (library: providers.Web3Provider) => {
+      const { chainId } = await library.detectNetwork();
+      const connectedNetworkName = await getNetworkName(chainId);
+      setNetworkName(connectedNetworkName);
+      console.log(connectedNetworkName);
+    };
+
+    const updateAccountDisplay = async (library: providers.Web3Provider, account: string) => {
+      setDisplayName(await getDisplayName(library, account));
+    };
+
+    if (!library || !account) return;
+    updateNetwork(library);
+    updateAccountDisplay(library, account);
+  }, [library, account]);
 
   return (
     <header className="grid grid-cols-2 py-2 px-4 border-b-2 border-truffle-light">
@@ -17,8 +40,9 @@ function Header({}: Props) {
           TRUFFLE DASHBOARD
         </span>
       </div>
-      <div className="flex justify-end items-center">
-        <Button onClick={() => activate(injectedConnector)} text={account ? 'WALLET CONNECTED' : 'CONNECT WALLET'} />
+      <div className="flex justify-end items-center gap-2">
+        {networkName && <Button onClick={()=>{}} text={networkName ?? ''} />}
+        <Button onClick={() => activate(injectedConnector)} text={displayName ?? 'CONNECT WALLET'} />
       </div>
     </header>
   );
