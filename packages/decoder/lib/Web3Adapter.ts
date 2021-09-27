ee27c14dffc19ebe79d1eb79f6481ed756597f0a
eggplantzzz <tmfeickert@gmail.com>
2021-09-27 16:37:47 -0400

    Adjust implementation for web3-eth to copy the style in the docs

--- packages/decoder/lib/Web3Adapter.ts
+++ packages/decoder/lib/Web3Adapter.ts
@@ -1,4 +1,4 @@
-import { Eth } from "web3-eth";
+import Web3Eth from "web3-eth";
 type PastLogsOptions = {
   toBlock?: string | number;
   fromBlock?: string | number;
@@ -8,42 +8,43 @@ import type BN from "bn.js";
 
 export class Web3Adapter {
   public provider: any;
-  public Eth: Eth;
+  private eth: any;
 
   constructor (provider: any) {
     this.provider = provider;
-    this.Eth = new Eth(provider);
+    // @ts-ignore - seems like the types are out of sync with the implementation
+    this.eth = new Web3Eth(provider);
   }
 
   public async getCode (address: string, block: string | number) {
-    return await this.Eth.getCode(address, block);
+    return await this.eth.getCode(address, block);
   }
 
   public async getBlock (block: string | number) {
-    return await this.Eth.getBlock(block);
+    return await this.eth.getBlock(block);
   }
 
   public async getPastLogs ({ address, fromBlock, toBlock }: PastLogsOptions): Promise<any[]> {
-    return await this.Eth.getPastLogs({ address, fromBlock, toBlock });
+    return await this.eth.getPastLogs({ address, fromBlock, toBlock });
   }
 
-  public async getNetworkId (): Promise<string> {
-    return (await this.Eth.net.getId()).toString();
+  public async getNetworkId () {
+    return (await this.eth.net.getId()).toString();
   }
 
   public async getBlockNumber (): Promise<number> {
-    return await this.Eth.getBlockNumber();
+    return await this.eth.getBlockNumber();
   }
 
   public async getBalance (address: string, blockNumber: any) {
-    return await this.Eth.getBalance(address, blockNumber);
+    return await this.eth.getBalance(address, blockNumber);
   }
 
   public async getTransactionCount (contractAddress: string, blockNumber: any) {
-    return this.Eth.getTransactionCount(contractAddress, blockNumber);
+    return this.eth.getTransactionCount(contractAddress, blockNumber);
   }
 
   public async getStorageAt (address: string, position: BN, block: string | number) {
-    return this.Eth.getStorageAt(address, position, block);
+    return this.eth.getStorageAt(address, position, block);
   }
 }
