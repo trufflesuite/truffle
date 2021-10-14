582658c00a3974b08dda4675f688efc051ccc30f
Rosco Kalis <roscokalis@gmail.com>
2021-10-14 17:20:40 +0200

    Remove close windoe confirmation

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -1,17 +1,8 @@
 import { getLibrary } from "./utils/utils";
-import { useEffect } from "react";
 import { Web3ReactProvider } from "@web3-react/core";
 import Dashboard from "./Dashboard";
 
 function App() {
-  // Add warning when trying to close the app
-  useEffect(() => {
-    window.onbeforeunload = function (e: any) {
-      e.returnValue = "";
-      return "";
-    };
-  }, []);
-
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <Dashboard />
