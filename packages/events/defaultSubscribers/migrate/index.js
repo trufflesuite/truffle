6230f22329f7c73b267562584a291b2bd6d61825
eggplantzzz <tmfeickert@gmail.com>
2021-11-10 18:14:51 -0500

    Only use logger from Subscriber and start simplifying how logging methods are triggered

--- packages/events/defaultSubscribers/migrate/index.js
+++ packages/events/defaultSubscribers/migrate/index.js
@@ -1,4 +1,5 @@
 const Reporter = require("./Reporter");
+const Messages = require("./Messages");
 
 module.exports = {
   initialize: () => {
@@ -8,51 +9,57 @@ module.exports = {
     "migrate:start": [
       function ({ config, Migrate }) {
         console.log(1)
-        Migrate.reporter = new Reporter(config.describeJson || false);
-        Migrate.logger = this.logger;
+        this.reporter = new Reporter({
+          logger: this.logger,
+          describeJson: config.describeJson || false,
+        });
       }
     ],
     "migrate:preAllMigrations": [
-      async function ({ dryRun, Migrate, migrations }) {
+      async function ({ dryRun, migrations, Migrate }) {
         console.log(2)
-        Migrate.reporter.setMigrator(Migrate);
-        Migrate.reporter.listenMigrator();
-        await Migrate.emitter.emit("preAllMigrations", {
-          dryRun,
-          migrations
+
+        const message = this.reporter.messages.steps("preAllMigrations", {
+          migrations,
+          dryRun
         });
+        this.logger.log(message);
       }
     ],
     "migrate:postAllMigrations": [
       async function ({ dryRun, error, Migrate }) {
         console.log(3)
-        await Migrate.emitter.emit("postAllMigrations", {
+        const message = this.reporter.messages.steps("postAllMigrations", {
           dryRun,
           error
         });
+        this.logger.log(message);
       }
     ],
+
     "migrate:migration:run:start": [
       async function ({ migration, deployer, confirmations }) {
         console.log(4)
-        if (migration.reporter) {
-          migration.reporter.setMigration(migration);
-          migration.reporter.setDeployer(deployer);
-          migration.reporter.confirmations = confirmations || 0;
-          migration.reporter.listen();
+        if (migration) {
+          this.reporter.setMigration(migration);
+          this.reporter.setDeployer(deployer);
+          this.reporter.confirmations = confirmations || 0;
+          this.reporter.listen();
         }
       }
     ],
     "migrate:migration:deploy:transaction:start": [
       async function ({ data, migration }) {
         console.log(5)
-        await migration.emitter.emit("startTransaction", data);
+        // await migration.emitter.emit("startTransaction", data);
+        await this.reporter.startTransaction(data);
       }
     ],
     "migrate:migration:deploy:transaction:succeed": [
       async function ({ data, migration }) {
         console.log(6)
-        await migration.emitter.emit("endTransaction", data);
+        // await migration.emitter.emit("endTransaction", data);
+        await this.reporter.endTransaction(data);
       }
     ],
     "migrate:migration:deploy:migrate:succeed": [
@@ -61,6 +68,7 @@ module.exports = {
         await migration.emitter.emit("postMigrate", eventArgs);
       }
     ],
+
     "migrate:migration:lastMigration:succeed": [
       async function ({ migration }) {
         console.log(8)
