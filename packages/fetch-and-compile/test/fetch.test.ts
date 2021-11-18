3cbdf29fc99e47c8a47eed46ba18e214c4b5d356
leeftk <lee.lara1219@gmail.com>
2021-11-18 12:02:28 -0600

    refactor switch statement to its own function

--- packages/fetch-and-compile/test/fetch.test.ts
+++ packages/fetch-and-compile/test/fetch.test.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 import sinon from "sinon";
 const fixture: any = require("./fixture.js");
 
-function axiosMock(url: string, address: string, data: object) {
+function stubAxiosGetMethod(url: string, address: string, data: object) {
   sinon.stub(axios, 'get').withArgs(url, {
     params: {
       module: "contract",
@@ -17,12 +17,12 @@ function axiosMock(url: string, address: string, data: object) {
     responseType: "json",
     maxRedirects: 50
   }).returns(Promise.resolve({ data: data }))
-}
+};
 afterEach(()=>
     //@ts-ignore
     //restoring stub
     axios.get.restore()
-)
+);
 describe("fetchAndCompile", () => {
   it('verifes contract from mainnet', async () => {
     const config = Config.default().merge({
@@ -33,14 +33,15 @@ describe("fetchAndCompile", () => {
       },
       network: "mainnet",
     });
+    const address = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
     //asserting that mainnet url and contract address is passed as args
-    axiosMock("https://api.etherscan.io/api", '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', fixture.mainnetData)
+    stubAxiosGetMethod("https://api.etherscan.io/api", address, fixture.mainnetData);
     //@ts-ignore
     axios.get.callThrough();
-    const result = await fetchAndCompile('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', config)
-    let contractName = result.sourceInfo.contractName
-    assert.equal(contractName, "UniswapV2Router02")
-    assert.notEqual(contractName, undefined)
+    const result = await fetchAndCompile(address, config);
+    let contractName = result.sourceInfo.contractName;
+    assert.equal(contractName, "UniswapV2Router02");
+    assert.notEqual(contractName, undefined);
   })
   it('verifes contract from arbitrum', async () => {
     const config = Config.default().merge({
@@ -51,14 +52,15 @@ describe("fetchAndCompile", () => {
       },
       network: "arbitrum",
     });
+    const address = '0xBf00759D7E329d7A7fa1D4DCdC914C53d1d2db86';
     //asserting that arbitrum url and contract address is passed as args
-    axiosMock("https://api.arbiscan.io/api", '0xBf00759D7E329d7A7fa1D4DCdC914C53d1d2db86', fixture.arbitrumData)
+    stubAxiosGetMethod("https://api.arbiscan.io/api", address, fixture.arbitrumData);
     //@ts-ignore
     axios.get.callThrough();
-    const result = await fetchAndCompile('0xBf00759D7E329d7A7fa1D4DCdC914C53d1d2db86', config)
-    let contractName = result.sourceInfo.contractName
-    assert.equal(contractName, "stARBIS")
-    assert.notEqual(contractName, undefined)
+    const result = await fetchAndCompile(address, config);
+    let contractName = result.sourceInfo.contractName;
+    assert.equal(contractName, "stARBIS");
+    assert.notEqual(contractName, undefined);
   });
   it('verfies contract from polygon', async () => {
     const config = Config.default().merge({
@@ -69,13 +71,14 @@ describe("fetchAndCompile", () => {
       },
       network: "polygon",
     });
+    const address = '0xBB6828C8228E5C641Eb6d89Ca22e09E6311CA398'
     //asserting that polygon url and contract address is passed as args
-    axiosMock("https://api.polygonscan.com/api", '0xBB6828C8228E5C641Eb6d89Ca22e09E6311CA398', fixture.polygonData)
+    stubAxiosGetMethod("https://api.polygonscan.com/api", address, fixture.polygonData);
     //@ts-ignore
     axios.get.callThrough();
-    const result = await fetchAndCompile('0xBB6828C8228E5C641Eb6d89Ca22e09E6311CA398', config)
-    let contractName = result.sourceInfo.contractName 
-    assert.equal(contractName, "GrowthVault")
-    assert.notEqual(contractName, undefined)
+    const result = await fetchAndCompile(address, config);
+    let contractName = result.sourceInfo.contractName;
+    assert.equal(contractName, "GrowthVault");
+    assert.notEqual(contractName, undefined);
   });
 });
