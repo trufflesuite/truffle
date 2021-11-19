da60fe5b35120de3af1305fb31a95b5803c62d80
teodor <31542280+tgbv@users.noreply.github.com>
2021-11-19 09:10:47 +0200

    Allow passing optional parameters to @truffle/contract instantiator

--- packages/contract/index.js
+++ packages/contract/index.js
@@ -2,10 +2,8 @@ const Schema = require("@truffle/contract-schema");
 const Contract = require("./lib/contract");
 const truffleContractVersion = require("./package.json").version;
 
-const contract = (json = {}) => {
-  const normalizedArtifactObject = Schema.normalize(json, {
-    validate: true
-  });
+const contract = (json = {}, opts = {}) => {
+  const normalizedArtifactObject = Schema.normalize(json, opts);
 
   // Note we don't use `new` here at all. This will cause the class to
   // "mutate" instead of instantiate an instance.
