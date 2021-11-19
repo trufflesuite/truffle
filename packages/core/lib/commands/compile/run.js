5b866981eefcab87d18ae2a91de1ecbf6a2c01cf
eggplantzzz <tmfeickert@gmail.com>
2021-11-19 11:44:27 -0500

    Remove some unused stuff in compile and correct error

--- packages/core/lib/commands/compile/run.js
+++ packages/core/lib/commands/compile/run.js
@@ -1,6 +1,46 @@
-const format = JSON.stringify;
 const path = require("path");
 const fse = require("fs-extra");
+
+module.exports = async function (options) {
+  const TruffleError = require("@truffle/error");
+  const WorkflowCompile = require("@truffle/workflow-compile");
+  const Config = require("@truffle/config");
+  const config = Config.detect(options);
+
+  if (config.list !== undefined) {
+    return await listVersions(config);
+  }
+
+  if (
+    options.saveIntermediate === true ||
+    (typeof options.saveIntermediate === "string" &&
+      options.saveIntermediate.trim() === "")
+  ) {
+    // user asked to save the intermediate compilation results
+    // but didn't provide the file to save the results to
+    throw new TruffleError(
+      "You must provide a file to save compilation results to."
+    );
+  }
+
+  const compilationOutput = await WorkflowCompile.compile(config);
+  if (options.saveIntermediate) {
+    // Get the filename the user provided to save the compilation results to
+    const compilationOutputFile = path.resolve(options.saveIntermediate);
+
+    await fse.writeFile(
+      compilationOutputFile,
+      JSON.stringify(compilationOutput),
+      { encoding: "utf8" }
+    );
+  }
+
+  const result = await WorkflowCompile.save(config, compilationOutput);
+  if (config.db && config.db.enabled) {
+    await WorkflowCompile.assignNames(config, result);
+  }
+  return result;
+};
 
 const listVersions = async function (options) {
   const { CompilerSupplier } = require("@truffle/compile-solidity");
@@ -21,11 +61,7 @@ const listVersions = async function (options) {
   const log = options.logger.log;
   options.list = options.list.length ? options.list : "releases";
 
-  const {
-    latestRelease,
-    releases,
-    prereleases
-  } = await supplier.list();
+  const { latestRelease, releases, prereleases } = await supplier.list();
   if (options.list === "latestRelease") {
     log(JSON.stringify(latestRelease, null, " "));
     return;
@@ -58,60 +94,4 @@ const listVersions = async function (options) {
 
   log(JSON.stringify(tags, null, " "));
   return;
-};
-
-const shortener = function (key, val) {
-  const defaultLength = 10;
-
-  if (Array.isArray(val) && val.length > defaultLength) {
-    const length = val.length;
-    const remaining = length - defaultLength;
-    const more =
-      ".. and " + remaining + " more. Use `--all` to see full list.";
-    val.length = defaultLength;
-    val.push(more);
-  }
-
-  return val;
-};
-
-module.exports = async function (options) {
-  const TruffleError = require("@truffle/error");
-  const WorkflowCompile = require("@truffle/workflow-compile");
-  const Config = require("@truffle/config");
-  const config = Config.detect(options);
-
-  if (config.list !== undefined) {
-    return await command.listVersions(config);
-  }
-
-  if (
-    options.saveIntermediate === true ||
-    (typeof options.saveIntermediate === "string" &&
-      options.saveIntermediate.trim() === "")
-  ) {
-    // user asked to save the intermediate compilation results
-    // but didn't provide the file to save the results to
-    throw new TruffleError(
-      "You must provide a file to save compilation results to."
-    );
-  }
-
-  const compilationOutput = await WorkflowCompile.compile(config);
-  if (options.saveIntermediate) {
-    // Get the filename the user provided to save the compilation results to
-    const compilationOutputFile = path.resolve(options.saveIntermediate);
-
-    await fse.writeFile(
-      compilationOutputFile,
-      JSON.stringify(compilationOutput),
-      { encoding: "utf8" }
-    );
-  }
-
-  const result = await WorkflowCompile.save(config, compilationOutput);
-  if (config.db && config.db.enabled) {
-    await WorkflowCompile.assignNames(config, result);
-  }
-  return result;
 };
