d8f4997929837b304ab2ca7ee93cff82f38dec06
liang liang <lsqd3v@gmail.com>
2021-11-24 16:48:32 +0000

    truffle(test): rename command runner

---
+++ packages/truffle/test/scenarios/commandrunner.js
@@ -0,0 +1,50 @@
+const exec = require("child_process").exec;
+const path = require("path");
+
+module.exports = {
+  run: function (command, config) {
+    let execString;
+
+    process.env.NO_BUILD
+      ? (execString =
+          "node " +
+          path.join(__dirname, "../", "../", "../", "core", "cli.js") +
+          " " +
+          command)
+      : (execString =
+          "node " +
+          path.join(__dirname, "../", "../", "build", "cli.bundled.js") +
+          " " +
+          command);
+
+    console.log(process.cwd());
+    console.log("CHILD", execString);
+
+    return new Promise((resolve, reject) => {
+      let child = exec(execString, {
+        cwd: config.working_directory
+      });
+
+      child.stdout.on("data", data => {
+        data = data.toString().replace(/\n$/, "");
+        console.log(data);
+        config.logger.log(data);
+      });
+      child.stderr.on("data", data => {
+        data = data.toString().replace(/\n$/, "");
+        config.logger.log(data);
+      });
+      child.on("close", code => {
+        // If the command didn't exit properly, show the output and throw.
+        if (code !== 0) {
+          reject(new Error("Unknown exit code: " + code));
+        }
+        resolve();
+      });
+
+      if (child.error) {
+        reject(child.error);
+      }
+    });
+  }
+};
