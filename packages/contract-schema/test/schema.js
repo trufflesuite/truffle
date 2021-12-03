be69a7fcd3c8f2305cce385d1adb30131fd04e45
teodor <31542280+tgbv@users.noreply.github.com>
2021-12-03 08:20:41 +0200

    Added tests for Schema.normalize()

--- packages/contract-schema/test/schema.js
+++ packages/contract-schema/test/schema.js
@@ -3,12 +3,12 @@ var assert = require("assert");
 
 var MetaCoin = require("./MetaCoin.json");
 
-describe("Schema", function() {
-  it("validates correct input", function() {
+describe("Schema", function () {
+  it("validates correct input", function () {
     Schema.validate(MetaCoin);
   });
 
-  it("throws exception on invalid input", function() {
+  it("throws exception on invalid input", function () {
     var invalid = {
       abi: -1
     };
@@ -16,7 +16,30 @@ describe("Schema", function() {
     try {
       Schema.validate(invalid);
     } catch (err) {
-      var abiErrors = err.errors.filter(function(error) {
+      var abiErrors = err.errors.filter(function (error) {
+        return error.dataPath === ".abi";
+      });
+      assert(abiErrors);
+    }
+  });
+
+  it("normalizes a correct input", function () {
+    Schema.normalize(MetaCoin, {
+      validate: true
+    });
+  });
+
+  it("throws exception when attempting to normalize an invalid schema", function () {
+    var invalid = {
+      abi: -1
+    };
+
+    try {
+      Schema.normalize(invalid, {
+        validate: true
+      });
+    } catch (err) {
+      var abiErrors = err.errors.filter(function (error) {
         return error.dataPath === ".abi";
       });
       assert(abiErrors);
