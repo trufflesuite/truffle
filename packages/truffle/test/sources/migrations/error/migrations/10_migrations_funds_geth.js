48db0f23e858b5d3954f55c6a06be828b462a874
eggplantzzz <tmfeickert@gmail.com>
2021-12-07 15:42:49 -0500

    adjust test

--- packages/truffle/test/sources/migrations/error/migrations/10_migrations_funds_geth.js
+++ packages/truffle/test/sources/migrations/error/migrations/10_migrations_funds_geth.js
@@ -1,19 +1,19 @@
 const Example = artifacts.require("Example");
 
 module.exports = async function (deployer, network, accounts) {
-  const emptyAccount = accounts[7];
-  let balance = await web3.eth.getBalance(emptyAccount);
+  const notEnoughFunds = accounts[7];
+  let balance = await web3.eth.getBalance(notEnoughFunds);
   const { baseFeePerGas } = await web3.eth.getBlock("latest");
-  // This transaction drains `emptyAccount` of all funds
   // 21,000 gas to send ether
   const priceOfGas = 21000 * baseFeePerGas;
 
   await web3.eth.sendTransaction({
     to: accounts[0],
-    from: emptyAccount,
-    value: balance - priceOfGas,
+    from: notEnoughFunds,
+    // this value is an approximation of how much it will take to deplete funds
+    value: "0x3634C9ADC5DE9FFFFF",
     gasPrice: baseFeePerGas
   });
 
-  await deployer.deploy(Example, { from: emptyAccount });
+  await deployer.deploy(Example, { from: notEnoughFunds });
 };
