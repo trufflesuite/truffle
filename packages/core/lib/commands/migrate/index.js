3771485e9d7e4eae1a8770684f94963a4e4a40cd
eggplantzzz <tmfeickert@gmail.com>
2021-12-08 11:43:08 -0500

    move method to its own file

--- packages/core/lib/commands/migrate/index.js
+++ packages/core/lib/commands/migrate/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   run: require("./run"),
   meta: require("./meta"),
-  determineDryRunSettings: require("./determineDryRunSettings")
+  determineDryRunSettings: require("./determineDryRunSettings"),
+  prepareConfigForRealMigrations: require("./prepareConfigForRealMigrations")
 };
--- packages/core/lib/commands/migrate/run.js
+++ packages/core/lib/commands/migrate/run.js
@@ -8,6 +8,7 @@ module.exports = async function (options) {
   const { promisify } = require("util");
   const copy = require("../../copy");
   const determineDryRunSettings = require("./determineDryRunSettings");
+  const prepareConfigForRealMigrations = require("./prepareConfigForRealMigrations");
   const tmp = require("tmp");
   tmp.setGracefulCleanup();
 
@@ -78,38 +79,5 @@ module.exports = async function (options) {
         return;
       }
     }
-  }
-};
-
-const prepareConfigForRealMigrations = async function (buildDir, options) {
-  const Artifactor = require("@truffle/artifactor");
-  const Resolver = require("@truffle/resolver");
-  const Migrate = require("@truffle/migrate");
-  const { Environment } = require("@truffle/environment");
-  const Config = require("@truffle/config");
-
-  let accept = true;
-
-  if (options.interactive) {
-    accept = await Migrate.acceptDryRun();
-  }
-
-  if (accept) {
-    const config = Config.detect(options);
-
-    config.contracts_build_directory = buildDir;
-    config.artifactor = new Artifactor(buildDir);
-    config.resolver = new Resolver(config);
-
-    try {
-      await Environment.detect(config);
-    } catch (error) {
-      throw new Error(error);
-    }
-
-    config.dryRun = false;
-    return { config, proceed: true };
-  } else {
-    return { proceed: false };
   }
 };
