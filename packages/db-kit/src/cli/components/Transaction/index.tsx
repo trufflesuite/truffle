0708836ed8af0cf6924c17035ff590c28b253ee3
tenthirtyone <alexsherbuck@gmail.com>
2021-12-14 19:15:12 -0600

    UserInput component

--- packages/db-kit/src/cli/components/Transaction/index.tsx
+++ packages/db-kit/src/cli/components/Transaction/index.tsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Text } from "ink";
 import { UserInput } from "../UserInput";
 
 export const Transaction = () => {
-  return <UserInput description={"Tx hash"} />;
+  const [txHash, setTxHash] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (txHash) {
+      console.log(txHash);
+    }
+  }, [txHash]);
+
+  return (
+    <Box flexDirection={"column"}>
+      <UserInput
+        description={"Tx hash"}
+        onSubmit={setTxHash}
+        enabled={!txHash}
+      />
+
+      <Box>
+        <Text>Fetching: </Text>
+        <Text>{txHash}</Text>
+      </Box>
+    </Box>
+  );
 };
--- packages/db-kit/src/cli/components/UserInput/index.tsx
+++ packages/db-kit/src/cli/components/UserInput/index.tsx
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, Text } from "ink";
 import { UncontrolledTextInput } from "ink-text-input";
 
 type UserInputProps = {
   description: string;
+  onSubmit: (data) => void;
+  enabled?: boolean;
 };
 
-export const UserInput = ({ description }: UserInputProps) => {
-  const [data, setData] = useState<string | undefined>();
-
+export const UserInput = ({
+  description,
+  onSubmit,
+  enabled
+}: UserInputProps) => {
   return (
     <Box>
-      <Text bold>
-        {description}: {data}
+      <Text bold>{description}:</Text>
+      <Text>
+        <UncontrolledTextInput
+          placeholder="0x..."
+          onSubmit={onSubmit}
+          focus={enabled}
+        />
       </Text>
-      <UncontrolledTextInput placeholder="0x..." onSubmit={setData} />
     </Box>
   );
 };
