2b4647994a367b255ecf82e1a900a08be45ecd16
Harry Altman <harry.j.altman@gmail.com>
2021-12-16 00:04:25 -0500

    Remove remote dev server from debugger

--- packages/debugger/lib/store/index.js
+++ packages/debugger/lib/store/index.js
@@ -1,7 +1,5 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./production");
-} else if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "test") {
   module.exports = require("./test");
 } else {
-  module.exports = require("./development");
+  module.exports = require("./production");
 }
