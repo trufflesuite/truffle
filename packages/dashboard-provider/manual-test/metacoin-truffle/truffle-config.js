ee4e6b86e201a3c1f7762acef048edb1ccf930d0
Rosco Kalis <roscokalis@gmail.com>
2021-12-20 16:22:00 +0100

    Update dependencies

--- packages/dashboard-provider/manual-test/metacoin-truffle/truffle-config.js
+++ packages/dashboard-provider/manual-test/metacoin-truffle/truffle-config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  networks: {
-    browser: {
-      url: "http://localhost:24012/rpc",
-      network_id: "*",
-      skipDryRun: true,
-      networkCheckTimeout: 120000
-    }
-  }
-};
+module.exports = {};
