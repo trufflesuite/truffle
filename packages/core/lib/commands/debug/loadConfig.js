2d6398bca9be9c90ab89e041b28f8d52736b4785
Luiz Santana <luiz.santana@camunda.com>
2021-12-21 09:56:55 +0100

    Documenting exception case

--- packages/core/lib/commands/debug/loadConfig.js
+++ packages/core/lib/commands/debug/loadConfig.js
@@ -7,6 +7,8 @@ module.exports = function (options) {
     config = Config.detect(options);
   } catch (error) {
     if (error instanceof TruffleError && options.url) {
+      // in case config file is not detected (exception) AND url is provided in the options
+      // we use default config and set compileNone to true
       config = Config.default();
       config.compileNone = true;
     } else {
