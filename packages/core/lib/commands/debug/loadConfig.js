b8387e7b1aa8d3bc9217228d2cc5a9404991420a
Luiz Santana <luiz.santana@camunda.com>
2021-12-22 20:00:18 +0100

    Adding provider to config network

--- packages/core/lib/commands/debug/loadConfig.js
+++ packages/core/lib/commands/debug/loadConfig.js
@@ -6,6 +6,7 @@ module.exports = function (options) {
   let config;
   try {
     config = Config.detect(options);
+    config = mergeConfigNetwork(config, options);
   } catch (error) {
     if (error instanceof TruffleError && options.url) {
       config = Config.default();
