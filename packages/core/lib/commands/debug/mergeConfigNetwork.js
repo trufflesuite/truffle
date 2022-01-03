975096226ca02b992722198cb05b34b4a3732d12
Harry Altman <harry.j.altman@gmail.com>
2022-01-03 16:30:41 -0500

    Avoid using url when no url specified

--- packages/core/lib/commands/debug/mergeConfigNetwork.js
+++ packages/core/lib/commands/debug/mergeConfigNetwork.js
@@ -1,10 +1,12 @@
 module.exports = function (config, options) {
-  const inlineConfigNetwork = "inline_config";
-  config.networks[inlineConfigNetwork] = {
-    url: options.url,
-    network_id: "*"
-  };
-  config.network = inlineConfigNetwork;
+  if (options.url) {
+    const inlineConfigNetwork = "inline_config";
+    config.networks[inlineConfigNetwork] = {
+      url: options.url,
+      network_id: "*"
+    };
+    config.network = inlineConfigNetwork;
+  }
   config.merge(options);
   return config;
 };
