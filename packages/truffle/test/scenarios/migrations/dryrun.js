d129686fe1ebd57f6e984c79ef0a04f81d6f5b12
eggplantzzz <tmfeickert@gmail.com>
2022-01-04 11:13:58 -0500

    unskip test

--- packages/truffle/test/scenarios/migrations/dryrun.js
+++ packages/truffle/test/scenarios/migrations/dryrun.js
@@ -6,7 +6,7 @@ const Server = require("../server");
 const Reporter = require("../reporter");
 const sandbox = require("../sandbox");
 
-describe.skip("migrate (dry-run)", function () {
+describe("migrate (dry-run)", function () {
   let config;
   const project = path.join(__dirname, "../../sources/migrations/success");
   const logger = new MemoryLogger();
