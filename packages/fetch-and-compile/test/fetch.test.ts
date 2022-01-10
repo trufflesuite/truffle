5b6a3ff5b6a0159d9753bd2b526844435d9ad1db
leeftk <lee.lara1219@gmail.com>
2022-01-10 09:53:17 -0600

    add assertion for testing compliations

--- packages/fetch-and-compile/test/fetch.test.ts
+++ packages/fetch-and-compile/test/fetch.test.ts
@@ -6,20 +6,23 @@ import axios from "axios";
 import sinon from "sinon";
 const fixture: any = require("./fixture.js");
 
-function stubAxiosGetMethod (url: string, address: string, data: object) {
-  sinon.stub(axios, 'get').withArgs(url, {
-    params: {
-      module: "contract",
-      action: "getsourcecode",
-      address: address,
-      apikey: ''
-    },
-    responseType: "json",
-    maxRedirects: 50
-  }).returns(Promise.resolve({ data: data }));
-};
+function stubAxiosGetMethod(url: string, address: string, data: object) {
+  sinon
+    .stub(axios, "get")
+    .withArgs(url, {
+      params: {
+        module: "contract",
+        action: "getsourcecode",
+        address: address,
+        apikey: ""
+      },
+      responseType: "json",
+      maxRedirects: 50
+    })
+    .returns(Promise.resolve({ data: data }));
+}
 
-afterEach(function (){
+afterEach(function () {
   //TS can't detect that is a sinon stub so we have to use ts-ignore
   //@ts-ignore
   //restoring stub
@@ -27,63 +30,98 @@ afterEach(function (){
 });
 
 describe("fetchAndCompile", function () {
-  it('verifes contract from mainnet', async function () {
+  it("verifes contract from mainnet", async function () {
     const config = Config.default().merge({
       networks: {
         mainnet: {
           network_id: 1
         }
       },
-      network: "mainnet",
+      network: "mainnet"
     });
-    const address = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+    const address = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
     //asserting that mainnet url and contract address is passed as args
-    stubAxiosGetMethod("https://api.etherscan.io/api", address, fixture.mainnetData);
+    stubAxiosGetMethod(
+      "https://api.etherscan.io/api",
+      address,
+      fixture.mainnetData
+    );
     //TS can't detect that is a sinon stub so we have to use ts-ignore
     //@ts-ignore
     axios.get.callThrough();
     const result = await fetchAndCompile(address, config);
-    let contractName = result.sourceInfo.contractName;
-    assert.equal(contractName, "UniswapV2Router02");
+    let contractNameSourceInfo = result.sourceInfo.contractName;
+    let contractNameCompliations =
+      result.compileResult.compilations[0].contracts;
+    assert(
+      contractNameCompliations.some(
+        item => item.contractName === "IUniswapV2Factory"
+      )
+    );
+    assert.equal(
+      contractNameSourceInfo,
+
+      "UniswapV2Router02"
+    );
   });
 
-  it('verifes contract from arbitrum', async function () {
+  it("verifes contract from arbitrum", async function () {
     const config = Config.default().merge({
       networks: {
         arbitrum: {
           network_id: 42161
         }
       },
-      network: "arbitrum",
+      network: "arbitrum"
     });
-    const address = '0xBf00759D7E329d7A7fa1D4DCdC914C53d1d2db86';
+    const address = "0xBf00759D7E329d7A7fa1D4DCdC914C53d1d2db86";
     //asserting that arbitrum url and contract address is passed as args
-    stubAxiosGetMethod("https://api.arbiscan.io/api", address, fixture.arbitrumData);
+    stubAxiosGetMethod(
+      "https://api.arbiscan.io/api",
+      address,
+      fixture.arbitrumData
+    );
     //TS can't detect that is a sinon stub so we have to use ts-ignore
     //@ts-ignore
     axios.get.callThrough();
     const result = await fetchAndCompile(address, config);
-    let contractName = result.sourceInfo.contractName;
-    assert.equal(contractName, "stARBIS");
+    let contractNameSourceInfo = result.sourceInfo.contractName;
+    let contractNameCompliations =
+      result.compileResult.compilations[0].contracts;
+    //console.log(contractNameCompliations)
+    assert(
+      contractNameCompliations.some(item => item.contractName === "stARBIS")
+    );
+    assert.equal(contractNameSourceInfo, "stARBIS");
+    //assert.equal(contractNameCompliations, "stARBIS");
   });
 
-  it('verfies contract from polygon', async function () {
+  it("verfies contract from polygon", async function () {
     const config = Config.default().merge({
       networks: {
         polygon: {
           network_id: 137
         }
       },
-      network: "polygon",
+      network: "polygon"
     });
-    const address = '0xBB6828C8228E5C641Eb6d89Ca22e09E6311CA398';
+    const address = "0xBB6828C8228E5C641Eb6d89Ca22e09E6311CA398";
     //asserting that polygon url and contract address is passed as args
-    stubAxiosGetMethod("https://api.polygonscan.com/api", address, fixture.polygonData);
+    stubAxiosGetMethod(
+      "https://api.polygonscan.com/api",
+      address,
+      fixture.polygonData
+    );
     //TS can't detect that is a sinon stub so we have to use ts-ignore
     //@ts-ignore
     axios.get.callThrough();
     const result = await fetchAndCompile(address, config);
-    let contractName = result.sourceInfo.contractName;
-    assert.equal(contractName, "GrowthVault");
+    let contractNameSourceInfo = result.sourceInfo.contractName;
+    let contractNameCompliations =
+      result.compileResult.compilations[0].contracts;
+    assert(
+      contractNameCompliations.some(item => item.contractName === "GrowthVault")
+    );
+    assert.equal(contractNameSourceInfo, "GrowthVault");
   });
 });
