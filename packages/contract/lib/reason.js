ff84ea0a20709dc1668e393e5d6ce6d29d9c4c87
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2022-01-19 15:40:26 -0500

    Restore old error-checking logic

--- packages/contract/lib/reason.js
+++ packages/contract/lib/reason.js
@@ -13,13 +13,7 @@ const reason = {
   _extract: function(res, web3, _interfaceAdapter) {
     //I'm not sure why interfaceAdapter is here if it's not used,
     //so I just put an underscore in front of its name for now...
-    if (!res) return;
-    if (!res.error && (
-        !res.result /* ganache 2.0 */
-        || (res.error && res.error.data && !res.error.data.result /* ganache 7.x */)
-      )
-    ) return;
-
+    if (!res || (!res.error && !res.result)) return;
 
     const errorStringHash = "0x08c379a0";
 
