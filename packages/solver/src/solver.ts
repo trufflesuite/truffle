5fc13bccb83b81b3e706c02c6f53381d9359fba0
fainashalts <faina@trufflesuite.com>
2022-01-21 06:15:40 -0800

    Move types into own folder

--- packages/solver/src/solver.ts
+++ packages/solver/src/solver.ts
@@ -1,22 +1,6 @@
 import { loadAll } from "js-yaml";
 import { readFile } from "fse";
-
-// @to-do: move to types folder/file
-type DeploymentTarget = {
-  contractName: string;
-  network: string;
-  // other contracts, any captured variables from a previous deployment
-  // not sure what the best way to arrange these will be, just an array of any for now
-  dependencies?: Array<any>;
-  //this will be a function to check whether the target in question
-  //has finished successfully
-  isCompleted: any;
-  //this is the function to complete this target, this is the function that will
-  //actually deploy, link, execute, etc.; ultimately the execution layer of the declarative
-  //deployments module will look something like DeploymentSteps[0].run(contractName, options), etc.;
-  run: any;
-};
-type DeploymentSteps = Array<DeploymentTarget>;
+import { DeploymentSteps } from "./types/";
 
 export const Solver = {
   readFile: async function (filepath: string): Promise<any> {
---
+++ packages/solver/src/types/index.ts
@@ -0,0 +1,19 @@
+//tslint-disable
+// solver typescript definitions
+
+export interface DeclarationTarget {
+  contractName: string;
+  network: string;
+  // other contracts, any captured variables from a previous deployment
+  // not sure what the best way to arrange these will be, just an array of any for now
+  dependencies?: Array<any>;
+  //this will be a function to check whether the target in question
+  //has finished successfully
+  isCompleted: any;
+  //this is the function to complete this target, this is the function that will
+  //actually deploy, link, execute, etc.; ultimately the execution layer of the declarative
+  //deployments module will look something like DeploymentSteps[0].run(contractName, options), etc.;
+  run: any;
+}
+
+export type DeploymentSteps = Array<DeclarationTarget>;
