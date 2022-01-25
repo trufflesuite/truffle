ff873b278d2fe52a27da0a0bc32f251689c3fc4e
cds-amal <cds.sudama@gmail.com>
2022-01-25 12:16:04 -0500

    wip: instrument commandRunner

--- packages/truffle/test/scenarios/commandRunner.js
+++ packages/truffle/test/scenarios/commandRunner.js
@@ -1,6 +1,19 @@
 const { exec } = require("child_process");
 const { EOL } = require("os");
 const path = require("path");
+const util = require("util");
+
+//prepare a helpful message to standout in CI log noise
+const Log = (msg, isErr) => {
+  const fmt = msg
+    .split("\n")
+    .map(
+      l =>
+        `\t---truffle commandRunner ${isErr ? "stderr" : "stdout"}--- |\t${l}`
+    )
+    .join("\n");
+  console.log(fmt);
+};
 
 module.exports = {
   getExecString: function () {
@@ -11,22 +24,37 @@ module.exports = {
   run: function (command, config) {
     const execString = `${this.getExecString()} ${command}`;
 
+    Log("CommandRunner");
+    Log(util.inspect(config.networks, { depth: Infinity }));
+    Log(util.inspect(config.network, { depth: Infinity }));
+    Log(util.inspect(config.truffle_directory, { depth: Infinity }));
+    Log(util.inspect(config.working_directory, { depth: Infinity }));
+    Log(`execString: ${execString}`);
+
     return new Promise((resolve, reject) => {
       let child = exec(execString, {
-        cwd: config.working_directory
+        cwd: config.working_directory,
+        env: {
+          ...process.env,
+          DEBUG:
+            "*,-develop*,-co*,-reselect*,-pro*,-dec*,-resolv*,-deb*,provider,-source*"
+        }
       });
 
       child.stdout.on("data", data => {
         data = data.toString().replace(/\n$/, "");
+        Log(data);
         config.logger.log(data);
       });
       child.stderr.on("data", data => {
         data = data.toString().replace(/\n$/, "");
+        Log(data, true);
         config.logger.log(data);
       });
       child.on("close", code => {
         // If the command didn't exit properly, show the output and throw.
         if (code !== 0) {
+          Log(`errorCode: ${code}`, true);
           reject(new Error("Unknown exit code: " + code));
         }
         resolve();
