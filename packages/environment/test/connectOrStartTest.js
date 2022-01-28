edb988452c09f1586354a6e459381ef29e70f351
cds-amal <cds.sudama@gmail.com>
2022-01-28 18:07:46 -0500

    environment: use finally for cleanup

--- packages/environment/test/connectOrStartTest.js
+++ packages/environment/test/connectOrStartTest.js
@@ -10,24 +10,45 @@ describe("connectOrStart test network", async function () {
   };
 
   it("starts Ganache when no Ganache instance is running", async function () {
-    const connection = await Develop.connectOrStart(ipcOptions, ganacheOptions);
-    assert.isTrue(connection.started);
-    assert.isFunction(connection.disconnect);
-    connection.disconnect();
+    let connection;
+    try {
+      connection = await Develop.connectOrStart(ipcOptions, ganacheOptions);
+      assert.isTrue(connection.started, "A new server did not spin up");
+      assert.isFunction(connection.disconnect, "Disconnect is not a function");
+    } catch (error) {
+      console.log(error);
+      assert.fail("Develop.connectOrStart should not have thrown!");
+    } finally {
+      if (connection) {
+        return connection.disconnect();
+      }
+    }
   });
 
   it("connects to an established ganache instance", async function () {
-    //establish a connection
-    const { disconnect: connectionOneDisconnect } =
-      await Develop.connectOrStart(ipcOptions, ganacheOptions);
+    let connectionOneDisconnect, connectionTwoDisconnect;
 
-    //invoke the method again
-    const { disconnect: connectionTwoDisconnect, started } =
-      await Develop.connectOrStart(ipcOptions, ganacheOptions);
-    assert.isFalse(started);
+    try {
+      //establish a connection
+      connectionOneDisconnect = (
+        await Develop.connectOrStart(ipcOptions, ganacheOptions)
+      ).disconnect;
 
-    //cleanup
-    await connectionOneDisconnect();
-    await connectionTwoDisconnect();
+      //invoke the method again
+      const result = await Develop.connectOrStart(ipcOptions, ganacheOptions);
+      connectionTwoDisconnect = result.disconnect;
+      assert.isFalse(result.started);
+    } catch (error) {
+      console.log(error);
+      assert.fail("Develop.connectOrStart should not have thrown!");
+    } finally {
+      //cleanup
+      if (connectionOneDisconnect) {
+        connectionOneDisconnect();
+      }
+      if (connectionTwoDisconnect) {
+        connectionTwoDisconnect();
+      }
+    }
   });
 });
