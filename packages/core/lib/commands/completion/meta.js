54a7ad254a7d26bec8cb6ca94ee4ba67feb3047a
hughes-ch <contact@chrishughesdev.com>
2022-02-08 20:50:02 -0500

    Uninstall CLI tab-complete

--- packages/core/lib/commands/completion/meta.js
+++ packages/core/lib/commands/completion/meta.js
@@ -1,10 +1,17 @@
+const install = require("./commands/install");
+const uninstall = require("./commands/uninstall");
+
 module.exports = {
   command: "completion",
-  description: false, // Install bash/zsh tab completion - only at install
+  description: false, // Not intended to be used by clients
   builder: {},
   help: {
-    usage: "truffle completion",
+    usage: "truffle completion <command>",
     options: [],
     allowedGlobalOptions: []
+  },
+  commands: {
+    install: install.meta,
+    uninstall: uninstall.meta
   }
 };
