915b300f4ef550cc9d619d207b3e3838b2a6c28b
hughes-ch <contact@chrishughesdev.com>
2022-02-09 14:16:43 -0500

    Rename to prevent unit test failure

---
+++ packages/core/lib/commands/autocomplete/commands/uninstall/meta.js
@@ -0,0 +1,10 @@
+module.exports = {
+  command: "uninstall",
+  description: "Uninstall CLI tab-completion",
+  builder: {},
+  help: {
+    usage: "truffle autocomplete uninstall",
+    options: [],
+    allowedGlobalOptions: []
+  }
+};
--- packages/core/lib/commands/autocomplete/meta.js
+++ packages/core/lib/commands/autocomplete/meta.js
@@ -2,11 +2,11 @@ const install = require("./commands/install");
 const uninstall = require("./commands/uninstall");
 
 module.exports = {
-  command: "completion",
+  command: "autocomplete",
   description: false, // Not intended to be used by clients
   builder: {},
   help: {
-    usage: "truffle completion <command>",
+    usage: "truffle autocomplete <command>",
     options: [],
     allowedGlobalOptions: []
   },
