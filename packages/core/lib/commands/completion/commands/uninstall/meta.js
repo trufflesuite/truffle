9b26590c80a9f369b5196ec9be101cbeb13302e7
hughes-ch <contact@chrishughesdev.com>
2022-02-08 20:50:02 -0500

    Uninstall CLI tab-complete

---
+++ packages/core/lib/commands/completion/commands/uninstall/meta.js
@@ -0,0 +1,10 @@
+module.exports = {
+  command: "install",
+  description: "Install CLI tab-completion",
+  builder: {},
+  help: {
+    usage: "truffle completion install",
+    options: [],
+    allowedGlobalOptions: []
+  }
+};
