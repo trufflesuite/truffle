a1e6b6de691d3474fe3de0e05ca9baa3b7cee278
hughes-ch <contact@chrishughesdev.com>
2022-02-09 14:16:43 -0500

    Rename to prevent unit test failure

--- packages/core/lib/commands/index.js
+++ packages/core/lib/commands/index.js
@@ -1,7 +1,7 @@
 module.exports = {
+  autocomplete: require("./autocomplete"),
   build: require("./build"),
   compile: require("./compile"),
-  completion: require("./completion"),
   config: require("./config"),
   console: require("./console"),
   create: require("./create"),
