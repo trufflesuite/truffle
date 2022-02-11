15cf491d212b94c48da7d709aec5123666430e79
fainashalts <faina@trufflesuite.com>
2022-02-10 22:00:08 -0800

    Add basic declarationSteps to solver

--- packages/solver/src/solver.ts
+++ packages/solver/src/solver.ts
@@ -1,34 +1,41 @@
 import { loadAll } from "js-yaml";
 import { readFile } from "fse";
-import { DeploymentSteps } from "./types/";
+import { DeploymentSteps, DeclarationEntry } from "./types/";
 
 export const Solver = {
-  readFile: async function (filepath: string): Promise<any> {
+  read: async function (filepath: string): Promise<any> {
     const declarations: any = loadAll(await readFile(filepath, "utf8"));
+    // @TODO check specifically for yaml in case this is a JSON file!
+    // const fileExtension = filepath.split('.').pop();
+    // if (fileExtension === 'yaml') {
+    //   declarations = loadAll(declarations);
+    // }
     return declarations;
   },
+  // put together a list of contracts that need to be deployed, with relevant information for the deployment
   orchestrate: async function (filepath: string): Promise<DeploymentSteps> {
-    const declarations: any = loadAll(await readFile(filepath, "utf8"));
-    let deploymentSteps: Array<DeploymentSteps> = [];
-    console.log("declarations loaded! " + JSON.stringify(declarations));
+    let declarations: any = await this.read(filepath);
+    let deploymentSteps: DeploymentSteps = [];
+
     declarations.map(declaration => {
-      console.log(
-        "declaration singular " + JSON.stringify(declaration.deployed)
-      );
-      deploymentSteps.push(declaration.deployed);
+      declaration.deployed.map((entry: DeclarationEntry) => {
+        // should I split up the actions here? a declaration with a linked contract will need the link
+        // part and then the deploy
+        const entryValues: Array<any> = Object.values(entry)[0];
+        entryValues.map(contract => {
+          // will have capture variables to add to dependencies also, this is just the start
+          const links = contract.links ? contract.links : [];
+          const declarationTarget = {
+            contractName: contract.contract,
+            network: Object.keys(entry)[0],
+            dependencies: links,
+            isCompleted: false,
+            run: () => {}
+          };
+          deploymentSteps.push(declarationTarget);
+        });
+      });
     });
-    console.log("deployment steps!" + JSON.stringify(deploymentSteps));
-    return [
-      {
-        contractName: "SimpleStorage",
-        network: "development",
-        dependencies: [
-          { contract: "SafeMathLib", SafeMathLibAddress: "0x12345" }
-        ],
-        run: () => {},
-        isCompleted: false
-      }
-    ];
+    return deploymentSteps;
   }
 };
-console.log("solver? " + JSON.stringify(Solver));
