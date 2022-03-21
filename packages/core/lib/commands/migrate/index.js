7603dddbab08d3b855f035e7b4efa001469c856d
eggplantzzz <tmfeickert@gmail.com>
2022-03-21 16:29:05 -0400

    factor out some migration helper methods and shut ganache logging up during testing

--- packages/core/lib/commands/migrate/index.js
+++ packages/core/lib/commands/migrate/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   run: require("./run"),
   meta: require("./meta"),
+  runMigrations: require("./runMigrations"),
+  setUpDryRunEnvironmentThenRunMigrations: require("./setUpDryRunEnvironmentThenRunMigrations"),
   determineDryRunSettings: require("./determineDryRunSettings"),
   prepareConfigForRealMigrations: require("./prepareConfigForRealMigrations")
 };
---
+++ packages/core/lib/commands/migrate/setUpDryRunEnvironmentThenRunMigrations.js
@@ -0,0 +1,37 @@
+const { Environment } = require("@truffle/environment");
+const { promisify } = require("util");
+const Artifactor = require("@truffle/artifactor");
+const Resolver = require("@truffle/resolver");
+const copy = require("../../copy");
+const tmp = require("tmp");
+tmp.setGracefulCleanup();
+const runMigrations = require("./runMigrations");
+
+module.exports = async function (config) {
+  await Environment.fork(config, {
+    logging: {
+      quiet: true
+    },
+    // we need to tell Ganache to not unlock any accounts so that only
+    // user's accounts are unlocked since this will be a dry run
+    wallet: {
+      totalAccounts: 0
+    }
+  });
+  // Copy artifacts to a temporary directory
+  const temporaryDirectory = tmp.dirSync({
+    unsafeCleanup: true,
+    prefix: "migrate-dry-run-"
+  }).name;
+
+  await promisify(copy)(config.contracts_build_directory, temporaryDirectory);
+
+  config.contracts_build_directory = temporaryDirectory;
+  // Note: Create a new artifactor and resolver with the updated config.
+  // This is because the contracts_build_directory changed.
+  // Ideally we could architect them to be reactive of the config changes.
+  config.artifactor = new Artifactor(temporaryDirectory);
+  config.resolver = new Resolver(config);
+
+  return await runMigrations(config);
+};
