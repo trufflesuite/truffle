96465a117fdfc68df56b3597316de7987ca95a26
MicaiahReid <micaiahreid@gmail.com>
2022-03-22 12:51:12 -0400

    add more data to default public chains

--- packages/config/src/configDefaults.ts
+++ packages/config/src/configDefaults.ts
@@ -47,11 +47,56 @@ export const getInitialConfig = ({
       port: 24012,
       verbose: false,
       publicChains: [
-        { chainId: "0x1", chainName: "mainnet" },
-        { chainId: "0x3", chainName: "ropsten" },
-        { chainId: "0x4", chainName: "rinkeby" },
-        { chainId: "0x2A", chainName: "kovan" },
-        { chainId: "0x5", chainName: "görli" },
+        {
+          chainId: "0x1",
+          chainName: "mainnet",
+          nativeCurrency: {
+            symbol: "ETH",
+            decimals: 18
+          },
+          rpcUrls: ["https://mainnet.infura.io/v3/"],
+          blockExplorerUrls: ["https://etherscan.com"]
+        },
+        {
+          chainId: "0x3",
+          chainName: "ropsten",
+          nativeCurrency: {
+            symbol: "ETH",
+            decimals: 18
+          },
+          rpcUrls: ["https://ropsten.infura.io/v3/"],
+          blockExplorerUrls: ["https://ropsten.etherscan.com"]
+        },
+        {
+          chainId: "0x4",
+          chainName: "rinkeby",
+          nativeCurrency: {
+            symbol: "ETH",
+            decimals: 18
+          },
+          rpcUrls: ["https://rinkeby.infura.io/v3/"],
+          blockExplorerUrls: ["https://rinkeby.etherscan.com"]
+        },
+        {
+          chainId: "0x2A",
+          chainName: "kovan",
+          nativeCurrency: {
+            symbol: "ETH",
+            decimals: 18
+          },
+          rpcUrls: ["https://ropsten.infura.io/v3/"],
+          blockExplorerUrls: ["https://kovan.etherscan.com"]
+        },
+        {
+          chainId: "0x5",
+          chainName: "goerli",
+          nativeCurrency: {
+            symbol: "ETH",
+            decimals: 18
+          },
+          rpcUrls: ["https://goerli.infura.io/v3/"],
+          blockExplorerUrls: ["https://goerli.etherscan.com"]
+        },
         {
           chainId: "0x38",
           chainName: "Binance Smart Chain Mainnet",
