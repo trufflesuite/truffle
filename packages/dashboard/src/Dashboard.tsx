c68b1d12a1ecc37da3946677eaded7989b618185
MicaiahReid <micaiahreid@gmail.com>
2022-03-22 20:15:41 -0400

    use new NetworkSwitcher react class

--- packages/dashboard/src/Dashboard.tsx
+++ packages/dashboard/src/Dashboard.tsx
@@ -83,7 +83,7 @@ function Dashboard() {
 
   return (
     <div className="h-full min-h-screen bg-gradient-to-b from-truffle-lighter to-truffle-light">
-      <Header />
+      <Header publicChains={publicChains} />
       {paused && chainId && connectedChainId && (
         <ConfirmNetworkChanged
           newChainId={chainId}
@@ -100,7 +100,6 @@ function Dashboard() {
           setRequests={setDashboardProviderRequests}
         />
       )}
-      {!paused && socket && <div>{JSON.stringify(publicChains)}</div>}
     </div>
   );
 }
