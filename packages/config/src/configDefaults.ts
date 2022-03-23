c2b8e0f23119f96e73051ae83b3fc870ca5bd338
MicaiahReid <micaiahreid@gmail.com>
2022-03-23 19:52:33 -0400

    move default chains from config to run.js

--- packages/config/src/configDefaults.ts
+++ packages/config/src/configDefaults.ts
@@ -45,59 +45,7 @@ export const getInitialConfig = ({
     dashboard: {
       host: "localhost",
       port: 24012,
-      verbose: false,
-      publicChains: [
-        {
-          chainId: "0x1",
-          chainName: "mainnet",
-          nativeCurrency: {
-            symbol: "ETH",
-            decimals: 18
-          },
-          rpcUrls: ["https://mainnet.infura.io/v3/"],
-          blockExplorerUrls: ["https://etherscan.com"]
-        },
-        {
-          chainId: "0x3",
-          chainName: "ropsten",
-          nativeCurrency: {
-            symbol: "ETH",
-            decimals: 18
-          },
-          rpcUrls: ["https://ropsten.infura.io/v3/"],
-          blockExplorerUrls: ["https://ropsten.etherscan.com"]
-        },
-        {
-          chainId: "0x4",
-          chainName: "rinkeby",
-          nativeCurrency: {
-            symbol: "ETH",
-            decimals: 18
-          },
-          rpcUrls: ["https://rinkeby.infura.io/v3/"],
-          blockExplorerUrls: ["https://rinkeby.etherscan.com"]
-        },
-        {
-          chainId: "0x2A",
-          chainName: "kovan",
-          nativeCurrency: {
-            symbol: "ETH",
-            decimals: 18
-          },
-          rpcUrls: ["https://ropsten.infura.io/v3/"],
-          blockExplorerUrls: ["https://kovan.etherscan.com"]
-        },
-        {
-          chainId: "0x5",
-          chainName: "goerli",
-          nativeCurrency: {
-            symbol: "ETH",
-            decimals: 18
-          },
-          rpcUrls: ["https://goerli.infura.io/v3/"],
-          blockExplorerUrls: ["https://goerli.etherscan.com"]
-        }
-      ]
+      verbose: false
     },
     ethpm: {
       ipfs_host: "ipfs.infura.io",
