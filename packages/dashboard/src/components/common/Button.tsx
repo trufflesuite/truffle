8dd5d68777aaca69ada6233dab62f76ce1c81055
tenthirtyone <alexsherbuck@gmail.com>
2022-03-23 16:34:03 -0500

    send transaction through footer button, form validation, error message

--- packages/dashboard/src/components/common/Button.tsx
+++ packages/dashboard/src/components/common/Button.tsx
@@ -3,13 +3,15 @@ import type { MouseEventHandler } from "react";
 interface Props {
   onClick: MouseEventHandler<HTMLElement>;
   text: string;
+  disabled?: boolean;
 }
 
-function Button({ onClick, text }: Props) {
+function Button({ onClick, text, disabled = false }: Props) {
   return (
     <button
       className="rounded p-2 bg-truffle-blue text-truffle-brown uppercase hover:bg-white"
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
