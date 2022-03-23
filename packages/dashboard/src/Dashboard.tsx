1249de3c48d2161a058bd78e496cc458c973a006
MicaiahReid <micaiahreid@gmail.com>
2022-03-23 11:23:32 -0400

    comment, cleanup

--- packages/dashboard/src/Dashboard.tsx
+++ packages/dashboard/src/Dashboard.tsx
@@ -10,7 +10,6 @@ import {
   LogMessage,
   sendAndAwait,
   createMessage,
-  jsonToBase64,
   isLogMessage
 } from "@truffle/dashboard-message-bus";
 import { useWeb3React } from "@web3-react/core";
@@ -111,13 +110,15 @@ function Dashboard() {
       }
     );
 
-    const message = createMessage("initialize", jsonToBase64({}));
+    // since our socket is open, request some initial data from the server
+    const message = createMessage("initialize", ""); // no payload needed
     const response = await sendAndAwait(socket, message);
     if (response.payload.error) {
       console.log("error on initialization: " + JSON.stringify(response));
     } else {
       setPublicChains(response.payload.publicChains);
     }
+
     setPublishSocket(socket);
   };
 
