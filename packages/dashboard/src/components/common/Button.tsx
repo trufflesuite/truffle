a942a58d25bcb10489f250493a3d5d7e1c792624
Dongming Hwang <dongming.hwang@consensys.net>
2022-03-26 10:15:59 -0400

    Add disabled to truffle dashboard Button()
    This PR adds *disabled* and conditional operator to className to Button()

--- packages/dashboard/src/components/common/Button.tsx
+++ packages/dashboard/src/components/common/Button.tsx
@@ -1,14 +1,19 @@
 import type { MouseEventHandler } from "react";
 
 interface Props {
+  disabled: boolean;
   onClick: MouseEventHandler<HTMLElement>;
   text: string;
 }
 
-function Button({ onClick, text }: Props) {
+function Button({ disabled, onClick, text }: Props) {
   return (
     <button
-      className="rounded p-2 bg-truffle-blue text-truffle-brown uppercase hover:bg-white"
+      className={
+        "rounded p-2 " + (disabled ? "" : "bg-truffle-blue") +
+        " text-truffle-brown uppercase hover:bg-white"
+      }
+      disabled={disabled}
       onClick={onClick}
     >
       {text}
