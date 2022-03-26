461d285f3d51849f51e9d2cfcf261aa3e74b86f7
fainashalts <faina@trufflesuite.com>
2022-03-25 17:17:33 -0700

    Make icon sticky and use truffle color

--- packages/dashboard/src/components/common/SettingsIcon.tsx
+++ packages/dashboard/src/components/common/SettingsIcon.tsx
@@ -8,7 +8,7 @@ interface Props {
 function SettingsIcon({ analyticsSet }: Props) {
   return (
     <div>
-      <button className="absolute bottom-2 right-2"><MdSettings size={56} />
+      <button className="absolute bottom-2 right-2 position:sticky"><MdSettings size={56} color="#5e464d" />
         {!analyticsSet &&
           (
             <span id="settings-button" className="animate-pulse absolute bg-red-600 h-4 w-4 rounded-full -top-0 -right-0" />
