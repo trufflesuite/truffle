da78b8090c2399cf14fb15cc8a8f64f1a93d84b7
David Killen <david.killen@gmail.com>
2022-03-28 10:58:06 +1100

    Adding and formatting block explorer link and address copy to clipboard.

---
+++ packages/dashboard/src/components/AccountDetails/Copy.tsx
@@ -0,0 +1,38 @@
+import { CheckIcon, DocumentDuplicateIcon } from '@heroicons/react/outline';
+import { classNames } from '../../functions';
+import { useCopyClipboard } from '../../utils/utils';
+import React, { FC } from 'react';
+
+interface CopyToClipboardProps {
+    className?: string
+    toCopy: string
+    children?: React.ReactNode
+}
+
+const CopyToClipboard: FC<CopyToClipboardProps> = ({
+                                           className,
+                                           toCopy,
+                                           children
+                                        }) => {
+    const [isCopied, setCopied] = useCopyClipboard();
+
+    return (
+        <div className={classNames(className)} onClick={() => setCopied(toCopy)}>
+            {isCopied && (
+                <div className="flex items-center gap-1 cursor-pointer">
+                    {children}
+                    <CheckIcon width={16} height={16} />
+                </div>
+            )}
+
+            {!isCopied && (
+                <div className="flex items-center gap-1 cursor-pointer">
+                    {children}
+                    <DocumentDuplicateIcon width={16} height={16} />
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default CopyToClipboard;
