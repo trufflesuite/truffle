3678e84cd15eb2706b2ea3602beb009ae1093a84
eggplantzzz <tmfeickert@gmail.com>
2022-04-05 12:59:09 -0400

    use mvdir instead of cpr for copying files/directories

--- packages/core/lib/commands/test/copyArtifactsToTempDir.js
+++ packages/core/lib/commands/test/copyArtifactsToTempDir.js
@@ -1,5 +1,4 @@
 const copyArtifactsToTempDir = async config => {
-  const {promisify} = require("util");
   const copy = require("../../copy");
   const fs = require("fs");
   const OS = require("os");
@@ -19,11 +18,11 @@ const copyArtifactsToTempDir = async config => {
     return {temporaryDirectory};
   }
 
-  await promisify(copy)(config.contracts_build_directory, temporaryDirectory);
+  await copy(config.contracts_build_directory, temporaryDirectory);
   if (config.runnerOutputOnly !== true) {
     config.logger.log("Using network '" + config.network + "'." + OS.EOL);
   }
-  return {temporaryDirectory};
+  return { temporaryDirectory };
 };
 
 module.exports = {
