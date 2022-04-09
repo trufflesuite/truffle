d1d5621fe5139546457cabcc3a3b5755b48bf6b3
eggplantzzz <tmfeickert@gmail.com>
2022-04-09 11:30:48 -0400

    destructure bip39 libs in hdwallet-provider

--- packages/hdwallet-provider/src/index.ts
+++ packages/hdwallet-provider/src/index.ts
@@ -1,4 +1,7 @@
-import * as bip39 from "ethereum-cryptography/bip39";
+import {
+  mnemonicToSeedSync,
+  validateMnemonic
+} from "ethereum-cryptography/bip39";
 import { wordlist } from "ethereum-cryptography/bip39/wordlists/english";
 import * as EthUtil from "ethereumjs-util";
 import ethJSWallet from "ethereumjs-wallet";
@@ -95,7 +98,7 @@ class HDWalletProvider {
         [
           `No provider or an invalid provider was specified: '${providerToUse}'`,
           "Please specify a valid provider or URL, using the http, https, " +
-          "ws, or wss protocol.",
+            "ws, or wss protocol.",
           ""
         ].join("\n")
       );
@@ -304,10 +307,10 @@ class HDWalletProvider {
     password?: string;
   }) {
     this.hdwallet = EthereumHDKey.fromMasterSeed(
-      bip39.mnemonicToSeedSync(phrase, password)
+      mnemonicToSeedSync(phrase, password)
     );
 
-    if (!bip39.validateMnemonic(phrase, wordlist)) {
+    if (!validateMnemonic(phrase, wordlist)) {
       throw new Error("Mnemonic invalid or undefined");
     }
 
