e9e1fb020a2a6a859c54696ede759430cfb9be4d
eggplantzzz <tmfeickert@gmail.com>
2022-06-07 11:43:24 -0400

    remove old file and update test scripts

--- packages/truffle/scripts/test.sh
+++ packages/truffle/scripts/test.sh
@@ -12,13 +12,13 @@ if ! yarn semver -r ">=12" $NODE_VERSION; then
 fi
 
 if [ "$GETH" == true ]; then
-  mocha --timeout 50000 --grep '@ganache|@standalone' --invert --colors $@
+  mocha ./test/scenarios/** --timeout 50000 --grep '@ganache|@standalone' --invert --colors $@
 elif [ "$FABRICEVM" == true ]; then
-  mocha --timeout 50000 --grep @fabric-evm --colors $@
+  mocha ./test/scenarios/** --timeout 50000 --grep @fabric-evm --colors $@
 elif [ "$COVERAGE" == true ]; then
-  NO_BUILD=true mocha --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
+  NO_BUILD=true mocha ./test/scenarios/** --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
 elif [ "$INTEGRATION" == true ]; then
-  mocha --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
+  mocha ./test/scenarios/** --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
 else
-  yarn build && mocha --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
+  yarn build && mocha ./test/scenarios/** --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
 fi
