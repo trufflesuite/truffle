8c62574e9dd0584083a560fe6c33188a1d85bc43
eggplantzzz <tmfeickert@gmail.com>
2022-06-08 16:29:27 -0400

    set gas value lower and decrease the number of loops in test contract

--- packages/truffle/test/sources/migrations/error/migrations/4_migrations_oog.js
+++ packages/truffle/test/sources/migrations/error/migrations/4_migrations_oog.js
@@ -3,6 +3,6 @@ const Loops = artifacts.require("Loops");
 module.exports = async function (deployer) {
   // we set a gas limit to make it run out of gas
   await deployer.deploy(Loops, {
-    gas: "0x146799"
+    gas: "0x146"
   });
 };
