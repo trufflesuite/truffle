9e150c8c5e15568c7402f2a38fbefa4c44ea064a
Sukanya Parashar <sukanyaparashar111@gmail.com>
2022-06-29 12:02:21 -0600

    Refactor console-child.js to remove hardcoded develop network settings

--- packages/core/lib/console-child.js
+++ packages/core/lib/console-child.js
@@ -23,33 +23,43 @@ global.crypto = {
 const { network, config, url } = yargs(input[0]).argv;
 const detectedConfig = Config.detect({ network, config });
 
-if (url) {
-  detectedConfig.networks[network] = {
-    network_id: "*",
-    provider: function () {
-      return new Web3.providers.HttpProvider(url, { keepAlive: false });
-    }
-  };
-} else {
-  const customConfig = detectedConfig.networks.develop || {};
-
-  //need host and port for provider url
-  const ganacheOptions = {
+function getConfiguredNetworkUrl(customConfig) {
+  const configuredNetworkOptions = {
     host: customConfig.host || managedGanacheDefaultHost,
     port: customConfig.port || managedGanacheDefaultPort
   };
-  const ganacheUrl = `http://${ganacheOptions.host}:${ganacheOptions.port}/`;
+  return `http://${configuredNetworkOptions.host}:${configuredNetworkOptions.port}/`;
+}
 
-  //set up the develop network to use, including setting up provider
-  detectedConfig.networks.develop = {
+let configuredNetwork;
+
+if (url) {
+  // implies not "develop"
+  // Use "url" to configure network
+  configuredNetwork = {
+    network_id: "*",
+    url
+  };
+} else {
+  // Otherwise derive network settings
+  const customConfig = detectedConfig.networks[network] || {};
+
+  configuredNetwork = {
     host: customConfig.host || managedGanacheDefaultHost,
     port: customConfig.port || managedGanacheDefaultPort,
     network_id: customConfig.network_id || managedGanacheDefaultNetworkId,
-    provider: function () {
-      return new Web3.providers.HttpProvider(ganacheUrl, { keepAlive: false });
-    }
+    url: getConfiguredNetworkUrl(customConfig)
   };
 }
+
+detectedConfig.networks[network] = {
+  ...configuredNetwork,
+  provider: function () {
+    return new Web3.providers.HttpProvider(configuredNetwork.url, {
+      keepAlive: false
+    });
+  }
+};
 
 const { getCommand, prepareOptions, runCommand } = require("./command-utils");
 const command = getCommand({ inputStrings, options: {}, noAliases: false });
--- packages/truffle/test/scenarios/commands/console.js
+++ packages/truffle/test/scenarios/commands/console.js
@@ -1,23 +1,32 @@
 const CommandRunner = require("../commandRunner");
 const Server = require("../server");
-const Config = require("@truffle/config");
 const MemoryLogger = require("../MemoryLogger");
 const assert = require("assert");
+const path = require("path");
+const Reporter = require("../reporter");
+const sandbox = require("../sandbox");
 
 describe("truffle console", () => {
-  let config;
+  let config, project;
   const logger = new MemoryLogger();
 
   before(async () => await Server.start());
   after(async () => await Server.stop());
 
-  before("before all setup", () => {
-    config = Config.default();
+  project = path.join(__dirname, "../../sources/console");
+
+  before(async function () {
+    this.timeout(10000);
+    config = await sandbox.create(project);
+    config.network = "development";
     config.logger = logger;
+    config.mocha = {
+      reporter: new Reporter(logger)
+    };
   });
 
-  describe("when run with --url option", () => {
-    it("displays the hostname in the prompt", async () => {
+  describe("when run with options", () => {
+    it("displays the url hostname in the prompt", async () => {
       const url = "http://localhost:8545";
       const parsedUrl = new URL(url);
       const displayHost = parsedUrl.host;
@@ -32,6 +41,24 @@ describe("truffle console", () => {
 
       const output = logger.contents();
       const expectedValue = `truffle(${displayHost})>`;
+      assert(
+        output.includes(expectedValue),
+        `Expected "${expectedValue}" in output`
+      );
+    }).timeout(90000);
+
+    it("displays the network name in the prompt", async () => {
+      const networkName = config.network;
+      await CommandRunner.runInREPL({
+        inputCommands: [],
+        config,
+        executableCommand: "console",
+        executableArgs: `--network ${networkName}`,
+        displayHost: networkName
+      });
+
+      const output = logger.contents();
+      const expectedValue = `truffle(${networkName})>`;
       assert(
         output.includes(expectedValue),
         `Expected "${expectedValue}" in output`
