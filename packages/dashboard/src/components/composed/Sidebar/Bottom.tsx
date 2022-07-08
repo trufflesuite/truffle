6c66b178548290617ef381bac48fad6eaa6fb413
Cliff Zhang <cliffzhang08@gmail.com>
2022-07-07 21:56:40 -0400

    dashboard: Basic network management

--- packages/dashboard/src/components/composed/Sidebar/Bottom.tsx
+++ packages/dashboard/src/components/composed/Sidebar/Bottom.tsx
@@ -1,11 +1,43 @@
-import { Navbar, Text, Center } from "@mantine/core";
+import { Navbar, Stack, Group, Text, Code, Button, Badge } from "@mantine/core";
+import { useAccount, useConnect, useDisconnect, useNetwork } from "wagmi";
+import { InjectedConnector } from "@wagmi/core";
 
 function Bottom(): JSX.Element {
+  const { connect } = useConnect({ connector: new InjectedConnector() });
+  const { disconnect } = useDisconnect();
+  const { address, isConnected } = useAccount();
+  const { chain } = useNetwork();
+
+  const handleConnectBtnClick = () => void connect();
+  const handleDisconnectBtnClick = () => void disconnect();
+
   return (
-    <Navbar.Section>
-      <Center my={50}>
-        <Text>network + wallet management</Text>
-      </Center>
+    <Navbar.Section py="xl">
+      <Stack spacing="md">
+        <Text align="center">network + wallet management area</Text>
+        <Code mx="lg">
+          address = {address?.slice(0, 8)}...{address?.slice(-6)}
+          <br />
+          chain id = {chain?.id}
+          <br />
+          chain name = {chain?.name}
+        </Code>
+        <Group position="center">
+          {isConnected ? (
+            <>
+              <Badge>Connected</Badge>
+              <Button color="gray" onClick={handleDisconnectBtnClick}>
+                Disconnect
+              </Button>
+            </>
+          ) : (
+            <>
+              <Badge color="gray">Disconnected</Badge>
+              <Button onClick={handleConnectBtnClick}>Connect</Button>
+            </>
+          )}
+        </Group>
+      </Stack>
     </Navbar.Section>
   );
 }
