a9060121e6ea1a89c27682ef825d50cd3d644eac
Cliff Zhang <cliffzhang08@gmail.com>
2022-07-08 13:54:13 -0400

    dashboard: Setup bus message handler in reducer

--- packages/dashboard/src/contexts/DashContext/DashProvider.tsx
+++ packages/dashboard/src/contexts/DashContext/DashProvider.tsx
@@ -1,6 +1,8 @@
 import { useReducer } from "react";
 import { useDidUpdate } from "@mantine/hooks";
 import { DashboardMessageBusClient } from "@truffle/dashboard-message-bus-client";
+import type { ReceivedMessageLifecycle } from "@truffle/dashboard-message-bus-client";
+import type { Message } from "@truffle/dashboard-message-bus-common";
 import { DashContext } from "src/contexts/DashContext";
 import { reducer, initialState } from "src/contexts/DashContext/state";
 
@@ -11,14 +13,23 @@ type DashProviderProps = {
 function DashProvider({ children }: DashProviderProps): JSX.Element {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  console.debug(state);
+  console.debug({ state });
   useDidUpdate(() => {
     async function init() {
       console.debug("Called init() in <DashProvider />");
+
+      // Create message bus client
       const { host, port } = state;
       const client = new DashboardMessageBusClient({ host, port });
       await client.ready();
       dispatch({ type: "set-client", data: client });
+      console.debug(`Connected to message bus at ws://${host}:${port}`);
+
+      // Client subscribes to and handles messages
+      const subscription = client.subscribe({});
+      const messageHandler = (lifecycle: ReceivedMessageLifecycle<Message>) =>
+        void dispatch({ type: "handle-message", data: lifecycle });
+      subscription.on("message", messageHandler);
     }
 
     init();
--- packages/dashboard/src/contexts/DashContext/state.ts
+++ packages/dashboard/src/contexts/DashContext/state.ts
@@ -1,11 +1,24 @@
-import type { DashboardMessageBusClient } from "@truffle/dashboard-message-bus-client";
+import type {
+  DashboardMessageBusClient,
+  ReceivedMessageLifecycle
+} from "@truffle/dashboard-message-bus-client";
+import {
+  isDashboardProviderMessage,
+  isInvalidateMessage,
+  isLogMessage,
+  isDebugMessage
+} from "@truffle/dashboard-message-bus-common";
+import type { Message } from "@truffle/dashboard-message-bus-common";
 
-type actionType = { type: "set-client"; data: DashboardMessageBusClient };
+type actionType =
+  | { type: "set-client"; data: DashboardMessageBusClient }
+  | { type: "handle-message"; data: ReceivedMessageLifecycle<Message> };
 
 type stateType = {
   host: string;
   port: number;
   client: DashboardMessageBusClient | null;
+  providerMessages: Map<number, ReceivedMessageLifecycle<Message>>;
 };
 
 const initialState: stateType = {
@@ -14,7 +27,8 @@ const initialState: stateType = {
     process.env.NODE_ENV === "development"
       ? 24012
       : Number(window.location.port),
-  client: null
+  client: null,
+  providerMessages: new Map()
 };
 
 const reducer = (state: stateType, action: actionType): stateType => {
@@ -22,6 +36,26 @@ const reducer = (state: stateType, action: actionType): stateType => {
   switch (type) {
     case "set-client":
       return { ...state, client: data };
+    case "handle-message":
+      const lifecycle = data;
+      const message = lifecycle.message;
+      const { id } = message;
+      const updatedProviderMessages = new Map(state.providerMessages);
+      if (isDashboardProviderMessage(message)) {
+        console.debug(`Received provider message (id: ${id})`, message);
+        updatedProviderMessages.set(id, lifecycle);
+      } else if (isInvalidateMessage(message)) {
+        console.debug(`Received invalidate message (id: ${id})`, message);
+        const invalidatedID = message.payload;
+        updatedProviderMessages.delete(invalidatedID);
+      } else if (isLogMessage(message)) {
+        console.debug(`Received log message (id: ${id})`, message);
+        lifecycle.respond({ payload: undefined });
+      } else if (isDebugMessage(message)) {
+        console.debug(`Received debug message (id: ${id})`, message);
+        lifecycle.respond({ payload: undefined });
+      }
+      return { ...state, providerMessages: updatedProviderMessages };
     default:
       throw new Error("Undefined reducer action type");
   }
