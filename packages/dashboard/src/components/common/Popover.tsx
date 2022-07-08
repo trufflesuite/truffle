9690c6612b2d3e2aa7a18b544e945de260b1c169
fainashalts <faina@trufflesuite.com>
2022-07-08 15:31:27 -0700

    Add link to analytics policy

--- packages/dashboard/src/components/common/Popover.tsx
+++ packages/dashboard/src/components/common/Popover.tsx
@@ -10,7 +10,7 @@ export default function SettingsPopover() {
       <Popover.Panel className="absolute z-10 bottom-14 right-2 mb-2">
         <div className="bg-truffle-light border-2 mb-4 block z-50 font-normal leading-normal text-lg max-w-xs no-underline break-words rounded-lg">
           <div className="bg-truffle-blue border-2 text-center text-truffle-brown font-semibold p-3 mb-2 uppercase rounded-t-lg">Help Improve Truffle!</div>
-          <div className="bg-truffle-light text-truffle-brown m-2">You can help us improve Truffle by allowing us to track how you use our tools. Would you like to allow tracking? You can always return to this menu if you change your mind.
+          <div className="bg-truffle-light text-truffle-brown m-2">You can help us improve Truffle by allowing us to track how you use our tools.Would you like to allow tracking?You can always return to this menu if you change your mind. View the Truffle Analytics Policy <a href="https://trufflesuite.com/analytics/">here.</a>
             <AnalyticsToggle />
           </div>
         </div>
