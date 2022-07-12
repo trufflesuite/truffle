75bb82b81ea71b2eb71c3722259a6288a0f6ef53
Sukanya Parashar <sukanyaparashar111@gmail.com>
2022-07-12 13:38:47 -0600

    Remove package.js from index.js

--- packages/core/index.js
+++ packages/core/index.js
@@ -6,7 +6,6 @@ module.exports = {
   create: require("./lib/commands/create/helpers"),
   // TODO: update this to non-legacy the next breaking change
   contracts: require("@truffle/workflow-compile/legacy"),
-  package: require("./lib/package"),
   test: require("./lib/testing/Test"),
   version: pkg.version,
   ganache: require("ganache")
