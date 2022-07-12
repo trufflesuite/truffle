414b1170bd73e2f028050b4b46473e30922c251a
Cliff Zhang <cliffzhang08@gmail.com>
2022-07-12 11:54:53 -0400

    dashboard: Hard `<Sidebar />` width

--- packages/dashboard/src/components/composed/Sidebar/index.tsx
+++ packages/dashboard/src/components/composed/Sidebar/index.tsx
@@ -3,11 +3,9 @@ import Top from "src/components/composed/Sidebar/Top";
 import Middle from "src/components/composed/Sidebar/Middle";
 import Bottom from "src/components/composed/Sidebar/Bottom";
 
-const width = { base: 366 };
-
 function Sidebar(): JSX.Element {
   return (
-    <Navbar width={width} px="sm">
+    <Navbar px="sm" sx={{ minWidth: 366, maxWidth: 366 }}>
       <Top />
       <Divider />
       <Middle />
