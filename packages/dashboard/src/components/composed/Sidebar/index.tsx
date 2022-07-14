ea66fd79c435021962a6546c26eb478ef0ffffc7
Cliff Zhang <cliffzhang08@gmail.com>
2022-07-14 12:51:23 -0400

    dashboard: `<Sidebar />` style

--- packages/dashboard/src/components/composed/Sidebar/index.tsx
+++ packages/dashboard/src/components/composed/Sidebar/index.tsx
@@ -1,12 +1,22 @@
-import { Navbar } from "@mantine/core";
+import { Navbar, createStyles } from "@mantine/core";
 import Top from "src/components/composed/Sidebar/Top";
 import Middle from "src/components/composed/Sidebar/Middle";
 import Bottom from "src/components/composed/Sidebar/Bottom";
 import Divider from "src/components/composed/Sidebar/Divider";
 
+const useStyles = createStyles((_theme, _params, _getRef) => ({
+  sideBar: {
+    minWidth: 366,
+    maxWidth: 366,
+    borderRight: "none"
+  }
+}));
+
 function Sidebar(): JSX.Element {
+  const { classes } = useStyles();
+
   return (
-    <Navbar px="sm" sx={{ minWidth: 366, maxWidth: 366 }}>
+    <Navbar px="sm" className={classes.sideBar}>
       <Top />
       <Divider />
       <Middle />
