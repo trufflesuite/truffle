5496c4e0cd91ab2d8b0f12fb4edc47562a303964
Cliff Zhang <cliffzhang08@gmail.com>
2022-07-14 12:23:09 -0400

    dashboard: `<Sidebar />` nav buttons style

--- packages/dashboard/src/components/composed/Sidebar/Middle/NavBtn.tsx
+++ packages/dashboard/src/components/composed/Sidebar/Middle/NavBtn.tsx
@@ -1,30 +1,66 @@
 import { Link } from "react-router-dom";
-import { UnstyledButton, Text } from "@mantine/core";
+import { UnstyledButton, Group, Text, createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme, _params, _getRef) => {
+  const { colors, colorScheme, radius, fn } = theme;
+  return {
+    btn: {
+      display: "block",
+      borderRadius: radius.sm
+    },
+    enabled: {
+      "&:hover": {
+        backgroundColor:
+          colorScheme === "dark"
+            ? fn.darken(colors["truffle-brown"][8], 0.08)
+            : fn.darken(colors["truffle-beige"][4], 0.08)
+      }
+    },
+    disabled: {
+      cursor: "default",
+      pointerEvents: "none",
+      color:
+        colorScheme === "dark"
+          ? colors["truffle-brown"][4]
+          : colors["truffle-beige"][5]
+    }
+  };
+});
 
 type NavBtnProps = {
   to: string;
   label?: string;
+  icon?: React.ReactNode;
+  badge?: React.ReactNode;
+  disabled?: boolean;
 };
 
-function NavBtn({ to, label }: NavBtnProps): JSX.Element {
+function NavBtn({
+  to,
+  label,
+  icon,
+  badge,
+  disabled
+}: NavBtnProps): JSX.Element {
+  const { classes } = useStyles();
+
   return (
     <UnstyledButton
       component={Link}
       to={to}
       px="lg"
       py="md"
-      sx={theme => ({
-        display: "block",
-        borderRadius: theme.radius.sm,
-        "&:hover": {
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[6]
-              : theme.colors.gray[1]
-        }
-      })}
+      className={`${classes.btn} ${
+        disabled ? classes.disabled : classes.enabled
+      }`}
     >
-      <Text>{label}</Text>
+      <Group position="apart">
+        <Group>
+          {icon}
+          <Text>{label}</Text>
+        </Group>
+        {badge}
+      </Group>
     </UnstyledButton>
   );
 }
--- packages/dashboard/src/components/composed/Sidebar/Middle/index.tsx
+++ packages/dashboard/src/components/composed/Sidebar/Middle/index.tsx
@@ -1,11 +1,34 @@
-import { Navbar } from "@mantine/core";
+import { Navbar, Badge } from "@mantine/core";
+import { Zap, Archive } from "react-feather";
 import NavBtn from "src/components/composed/Sidebar/Middle/NavBtn";
 
 function Middle(): JSX.Element {
+  const featherIconProps = { size: 18 };
+
+  const comingSoonBadge = (
+    <Badge
+      variant="gradient"
+      gradient={{ from: "truffle-teal", to: "pink" }}
+      sx={{ opacity: 0.4 }}
+    >
+      coming soon
+    </Badge>
+  );
+
   return (
     <Navbar.Section grow py="sm">
-      <NavBtn label="Transactions" to="/txs" />
-      <NavBtn label="Contracts" to="/contracts" />
+      <NavBtn
+        label="Transactions / Calls"
+        to="/txs"
+        icon={<Zap {...featherIconProps} />}
+      />
+      <NavBtn
+        label="Contracts"
+        to="/contracts"
+        icon={<Archive {...featherIconProps} />}
+        badge={comingSoonBadge}
+        disabled={true}
+      />
     </Navbar.Section>
   );
 }
