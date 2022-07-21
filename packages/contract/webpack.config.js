209259e514c24cc28a69172c55c768629f6b9fdd
eggplantzzz <tmfeickert@gmail.com>
2022-07-21 16:17:54 -0400

    update folder name for browser dist stuff

--- packages/contract/webpack.config.js
+++ packages/contract/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "browser-dist"),
     library: {
       name: "TruffleContract",
       type: "global"
