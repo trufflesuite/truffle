f1e66ff5cbe0b015c054203c5207748b370b642d
Cliff Zhang <cliffzhang08@gmail.com>
2022-07-28 03:31:04 +0800

    dashboard: Update routes

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -8,7 +8,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // Components
 import Layout from "src/components/composed/Layout";
 import Txs from "src/components/composed/Txs";
-import Contracts from "src/components/composed/Contracts";
 import Palette from "src/components/composed/Palette";
 import MantineGlobal from "src/components/MantineGlobal";
 
@@ -21,17 +20,16 @@ function App(): JSX.Element {
           <WagmiWrapper>
             <DashProvider>
               <BrowserRouter>
-                {/*  Everything maps to /txs except for /contracts  */}
                 <Routes>
                   <Route path="/" element={<Layout />}>
                     <Route index element={<Navigate to="/txs" replace />} />
                     <Route path="txs" element={<Txs />} />
-                    <Route path="contracts" element={<Contracts />} />
                   </Route>
-                  <Route path="colors" element={<Palette />} />
+                  {process.env.NODE_ENV === "development" && (
+                    <Route path="colors" element={<Palette />} />
+                  )}
                   <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
-                {/*    */}
               </BrowserRouter>
             </DashProvider>
           </WagmiWrapper>
