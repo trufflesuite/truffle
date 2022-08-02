41aeec9e544d1b0b7a7f67232c905587cabbe5a8
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2022-08-02 12:39:10 +1200

    Error on inexact caret version range match

--- scripts/check-truffle-namespace-dependency-versions.js
+++ scripts/check-truffle-namespace-dependency-versions.js
@@ -3,52 +3,30 @@ const path = require("path");
 
 const chalk = require("chalk");
 
-const { Range } = require("semver");
-
 const debug = require("debug")("check-package-versions");
 
 function main() {
   const packages = readPackages();
   const errors = [];
-  const warnings = [];
 
   for (const candidateName in packages) {
     for (const dependencyName in packages) {
-      try {
-        checkDependencyVersion(
+      errors.push(
+        ...checkDependencyVersionRange(
           packages[candidateName],
           packages[dependencyName]
-        );
-      } catch (err) {
-        errors.push(err.message);
-      }
-      try {
-        checkDependencyVersionRange(
-          packages[candidateName],
-          packages[dependencyName]
-        );
-      } catch (err) {
-        warnings.push(err.message);
-      }
+        )
+      );
     }
   }
 
   for (const error of errors) {
     console.error(chalk.red("Error:"), error);
   }
-  for (const warning of warnings) {
-    console.error(chalk.yellow("Warning:"), warning);
-  }
-
-  const doneColor =
-    errors.length > 0
-      ? chalk.red
-      : warnings.length > 0
-      ? chalk.yellow
-      : chalk.green;
+  const doneColor = errors.length > 0 ? chalk.red : chalk.green;
   console.error(
     doneColor("Done."),
-    `${errors.length} errors, ${warnings.length} warnings found for @truffle namespace version dependencies.`
+    `Found ${errors.length} errors for @truffle namespace version dependencies.`
   );
 
   if (errors.length > 0) {
@@ -56,12 +34,12 @@ function main() {
   }
 }
 
-function getPackageNames() {
+function getPackageDirectoryNames() {
   return fs.readdirSync(path.join(__dirname, "..", "packages"));
 }
 
 function readPackages() {
-  const packages = getPackageNames();
+  const packages = getPackageDirectoryNames();
   const packageSpecs = {};
   for (const packageName of packages) {
     const packageFilePath = path.join(
@@ -75,51 +53,25 @@ function readPackages() {
   return packageSpecs;
 }
 
-function checkDependencyVersion(candidate, dependency) {
+const depTypes = [
+  "dependencies",
+  "devDependencies",
+  "peerDependencies",
+  "optionalDependencies",
+  "bundleDependencies"
+];
+
+function* checkDependencyVersionRange(candidate, dependency) {
   const name = dependency.name;
   const version = dependency.version;
 
-  for (const depType of [
-    "dependencies",
-    "devDependencies",
-    "peerDependencies"
-  ]) {
-    const deps = candidate[depType];
-    if (deps && deps[name]) {
-      const rawRange = deps[name];
-      const range = new Range(rawRange);
-      if (!range.test(version)) {
-        throw new Error(
-          `Package "${candidate.name}" depends on "${name}@${rawRange}", but local "${name}" has version ${version}, which is not in range ${rawRange}`
-        );
-      } else {
-        debug(
-          `${candidate.name} requires ${name}@${version} (${version} in range ${deps[name]})`
-        );
-      }
-    } else {
-      debug(`${candidate.name} does not require ${name}`);
-    }
-  }
-}
-
-function checkDependencyVersionRange(candidate, dependency) {
-  const name = dependency.name;
-  const version = dependency.version;
-
-  for (const depType of [
-    "dependencies",
-    "devDependencies",
-    "peerDependencies"
-  ]) {
+  for (const depType of depTypes) {
     const deps = candidate[depType];
     if (deps && deps[name]) {
       const rawRange = deps[name];
 
       if (rawRange !== `^${version}`) {
-        throw new Error(
-          `Package "${candidate.name}" depends on "${name}@${rawRange}", but range has not been updated for version ${version}`
-        );
+        yield `Package "${candidate.name}" depends on "${name}@${rawRange}", but range has not been updated for version ${version}`;
       } else {
         debug(
           `${candidate.name} requires ${name}@${version} (${rawRange} is up-to-date for version ${version})`
