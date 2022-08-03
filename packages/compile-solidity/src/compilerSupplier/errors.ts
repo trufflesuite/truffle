9d9808e310bf4f6ff690dee947032a5f59793477
eggplantzzz <tmfeickert@gmail.com>
2022-08-03 13:08:47 -0400

    update errors for CompilerSupplier

--- packages/compile-solidity/src/compilerSupplier/errors.ts
+++ packages/compile-solidity/src/compilerSupplier/errors.ts
@@ -1,6 +1,7 @@
 export class NoVersionError extends Error {
   constructor(input) {
-    const message = `Could not find a compiler version matching ${input}. ` +
+    const message =
+      `Could not find a compiler version matching ${input}. ` +
       `Please ensure you are specifying a valid version, constraint or ` +
       `build in the truffle config. Run \`truffle compile --list\` to ` +
       `see available versions.`;
@@ -8,12 +9,11 @@ export class NoVersionError extends Error {
   }
 }
 
-export class NoRequestError extends Error {
-  constructor(input, error) {
+export class CompilerFetchingError extends Error {
+  constructor(compilerRoots) {
     const message =
-      `Failed to complete request to: ${input}. Are you connected to ` +
-      `the internet?\n\n` +
-      error;
+      `Failed to fetch the Solidity compiler from the following locations: ` +
+      `${compilerRoots}. Are you connected to the internet?\n\n`;
     super(message);
   }
 }
