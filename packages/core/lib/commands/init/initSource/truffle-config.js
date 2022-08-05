c4bd079ac7c9017ff12d89507290dddd5b151f3f
Sukanya Parashar <sukanyaparashar111@gmail.com>
2022-08-05 13:22:38 -0600

    Use managed Ganache instance replacing development blockchain in the comments

--- packages/core/lib/commands/init/initSource/truffle-config.js
+++ packages/core/lib/commands/init/initSource/truffle-config.js
@@ -31,7 +31,7 @@ module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
+   * will spin up a managed Ganache instance for you on port 9545 when you
    * run `develop` or `test`. You can ask a truffle command to use a specific
    * network from the command line, e.g
    *
