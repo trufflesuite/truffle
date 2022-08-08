f74ebba0f7d69c656d8447ec594cc50805e7d0ba
Sukanya Parashar <sukanyaparashar111@gmail.com>
2022-08-08 12:52:35 -0600

    Add the required whitespaces in the comments

--- packages/core/lib/commands/init/initSource/truffle-config.js
+++ packages/core/lib/commands/init/initSource/truffle-config.js
@@ -23,9 +23,9 @@
  */
 
 // require('dotenv').config();
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-
 // const { MNEMONIC, PROJECT_ID } = process.env;
+
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   /**
@@ -87,7 +87,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.15", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.15",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
