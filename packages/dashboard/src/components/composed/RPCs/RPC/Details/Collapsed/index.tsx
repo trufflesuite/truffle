52735efe5b7548abb9e6f0ef713553c620a4ec5a
Cliff Zhang <cliffzhang08@gmail.com>
2022-08-13 06:46:32 +0800

    dashboard: `<RPC />` chevrons up icon jump

--- packages/dashboard/src/components/composed/RPCs/RPC/Details/Collapsed/index.tsx
+++ packages/dashboard/src/components/composed/RPCs/RPC/Details/Collapsed/index.tsx
@@ -112,6 +112,7 @@ function Collapsed({
         component={ChevronsUp}
         show={showChevronsUp}
         color={iconColors.accent}
+        animate={true}
       />
       <Icon component={Check} show={showCheck} color={iconColors.green} />
       <Icon component={X} show={showX} color={iconColors.red} />
