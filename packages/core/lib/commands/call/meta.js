ede02c08c9acc3aa851dbbac45977db23b564561
Sukanya Parashar <sukanyaparashar111@gmail.com>
2022-08-18 20:21:38 -0600

    Add options array

--- packages/core/lib/commands/call/meta.js
+++ packages/core/lib/commands/call/meta.js
@@ -1,15 +1,54 @@
 module.exports = {
   command: "call",
   description: "Call read-only contract function with arguments",
-  builder: {},
+  builder: {
+    "url": {
+      describe: "Use specified URL for provider",
+      type: "string"
+    },
+    "fetch-external": {
+      describe: "Allow debugging of external contracts",
+      alias: "x",
+      type: "boolean",
+      default: false
+    }
+  },
   help: {
     usage:
-      "truffle call <contract-name> <function-name> [<function-args...>] [--network=<network-name>",
-    usage:
-      "truffle call <contract-name> <function-name> [<args...>]\n" +
-      "                                " + // spacing to align with previous line
-      "[--network=<network-name>] [--from=<account>]",
-    options: [],
-    allowedGlobalOptions: ["from", "network", "config"]
+      "truffle call [<contract-name>] [<function-name>] [<args...>]\n" +
+      "                             " + // spacing to align with previous line
+      "[--fetch-external|-x] [--network <network>|--url <provider_url>]\n" +
+      "                             ",
+    options: [
+      {
+        option: "<contract-name>",
+        description: "The specified contract"
+      },
+      {
+        option: "<function-name>",
+        description: "The function inside the specified contract to be called"
+      },
+      {
+        option: "<args...>",
+        description: "List of arguments of the function to be called"
+      },
+      {
+        option: "--fetch-external|-x",
+        description:
+          "Allows calling functions of external contracts with verified sources."
+      },
+      {
+        option: "--url",
+        description:
+          "Creates a provider using the given url and connects to the network.\n" +
+          "                    This can be used outside of a Truffle project."
+      },
+      {
+        option: "--network",
+        description:
+          "The network to connect to, as specified in the Truffle config."
+      }
+    ],
+    allowedGlobalOptions: ["from", "config"]
   }
 };
