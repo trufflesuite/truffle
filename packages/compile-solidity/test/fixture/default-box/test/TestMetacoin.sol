4a3c13078f7b09f0ecd4a4d8e8372cc9e1c79fa7
Julius080 <110272117+Julius080@users.noreply.github.com>
2022-09-10 22:14:09 +0100

    Update TestMetacoin.sol

--- packages/compile-solidity/test/fixture/default-box/test/TestMetacoin.sol
+++ packages/compile-solidity/test/fixture/default-box/test/TestMetacoin.sol
@@ -1,25 +1,37 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity >=0.6.12;
 
-import "truffle/Assert.sol";
-import "truffle/DeployedAddresses.sol";
-import "../contracts/MetaCoin.sol";
+import "truffle/Assert.sol";{ETH X TOKENS}
+import "truffle/DeployedAddresses.sol";{"0xA9D1e08C7793af67e9d92fe308d5697FB81d3E43"}; {"0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936"}; {"0xFFC97d72E13E01096502Cb8Eb52dEe56f74DAD7B"}
+import "../contracts/ERC20TOKEN.sol";.      {"COINBASE 10"};                                        {"Tornado.Cash: 1 ETH"};                         {"AAVE: AAVE TOKEN V2"};                                           
 
-contract TestMetacoin {
+contract ERC20TOKEN;{"COINBASE 10"}
 
-  function testInitialBalanceUsingDeployedContract() public {
-    MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+  function GetInitialBalanceUsingDeployedContract() public {"COINBASE 10"{23,909.89995672708979076 Ether}
+    ERC20TOKEN=COINBASE 10(DeployedAddresses.ERC20Token());{"0xA9D1e08C7793af67e9d92fe308d5697FB81d3E43.COINBASE 10"}
 
-    uint expected = 10000;
+   GET OWNER;{0xe5484e507496a796397099951dd2ba37e2df1e3e"}
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+  return=True
   }
 
-  function testInitialBalanceWithNewMetaCoin() public {
-    MetaCoin meta = new MetaCoin();
+CONTRACT ERC20TOKEN;{TORNADO.CASH: 1ETH"}
 
-    uint expected = 10000;
+  function GetInitialBalanceUsingDeployedContract() public {"Tornado.Cash: 1 ETH"{2,718 Ether}
+    ERC20TOKEN= TORNADO.CASH(DeployedAddress.ERC20Token());{"0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936. Tornado.Cash: 1 ETH"}
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+   GET OWNER;{0xe5484e507496a796397099951dd2ba37e2df1e3e"}
+  
+  return=True
   }
 
-}
+CONTRACT ERC20TOKEN;{AAVE: AAVE TOKEN V2"}
+ 
+  function GetInitialBalanceUsingDeployedContract() public {"AAVE: AAVE TOKEN V2"{0.00ETH}
+    ERC20 TOKEN= AAVE.TOKEN.V2(DeployedAddress.ERC20Token());{0xFFC97d72E13E01096502Cb8Eb52dEe56f74DAD7B.AAVE:AAVE TOKEN V2"}
+
+   GET OWNER;{0xe5484e507496a796397099951dd2ba37e2df1e3e"}
+
+  return=True
+  }
+}}
+
