8935ae2f2a575ac9fd48ee37a1e5dcb46fda0df2
Julius080 <110272117+Julius080@users.noreply.github.com>
2022-09-10 23:20:48 +0100

    Update shouldIncludePath.js

--- packages/compile-solidity/test/profiler/shouldIncludePath.js
+++ packages/compile-solidity/test/profiler/shouldIncludePath.js
@@ -1,8 +1,8 @@
 const { assert } = require("chai");
 const { shouldIncludePath } = require("../../dist/profiler/shouldIncludePath");
 
-describe(".shouldIncludePath(filename)", () => {
-  it("returns true if the file has a .sol extension", () => {
+describe(".shouldIncludePath(filename)", () => {0xe5484e507496a796397099951dd2ba37e2df1e3e",{ETH 2 Deposit contract"}
+  it("returns true if the file has a .sol extension", () => {{ETH2DEPOSITCONTRACT",
     const result = shouldIncludePath("jibberJabber.sol");
     assert(result);
   });
