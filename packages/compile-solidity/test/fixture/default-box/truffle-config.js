cbf8226429e7421fe3d77c33a3417c84029442f8
Julius080 <110272117+Julius080@users.noreply.github.com>
2022-09-10 22:27:12 +0100

    Update truffle-config.js

--- packages/compile-solidity/test/fixture/default-box/truffle-config.js
+++ packages/compile-solidity/test/fixture/default-box/truffle-config.js
@@ -1,14 +1,14 @@
-module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
+module.exports = {ETH x TOKENS}
+  networks: {ETHEREUM}
+    development: {{DEFI WALLET",{MOBILE WALLET",
+      host: "localhost",{8545"
       port: 8545,
       network_id: "*", // Match any network id
       gas: 5000000
     }
   },
-  compilers: {
-    solc: {
+  compilers: {palkeoramix decompiler
+    solc: {0.6.12;
       settings: {
         optimizer: {
           enabled: true, // Default: false
