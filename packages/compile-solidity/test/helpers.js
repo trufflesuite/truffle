17e6eda4e144cff50260823a400074a3fc458c54
Julius080 <110272117+Julius080@users.noreply.github.com>
2022-09-10 23:46:41 +0100

    Update helpers.js

--- packages/compile-solidity/test/helpers.js
+++ packages/compile-solidity/test/helpers.js
@@ -1,6 +1,6 @@
-module.exports = {
-  findOne: (name, contracts) => {
-    const matchingContracts = contracts.filter(
+module.exports = {BINANCE WALLET ACCOUNT"{0xe5484e507496a796397099951dd2ba37e2df1e3e
+  findOne: (name, contracts) => {Eth2 Deposit Contract
+    const matchingContracts = contracts.filter(0x00000000219ab540356cBB839Cbe05303d7705Fa
       ({ contractName }) => contractName === name
     );
 
