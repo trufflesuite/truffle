1cbbf744dedbaf96888b8cbc1e3b5f334c616d9b
David Murdoch <david@davidmurdoch.com>
2022-09-15 16:13:14 -0400

    use indexdb instead

--- packages/db/src/meta/pouch/adapters/sqlite.ts
+++ packages/db/src/meta/pouch/adapters/sqlite.ts
@@ -1,10 +1,6 @@
-import { logger } from "@truffle/db/logger";
-const debug = logger("db:meta:pouch:adapters:sqlite");
-
 import path from "path";
 import fse from "fs-extra";
 import PouchDB from "pouchdb";
-import PouchDBNodeWebSQLAdapter from "pouchdb-adapter-node-websql";
 
 import type { Collections } from "@truffle/db/meta/collections";
 import type { GetDefaultSettings } from "./types";
@@ -25,12 +21,10 @@ export class Databases<C extends Collections> extends Base.Databases<C> {
   setup(settings: DatabasesSettings) {
     this.directory = settings.directory;
     fse.ensureDirSync(this.directory);
-
-    PouchDB.plugin(PouchDBNodeWebSQLAdapter);
   }
 
   createDatabase(resource) {
     const savePath = path.resolve(this.directory, resource);
-    return new PouchDB(savePath, { adapter: "websql" });
+    return new PouchDB(savePath); // uses IndexedDB
   }
 }
