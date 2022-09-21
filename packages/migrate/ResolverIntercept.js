bffc44e0b620def65122c69cee21a6d11861936e
eggplantzzz <tmfeickert@gmail.com>
2022-09-21 18:00:14 -0400

    don't return things from the ResolverIntercept cache

--- packages/migrate/ResolverIntercept.js
+++ packages/migrate/ResolverIntercept.js
@@ -10,14 +10,6 @@ class ResolverIntercept {
       .replace(/^\.\//, "")
       .replace(/\.sol$/i, "");
 
-    // there may be more than one contract of the same name which will be
-    // problematic - only return the first one found in the cache for now
-    for (const contract of this.cache) {
-      if (contract.contract_name === sanitizedContractName) {
-        return contract;
-      }
-    }
-
     // Note, will error if nothing is found.
     const resolved = this.resolver.require(sanitizedContractName);
 
