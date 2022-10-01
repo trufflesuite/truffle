aff59efbf6ccdfc9443f2e776348f00bd97e9fbe
Harry Altman <harry.j.altman@gmail.com>
2022-09-30 20:56:59 -0400

    Update types for new internal function pointer format

--- packages/codec/lib/format/values.ts
+++ packages/codec/lib/format/values.ts
@@ -557,8 +557,7 @@ export type FunctionInternalValueInfo =
 export interface FunctionInternalValueInfoKnown {
   kind: "function";
   context: Types.ContractType;
-  deployedProgramCounter: number;
-  constructorProgramCounter: number;
+  rawInformation: FunctionInternalRawInfo;
   name: string;
   /**
    * Is null for a free function
@@ -583,8 +582,7 @@ export interface FunctionInternalValueInfoKnown {
 export interface FunctionInternalValueInfoException {
   kind: "exception";
   context: Types.ContractType;
-  deployedProgramCounter: number;
-  constructorProgramCounter: number;
+  rawInformation: FunctionInternalRawInfo;
 }
 
 /**
@@ -604,8 +602,43 @@ export interface FunctionInternalValueInfoException {
 export interface FunctionInternalValueInfoUnknown {
   kind: "unknown";
   context: Types.ContractType;
+  rawInformation: FunctionInternalRawInfo;
+}
+
+/**
+ * The raw information for an internal function pointer can take two forms.
+ * In non-IR Solidity, it's given as a pair of program counter values (one
+ * for the constructor context and one for the deployed context).  In IR-based
+ * Solidity, it's given as an index into a jump table.
+ *
+ * @Category Function types
+ */
+export type FunctionInternalRawInfo =
+  | FunctionInternalRawInfoPcPair
+  | FunctionInternalRawInfoIndex;
+
+/**
+ * In non-IR Solidity, the raw information for an internal function pointer is
+ * given as pair of program counter values; one for the constructor context and
+ * one for the deployed context).
+ *
+ * @Category Function types
+ */
+export interface FunctionInternalRawInfoPcPair {
+  kind: "pcpair";
   deployedProgramCounter: number;
   constructorProgramCounter: number;
+}
+
+/**
+ * In IR-based Solidity, the raw information for an internal function pointer is
+ * given as an index into a jump table.
+ *
+ * @Category Function types
+ */
+export interface FunctionInternalRawInfoIndex {
+  kind: "index";
+  functionIndex: number;
 }
 
 /*
