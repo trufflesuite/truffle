6a09caf0dd86979a68975c1cfee8e0a90e725857
Harry Altman <harry.j.altman@gmail.com>
2022-10-20 18:17:56 -0400

    Delete no-longer-used test fixture

--- packages/db/test/truffle-projects/drizzle-box/migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
-
-module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
-};
