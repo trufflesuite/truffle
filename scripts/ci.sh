cc31940751ae2f3fe4c337f5f7645514690a96eb
cds-amal <cds.sudama@gmail.com>
2022-10-28 11:45:41 -0400

    wip scope tests

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -47,36 +47,6 @@ elif [ "$GETH" = true ]; then
 	lerna run --scope truffle test --stream -- --exit
 	lerna run --scope @truffle/contract test --stream -- --exit
 
-elif [ "$FABRICEVM" = true ]; then
-
-	root=$(pwd)
-	sudo add-apt-repository -y ppa:rmescandon/yq
-	sudo apt update
-	sudo apt install -y yq
-	eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=1.12 bash)"
-	cd $GOPATH
-	mkdir -p src/github.com/hyperledger
-	cd src/github.com/hyperledger
-	git clone https://github.com/hyperledger/fabric-chaincode-evm
-	curl -sSL http://bit.ly/2ysbOFE | bash -s 1.4.1
-	cd fabric-samples/first-network
-	yq w -i docker-compose-cli.yaml "services.cli.volumes[+]" "./../../fabric-chaincode-evm:/opt/gopath/src/github.com/hyperledger/fabric-chaincode-evm"
-	yes Y | ./byfn.sh up
-	docker exec -it cli sh -c "peer chaincode install -n evmcc -l golang -v 0 -p github.com/hyperledger/fabric-chaincode-evm/evmcc &&
-    peer chaincode instantiate -n evmcc -v 0 -C mychannel -c '{\"Args\":[]}' -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
-	cd ../../fabric-chaincode-evm
-	make fab3
-	# Environment Variables for Fab3:
-	export FAB3_CONFIG=${GOPATH}/src/github.com/hyperledger/fabric-chaincode-evm/examples/first-network-sdk-config.yaml
-	export FAB3_USER=User1
-	export FAB3_ORG=Org1
-	export FAB3_CHANNEL=mychannel
-	export FAB3_CCID=evmcc
-	export FAB3_PORT=5000
-	bin/fab3 &>/dev/null &
-	cd $root
-	lerna run --scope truffle test --stream -- --exit
-
 elif [ "$PACKAGES" = true ]; then
 
 	docker pull ethereum/solc:0.4.22
@@ -87,7 +57,7 @@ elif [ "$PACKAGES" = true ]; then
 	wget https://bootstrap.pypa.io/get-pip.py
 	sudo python3.8 get-pip.py
 	sudo pip3 install vyper
-	lerna run --ignore truffle test --stream --concurrency=1
+	lerna run --ignore truffle --scope @truffle/box test --stream --concurrency=1
 
 elif [ "$COVERAGE" = true ]; then
 
