93be1c7915204a9a0764aa28e5e8cdc22bb0ae88
Cliff Zhang <cliffzhang08@gmail.com>
2022-11-08 12:59:58 -0800

    dashboard: Use tabs to display simulated transaction details

--- packages/dashboard/src/components/composed/Simulations/Simulation/Transaction/Details.tsx
+++ packages/dashboard/src/components/composed/Simulations/Simulation/Transaction/Details.tsx
@@ -1,5 +1,5 @@
 import type { Ethereum } from "ganache";
-import { Text } from "@mantine/core";
+import { Tabs, Text } from "@mantine/core";
 import { Prism } from "@mantine/prism";
 
 interface DetailsProps {
@@ -10,13 +10,21 @@ export default function Details({ receipt }: DetailsProps): JSX.Element {
   const receiptStringified = JSON.stringify(receipt, null, 2);
 
   return (
-    <>
-      <Text size="sm" color="teal" weight={700}>
-        Transaction receipt
-      </Text>
-      <Prism language="json" copyLabel="Copy to clipboard" withLineNumbers>
-        {receiptStringified}
-      </Prism>
-    </>
+    <Tabs defaultValue="overview">
+      <Tabs.List grow>
+        <Tabs.Tab value="overview">Overview</Tabs.Tab>
+        <Tabs.Tab value="logs">Logs</Tabs.Tab>
+        <Tabs.Tab value="debug">Debug</Tabs.Tab>
+      </Tabs.List>
+
+      <Tabs.Panel value="overview">
+        <Text size="sm" color="teal" weight={700}>
+          Transaction receipt
+        </Text>
+        <Prism language="json" copyLabel="Copy to clipboard" withLineNumbers>
+          {receiptStringified}
+        </Prism>
+      </Tabs.Panel>
+    </Tabs>
   );
 }
