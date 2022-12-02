ed3e754f48983e491734a20c581850be77787980
fainashalts <faina@trufflesuite.com>
2022-12-01 21:38:14 -0800

    Update runner to deploy and link files based on declaration

---
+++ packages/solver/src/ResolverIntercept.ts
@@ -0,0 +1,38 @@
+import type Resolver from "@truffle/resolver";
+
+export class ResolverIntercept {
+  public resolver: Resolver;
+  public cache: object[];
+
+  constructor(resolver: Resolver) {
+    this.resolver = resolver;
+    this.cache = [];
+  }
+
+  require(contractName: string) {
+    // remove file extension if present on name
+    const sanitizedContractName = contractName
+      .replace(/^\.\//, "")
+      .replace(/\.sol$/i, "");
+
+    // there may be more than one contract of the same name which will be
+    // problematic - only return the first one found in the cache for now
+    for (const contract of this.cache) {
+      // @ts-ignore
+      if (contract.contract_name === sanitizedContractName) {
+        return contract;
+      }
+    }
+
+    // Note, will error if nothing is found.
+    const resolved = this.resolver.require(sanitizedContractName);
+
+    this.cache.push(resolved);
+
+    return resolved;
+  }
+
+  contracts() {
+    return this.cache;
+  }
+}
--- packages/solver/src/runner.ts
+++ packages/solver/src/runner.ts
@@ -1,33 +1,63 @@
 import { Solver } from "@truffle/solver";
 import { DeclarationTarget } from "./types/";
 import Deployer from "@truffle/deployer";
-import { Resolver } from "@truffle/resolver";
 import Config from "@truffle/config";
+import { ResolverIntercept } from "./ResolverIntercept";
 
 const Runner = {
-  orchestrate: async function (declarationSteps, options) {
+  orchestrate: async function (
+    declarationSteps: any,
+    config: any,
+    options: any
+  ) {
     // run each target through the run function, hold output until all are completed,
     // or throw an error
     // TODO: add error handling
-    const deploymentSteps = declarationSteps.map(async step => {
-      const result = await this.run(step, options);
-      return result;
-    });
+    // const deploymentSteps = await Promise.all(declarationSteps.map(async step => {
+    //   const result = await this.deploy(step, config, options);
+    //   return result;
+    // }));
+    //@TODO: switch to using allSettled
+    //@TODO: handle state for intermediate steps in case deployment fails partway
+    let deploymentSteps: DeclarationTarget[] = [];
+    for (const step of declarationSteps) {
+      const result = await this.run(step, config, options);
+      deploymentSteps.push(result);
+    }
+
     return deploymentSteps;
   },
-  solve: async function (filepath: string, options: any) {
-    const declarationSteps = await Solver.orchestrate(filepath);
-    this.orchestrate(declarationSteps, options);
+  solve: async function (config: any, options: any) {
+    const declarationSteps = await Solver.orchestrate(
+      config.declarativeDeployment.filepath
+    );
+    await this.orchestrate(declarationSteps, config, options);
     return declarationSteps;
   },
-  run: async function (deploymentStep: DeclarationTarget, options: any) {
-    options.network = "development";
-    const config = Config.detect(options);
-    // since the whole point of the runner is to run deployments for more than one network,
-    // we need to set the network to the one specified in the declaration, after checking it exists
-    // in the config
-    // TODO update this to play with environment-based config, when that is ready; this way of
-    // handling networks is temporary
+  prepareForMigrations: function (options: Config) {
+    //this function comes from the current migration flow; may or may not be needed
+    // ultimately; for now have commented out the irrelevant parts
+
+    // const interfaceAdapter = createInterfaceAdapter({
+    //   provider: options.provider,
+    //   networkType: options.networks[options.network].type
+    // });
+    // const web3 = new Web3Shim({
+    //   provider: options.provider,
+    //   networkType: options.networks[options.network].type
+    // });
+
+    const resolver = new ResolverIntercept(options.resolver);
+
+    // Initial context.
+    // const context = { web3, interfaceAdapter, config: options };
+
+    const deployer = new Deployer(options);
+
+    return { resolver, deployer };
+  },
+  link: async function (deploymentStep: DeclarationTarget, config: Config) {
+    const { deployer, resolver } = this.prepareForMigrations(config);
     const configNetworks = config.networks;
     if (configNetworks[deploymentStep.network]) {
       config.network = deploymentStep.network;
@@ -35,31 +65,87 @@ const Runner = {
       throw new Error(`Network ${deploymentStep.network} not found in config`);
     }
 
-    // Config.detect doesn't seem to set resolver, so we'll do it manually
-    config.resolver = new Resolver(config);
-
     let Contract;
+
     try {
-      Contract = config.resolver.require(deploymentStep.contractName);
+      Contract = resolver.require(deploymentStep.contractName);
+    } catch (e) {
+      console.log("error", e);
+      throw new Error(JSON.stringify(e));
+    }
+
+    await deployer.start();
+    let link = resolver.require(deploymentStep.links[0]);
+    let linkFunc = async function (link, Contract) {
+      await deployer.link(link, Contract);
+    };
+
+    await linkFunc(link, Contract);
+
+    let artifacts = resolver
+      .contracts()
+      .map((abstraction: any) => abstraction._json);
+    await config.artifactor.saveAll(artifacts);
+
+    await deployer.finish();
+  },
+  // @TODO: will need to add options here for the deployment like gasPrice, etc.
+  run: async function (deploymentStep: DeclarationTarget, config: Config) {
+    const { deployer, resolver } = this.prepareForMigrations(config);
+    // TODO update this to play with environment-based config, when that is ready; this way of handling networks is temporary
+    const configNetworks = config.networks;
+    if (configNetworks[deploymentStep.network]) {
+      config.network = deploymentStep.network;
+    } else {
+      throw new Error(`Network ${deploymentStep.network} not found in config`);
+    }
+
+    // @TODO: add Contract type!
+    let Contract;
+
+    try {
+      Contract = resolver.require(deploymentStep.contractName);
     } catch (e) {
       throw new Error(JSON.stringify(e));
     }
 
-    // need something here to handle the state
-
-    //add any linked contracts
+    await deployer.start();
 
     if (deploymentStep.run.includes("link")) {
-      // await deployer.link(contract, deploymentStep.links);
+      await this.link(deploymentStep, config);
     }
+
     if (deploymentStep.run.includes("deploy")) {
-      // @TODO: will need to add options here for the deployment like gasPrice, etc.
-      const deployer = new Deployer(config);
-      deployer.deploy(Contract);
+      const deploy = async function () {
+        await deployer.deploy(Contract);
+
+        let artifacts = resolver
+          .contracts()
+          .map((abstraction: any) => abstraction._json);
+        await config.artifactor.saveAll(artifacts);
+        return await Contract.isDeployed();
+      };
+
+      try {
+        await deploy();
+      } catch (e) {
+        throw e;
+      }
+
+      await deployer.finish();
+
+      if (await Contract.isDeployed()) {
+        deploymentStep.isCompleted = true;
+      }
+
+      // @TODO: add Contract/artifact info to deploymentStep?
+      // Or save somewhere
+      return deploymentStep;
     }
+    // @TODO: add script handling
     // if (deploymentStep.run.includes("script")) {
-    //   //execute script
-    // }
+    //   //   //execute script
+    //   // }
   }
 };
 
