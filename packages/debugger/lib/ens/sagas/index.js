4f626eec48a8d6ecba5e4fddd41a607c952bb14c
Harry Altman <harry.j.altman@gmail.com>
2022-12-07 20:00:07 -0500

    Fix incorrect conversion

--- packages/debugger/lib/ens/sagas/index.js
+++ packages/debugger/lib/ens/sagas/index.js
@@ -12,20 +12,26 @@ import { Conversion } from "@truffle/codec";
 
 //note: name may be null
 export function* reverseResolve(address) {
+  debug("reverse resolving %s", address);
   const cache = yield select(ens.current.cache);
   if (address in cache) {
+    debug("got cached %o", cache[address]);
     return cache[address];
   } else {
     let name = yield* web3.reverseEnsResolve(address); //may be null
+    debug("got name %o", name);
     //now: do a forward resolve as a check
     if (name !== null) {
+      debug("forward resolving %s", name);
       const checkAddress = yield* resolve(name);
+      debug("got check address %o", checkAddress);
       if (checkAddress !== address) {
         //if forward resolution doesn't match, this name is no good!
         name = null;
       }
     }
     yield put(actions.record(address, name));
+    debug("returning %o", name);
     return name;
   }
 }
@@ -43,7 +49,7 @@ export function* reverseResolveAsBytes(address) {
     return null;
   }
   try {
-    return Conversion.stringToBytes(address);
+    return Conversion.stringToBytes(name);
   } catch {
     return null;
   }
