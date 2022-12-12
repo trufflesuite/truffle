6fe5a178acb13f6d0564c2045a75242047a8f95b
eggplantzzz <tmfeickert@gmail.com>
2022-12-12 11:17:35 -0500

    remove require cache invalidation which didn't work in the first place - and unused Module import

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,6 +1,5 @@
 import path from "path";
 import merge from "lodash/merge";
-import Module from "module";
 import findUp from "find-up";
 import Conf from "conf";
 import TruffleError from "@truffle/error";
@@ -187,10 +186,6 @@ class TruffleConfig {
 
     const config = new TruffleConfig(undefined, workingDirectory, undefined);
 
-    // The require-nocache module used to do this for us, but
-    // it doesn't bundle very well. So we've pulled it out ourselves.
-    //@ts-ignore
-    delete require.cache[Module._resolveFilename(file, module)];
     const staticConfig = originalRequire(file);
 
     config.merge(staticConfig);
