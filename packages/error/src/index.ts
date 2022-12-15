a5dc7da6b504141bd953eabb8f58f2384ed74605
cds-amal <cds.sudama@gmail.com>
2022-12-14 22:09:43 -0500

    error: change name and disable stack
    @truffle/error's intent is to give the user an action to perform and
    does not need a stack trace.
    
    - rename @truffle/error to be TruffleError
    - set the stack to  so there is not stacktrace when using bundle

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,11 +1,9 @@
-//Note: This class only exists for compatibility with some old Javascript
-//stuff that avoided using Error directly for whatever reason.  Eventually
-//it should be eliminated.
-class ExtendableError extends Error {
+class TruffleError extends Error {
   constructor(message: string) {
     super(message);
     this.name = this.constructor.name;
+    this.stack = "";
   }
 }
 
-export = ExtendableError;
+export = TruffleError;
