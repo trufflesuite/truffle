f373b4c938d7100d0da51a488c3df9188564213e
liang liang <lsqd3v@gmail.com>
2023-01-06 14:50:15 -0500

    Add complete Help command usage info

--- packages/core/lib/commands/help/meta.js
+++ packages/core/lib/commands/help/meta.js
@@ -1,9 +1,16 @@
+const OS = require("os");
+
 module.exports = {
   command: "help",
   description:
     "List all commands or provide information about a specific command",
   help: {
-    usage: "truffle help [<command>]",
+    usage:
+      "truffle help [<command> <subCommand>]" +
+      OS.EOL +
+      "                truffle --help [<command> <subCommand>]" +
+      OS.EOL +
+      "                truffle [<command> <subCommand>] --help",
     options: [
       {
         option: "<command>",
