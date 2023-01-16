c9994220d9ac8ea89f5b30115243ab2d42526d14
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2023-01-16 18:14:59 +1300

    webpack-test-helper: show module name hint in error message

--- packages/webpack-test-helper/lib/index.ts
+++ packages/webpack-test-helper/lib/index.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import debugModule from "debug";
+import stringSimilarity from "string-similarity";
 
 // ugly hack so that we can access the original require function even though we
 // webpack this module
@@ -8,15 +9,26 @@ const originalRequire = eval("require");
 
 const debug = debugModule("@truffle/webpack-test-helper");
 
+interface WebpackRequire {
+  (id: string): any;
+
+  // purposefully leaving this opaque, as we don't care about anything here but
+  // the keys in the index signature
+  m: { [id: string]: unknown };
+}
+
 export class WebpackTestHelper {
   private packageRoot: string;
-  private webpackRequire: (id: string) => any;
+  private webpackRequire: WebpackRequire;
 
   constructor(bundledPackageName: string) {
     this.packageRoot = findPackageRoot(bundledPackageName);
     this.webpackRequire = originalRequire(
       path.join(this.packageRoot, "dist", "runtime")
     );
+
+    // populate the module listing in this.webpackRequire.m by loading the main chunk
+    originalRequire(path.join(this.packageRoot, "dist"));
   }
 
   /**
@@ -29,7 +41,20 @@ export class WebpackTestHelper {
       debug(
         `Module ${moduleName} is relative, passing it on to __webpack_require__ without modification`
       );
-      return this.webpackRequire(moduleName);
+      try {
+        return this.webpackRequire(moduleName);
+      } catch (err: unknown) {
+        if (
+          err instanceof Error &&
+          err.message.endsWith(
+            "Cannot read properties of undefined (reading 'call')"
+          )
+        ) {
+          throw this._getMissingModuleError(moduleName, err);
+        } else {
+          throw err;
+        }
+      }
     }
 
     const absoluteModulePath = originalRequire.resolve(moduleName);
@@ -56,18 +81,46 @@ export class WebpackTestHelper {
           "Cannot read properties of undefined (reading 'call')"
         )
       ) {
-        // The cast to `any` is necessary to support error causes in newer node
-        // versions. In older node versions the extra argument is ignored.
-        throw new (Error as any)(
-          `Module ID "${moduleName}" could not be loaded from ${this.packageRoot}. ` +
-            `Either the module ID is incorrect, or it is not included in the bundle. ` +
-            `Please check your module ID, package.json, and/or webpack config.`,
-          { cause: err }
-        );
+        throw this._getMissingModuleError(moduleName, err);
+      } else {
+        throw err;
       }
-
-      throw err;
     }
+  }
+
+  private _getMissingModuleError(moduleName: string, err: Error) {
+    const { key: mostSimilarKey, score: similarityScore } = Object.keys(
+      this.webpackRequire.m
+    )
+      .map(key => ({
+        key,
+        score: stringSimilarity.compareTwoStrings(key, moduleName)
+      }))
+      // find maximum key similarity score using reduce function
+      .reduce((max, current) => (current.score > max.score ? current : max), {
+        key: null,
+        score: -1
+      });
+
+    if (similarityScore > 0.5) {
+      // The cast to `any` is necessary to support error causes in newer node
+      // versions. In older node versions the extra argument is ignored.
+      return new (Error as any)(
+        `Module ID "${moduleName}" could not be loaded from ${this.packageRoot}. ` +
+          `Either the module ID is incorrect, or it is not included in the bundle. ` +
+          `Did you mean "${mostSimilarKey}"?`,
+        { cause: err }
+      );
+    }
+
+    // The cast to `any` is necessary to support error causes in newer node
+    // versions. In older node versions the extra argument is ignored.
+    return new (Error as any)(
+      `Module ID "${moduleName}" could not be loaded from ${this.packageRoot}. ` +
+        `Either the module ID is incorrect, or it is not included in the bundle. ` +
+        `Please check your module ID, package.json, and/or webpack config.`,
+      { cause: err }
+    );
   }
 }
 
