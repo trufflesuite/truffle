23139cc45bc789bae637715287b17eaa8b6036e6
Sukanya Parashar <sukanyaparashar111@gmail.com>
2023-01-17 21:05:35 -0700

    Add scenario test for --url option

--- packages/truffle/test/scenarios/commands/exec.js
+++ packages/truffle/test/scenarios/commands/exec.js
@@ -6,7 +6,7 @@ const assert = require("assert");
 const Server = require("../server");
 const sandbox = require("../sandbox");
 
-describe("truffle exec [ @standalone ]", function () {
+describe.only("truffle exec [ @standalone ]", function () {
   let config, cleanupSandboxDir;
   const project = path.join(__dirname, "../../sources/exec");
   const logger = new MemoryLogger();
@@ -55,6 +55,23 @@ describe("truffle exec [ @standalone ]", function () {
   it("succeeds when -c flag is set", async function () {
     this.timeout(30000);
     await CommandRunner.run("exec -c script.js", config);
+    const output = logger.contents();
+    assert(output.includes("5"));
+  });
+
+  it("runs script when --url option is passed", async function () {
+    this.timeout(30000);
+    await CommandRunner.run("compile", config);
+    assert(
+      fs.existsSync(
+        path.join(config.contracts_build_directory, "Executable.json")
+      )
+    );
+
+    await CommandRunner.run(
+      "exec script.js --url http://127.0.0.1:8545",
+      config
+    );
     const output = logger.contents();
     assert(output.includes("5"));
   });
