16be64afd40327de4377d04bdcf245383938550c
Sukanya Parashar <sukanyaparashar@Sukanyas-MacBook-Pro.local>
2023-01-23 17:21:04 -0700

    Add url option to the usage metadata

--- packages/core/lib/commands/exec/meta.js
+++ packages/core/lib/commands/exec/meta.js
@@ -12,10 +12,15 @@ module.exports = {
     compile: {
       type: "boolean",
       default: false
+    },
+    url: {
+      describe: "Connect to a specified provider given via URL",
+      type: "string"
     }
   },
   help: {
-    usage: "truffle exec <script.js> [--compile]",
+    usage:
+      "truffle exec <script.js> [--compile] [--network <network>|--url <provider_url>]",
     options: [
       {
         option: "<script.js>",
@@ -26,8 +31,18 @@ module.exports = {
       {
         option: "--compile",
         description: "Compile contracts before executing the script."
+      },
+      {
+        option: "--url",
+        description:
+          "Connects to a specified provider given via URL, ignoring networks in config."
+      },
+      {
+        option: "--network",
+        description:
+          "The network to connect to, as specified in the Truffle config."
       }
     ],
-    allowedGlobalOptions: ["network", "config"]
+    allowedGlobalOptions: ["config"]
   }
 };
