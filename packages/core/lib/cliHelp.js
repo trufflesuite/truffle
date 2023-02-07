18e4148957603d2602524cc92d2c73192e1f5ca1
eggplantzzz <tmfeickert@gmail.com>
2023-02-07 16:17:25 -0500

    simplify the logic for help utility and update comments

--- packages/core/lib/cliHelp.js
+++ packages/core/lib/cliHelp.js
@@ -1,9 +1,9 @@
 const handleHelpInput = function ({ inputStrings }) {
-  //User only enter truffle with no commands, let's show them what's available.
+  // if no command was provided, display all available commands
   if (inputStrings.length === 0) {
     return { displayHelp: true };
   }
-  // handle 'truffle help' and 'truffle --help'
+  // if only 'help' or '--help' was entered, display all available commands
   if (
     inputStrings.length === 1 &&
     (inputStrings[0] === "help" || inputStrings[0] === "--help")
@@ -11,19 +11,16 @@ const handleHelpInput = function ({ inputStrings }) {
     return { displayHelp: true };
   }
 
-  //if `--help` is in the input, validate and transform the input
-  //in order to give the user help
-  if (inputStrings.some(inputString => ["--help"].includes(inputString))) {
-    //check where --help is used, mutate argument into something the rest
-    //of Truffle can deal with
-    const helpIndex = inputStrings.indexOf("--help");
+  // if `--help` is in the input, assume the user wants help for the command
+  // provided and transform the input into something the rest
+  // of Truffle can handle
+  const helpIndex = inputStrings.indexOf("--help");
 
-    if (helpIndex !== -1) {
-      //remove `--help` from array
-      inputStrings.splice(helpIndex, 1);
-      //insert `help` in first position
-      inputStrings.unshift("help");
-    }
+  if (helpIndex !== -1) {
+    //remove `--help` from array
+    inputStrings.splice(helpIndex, 1);
+    //insert `help` in first position
+    inputStrings.unshift("help");
   }
   // let Truffle know whether to display the general help
   return { displayHelp: false };
