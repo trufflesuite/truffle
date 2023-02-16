2f383794e4dac80b1e805228c8f10475807e3314
cds-amal <cds.sudama@gmail.com>
2023-02-15 22:11:55 -0500

    CI(scripts): pin geth to v1.10.26 (#5900)
    Declared GETH version in one location per bash script. 
    
    This will make pinning and unpinning GETH easier.

--- scripts/geth.sh
+++ scripts/geth.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-docker pull ethereum/client-go:stable
+# Set geth version to stable, or pin to a specific version
+# as necessary when geth release breaks CI
+# Snoop these 
+# - https://hub.docker.com/r/ethereum/client-go
+# - https://github.com/ethereum/go-ethereum/releases
+# GETH_VERSION="stable"
+GETH_VERSION="v1.10.26"
+
+docker pull "ethereum/client-go:$GETH_VERSION"
 
 CID=$(docker run \
   -v /$PWD/scripts:/scripts \
@@ -9,7 +17,7 @@ CID=$(docker run \
   -p 8545:8545 \
   -p 8546:8546 \
   -p 30303:30303 \
-  ethereum/client-go:stable \
+  "ethereum/client-go:$GETH_VERSION" \
   --http \
   --http.addr '0.0.0.0' \
   --http.port 8545 \
