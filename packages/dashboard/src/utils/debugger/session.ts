b2c8a1f153768d3d28a66d4f5781758901592678
eggplantzzz <tmfeickert@gmail.com>
2023-02-27 11:57:26 -0500

    fetch verified source material after loading compilations from browser

--- packages/dashboard/src/utils/debugger/session.ts
+++ packages/dashboard/src/utils/debugger/session.ts
@@ -17,14 +17,14 @@ export async function setupSession(
   }
 ): Promise<{ session: Session; sources: Source[] }> {
   callbacks?.onInit?.();
-  const { session, sources } = await createSession(
+  const { session, sources, networkId, unknownAddresses } = await createSession(
     transactionHash,
     providedProvider,
     compilations
   );
 
   callbacks?.onFetch?.();
-  // await fetchCompilationsAndAddToSession(session, networkName);
+  await fetchCompilationsAndAddToSession(session, networkId, unknownAddresses);
 
   callbacks?.onStart?.();
   await session.startFullMode();
@@ -37,14 +37,37 @@ async function createSession(
   transactionHash: string,
   providedProvider: any,
   compilations: Compilation[]
-): Promise<{ session: Session; sources: Source[] }> {
+): Promise<{
+  session: Session;
+  sources: Source[];
+  networkId: string;
+  unknownAddresses: string[];
+}> {
   const bugger = await forTx(transactionHash, {
     provider: provider({ fork: { provider: providedProvider } }),
     compilations: Codec.Compilations.Utils.shimCompilations(compilations),
     lightMode: true
   });
-  const affectedInstances = bugger.view($.session.info.affectedInstances);
-  console.log("the instances -- %o", affectedInstances);
+  const affectedInstances: { [address: string]: any } = bugger.view(
+    $.session.info.affectedInstances
+  );
+  const networkId = await providedProvider.request({
+    method: "net_version",
+    params: []
+  });
+  const unknownAddresses: string[] = Object.entries(affectedInstances).reduce(
+    (a, item) => {
+      if (item[1].contractName === undefined) {
+        // @ts-ignore
+        a.push(item[0]);
+      }
+      return a;
+    },
+    []
+  );
+  if (unknownAddresses.length > 0 && networkId) {
+    await fetchCompilationsAndAddToSession(bugger, networkId, unknownAddresses);
+  }
   const sources = await getTransactionSourcesBeforeStarting(bugger);
   // we need to transform these into the format dashboard uses
   const transformedSources = Object.values(sources).flatMap(
@@ -54,31 +77,30 @@ async function createSession(
   await bugger.startFullMode();
   return {
     sources: transformedSources,
-    session: bugger
+    session: bugger,
+    networkId,
+    unknownAddresses
   };
 }
 
 async function fetchCompilationsAndAddToSession(
   session: Session,
-  networkName: string
+  networkId: string,
+  addresses: string[]
 ) {
-  const $ = session.selectors;
-
   const host = window.location.hostname;
   const port =
     process.env.NODE_ENV === "development" ? 24012 : window.location.port;
   const fetchAndCompileEndpoint = `http://${host}:${port}/fetch-and-compile`;
-  const instances = session.view($.session.info.affectedInstances);
-  // @ts-ignore
-  const addresses = Object.entries(instances)
-    .filter(([_, { contractName }]: any) => contractName === undefined)
-    .map(([address, _]) => address);
 
   for (const address of addresses) {
     const fetchResult = await fetch(
-      `${fetchAndCompileEndpoint}?address=${address}&network=${networkName}`
+      `${fetchAndCompileEndpoint}?address=${address}&networkId=${networkId}`
     );
     const { compilations } = await fetchResult.json();
+    if (compilations.length === 0) {
+      continue;
+    }
     const shimmedCompilations = Codec.Compilations.Utils.shimCompilations(
       compilations,
       `externalFor(${address})Via(Etherscan)`
