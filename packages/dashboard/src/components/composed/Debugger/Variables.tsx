81728da50c3fed9ff80b96a89a6afcf2c6f7275a
eggplantzzz <tmfeickert@gmail.com>
2023-03-01 15:40:38 -0500

    add new inspect lib, create Variables component, and update operations for setting debugger data

---
+++ packages/dashboard/src/components/composed/Debugger/Variables.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import type { Session } from "src/utils/debugger";
+import inspect from "browser-util-inspect";
+import * as Codec from "@truffle/codec";
+
+type VariablesArgs = {
+  session: Session;
+  currentStep: string;
+};
+
+function Variables({
+  session,
+  currentStep
+}: VariablesArgs): JSX.Element | null {
+  const [variables, setVariables] = useState({});
+  // when the debugger step changes, update variables
+  useEffect(() => {
+    async function getVariables() {
+      const variables = await session!.variables();
+      setVariables(variables);
+    }
+    getVariables();
+  }, [currentStep]);
+
+  return (
+    <div>
+      {Object.entries(variables).map(([name, result]) => {
+        return (
+          <div key={name}>
+            <>
+              {name} -{" "}
+              {inspect(new Codec.Export.ResultInspector(result as any))}
+            </>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Variables;
