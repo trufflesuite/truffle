b9ad6a02b96a422c8dd303d1e70715a27ccd623e
eggplantzzz <tmfeickert@gmail.com>
2023-03-16 10:59:05 -0400

    start work on arranging for a way to set the current source manually

---
+++ packages/dashboard/src/components/composed/Debugger/Breakpoints/index.tsx
@@ -0,0 +1,54 @@
+import { Source } from "src/utils/debugger";
+import { useDash } from "src/hooks";
+import * as path from "path";
+import Breakpoint from "src/components/composed/Debugger/Breakpoints/Breakpoint";
+import type { SourceRange } from "src/utils/debugger";
+
+type BreakpointsArgs = {
+  sources: Source[];
+  handleBreakpointComponentClick: (sourceRange: SourceRange) => void;
+};
+
+function Breakpoints({
+  sources,
+  handleBreakpointComponentClick
+}: BreakpointsArgs): JSX.Element | null {
+  const {
+    state: {
+      debugger: { breakpoints }
+    }
+  } = useDash()!;
+  const breakpointsList: JSX.Element[] = [];
+  for (const source of sources) {
+    if (
+      breakpoints[source.id] === undefined ||
+      breakpoints[source.id].size === 0
+    ) {
+      continue;
+    }
+    if (!source?.sourcePath) {
+      continue;
+    }
+    const iterator = breakpoints[source.id].values();
+    const sourceName = path.basename(source.sourcePath);
+    for (const line of iterator) {
+      breakpointsList.push(
+        <Breakpoint
+          key={`${source.id}${line.toString()}`}
+          sourceName={sourceName}
+          line={line}
+          sourceId={source.id}
+          handleBreakpointComponentClick={handleBreakpointComponentClick}
+        />
+      );
+    }
+  }
+  return (
+    <div className="truffle-debugger-breakpoints">
+      <h2>breakpoints</h2>
+      <pre>{breakpointsList}</pre>
+    </div>
+  );
+}
+
+export default Breakpoints;
