b33dd9596a774963466a66a2925f8f9c45a3cfe6
eggplantzzz <tmfeickert@gmail.com>
2023-03-20 15:59:49 -0400

    create delete breakpoint method and use it in breakpoint section

--- packages/dashboard/src/components/MantineGlobal.tsx
+++ packages/dashboard/src/components/MantineGlobal.tsx
@@ -103,6 +103,19 @@ function MantineGlobal(): JSX.Element {
             ".truffle-debugger-error": {
               width: "50%"
             },
+            ".truffle-debugger-breakpoint-group": {
+              display: "flex",
+              justifyContent: "space-between"
+            },
+            ".truffle-debugger-breakpoint-delete": {
+              borderRadius: 25,
+              textAlign: "center",
+              width: 25
+            },
+            ".truffle-debugger-breakpoint-delete:hover": {
+              cursor: "pointer",
+              backgroundColor: "red"
+            },
             ".mantine-Alert-icon": {
               "width": 28,
               "height": "auto",
