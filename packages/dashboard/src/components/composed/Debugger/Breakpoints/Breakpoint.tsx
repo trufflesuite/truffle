6ed767abf4add2c68c599bfca74da676dc61847c
eggplantzzz <tmfeickert@gmail.com>
2023-03-20 15:59:49 -0400

    create delete breakpoint method and use it in breakpoint section

--- packages/dashboard/src/components/composed/Debugger/Breakpoints/Breakpoint.tsx
+++ packages/dashboard/src/components/composed/Debugger/Breakpoints/Breakpoint.tsx
@@ -4,25 +4,40 @@ type BreakpointProps = {
   line: number;
   sourceId: string;
   handleBreakpointComponentClick: (arg: BreakpointType) => void;
+  handleBreakpointDeleteClick: (arg: BreakpointType) => void;
 };
 
 function Breakpoint({
   sourceName,
   line,
   sourceId,
-  handleBreakpointComponentClick
+  handleBreakpointComponentClick,
+  handleBreakpointDeleteClick
 }: BreakpointProps): JSX.Element | null {
   return (
-    <div
-      className="truffle-debugger-breakpoint"
-      onClick={() =>
-        handleBreakpointComponentClick({
-          sourceId,
-          line: line.toString()
-        })
-      }
-    >
-      {sourceName} - line {line}
+    <div className="truffle-debugger-breakpoint-group">
+      <div
+        className="truffle-debugger-breakpoint"
+        onClick={() =>
+          handleBreakpointComponentClick({
+            sourceId,
+            line: line.toString()
+          })
+        }
+      >
+        {sourceName} - line {line}
+      </div>
+      <div
+        className="truffle-debugger-breakpoint-delete"
+        onClick={() =>
+          handleBreakpointDeleteClick({
+            sourceId,
+            line: line.toString()
+          })
+        }
+      >
+        X
+      </div>
     </div>
   );
 }
