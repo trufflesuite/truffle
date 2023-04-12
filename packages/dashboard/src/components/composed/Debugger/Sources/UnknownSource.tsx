7ce71c74633d8aaf3bd035422ad705c778814882
eggplantzzz <tmfeickert@gmail.com>
2023-04-12 15:54:08 -0400

    update html a bit and the scrolling algorithm

--- packages/dashboard/src/components/composed/Debugger/Sources/UnknownSource.tsx
+++ packages/dashboard/src/components/composed/Debugger/Sources/UnknownSource.tsx
@@ -1,5 +1,9 @@
 function UnknownSource({ address }: { address: string }): JSX.Element {
-  return <pre>Could not locate source material for {address}.</pre>;
+  return (
+    <div className="truffle-debugger-source-container">
+      <pre>Could not locate source material for {address}.</pre>
+    </div>
+  );
 }
 
 export default UnknownSource;
