ca84e66ee1a0cf3563e3c48b4c1124d910557276
eggplantzzz <tmfeickert@gmail.com>
2023-04-19 15:42:17 -0400

    continue moving css to component files
    more

--- packages/dashboard/src/components/composed/Debugger/Breakpoints/index.tsx
+++ packages/dashboard/src/components/composed/Debugger/Breakpoints/index.tsx
@@ -3,6 +3,41 @@ import { useDash } from "src/hooks";
 import * as path from "path";
 import { BreakpointType } from "src/utils/debugger";
 import Breakpoint from "src/components/composed/Debugger/Breakpoints/Breakpoint";
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles(theme => ({
+  breakpointsContainer: {
+    overflow: "hidden",
+    height: "30%",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderRadius: 4,
+    marginBottom: 20,
+    borderColor:
+      theme.colorScheme === "dark"
+        ? theme.colors["truffle-brown"][5]
+        : `${theme.colors["truffle-beige"][5]}73`
+  },
+  sectionHeader: {
+    height: 42,
+    fontSize: 16,
+    paddingTop: 10,
+    paddingLeft: 16,
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? `${theme.colors["truffle-beige"][8]}33`
+        : theme.colors["truffle-beige"][2],
+    borderBottom: "1px solid",
+    borderColor:
+      theme.colorScheme === "dark"
+        ? theme.colors["truffle-brown"][5]
+        : `${theme.colors["truffle-beige"][5]}73`
+  },
+  breakpoints: {
+    overflow: "scroll",
+    height: "100%"
+  }
+}));
 
 type BreakpointsArgs = {
   sources: Source[];
@@ -15,6 +50,7 @@ function Breakpoints({
   handleBreakpointComponentClick,
   handleBreakpointDeleteClick
 }: BreakpointsArgs): JSX.Element | null {
+  const { classes } = useStyles();
   const {
     state: {
       debugger: { breakpoints }
@@ -47,9 +83,9 @@ function Breakpoints({
     }
   }
   return (
-    <div className="truffle-debugger-breakpoints-container">
-      <div className="truffle-debugger-section-header">Breakpoints</div>
-      <div className="truffle-debugger-breakpoints">
+    <div className={classes.breakpointsContainer}>
+      <div className={classes.sectionHeader}>Breakpoints</div>
+      <div className={classes.breakpoints}>
         <pre>{breakpointsList}</pre>
       </div>
     </div>
