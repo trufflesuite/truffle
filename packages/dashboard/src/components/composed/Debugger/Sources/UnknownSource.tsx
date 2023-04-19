11ab1807122f5be7394f281de106833611615f4d
eggplantzzz <tmfeickert@gmail.com>
2023-04-19 16:18:22 -0400

    and yet more css movinaround

--- packages/dashboard/src/components/composed/Debugger/Sources/UnknownSource.tsx
+++ packages/dashboard/src/components/composed/Debugger/Sources/UnknownSource.tsx
@@ -1,8 +1,25 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles(theme => ({
+  unknownSourceContainer: {
+    height: "100%",
+    padding: 15,
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors["truffle-beige"][9]
+        : "#FFF3BF"
+  },
+  unknownSourceTitle: {
+    fontSize: 18
+  }
+}));
+
 function UnknownSource({ address }: { address: string }): JSX.Element {
+  const { classes } = useStyles();
   return (
-    <div className="truffle-debugger-unknown-source-container">
-      <div className="truffle-debugger-unknown-title">Unknown Source</div>
-      <div className="truffle-debugger-unknown-content">
+    <div className={classes.unknownSourceContainer}>
+      <div className={classes.unknownSourceTitle}>Unknown Source</div>
+      <div>
         We're unable to locate the source material for the contract at the
         following address: {address}. Please consider recompiling with Truffle
         Dashboard running if you have the sources locally.
