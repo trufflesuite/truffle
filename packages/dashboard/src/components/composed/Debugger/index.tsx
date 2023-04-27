0455a5fb265def38deab0b25e7cbafb37b6cbb59
eggplantzzz <tmfeickert@gmail.com>
2023-04-27 16:05:43 -0400

    rework how ganache/provider options are passed through init functions

--- packages/dashboard/src/components/composed/Debugger/index.tsx
+++ packages/dashboard/src/components/composed/Debugger/index.tsx
@@ -110,11 +110,28 @@ function Debugger(): JSX.Element {
   };
 
   const onButtonClick = async () => {
+    const provider = window.ethereum;
+    if (!provider) {
+      throw new Error(
+        "There was no provider found in the browser. Ensure you have " +
+          "MetaMask connected to the current page."
+      );
+    }
+    const ganacheOptions = {
+      fork: { provider },
+      logging: {
+        logger: {
+          log: (message: string) => {
+            setLoggingOutput(message);
+          }
+        }
+      }
+    };
     await initDebugger({
-      chainOptions: {},
+      ganacheOptions,
       operations,
       setStatus,
-      setLoggingOutput,
+      provider,
       etherscanApiKey
     });
   };
