ee41706aeb829be87f4c2686423c7f8b2e21eb35
cds-amal <cds.sudama@gmail.com>
2023-04-28 22:25:25 -0400

    resolver: export ProvisionerOptions

--- packages/resolver/lib/index.ts
+++ packages/resolver/lib/index.ts
@@ -1,6 +1,7 @@
 import { Resolver } from "./resolver";
 import type { ResolverSource, ResolvedSource } from "./source";
+import type { ProvisionerOptions } from "@truffle/provisioner";
 
-export { Resolver, ResolverSource, ResolvedSource };
+export { ProvisionerOptions, Resolver, ResolverSource, ResolvedSource };
 
 export default Resolver;
