7253c0d66f2adf474b03d74d2495089d1ce40120
eggplantzzz <tmfeickert@gmail.com>
2023-05-01 13:58:28 -0400

    update etherscan message and tweak scrolling algorithm
    refine design on etherscan key message

--- packages/dashboard/src/components/composed/Debugger/Home/EtherscanApiKeyPrompt.tsx
+++ packages/dashboard/src/components/composed/Debugger/Home/EtherscanApiKeyPrompt.tsx
@@ -1,5 +1,6 @@
 import {
   Container,
+  Stack,
   Flex,
   Text,
   Button,
@@ -26,6 +27,9 @@ const useStyles = createStyles(theme => ({
     "textAlign": "right",
     "fontSize": 14
   },
+  halfWidth: {
+    width: "50%"
+  },
   promptContainer: {
     width: "50%",
     borderRadius: 4,
@@ -35,6 +39,9 @@ const useStyles = createStyles(theme => ({
         : theme.colors["truffle-teal"][0],
     border: `solid ${theme.colors["truffle-teal"][7]}`,
     padding: 15
+  },
+  etherscanMessageContainer: {
+    width: "100%"
   }
 }));
 
@@ -61,7 +68,7 @@ function EtherScanApiKeyPrompt() {
     return (
       <Container className={classes.promptContainer}>
         <Text size="sm">
-          Would you like a faster experience? Truffle will sometimes download
+          Would you like a faster experience? Truffle will attempt download
           source material from Etherscan when it is missing. Enter your
           Etherscan API key in the box below to save it in the browser and speed
           up your downloads. Alternatively you may set an API key in your
@@ -85,15 +92,19 @@ function EtherScanApiKeyPrompt() {
   } else {
     const onElementClick = () => removeEtherscanApiKey();
     return (
-      <>
-        <Text size="sm">
+      <Stack align="center" className={classes.etherscanMessageContainer}>
+        <Text size="sm" className={classes.halfWidth}>
           We have found an Etherscan API key that you previously entered in your
           browser. To delete this from browser storage, click the link below.
         </Text>
-        <Text size="sm" className={classes.reset} onClick={onElementClick}>
+        <Text
+          size="sm"
+          className={`${classes.reset} ${classes.halfWidth}`}
+          onClick={onElementClick}
+        >
           delete your api key
         </Text>
-      </>
+      </Stack>
     );
   }
 }
