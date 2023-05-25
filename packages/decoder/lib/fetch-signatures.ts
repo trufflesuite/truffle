0638572e94a025cd49e94f843e5856d1f0ebdf05
Harry Altman <harry.j.altman@gmail.com>
2023-05-24 20:25:32 -0400

    Add code to actually get signatures from 4byte.directory

--- packages/decoder/lib/fetch-signatures.ts
+++ packages/decoder/lib/fetch-signatures.ts
@@ -1,7 +1,60 @@
 import debugModule from "debug";
 const debug = debugModule("decoder:fetch-signatures");
 
+import axios from "axios";
+import retry from "async-retry";
+
+import { Conversion } from "@truffle/codec";
+
+interface DirectoryResponse {
+  next: string | null;
+  previous: string | null;
+  count: number;
+  results: DirectoryEntry[];
+}
+
+interface DirectoryEntry {
+  id: number;
+  created_at: string;
+  text_signature: string;
+  hex_signature: string; //the only part we care about!
+  bytes_signature: string;
+}
+
+//note: input should be 4 bytes long
 export async function fetchSignatures(selector: Uint8Array): Promise<string[]> {
-  debug("selector: %O", selector);
-  return []; //TODO
+  const selectorString = Conversion.toHexString(selector);
+  let page: number = 1;
+  let signatures: string[] = [];
+  while (true) {
+    const response = await getSuccessfulResponse(selectorString, page);
+    signatures = signatures.concat(
+      response.results.map(({ text_signature }) => text_signature)
+    ); //append new signatures
+    if (response.next === null) {
+      break;
+    }
+    page++; //ideally we'd use the actual value of response.next, but this is easier
+  }
+  return signatures;
+}
+
+async function getSuccessfulResponse(
+  selector: string,
+  page: number
+): Promise<DirectoryResponse> {
+  return await retry(
+    async () =>
+      (
+        await axios.get("https://www.4byte.directory/api/v1/signatures/", {
+          params: {
+            hex_signature: selector,
+            page
+          },
+          responseType: "json",
+          maxRedirects: 50
+        })
+      ).data,
+    { retries: 3 } //we'll leave minTimeout as the default 1000
+  );
 }
