3497a5a0e300c176b300d8c5f4a92ce57b9a1146
eggplantzzz <tmfeickert@gmail.com>
2023-05-26 12:09:22 -0400

    use vs code icons for debugger buttons

---
+++ packages/dashboard/src/components/composed/Debugger/Controls/icons/index.tsx
@@ -0,0 +1,6 @@
+export * from "src/components/composed/Debugger/Controls/icons/Continue";
+export * from "src/components/composed/Debugger/Controls/icons/Into";
+export * from "src/components/composed/Debugger/Controls/icons/Next";
+export * from "src/components/composed/Debugger/Controls/icons/Out";
+export * from "src/components/composed/Debugger/Controls/icons/Over";
+export * from "src/components/composed/Debugger/Controls/icons/Reset";
