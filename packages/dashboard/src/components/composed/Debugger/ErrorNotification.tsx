2528d30134f3c498d0588f4185853148a452634f
eggplantzzz <tmfeickert@gmail.com>
2023-06-02 16:56:13 -0400

    tighten up design for error notification

--- packages/dashboard/src/components/composed/Debugger/ErrorNotification.tsx
+++ packages/dashboard/src/components/composed/Debugger/ErrorNotification.tsx
@@ -1,4 +1,5 @@
 import { Flex, Text, Container, createStyles } from "@mantine/core";
+import { WarningIcon } from "src/components/composed/Debugger/WarningIcon";
 
 const useStyles = createStyles(theme => ({
   notificationContainer: {
@@ -11,9 +12,25 @@ const useStyles = createStyles(theme => ({
       theme.colorScheme === "dark"
         ? "#082720"
         : theme.colors["truffle-teal"][0],
-    padding: 15,
-    border: `solid red`,
+    border: `solid #C92A2A`,
     borderRadius: 4
+  },
+  notificationHeader: {
+    backgroundColor: "#C92A2A",
+    width: "100%",
+    padding: 15,
+    display: "flex",
+    alignItems: "center"
+  },
+  notificationHeaderText: {
+    color: "white"
+  },
+  icon: {
+    marginRight: 10,
+    paddingTop: 5
+  },
+  errorTextContainer: {
+    padding: 15
   }
 }));
 
@@ -25,17 +42,28 @@ function ErrorNotification({ error }: { error: Error }): JSX.Element {
       className={classes.notificationContainer}
       align="center"
       justify="center"
+      direction="column"
     >
-      <Container className={classes.notificationAndError}>
-        <Text>
-          An error occurred while initializing the debugger. Often errors can be
-          caused by being connected to the incorrect network for a specific
-          transaction. Ensure you have MetaMask connected to the appropriate
-          network for your transaction. See the error below for more
-          information.
-        </Text>
-        <Text fw={700}>{error.message}</Text>
-      </Container>
+      <div className={classes.notificationAndError}>
+        <div className={classes.notificationHeader}>
+          <div className={classes.icon}>
+            <WarningIcon />
+          </div>
+          <div className={classes.notificationHeaderText}>
+            There's an issue...
+          </div>
+        </div>
+        <div className={classes.errorTextContainer}>
+          <Text>
+            An error occurred while initializing the debugger. Often errors can
+            be caused by being connected to the incorrect network for a specific
+            transaction. Ensure you have MetaMask connected to the appropriate
+            network for your transaction. See the error below for more
+            information.
+          </Text>
+          <Text fw={700}>{error.message}</Text>
+        </div>
+      </div>
     </Flex>
   );
 }
