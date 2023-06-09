002103e5f06435e2384871ff5ca349dccd586d3e
cliffoo <41348973+cliffoo@users.noreply.github.com>
2023-06-09 17:05:39 -0400

    codec-components: Explicitly set displayName

--- packages/codec-components/src/react/utils/create-codec-component.ts
+++ packages/codec-components/src/react/utils/create-codec-component.ts
@@ -45,6 +45,8 @@ export function createCodecComponent<
       : createDefaultElement(data);
   }
 
+  PolymorphicComponent.displayName = name;
+
   return {
     [name]: PolymorphicComponent
   } as {
