dda2b26fa083e6819788a447f68f783e20055501
cliffoo <41348973+cliffoo@users.noreply.github.com>
2023-06-09 17:02:28 -0400

    codec-components: Add missing injected node usage

--- packages/codec-components/src/react/components/codec/format.values.function-external-value-info-invalid.tsx
+++ packages/codec-components/src/react/components/codec/format.values.function-external-value-info-invalid.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import type { Format } from "@truffle/codec";
 import { createCodecComponent } from "../../utils/create-codec-component";
 import { ContractValueInfoKnown } from "./format.values.contract-value-info-known";
+import { useInjectedNode } from "../../contexts/injected-node";
 import { Code } from "../common/code";
 
 export const { FunctionExternalValueInfoInvalid } = createCodecComponent(
   "FunctionExternalValueInfoInvalid",
-  ({ contract, selector }: Format.Values.FunctionExternalValueInfoInvalid) => (
-    <Code>
-      <ContractValueInfoKnown data={contract} />
-      <Code type="period">.</Code>
-      <Code type="function">{selector}</Code>
-    </Code>
-  )
+  ({ contract, selector }: Format.Values.FunctionExternalValueInfoInvalid) => {
+    const { prefix, content } = useInjectedNode();
+    return (
+      <Code>
+        {prefix?.prefix}
+        <ContractValueInfoKnown data={contract} />
+        <Code type="period">.</Code>
+        <Code type="function">{selector}</Code>
+        {content?.suffix}
+      </Code>
+    );
+  }
 );
--- packages/codec-components/src/react/components/codec/format.values.function-external-value-info-unknown.tsx
+++ packages/codec-components/src/react/components/codec/format.values.function-external-value-info-unknown.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import type { Format } from "@truffle/codec";
 import { createCodecComponent } from "../../utils/create-codec-component";
 import { ContractValueInfoUnknown } from "./format.values.contract-value-info-unknown";
+import { useInjectedNode } from "../../contexts/injected-node";
 import { Code } from "../common/code";
 
 export const { FunctionExternalValueInfoUnknown } = createCodecComponent(
   "FunctionExternalValueInfoUnknown",
-  ({ contract, selector }: Format.Values.FunctionExternalValueInfoUnknown) => (
-    <Code>
-      <ContractValueInfoUnknown data={contract} />
-      <Code type="period">.</Code>
-      <Code type="function">{selector}</Code>
-    </Code>
-  )
+  ({ contract, selector }: Format.Values.FunctionExternalValueInfoUnknown) => {
+    const { prefix, content } = useInjectedNode();
+    return (
+      <Code>
+        {prefix?.prefix}
+        <ContractValueInfoUnknown data={contract} />
+        <Code type="period">.</Code>
+        <Code type="function">{selector}</Code>
+        {content?.suffix}
+      </Code>
+    );
+  }
 );
