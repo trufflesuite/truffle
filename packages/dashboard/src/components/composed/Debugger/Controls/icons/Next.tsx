53a62a018922b0eaa5a51d06876542a7f96dfa1c
eggplantzzz <tmfeickert@gmail.com>
2023-06-13 15:43:00 -0400

    make button icons larger

--- packages/dashboard/src/components/composed/Debugger/Controls/icons/Next.tsx
+++ packages/dashboard/src/components/composed/Debugger/Controls/icons/Next.tsx
@@ -1,8 +1,8 @@
 export const Next = () => {
   return (
     <svg
-      width="16"
-      height="16"
+      width="25"
+      height="25"
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
