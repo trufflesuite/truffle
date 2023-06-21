546188bc6b4027d75e68a79421f2f4610d0b6b1b
eggplantzzz <tmfeickert@gmail.com>
2023-06-21 16:00:37 -0400

    separate source material into two panes: line number and source material

--- packages/dashboard/src/components/composed/Debugger/Sources/Source/SourceLine.tsx
+++ packages/dashboard/src/components/composed/Debugger/Sources/Source/SourceLine.tsx
@@ -1,9 +1,5 @@
 import { createStyles } from "@mantine/core";
 import { useDash } from "src/hooks";
-import {
-  highlightedTextTag,
-  closingHighlightedTextTag
-} from "src/components/composed/Debugger/utils";
 
 const useStyles = createStyles((theme, _params, _getRef) => ({
   lineNumber: {
@@ -16,27 +12,8 @@ const useStyles = createStyles((theme, _params, _getRef) => ({
     height: 22,
     display: "flex"
   },
-  spacer: {
-    minHeight: 22,
-    minWidth: 22,
-    width: 22,
-    marginRight: 5
-  },
-  breakpointSpacerContainer: {
-    minHeight: 22,
-    minWidth: 22,
-    width: 22,
-    display: "flex",
-    marginRight: 5,
-    alignItems: "center"
-  },
-  breakpointSpacer: {
-    width: 16,
-    height: 16,
-    backgroundColor: "#FA5252",
-    borderRadius: 25
-  },
   content: {
+    height: 22,
     display: "flex"
   }
 }));
@@ -44,55 +21,23 @@ const useStyles = createStyles((theme, _params, _getRef) => ({
 interface SourceLineProps {
   line: string;
   lineNumber: number;
-  lineNumberGutterWidth: number;
   lastLine: boolean;
   firstHighlightedLine: boolean;
   sourceId: string;
-  lineHasHighlighting: boolean;
 }
 
 function SourceLine({
   line,
   lineNumber,
-  lineNumberGutterWidth,
   lastLine,
-  sourceId,
-  lineHasHighlighting
+  sourceId
 }: SourceLineProps): JSX.Element {
   const { classes } = useStyles();
   const {
-    state: {
-      debugger: { breakpoints }
-    },
     operations: { toggleDebuggerBreakpoint }
   } = useDash()!;
 
   if (!lastLine) line += "\n";
-  // if the line contains highlighting we highlight the line number as well
-  const lineNumberDisplay = lineHasHighlighting
-    ? `<span class="${classes.lineNumber}">` +
-      " ".repeat(lineNumberGutterWidth - lineNumber.toString().length) +
-      highlightedTextTag +
-      lineNumber +
-      closingHighlightedTextTag +
-      "  " +
-      `</span>`
-    : `<span class="${classes.lineNumber}">` +
-      " ".repeat(lineNumberGutterWidth - lineNumber.toString().length) +
-      lineNumber +
-      "  " +
-      `</span>`;
-
-  const spacer =
-    breakpoints &&
-    breakpoints[sourceId] &&
-    breakpoints[sourceId].has(lineNumber) ? (
-      <div className={classes.breakpointSpacerContainer}>
-        <div className={classes.breakpointSpacer} />
-      </div>
-    ) : (
-      <div className={classes.spacer} />
-    );
 
   const handleClick = () => {
     toggleDebuggerBreakpoint({ line: lineNumber, sourceId });
@@ -102,11 +47,10 @@ function SourceLine({
 
   return (
     <div className={classes.sourceLine} onClick={handleClick}>
-      {spacer}
       <div
         className={classes.content}
         id={lineId}
-        dangerouslySetInnerHTML={{ __html: lineNumberDisplay + line }}
+        dangerouslySetInnerHTML={{ __html: line }}
       />
     </div>
   );
