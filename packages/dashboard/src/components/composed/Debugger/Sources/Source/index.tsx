b3a5fc7275c246ddc850aa5e76f01f7ee32a3c27
eggplantzzz <tmfeickert@gmail.com>
2023-06-21 16:00:37 -0400

    separate source material into two panes: line number and source material

--- packages/dashboard/src/components/composed/Debugger/Sources/Source/index.tsx
+++ packages/dashboard/src/components/composed/Debugger/Sources/Source/index.tsx
@@ -1,10 +1,11 @@
 import SourceLine from "src/components/composed/Debugger/Sources/Source/SourceLine";
+import LineNumber from "src/components/composed/Debugger/Sources/Source/LineNumber";
 import type {
   Source as SourceType,
   SourceRange
 } from "src/components/composed/Debugger/utils";
 import { convertSourceToHtml } from "src/components/composed/Debugger/utils";
-import { createStyles } from "@mantine/core";
+import { createStyles, Flex } from "@mantine/core";
 
 const useStyles = createStyles(theme => ({
   sourceContainer: {
@@ -19,6 +20,16 @@ const useStyles = createStyles(theme => ({
     },
     "paddingTop": 15,
     "paddingLeft": 15
+  },
+  lineNumbersContainer: {
+    height: 22,
+    display: "flex",
+    flexDirection: "column"
+  },
+  sourceLineContainer: {
+    height: 22,
+    display: "flex",
+    flexDirection: "column"
   }
 }));
 
@@ -29,41 +40,61 @@ interface SourceProps {
 
 function Source({ source, sourceRange }: SourceProps): JSX.Element {
   const { classes } = useStyles();
-  const sourceLines = convertSourceToHtml({ source, sourceRange });
+  const rawSourceLines = convertSourceToHtml({ source, sourceRange });
 
   const { start, end } = sourceRange;
-  const lineNumberGutterWidth = sourceLines.length.toString().length;
+  const lineNumberGutterWidth = rawSourceLines.length.toString().length;
+
+  const lineNumbers: JSX.Element[] = [];
+  const sourceLines: JSX.Element[] = [];
+
+  rawSourceLines.map((line: string, index: number) => {
+    const sourceLineKey = `${source.id}-sourceLine-${index}`;
+    const lineNumberKey = `${source.id}-lineNumber-${index}`;
+    const lineHasHighlighting =
+      source.id === sourceRange.source.id &&
+      index >= start.line &&
+      (end.line === null ||
+        end.column === null ||
+        (end.column === 0 && index < end.line) ||
+        (end.column > 0 && index <= end.line));
+    const firstHighlightedLine = lineHasHighlighting && index === start.line;
+
+    const lineNumber = index + 1;
+
+    const sourceLineProps = {
+      key: sourceLineKey,
+      line,
+      lineNumber,
+      lineNumberGutterWidth,
+      lastLine: index === rawSourceLines.length - 1,
+      firstHighlightedLine,
+      sourceId: source.id,
+      lineHasHighlighting
+    };
+
+    const lineNumberProps = {
+      key: lineNumberKey,
+      lineHasHighlighting,
+      lineNumber,
+      lineNumberGutterWidth,
+      sourceId: source.id
+    };
+
+    sourceLines.push(<SourceLine {...sourceLineProps} />);
+    lineNumbers.push(<LineNumber {...lineNumberProps} />);
+  });
+
   return (
     <div
       className={classes.sourceContainer}
       id={`source-${source.id.slice(-10)}`}
     >
       <pre className={classes.source}>
-        {sourceLines.map((line: string, index: number) => {
-          const key = `${source.id}-line-${index}`;
-          const lineHasHighlighting =
-            source.id === sourceRange.source.id &&
-            index >= start.line &&
-            (end.line === null ||
-              end.column === null ||
-              (end.column === 0 && index < end.line) ||
-              (end.column > 0 && index <= end.line));
-          const firstHighlightedLine =
-            lineHasHighlighting && index === start.line;
-
-          const props = {
-            key,
-            line,
-            lineNumber: index + 1,
-            lineNumberGutterWidth,
-            lastLine: index === sourceLines.length - 1,
-            firstHighlightedLine,
-            sourceId: source.id,
-            lineHasHighlighting
-          };
-
-          return <SourceLine {...props} />;
-        })}
+        <Flex>
+          <div className={classes.lineNumbersContainer}>{lineNumbers}</div>
+          <div className={classes.sourceLineContainer}>{sourceLines}</div>
+        </Flex>
       </pre>
     </div>
   );
