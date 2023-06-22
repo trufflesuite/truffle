7d7c243b64909cf0861b3564a1e40e167b1af456
Harry Altman <harry.j.altman@gmail.com>
2023-06-22 16:30:26 -0400

    Revert "Set decoder tests to use 0.8.20 and viaIR"
    This reverts commit 103f9e5636ec3e844dfac5dd74d7a7e9e3fdc69c.

--- packages/decoder/test/current/truffle-config.js
+++ packages/decoder/test/current/truffle-config.js
@@ -56,16 +56,15 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.20", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.9" // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {
-        // See the solidity docs for advice about optimization and evmVersion
-        viaIR: true,
-        optimizer: {
-          enabled: true,
-          runs: 200
-        }
-      }
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "constantinople"
+      // }
     }
   }
 };
