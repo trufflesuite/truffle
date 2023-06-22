74118cb87d0a1bfecf5c0bcf124370f4b720df34
Harry Altman <harry.j.altman@gmail.com>
2023-06-22 16:51:38 -0400

    Re-add viaIR decoding tests, now in separate directory

---
+++ packages/decoder/test/viair/contracts/DecodingSample.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract DecodingSample {
+
+  function() internal functionInternal = example;
+
+  function example() public {
+    functionInternal = example;
+  }
+}
