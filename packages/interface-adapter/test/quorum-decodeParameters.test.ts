8171652208193b2ff9257002cc1d667a8093c547
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-08-11 02:15:57 +0200

    fix Quorum decodeParameters tests

--- packages/interface-adapter/test/quorum-decodeParameters.test.ts
+++ packages/interface-adapter/test/quorum-decodeParameters.test.ts
@@ -32,8 +32,7 @@ const expectedOutput = [{ name: "retVal", type: "uint256" }];
 const emptyByte = "";
 
 describe("Quorum decodeParameters Overload", function () {
-  // web3@4.0.0-alpha does not encode/decode empty or small length bytes values
-  it.skip("decodes an empty byte to a '0' string value w/ quorum=true", async function () {
+  it("decodes an empty byte to a '0' string value w/ quorum=true", async function () {
     const preparedGanache = await prepareGanache(true);
     try {
       const result = preparedGanache.web3Shim.eth.abi.decodeParameters(
@@ -47,22 +46,26 @@ describe("Quorum decodeParameters Overload", function () {
     }
   });
 
-  // ganache uses web3@1.0.0-beta.35 which doesn't include the 'Out of Gas?' decoder guard!
-  it.skip("throws an 'Out of Gas?' error when decoding an empty byte w/ quorum=false", async function () {
-    return new Promise(async (resolve, reject) => {
-      let preparedGanache: any;
-      try {
-        preparedGanache = await prepareGanache(false);
-        assert.throws(async () => {
-          await preparedGanache.web3Shim.eth.abi.decodeParameters(
-            expectedOutput,
-            emptyByte
-          );
-        });
-        preparedGanache.provider.disconnect().then(() => resolve());
-      } catch (e) {
-        preparedGanache.provider.disconnect().then(() => reject(2));
+  // ganache uses web3@4.x which throws 'AbiError: Parameter decoding error...' when an empty string is supplied!
+  it("throws an 'AbiError: Parameter decoding error ...' error when decoding an empty byte w/ quorum=false", async function () {
+    let preparedGanache;
+    let error;
+    try {
+      preparedGanache = await prepareGanache(false);
+      preparedGanache.web3Shim.eth.abi.decodeParameters(
+        expectedOutput,
+        emptyByte
+      );
+    } catch (e) {
+      // this is the expected scenario
+      error = e;
+    } finally {
+      await preparedGanache.provider.disconnect();
+      if (!error) {
+        throw Error(
+          "decodeParameters should have thrown with `quorum=false`! But it did not."
+        );
       }
-    });
+    }
   });
 });
