7a289db62d0beee925d593d1abceba6c47d0d2e4
legobt <6wbvkn0j@anonaddy.me>
2023-09-03 21:54:57 +0000

    ci(scripts): pin geth to v1.11.6

--- scripts/geth.sh
+++ scripts/geth.sh
@@ -6,7 +6,7 @@
 # - https://hub.docker.com/r/ethereum/client-go
 # - https://github.com/ethereum/go-ethereum/releases
 # GETH_VERSION="stable"
-GETH_VERSION="v1.10.26"
+GETH_VERSION="v1.11.6"
 
 docker pull "ethereum/client-go:$GETH_VERSION"
 
